90a8a83b5fb27639c108d588e4bcf7decc1fe769
shterevg <shterevg@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-04-29 16:06:28 +0000

    Initial import.

---
+++ org.unicase.ui.repository/src/org/unicase/ui/repository/views/RepositoryView.java
@@ -0,0 +1,317 @@
+package org.unicase.ui.repository.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.*;
+import org.eclipse.jface.viewers.*;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.jface.action.*;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.*;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+import org.eclipse.core.runtime.IAdaptable;
+import org.unicase.workspace.ProjectInfo;
+import org.unicase.workspace.ServerInfo;
+import org.unicase.workspace.UCUserSession;
+
+/**
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
+ * <p>
+ */
+
+public class RepositoryView extends ViewPart {
+	private TreeViewer viewer;
+	private DrillDownAdapter drillDownAdapter;
+	private Action checkout;
+	private ServerAction login;
+	private Action doubleClickAction;
+
+	/*
+	 * The content provider class is responsible for providing objects to the
+	 * view. It can wrap existing objects in adapters or simply return objects
+	 * as-is. These objects may be sensitive to the current input of the view,
+	 * or ignore it and always show the same content (like Task List, for
+	 * example).
+	 */
+
+	class ProjectNode implements IAdaptable {
+		private String name;
+		private ServerNode parent;
+
+		public ProjectNode(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setParent(ServerNode parent) {
+			this.parent = parent;
+		}
+
+		public ServerNode getParent() {
+			return parent;
+		}
+
+		public String toString() {
+			return getName();
+		}
+
+		public Object getAdapter(Class key) {
+			return null;
+		}
+	}
+
+	class ServerNode extends ProjectNode {
+		private ArrayList children;
+		private ServerInfo serverInfo;
+		
+		public ServerNode(ServerInfo serverInfo) {
+			super(serverInfo.getName());
+			children = new ArrayList();
+			this.serverInfo = serverInfo;
+		}
+
+		public void addChild(ProjectNode child) {
+			children.add(child);
+			child.setParent(this);
+		}
+
+		public void removeChild(ProjectNode child) {
+			children.remove(child);
+			child.setParent(null);
+		}
+
+		public ProjectNode[] getChildren() {
+			return (ProjectNode[]) children.toArray(new ProjectNode[children
+					.size()]);
+		}
+
+		public boolean hasChildren() {
+			return children.size() > 0;
+		}
+	}
+
+	class ViewContentProvider implements IStructuredContentProvider,
+			ITreeContentProvider {
+		private ServerNode invisibleRoot;
+
+		public void inputChanged(Viewer v, Object oldInput, Object newInput) {
+		}
+
+		public void dispose() {
+		}
+
+		public Object[] getElements(Object parent) {
+			if (parent.equals(getViewSite())) {
+				if (invisibleRoot == null)
+					initialize();
+				return getChildren(invisibleRoot);
+			}
+			return getChildren(parent);
+		}
+
+		public Object getParent(Object child) {
+			if (child instanceof ProjectNode) {
+				return ((ProjectNode) child).getParent();
+			}
+			return null;
+		}
+
+		public Object[] getChildren(Object parent) {
+			if (parent instanceof ServerNode) {
+				return ((ServerNode) parent).getChildren();
+			}
+			return new Object[0];
+		}
+
+		public boolean hasChildren(Object parent) {
+			if (parent instanceof ServerNode)
+				return ((ServerNode) parent).hasChildren();
+			return false;
+		}
+
+		/*
+		 * We will set up a dummy model to initialize tree heararchy. In a real
+		 * code, you will connect to a real model and expose its hierarchy.
+		 */
+		private void initialize() {
+			invisibleRoot = new ServerNode(UCUserSession.getDefaultServerInfo());
+
+			ServerNode testServer = new ServerNode(UCUserSession
+					.getDefaultServerInfo());
+			invisibleRoot.addChild(testServer);
+		}
+
+	}
+
+	class ViewLabelProvider extends LabelProvider {
+
+		public String getText(Object obj) {
+			return obj.toString();
+		}
+
+		public Image getImage(Object obj) {
+			String imageKey = ISharedImages.IMG_OBJ_ELEMENT;
+			if (obj instanceof ServerNode)
+				imageKey = ISharedImages.IMG_OBJ_FOLDER;
+			return PlatformUI.getWorkbench().getSharedImages().getImage(
+					imageKey);
+		}
+	}
+
+	class NameSorter extends ViewerSorter {
+	}
+
+	/**
+	 * The constructor.
+	 */
+	public RepositoryView() {
+	}
+
+	/**
+	 * This is a callback that will allow us to create the viewer and initialize
+	 * it.
+	 */
+	public void createPartControl(Composite parent) {
+		viewer = new TreeViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+		drillDownAdapter = new DrillDownAdapter(viewer);
+		viewer.setContentProvider(new ViewContentProvider());
+		viewer.setLabelProvider(new ViewLabelProvider());
+		viewer.setSorter(new NameSorter());
+		viewer.setInput(getViewSite());
+
+		// Create the help context id for the viewer's control
+		PlatformUI.getWorkbench().getHelpSystem().setHelp(viewer.getControl(),
+				"org.unicase.repositoryview.viewer");
+		makeActions();
+		hookContextMenu();
+		hookDoubleClickAction();
+		contributeToActionBars();
+	}
+
+	private void hookContextMenu() {
+		MenuManager menuMgr = new MenuManager("#PopupMenu");
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.addMenuListener(new IMenuListener() {
+			public void menuAboutToShow(IMenuManager manager) {
+				RepositoryView.this.fillContextMenu(manager);
+			}
+		});
+		Menu menu = menuMgr.createContextMenu(viewer.getControl());
+		viewer.getControl().setMenu(menu);
+		getSite().registerContextMenu(menuMgr, viewer);
+	}
+
+	private void contributeToActionBars() {
+		IActionBars bars = getViewSite().getActionBars();
+		fillLocalPullDown(bars.getMenuManager());
+		fillLocalToolBar(bars.getToolBarManager());
+	}
+
+	private void fillLocalPullDown(IMenuManager manager) {
+		manager.add(checkout);
+	}
+
+	private void fillContextMenu(IMenuManager manager) {
+		ISelection selection = viewer.getSelection();
+		Object obj = ((IStructuredSelection) selection).getFirstElement();
+		if (obj instanceof ServerNode) {
+			manager.add(login);
+		} else {
+			manager.add(checkout);
+		}
+	}
+
+	private void fillLocalToolBar(IToolBarManager manager) {
+		manager.add(checkout);
+	}
+	
+	private class ServerAction extends Action{
+		ServerNode serverNode;
+		public void setServerNode(ServerNode serverNode){
+			this.serverNode = serverNode;
+		}
+	}
+	
+	private void makeActions() {
+		checkout = new Action() {
+			public void run() {
+				showMessage("Checking out ...");
+			}
+		};
+		checkout.setText("Checkout");
+		checkout.setToolTipText("Click to checkout this project");
+		checkout.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_TOOL_FORWARD));
+
+		login = new ServerAction() {
+			public void run() {
+				showMessage("Logging in...");
+				try{
+					UCUserSession session = new UCUserSession(serverNode.serverInfo);
+					List<ProjectInfo> projectInfos = session.getRemoteProjectList();
+					for (ProjectInfo projectInfo : projectInfos){
+						serverNode.addChild(new ProjectNode(projectInfo.getProjectName()));
+					}
+					viewer.refresh();
+				}catch(NullPointerException e){
+					showMessage("ServerInfo is not set for this ServerNode!");
+				}
+			}
+
+		};
+		login.setText("Login");
+		login.setToolTipText("Click to log on the server");
+		login.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages()
+				.getImageDescriptor(ISharedImages.IMG_OBJ_ELEMENT));
+
+		doubleClickAction = new Action() {
+			public void run() {
+				ISelection selection = viewer.getSelection();
+				Object obj = ((IStructuredSelection) selection)
+						.getFirstElement();
+				if (obj instanceof ServerNode) {
+					login.setServerNode((ServerNode)obj);
+					login.run();
+				} else {
+					checkout.run();
+				}
+			}
+		};
+	}
+
+	private void hookDoubleClickAction() {
+		viewer.addDoubleClickListener(new IDoubleClickListener() {
+			public void doubleClick(DoubleClickEvent event) {
+				doubleClickAction.run();
+			}
+		});
+	}
+
+	private void showMessage(String message) {
+		MessageDialog.openInformation(viewer.getControl().getShell(),
+				"Project Repositories", message);
+	}
+
+	/**
+	 * Passing the focus request to the viewer's control.
+	 */
+	public void setFocus() {
+		viewer.getControl().setFocus();
+	}
+}
