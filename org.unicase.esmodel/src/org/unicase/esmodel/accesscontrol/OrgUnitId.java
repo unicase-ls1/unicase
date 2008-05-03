73446ffff5aa9e1545516ddae556a238c9b74d3c
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-05-03 09:43:11 +0000

    refactored esmodel: added unique ids added accesscontrol

---
+++ org.unicase.esmodel/src/org/unicase/esmodel/accesscontrol/OrgUnitId.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.unicase.esmodel.accesscontrol;
+
+import org.unicase.model.UniqueIdentifier;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Org Unit Id</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.unicase.esmodel.accesscontrol.AccesscontrolPackage#getOrgUnitId()
+ * @model
+ * @generated
+ */
+public interface OrgUnitId extends UniqueIdentifier {
+} // OrgUnitId
---
+++ org.unicase.esmodel/src/org/unicase/esmodel/accesscontrol/impl/OrgUnitIdImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.unicase.esmodel.accesscontrol.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.unicase.esmodel.accesscontrol.AccesscontrolPackage;
+import org.unicase.esmodel.accesscontrol.OrgUnitId;
+
+import org.unicase.model.impl.UniqueIdentifierImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Org Unit Id</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class OrgUnitIdImpl extends UniqueIdentifierImpl implements OrgUnitId {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected OrgUnitIdImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return AccesscontrolPackage.Literals.ORG_UNIT_ID;
+	}
+
+} //OrgUnitIdImpl
