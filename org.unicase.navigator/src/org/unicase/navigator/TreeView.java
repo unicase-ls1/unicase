928a090b2a95f5754810652457bf3373400a07f7
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-05-04 11:18:23 +0000

    refactored for new workspace API

--- org.unicase.navigator/src/org/unicase/navigator/TreeView.java
+++ org.unicase.navigator/src/org/unicase/navigator/TreeView.java
@@ -2,27 +2,43 @@ package org.unicase.navigator;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.ui.navigator.CommonNavigator;
+import org.unicase.esmodel.ProjectInfo;
 import org.unicase.model.Project;
-import org.unicase.model.impl.ProjectImpl;
-import org.unicase.workspace.PrimaryVersionSpec;
-import org.unicase.workspace.ProjectInfo;
-import org.unicase.workspace.UCUserSession;
-import org.unicase.workspace.UCWorkspace;
+import org.unicase.workspace.Configuration;
+import org.unicase.workspace.ConnectionException;
+import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.Usersession;
+import org.unicase.workspace.Workspace;
+import org.unicase.workspace.WorkspaceManager;
+import org.unicase.workspace.impl.WorkspaceFactoryImpl;
 
 public class TreeView extends CommonNavigator {
 
 	@Override
 	protected IAdaptable getInitialInput() {
 
-		UCUserSession session = new UCUserSession(UCUserSession
-				.getDefaultServerInfo());
-		session.logIn("", "");
-		ProjectInfo projectInfo = session.getRemoteProjectList().get(0);
-		UCWorkspace checkOut = session.checkOut(projectInfo.getProjectId(),
-				projectInfo.getVersion());
-		Project project = checkOut.getProject();
+		Usersession usersession = WorkspaceFactoryImpl.eINSTANCE
+				.createUsersession();
+		usersession.setServerInfo(Configuration.getDefaultServerInfo());
+		usersession.setUsername("user");
+		usersession.logIn("password");
 
-		return project;
+		Workspace workspace = WorkspaceManager.getInstance()
+				.getCurrentWorkspace();
+		ProjectInfo projectInfo;
+		try {
+			projectInfo = usersession.getRemoteProjectList().get(0);
+			ProjectSpace projectSpace;
+			projectSpace = usersession.checkout(projectInfo.getProjectId(),
+					projectInfo.getVersion());
+			Project project = projectSpace.getProject();
+			return project;
+		} catch (ConnectionException e) {
+			// MK Auto-generated catch block
+			e.printStackTrace();
+			throw new IllegalStateException();
+		}
+		
 	}
 
 }
