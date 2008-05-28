a972d0475d2a2149f4815239134685bf70d60f27
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-05-28 14:27:50 +0000

    modified dummyproject

--- org.unicase.workspace/src/org/unicase/workspace/connectionmanager/StubConnectionManagerImpl.java
+++ org.unicase.workspace/src/org/unicase/workspace/connectionmanager/StubConnectionManagerImpl.java
@@ -3,6 +3,7 @@ package org.unicase.workspace.connectionmanager;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.unicase.emfstore.EmfStoreStub;
 import org.unicase.emfstore.exceptions.ConnectionException;
 import org.unicase.emfstore.model.EsmodelFactory;
 import org.unicase.emfstore.model.ProjectId;
@@ -16,13 +17,13 @@ import org.unicase.emfstore.model.changemanagment.LogMessage;
 import org.unicase.emfstore.model.changemanagment.PrimaryVersionSpec;
 import org.unicase.emfstore.model.changemanagment.VersionSpec;
 import org.unicase.emfstore.model.changemanagment.impl.ChangemanagmentFactoryImpl;
-import org.unicase.model.CompositeSection;
 import org.unicase.model.FunctionalRequirement;
-import org.unicase.model.LeafSection;
 import org.unicase.model.ModelFactory;
 import org.unicase.model.Project;
 import org.unicase.model.diagram.DiagramFactory;
 import org.unicase.model.diagram.MEDiagram;
+import org.unicase.model.document.CompositeSection;
+import org.unicase.model.document.LeafSection;
 import org.unicase.workspace.ServerInfo;
 
 public class StubConnectionManagerImpl implements ConnectionManager {
@@ -47,7 +48,7 @@ public class StubConnectionManagerImpl implements ConnectionManager {
 
 	public Project getProject(SessionId sessionId, ProjectId projectId,
 			VersionSpec versionSpec) throws ConnectionException {
-		return createDummyProject();
+		return EmfStoreStub.createDummyProject();
 	}
 
 	public List<ProjectInfo> getProjectList(SessionId sessionId)
@@ -79,87 +80,6 @@ public class StubConnectionManagerImpl implements ConnectionManager {
 		return primaryVersionSpec;
 	}
 	
-	private Project createDummyProject() {
-		// Generate the elements of the model
-		
-		ModelFactory factory = ModelFactory.eINSTANCE;
-		Project project = factory.createProject();
-		
-		//RootSection
-		CompositeSection rootSection = factory.createCompositeSection();
-		rootSection.setName("RAD");
-		rootSection
-				.setDescription("The requirements analysis document of the project");
-		project.addModelElement(rootSection);
-
-		//Functional Requirements
-		LeafSection reqLeafSection = factory.createLeafSection();
-		reqLeafSection.setName("Functional Requirements");
-		reqLeafSection
-				.setDescription("Lists all functional requirements in this project");
-		reqLeafSection.setElementClass(FunctionalRequirement.class);
-		reqLeafSection.setParent(rootSection);
-		FunctionalRequirement fr = factory.createFunctionalRequirement();
-		fr.setName("My Reuirement");
-		FunctionalRequirement fr2 = factory.createFunctionalRequirement();
-		fr2.setName("My Reuirement2");
-		reqLeafSection.getModelElements().add(fr);
-		reqLeafSection.getModelElements().add(fr2);
-
-		//ActionItems
-		LeafSection actionItemLeafSection = factory.createLeafSection();
-		actionItemLeafSection.setName("Action Items");
-		actionItemLeafSection.setDescription("Lists all ais of this project.");
-		actionItemLeafSection.setParent(rootSection);
-		actionItemLeafSection.setElementClass(FunctionalRequirement.class);
-		//TODO: Create some AI«s here
-		
-		
-		//Diagrams
-		LeafSection diagramSection = factory.createLeafSection();
-		diagramSection.setName("Diagrams");
-		diagramSection.setDescription("Lists all diagrams of this project.");
-		diagramSection.setParent(rootSection);
-		diagramSection.setElementClass(FunctionalRequirement.class);
-		
-		DiagramFactory diagramFactory = DiagramFactory.eINSTANCE;
-		MEDiagram diagram = diagramFactory.createMEDiagram();
-		diagram.setName("Super Diagram");
-		diagramSection.getModelElements().add(diagram);
-		
-		
-		//Other Crap
-		CompositeSection ucmCompositeSection = factory.createCompositeSection();
-		ucmCompositeSection.setName("Use Case Modeling");
-		ucmCompositeSection
-				.setDescription("Contains sections concerning use cases.");
-		ucmCompositeSection.setParent(rootSection);
-
-		
-
-		LeafSection useCaseLeafSection = factory.createLeafSection();
-		useCaseLeafSection.setName("Use Cases");
-		useCaseLeafSection
-				.setDescription("Lists all use cases of this project.");
-		useCaseLeafSection.setParent(ucmCompositeSection);
-		useCaseLeafSection.setElementClass(FunctionalRequirement.class);
-
-		LeafSection useCaseDiagramLeafSection = factory.createLeafSection();
-		useCaseDiagramLeafSection.setName("Use Case Diagrams");
-		useCaseDiagramLeafSection
-				.setDescription("Lists all use case diagrams of this project.");
-		useCaseDiagramLeafSection.setParent(ucmCompositeSection);
-		useCaseDiagramLeafSection.setElementClass(FunctionalRequirement.class);
-		
-		
-		useCaseDiagramLeafSection.setName("Use Case Diagrams");
-		useCaseDiagramLeafSection
-				.setDescription("Lists all use case diagrams of this project.");
-		useCaseDiagramLeafSection.setParent(ucmCompositeSection);
-		useCaseDiagramLeafSection.setElementClass(FunctionalRequirement.class);
-
-		return project;
-		// end of generation
-	}
+	
 
 }
