14be38bb6690c99500c259f53886693189103b28
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-05-30 16:26:39 +0000

    Implemented Roles and extended AccessControl

---
+++ org.unicase.emfstore/src/org/unicase/emfstore/accesscontrol/AuthorizationControl.java
@@ -0,0 +1,23 @@
+package org.unicase.emfstore.accesscontrol;
+
+import java.util.Set;
+
+import org.unicase.emfstore.model.ProjectId;
+import org.unicase.emfstore.model.SessionId;
+import org.unicase.emfstore.model.accesscontrol.roles.Role;
+import org.unicase.model.ModelElement;
+
+public interface AuthorizationControl {
+	
+	public void checkSession(SessionId sessionId);
+	
+	public Role getRole(SessionId sessionId);
+	
+	public void checkReadAccess(SessionId sessionId, ProjectId projectId);
+	
+	public void checkWriteAccess(SessionId sessionId, ProjectId projectId, Set<ModelElement> modelElements);
+	
+	
+	
+
+}
