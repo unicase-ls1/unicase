cf5bcdf8ff7bcfaf1293f2848da187d54803b078
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-06-09 08:41:16 +0000

    added gantcontrol

---
+++ org.unicase.ui.meeditor/src/org/unicase/ui/meeditor/mecontrols/MEGantControl.java
@@ -0,0 +1,22 @@
+package org.unicase.ui.meeditor.mecontrols;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+public class MEGantControl extends AbstractMEControl{
+
+	public MEGantControl(EditingDomain editingDomain, EObject modelElement,
+			FormToolkit toolkit) {
+		super(editingDomain, modelElement, toolkit);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Control createControl(Composite parent, int style) {
+		
+		return null;
+	}
+	
+}
