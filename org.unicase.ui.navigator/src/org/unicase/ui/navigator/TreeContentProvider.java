08ccfdff2fde9361075b84f17da02a9ae3517155
Hodaie <Hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-06-12 15:15:04 +0000

    fixed a silly mistake in navigator :).

--- org.unicase.ui.navigator/src/org/unicase/ui/navigator/TreeContentProvider.java
+++ org.unicase.ui.navigator/src/org/unicase/ui/navigator/TreeContentProvider.java
@@ -1,13 +1,13 @@
 package org.unicase.ui.navigator;
 
-//import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
-//import org.eclipse.emf.transaction.TransactionalEditingDomain;
-//import org.eclipse.emf.transaction.ui.provider.TransactionalAdapterFactoryContentProvider;
+import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.ui.provider.TransactionalAdapterFactoryContentProvider;
 
-//public class TreeContentProvider extends TransactionalAdapterFactoryContentProvider {
-//
-//	public TreeContentProvider() {
-//		super(TransactionalEditingDomain.Registry.INSTANCE.getEditingDomain("org.unicase.EditingDomain"),new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
-//	}
-//
-//}
+public class TreeContentProvider extends TransactionalAdapterFactoryContentProvider {
+
+	public TreeContentProvider() {
+		super(TransactionalEditingDomain.Registry.INSTANCE.getEditingDomain("org.unicase.EditingDomain"),new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
+	}
+
+}
