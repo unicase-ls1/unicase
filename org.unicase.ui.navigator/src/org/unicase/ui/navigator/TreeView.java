754375e282a777d69bd9dad78ec72f9984c1d15c
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-06-16 08:22:49 +0000

    moodified open procedure for diagram

--- org.unicase.ui.navigator/src/org/unicase/ui/navigator/TreeView.java
+++ org.unicase.ui.navigator/src/org/unicase/ui/navigator/TreeView.java
@@ -3,6 +3,8 @@ package org.unicase.ui.navigator;
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.common.ui.URIEditorInput;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.PartInitException;
@@ -14,74 +16,69 @@ import org.unicase.ui.meeditor.MEEditorInput;
 import org.unicase.workspace.Workspace;
 import org.unicase.workspace.WorkspaceManager;
 
+
 public class TreeView extends CommonNavigator {
 
 	@Override
 	protected IAdaptable getInitialInput() {
 
 		Workspace workspace = WorkspaceManager.getInstance()
-				.getCurrentWorkspace();
-
-		// Usersession usersession = WorkspaceFactory.eINSTANCE
-		// .createUsersession();
-		// usersession.setServerInfo(workspace.getServerInfos().get(0));
-		// usersession.setUsername("user");
-		// usersession.setPassword("password");
-		// usersession.setSavePassword(true);
-		// workspace.getUsersessions().add(usersession);
-		//
-		// if (workspace.getProjectSpaces().size() < 2) {
-		//
-		// ProjectInfo projectInfo;
-		// try {
-		// try {
-		// usersession.logIn();
-		// } catch (AccessControlException e) {
-		// // MK Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// projectInfo = usersession.getRemoteProjectList().get(0);
-		// usersession.checkout(projectInfo);
-		// projectInfo = usersession.getRemoteProjectList().get(0);
-		// ProjectSpace checkout = usersession.checkout(projectInfo);
-		//
-		//
-		//
-		// } catch (EmfStoreException e) {
-		// // MK Auto-generated catch block
-		// e.printStackTrace();
-		// throw new IllegalStateException();
-		// }
-		// }
+		.getCurrentWorkspace();
+		
+//		Usersession usersession = WorkspaceFactory.eINSTANCE
+//				.createUsersession();
+//		usersession.setServerInfo(workspace.getServerInfos().get(0));
+//		usersession.setUsername("user");
+//		usersession.setPassword("password");
+//		usersession.setSavePassword(true);
+//		workspace.getUsersessions().add(usersession);
+//		
+//		if (workspace.getProjectSpaces().size() < 2) {
+//
+//			ProjectInfo projectInfo;
+//			try {
+//				try {
+//					usersession.logIn();
+//				} catch (AccessControlException e) {
+//					// MK Auto-generated catch block
+//					e.printStackTrace();
+//				}
+//				projectInfo = usersession.getRemoteProjectList().get(0);
+//				usersession.checkout(projectInfo);
+//				projectInfo = usersession.getRemoteProjectList().get(0);
+//				ProjectSpace checkout = usersession.checkout(projectInfo);
+//				
+//				
+//
+//			} catch (EmfStoreException e) {
+//				// MK Auto-generated catch block
+//				e.printStackTrace();
+//				throw new IllegalStateException();
+//			}
+//		}
 
 		return workspace;
 
 	}
-
-	/**
-	 * . ({@inheritDoc}) On DoubleClick open the ModelElement in Editor
+	
+	/**.
+	 * ({@inheritDoc})
+	 * On DoubleClick open the ModelElement in Editor
 	 */
 	@Override
 	protected void handleDoubleClick(DoubleClickEvent anEvent) {
-
+		
 		TreeSelection selection = (TreeSelection) anEvent.getSelection();
 		Object object = selection.getFirstElement();
 		if (object instanceof ModelElement) {
 			if (object instanceof MEDiagram) {
-				ModelElement modelElement = (ModelElement) object;
-
-				URIEditorInput input = new URIEditorInput(URI
-						.createURI(modelElement.eResource().getURIFragment(
-								modelElement)));
+				ModelElement modelElement = (ModelElement)object;
+				Resource resource=  modelElement.eResource();
+				URIEditorInput input = new URIEditorInput(URI.createURI(modelElement.eResource().getURIFragment(modelElement)));
 				try {
-					PlatformUI
-							.getWorkbench()
-							.getActiveWorkbenchWindow()
-							.getActivePage()
-							.openEditor(
-									input,
-									"org.unicase.model.classDiagram.part.ModelDiagramEditorID",
-									true);
+					PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+					.getActivePage().openEditor(input,
+							"org.unicase.model.classDiagram.part.ModelDiagramEditorID", true);
 				} catch (PartInitException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -99,5 +96,6 @@ public class TreeView extends CommonNavigator {
 			}
 		}
 	}
+	
 
 }
