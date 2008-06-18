b88e3a904bc9cabaf2ac49c93a9ba31e3c26cb44
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-06-18 18:29:46 +0000

    removed warnings

--- org.unicase.emfstore/src/org/unicase/emfstore/connection/rmi/UnicaseSecurityManager.java
+++ org.unicase.emfstore/src/org/unicase/emfstore/connection/rmi/UnicaseSecurityManager.java
@@ -10,41 +10,40 @@ import java.io.FileDescriptor;
 import java.net.InetAddress;
 import java.security.Permission;
 
+/**
+ * This SecurityManager will make no restrictions at all to get RMI running.
+ *
+ * @author koegel
+ *
+ */
+//FIXME: make some restrictions
 public class UnicaseSecurityManager extends SecurityManager {
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkAccept(java.lang.String, int)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkAccept(String host, int port) {
-		// super.checkAccept(host, port);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkAccess(java.lang.Thread)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkAccess(Thread t) {
-		// MK Auto-generated method stub
-		// super.checkAccess(t);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkAccess(java.lang.ThreadGroup)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkAccess(ThreadGroup g) {
-		// MK Auto-generated method stub
-		// super.checkAccess(g);
+		//do nothing
 	}
 
 	/**
@@ -55,335 +54,243 @@ public class UnicaseSecurityManager extends SecurityManager {
 
 	@Override
 	public void checkAwtEventQueueAccess() {
-		// MK Auto-generated method stub
-		// super.checkAwtEventQueueAccess();
+		//do nothing
 	}
 
-	/**
-	 * @see java.lang.SecurityManager#checkConnect(java.lang.String, int,
-	 *      java.lang.Object)
-	 * 
-	 * @generated NOT
+	/** 
+	 * {@inheritDoc}
+	 * @see java.lang.SecurityManager#checkConnect(java.lang.String, int, java.lang.Object)
 	 */
-
 	@Override
 	public void checkConnect(String host, int port, Object context) {
-		// MK Auto-generated method stub
-		// super.checkConnect(host, port, context);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkConnect(java.lang.String, int)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkConnect(String host, int port) {
-		// MK Auto-generated method stub
-		// super.checkConnect(host, port);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkCreateClassLoader()
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkCreateClassLoader() {
-		// MK Auto-generated method stub
-		// super.checkCreateClassLoader();
+		//do nothing
 	}
 
-	/**
+	
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkDelete(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkDelete(String file) {
-		// MK Auto-generated method stub
-		// super.checkDelete(file);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkExec(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkExec(String cmd) {
-		// MK Auto-generated method stub
-		// super.checkExec(cmd);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkExit(int)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkExit(int status) {
-		// MK Auto-generated method stub
-		// super.checkExit(status);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkLink(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkLink(String lib) {
-		// MK Auto-generated method stub
-		// super.checkLink(lib);
+		//do nothing
 	}
-
-	/**
+	
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkListen(int)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkListen(int port) {
-		// MK Auto-generated method stub
-		// super.checkListen(port);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkMemberAccess(java.lang.Class, int)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkMemberAccess(Class<?> clazz, int which) {
-		// MK Auto-generated method stub
-		// super.checkMemberAccess(clazz, which);
+		//do nothing;
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkMulticast(java.net.InetAddress, byte)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkMulticast(InetAddress maddr, byte ttl) {
-		// MK Auto-generated method stub
-		// super.checkMulticast(maddr, ttl);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkMulticast(java.net.InetAddress)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkMulticast(InetAddress maddr) {
-		// MK Auto-generated method stub
-		// super.checkMulticast(maddr);
+		//do nothing
 	}
 
-	/**
+	
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkPackageAccess(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkPackageAccess(String pkg) {
-		// MK Auto-generated method stub
-		// super.checkPackageAccess(pkg);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkPackageDefinition(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkPackageDefinition(String pkg) {
-		// MK Auto-generated method stub
-		// super.checkPackageDefinition(pkg);
+		//do nothing
 	}
 
-	/**
-	 * @see java.lang.SecurityManager#checkPermission(java.security.Permission,
-	 *      java.lang.Object)
-	 * 
-	 * @generated NOT
+	/** 
+	 * {@inheritDoc}
+	 * @see java.lang.SecurityManager#checkPermission(java.security.Permission, java.lang.Object)
 	 */
-
 	@Override
 	public void checkPermission(Permission perm, Object context) {
-		// MK Auto-generated method stub
-		// super.checkPermission(perm, context);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkPermission(java.security.Permission)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkPermission(Permission perm) {
-		// MK Auto-generated method stub
-		// super.checkPermission(perm);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkPrintJobAccess()
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkPrintJobAccess() {
-		// MK Auto-generated method stub
-		// super.checkPrintJobAccess();
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkPropertiesAccess()
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkPropertiesAccess() {
-		// MK Auto-generated method stub
-		// super.checkPropertiesAccess();
+		//do nothing
 	}
-
-	/**
+	
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkPropertyAccess(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkPropertyAccess(String key) {
-		// MK Auto-generated method stub
-		// super.checkPropertyAccess(key);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkRead(java.io.FileDescriptor)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkRead(FileDescriptor fd) {
-		// MK Auto-generated method stub
-		// super.checkRead(fd);
+		//do nothing
 	}
 
-	/**
-	 * @see java.lang.SecurityManager#checkRead(java.lang.String,
-	 *      java.lang.Object)
-	 * 
-	 * @generated NOT
+	/** 
+	 * {@inheritDoc}
+	 * @see java.lang.SecurityManager#checkRead(java.lang.String, java.lang.Object)
 	 */
-
 	@Override
 	public void checkRead(String file, Object context) {
-		// MK Auto-generated method stub
-		// super.checkRead(file, context);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkRead(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkRead(String file) {
-		// MK Auto-generated method stub
-		// super.checkRead(file);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkSecurityAccess(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkSecurityAccess(String target) {
-		// MK Auto-generated method stub
-		// super.checkSecurityAccess(target);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkSetFactory()
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkSetFactory() {
-		// MK Auto-generated method stub
-		// super.checkSetFactory();
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkSystemClipboardAccess()
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkSystemClipboardAccess() {
-		// MK Auto-generated method stub
-		// super.checkSystemClipboardAccess();
+		//do nothing
 	}
 
-	/**
-	 * @see java.lang.SecurityManager#checkTopLevelWindow(java.lang.Object)
-	 * 
-	 * @generated NOT
-	 */
-
-	@Override
-	public boolean checkTopLevelWindow(Object window) {
-		// MK Auto-generated method stub
-		return super.checkTopLevelWindow(window);
-	}
-
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkWrite(java.io.FileDescriptor)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkWrite(FileDescriptor fd) {
-		// MK Auto-generated method stub
-		// super.checkWrite(fd);
+		//do nothing
 	}
 
-	/**
+	/** 
+	 * {@inheritDoc}
 	 * @see java.lang.SecurityManager#checkWrite(java.lang.String)
-	 * 
-	 * @generated NOT
 	 */
-
 	@Override
 	public void checkWrite(String file) {
-		// MK Auto-generated method stub
-		// super.checkWrite(file);
+		//do nothing
 	}
 
 }
