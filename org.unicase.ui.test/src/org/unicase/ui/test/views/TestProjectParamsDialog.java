af0fac418d1cff6f4fa1cc5554e2c550b528da74
Hodaie <Hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-06-23 15:06:48 +0000

    Initial import of org.unicase.ui.test plug-in

---
+++ org.unicase.ui.test/src/org/unicase/ui/test/views/TestProjectParamsDialog.java
@@ -0,0 +1,117 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Kšgel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.unicase.ui.test.views;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.unicase.model.testproject.TestProject;
+
+public class TestProjectParamsDialog extends Dialog {
+
+	
+	private Shell shell;
+	
+	private Label lblNumOfEachME;
+	private Label lblRandomSeed;
+	private Label lblProjectWidth;
+	private Label lblProjectDepth;
+	
+	private Text txtNumOfEachME;
+	private Text txtRandomSeed;
+	private Text txtProjectWidth;
+	private Text txtProjectDepth;
+	
+	
+	public TestProjectParamsDialog(Shell parentShell) {
+		super(parentShell);
+	
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		
+		Composite composite = (Composite) super.createDialogArea(parent);
+		createControls(composite);
+		return composite;
+	}
+	
+	private void createControls(Composite composite){
+		
+		shell = composite.getShell(); 
+		shell.setText("Input Dialog"); 
+
+		Group group = new Group(composite, SWT.None); 
+		group.setText(""); 
+		group.setLayoutData(new GridData(GridData.FILL_HORIZONTAL)); 
+		group.setLayout(new GridLayout(2, true)); 
+		
+		lblNumOfEachME = new Label(group, SWT.None); 
+		lblNumOfEachME.setText("Number of each ME:"); 
+		txtNumOfEachME = new Text(group,SWT.BORDER); 
+		txtNumOfEachME.setText("5"); 
+		txtNumOfEachME.setLayoutData(new GridData(GridData.FILL_HORIZONTAL)); 
+
+		lblRandomSeed = new Label(group, SWT.None); 
+		lblRandomSeed.setText("Random seed:"); 
+		txtRandomSeed = new Text(group,SWT.BORDER); 
+		txtRandomSeed.setText("1334566732"); 
+		txtRandomSeed.setLayoutData(new GridData(GridData.FILL_HORIZONTAL)); 
+		
+		lblProjectWidth = new Label(group, SWT.None); 
+		lblProjectWidth.setText("Project width:"); 
+		txtProjectWidth = new Text(group,SWT.BORDER); 
+		txtProjectWidth.setText("2"); 
+		txtProjectWidth.setLayoutData(new GridData(GridData.FILL_HORIZONTAL)); 
+		
+		lblProjectDepth = new Label(group, SWT.None); 
+		lblProjectDepth.setText("Project depth:"); 
+		txtProjectDepth = new Text(group,SWT.BORDER); 
+		txtProjectDepth.setText("2"); 
+		txtProjectDepth.setLayoutData(new GridData(GridData.FILL_HORIZONTAL)); 
+		
+		
+		group.pack(); 
+		composite.pack(); 
+
+		
+	}
+	
+	
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) { 
+		 
+		super.createButtonsForButtonBar(parent); 
+	}
+
+
+
+	@Override
+	protected void okPressed() {
+		
+		int numOfEachME = Integer.parseInt(txtNumOfEachME.getText()) ;
+		int randomSeed = Integer.parseInt(txtRandomSeed.getText()) ;
+		int projectWidth = Integer.parseInt(txtProjectWidth.getText()) ;
+		int projectDepth = Integer.parseInt(txtProjectDepth.getText()) ;
+		
+		super.okPressed();
+		
+		TestProject testProject = new TestProject(numOfEachME, randomSeed, projectWidth, projectDepth);		
+		testProject.createProject();
+		
+		
+		
+	} 
+	
+}
---
+++ org.unicase.ui.test/src/org/unicase/ui/test/views/TestView.java
@@ -0,0 +1,24 @@
+package org.unicase.ui.test.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class TestView extends ViewPart {
+
+	public TestView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
