9ab757eaefb7c7b4286ff84dc49b2838e27d1a16
Hodaie <Hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-01 21:13:38 +0000

    added required method signatures to AdminEmfStore interface

--- org.unicase.emfstore/src/org/unicase/emfstore/AdminEmfStore.java
+++ org.unicase.emfstore/src/org/unicase/emfstore/AdminEmfStore.java
@@ -1,5 +1,18 @@
 package org.unicase.emfstore;
 
+import org.eclipse.emf.common.util.EList;
+import org.unicase.emfstore.esmodel.ProjectInfo;
+import org.unicase.emfstore.esmodel.accesscontrol.ACGroup;
+import org.unicase.emfstore.esmodel.accesscontrol.ACOrgUnit;
+import org.unicase.emfstore.esmodel.accesscontrol.ACUser;
+
 public interface AdminEmfStore {
 
+	public EList<ProjectInfo> getProjectInfos();
+	public EList<ACGroup> getGroups();
+	public EList<ACUser> getUsers();
+	public EList<ACOrgUnit> getOrgUnits();
+	public EList<ACGroup> getGroups(ACUser user);
+	public EList<ACOrgUnit> getParticipants(ProjectInfo project);
+	
 }
