7748127272c277032dcc8f104f8ee386c1a5ba76
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-07 10:10:19 +0000

    removed warnings

--- org.unicase.ui.test/src/org/unicase/ui/test/Activator.java
+++ org.unicase.ui.test/src/org/unicase/ui/test/Activator.java
@@ -1,3 +1,9 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Kšgel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * </copyright>
+ *
+ * $Id$
+ */
 package org.unicase.ui.test;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -27,22 +33,26 @@ public class Activator extends AbstractUIPlugin {
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
+	//BEGIN SUPRESS CATCH EXCEPTION
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
+	//END SUPRESS CATCH EXCEPTION
 
 	/*
 	 * 
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
+	//BEGIN SUPRESS CATCH EXCEPTION
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
+	//END SUPRESS CATCH EXCEPTION
 
 	/**.
 	 * Returns the shared instance
