26e3f3b43da1ba0c4f7e2a36e4ce2353a9d54de5
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-07 12:39:21 +0000

    replaced o umlaut in header by oe changed checkstyle accordingly

--- org.unicase.model/src/org/unicase/model/task/util/MEState.java
+++ org.unicase.model/src/org/unicase/model/task/util/MEState.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008 Jonas Helming, Maximilian Kšgel
+ * Copyright (c) 2008 Jonas Helming, Maximilian Koegel
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
