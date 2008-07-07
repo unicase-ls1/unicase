15ba870e2c068872f55e5fad06a67fabd1b76daf
Hodaie <Hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-07 17:28:54 +0000

    removed CheckStyle warnings

--- org.unicase.ui.tableview/src/org/unicase/ui/tableview/Activator.java
+++ org.unicase.ui.tableview/src/org/unicase/ui/tableview/Activator.java
@@ -14,7 +14,9 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-	// The plug-in ID
+	/**.
+	 *  The plug-in ID
+	 */
 	public static final String PLUGIN_ID = "org.unicase.ui.tableview";
 
 	// The shared instance
@@ -31,21 +33,24 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
+	//BEGIN SUPRESS CATCH EXCEPTION
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
-
+	//END SUPRESS CATCH EXCEPTION
+	
 	/** 
 	 * {@inheritDoc}
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
+	//BEGIN SUPRESS CATCH EXCEPTION
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
-
+	//END SUPRESS CATCH EXCEPTION
 	/**
 	 * Returns the shared instance.
 	 * 
