b7799bea363a9d8168ed59e31dab356519c6d4b6
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-16 12:28:18 +0000

    added delete handler

---
+++ org.unicase.model.edit/src/org/unicase/model/edit/commands/DeleteModelelementHandler.java
@@ -0,0 +1,14 @@
+package org.unicase.model.edit.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DeleteModelelementHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
