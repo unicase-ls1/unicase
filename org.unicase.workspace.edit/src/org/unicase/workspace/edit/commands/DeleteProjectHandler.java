39304eb3537b2f4f20b55494ad96783780c145af
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-21 21:39:05 +0000

    added delete project action

---
+++ org.unicase.workspace.edit/src/org/unicase/workspace/edit/commands/DeleteProjectHandler.java
@@ -0,0 +1,14 @@
+package org.unicase.workspace.edit.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DeleteProjectHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		//MK: Delete Project
+		return null;
+	}
+
+}
