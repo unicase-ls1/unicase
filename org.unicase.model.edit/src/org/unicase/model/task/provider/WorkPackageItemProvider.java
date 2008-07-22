01539a00aecff4df9b3fbe445a473fff70a70a39
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-22 12:51:41 +0000

    adapted getText()

--- org.unicase.model.edit/src/org/unicase/model/task/provider/WorkPackageItemProvider.java
+++ org.unicase.model.edit/src/org/unicase/model/task/provider/WorkPackageItemProvider.java
@@ -131,13 +131,11 @@ public class WorkPackageItemProvider extends AnnotationItemProvider implements
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc
 	 * --> <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
-		String label = ((WorkPackage) object).getName();
-		return label == null || label.length() == 0 ? getString("_UI_WorkPackage_type")
-				: getString("_UI_WorkPackage_type") + " " + label;
+		return super.getText(object);
 	}
 
 	/**
