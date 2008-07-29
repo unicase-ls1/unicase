f5cb3a1b1ce1a397bac24529b8bf92fedc32325d
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-07-29 13:14:13 +0000

    added state decorator

--- org.unicase.ui.common/src/org/unicase/ui/common/decorators/ReadOnly.java
+++ org.unicase.ui.common/src/org/unicase/ui/common/decorators/ReadOnly.java
@@ -10,18 +10,15 @@
  *******************************************************************************/
 package org.unicase.ui.common.decorators;
 
-import java.net.MalformedURLException;
 import java.net.URL;
-
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourceAttributes;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ILightweightLabelDecorator;
-import org.unicase.model.ModelElement;
-import org.unicase.model.task.util.CircularDependencyException;
-import org.unicase.model.task.util.MEState;
 
 /**
  * An example showing how to control when an element is decorated. This example
@@ -30,7 +27,7 @@ import org.unicase.model.task.util.MEState;
  * 
  * @see ILightweightLabelDecorator
  */
-public class StateDecorator implements ILightweightLabelDecorator {
+public class ReadOnly implements ILightweightLabelDecorator {
 	/**
 	 * String constants for the various icon placement options from the template
 	 * wizard.
@@ -49,10 +46,7 @@ public class StateDecorator implements ILightweightLabelDecorator {
 	private int quadrant;
 
 	/** The icon image location in the project folder */
-	private String openPath = "icons/open.gif"; // NON-NLS-1
-
-	/** The icon image location in the project folder */
-	private String blockedPath = "icons/blocked.gif"; // NON-NLS-1
+	private String iconPath = "icons/1open.gif"; //NON-NLS-1
 
 	/**
 	 * The image description used in
@@ -60,88 +54,50 @@ public class StateDecorator implements ILightweightLabelDecorator {
 	 */
 	private ImageDescriptor descriptor;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.jface.viewers.ILightweightLabelDecorator#decorate(java.lang
-	 * .Object, org.eclipse.jface.viewers.IDecoration)
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ILightweightLabelDecorator#decorate(java.lang.Object, org.eclipse.jface.viewers.IDecoration)
 	 */
 	public void decorate(Object element, IDecoration decoration) {
 		/**
-		 * Checks that the element is an IResource with the 'Read-only'
-		 * attribute and adds the decorator based on the specified image
-		 * description and the integer representation of the placement option.
+		 * Checks that the element is an IResource with the 'Read-only' attribute
+		 * and adds the decorator based on the specified image description and the
+		 * integer representation of the placement option.
 		 */
-		URL url = null;
+		IResource resource = (IResource) element;
+		ResourceAttributes attrs = resource.getResourceAttributes();
+		
+			URL url = Platform.find(
+					Platform.getBundle("org.unicase.ui.common"), new Path(iconPath)); //NON-NLS-1
 
-		ModelElement me;
-		if (element instanceof ModelElement) {
-			me = (ModelElement) element;
-		} else {
-			return;
-		}
-		try {
-			if (me.getMEState().getStatus().equals(MEState.OPEN)) {
-				url = Platform.find(
-						Platform.getBundle("org.unicase.ui.common"), new Path(openPath)); //NON-NLS-1
-
-			}
-			if (me.getMEState().getStatus().equals(MEState.BLOCKED)) {
-				url = Platform.find(
-						Platform.getBundle("org.unicase.ui.common"), new Path(
-								blockedPath));
-			}
-		} catch (CircularDependencyException e) {
-			// JH : add questionmark image
-			e.printStackTrace();
-			return;
-		}
-
-		if (url == null) {
-			return;
-		}
-
-		descriptor = ImageDescriptor.createFromURL(url);
-		quadrant = IDecoration.TOP_RIGHT;
-		decoration.addOverlay(descriptor, quadrant);
+			if (url == null)
+				return;
+			descriptor = ImageDescriptor.createFromURL(url);			
+			quadrant = IDecoration.BOTTOM_RIGHT;
+			decoration.addOverlay(descriptor,quadrant);
+		
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.jface.viewers.IBaseLabelProvider#addListener(org.eclipse.
-	 * jface.viewers.ILabelProviderListener)
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#addListener(org.eclipse.jface.viewers.ILabelProviderListener)
 	 */
 	public void addListener(ILabelProviderListener listener) {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
+	/* (non-Javadoc)
 	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#dispose()
 	 */
 	public void dispose() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(java.lang
-	 * .Object, java.lang.String)
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#isLabelProperty(java.lang.Object, java.lang.String)
 	 */
 	public boolean isLabelProperty(Object element, String property) {
 		return false;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.eclipse.jface.viewers.IBaseLabelProvider#removeListener(org.eclipse
-	 * .jface.viewers.ILabelProviderListener)
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#removeListener(org.eclipse.jface.viewers.ILabelProviderListener)
 	 */
 	public void removeListener(ILabelProviderListener listener) {
 	}
