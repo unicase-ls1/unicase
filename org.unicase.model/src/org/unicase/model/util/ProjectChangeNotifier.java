fd16ac549cc60a24e6d87eae8a533ab7742fc7da
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-09-24 15:47:58 +0000

    added documentation, removed warnings

--- org.unicase.model/src/org/unicase/model/util/ProjectChangeNotifier.java
+++ org.unicase.model/src/org/unicase/model/util/ProjectChangeNotifier.java
@@ -1,3 +1,9 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * </copyright>
+ *
+ * $Id$
+ */
 package org.unicase.model.util;
 
 import java.util.List;
@@ -11,13 +17,34 @@ import org.unicase.model.ModelElement;
 import org.unicase.model.ModelPackage;
 import org.unicase.model.Project;
 
+/**
+ * Notifies about changes in a project. WARNING: Only for use in Project! Use
+ * addProjectObserver of Project to listen for a Project.
+ * 
+ * @author koegel
+ * 
+ */
 public class ProjectChangeNotifier extends AdapterImpl {
 
 	private ProjectChangeObserver projectChangeObserver;
 	private Project project;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param project
+	 *            the project
+	 * @param projectChangeObserver
+	 *            the listener
+	 */
 	public ProjectChangeNotifier(Project project,
 			ProjectChangeObserver projectChangeObserver) {
+
+		if (!(projectChangeObserver instanceof Project)) {
+			throw new IllegalArgumentException(
+					"This Notifier is only to be used by Project!");
+		}
+
 		this.projectChangeObserver = projectChangeObserver;
 		this.project = project;
 		TreeIterator<EObject> allContents = project.eAllContents();
@@ -31,8 +58,14 @@ public class ProjectChangeNotifier extends AdapterImpl {
 		project.eAdapters().add(this);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.common.notify.impl.AdapterImpl#notifyChanged(org.eclipse.emf.common.notify.Notification)
+	 */
+	@Override
 	public void notifyChanged(Notification notification) {
-		
+
 		if (notification.isTouch()) {
 			return;
 		}
@@ -41,76 +74,23 @@ public class ProjectChangeNotifier extends AdapterImpl {
 			if (reference.isContainment()) {
 				switch (notification.getEventType()) {
 				case Notification.ADD:
-
-					EObject newValue = (EObject) notification.getNewValue();
-					if (ModelPackage.eINSTANCE.getModelElement().isInstance(
-							newValue)) {
-						ModelElement modelElement = (ModelElement) newValue;
-						if (!project.contains(modelElement)) {
-							newValue.eAdapters().add(this);
-							this.projectChangeObserver.modelElementAdded(
-									project, (ModelElement) newValue);
-						}
-					}
-
+					handleAddNotification(notification);
 					break;
 				case Notification.ADD_MANY:
-
-					List<EObject> newValues = (List<EObject>) notification
-							.getNewValue();
-					for (EObject newElement : newValues) {
-						if (ModelPackage.eINSTANCE.getModelElement()
-								.isInstance(newElement)) {
-							ModelElement modelElement = (ModelElement) newElement;
-							if (!project.contains(modelElement)) {
-								newElement.eAdapters().add(this);
-								this.projectChangeObserver.modelElementAdded(
-										project, modelElement);
-							}
-						}
-
-					}
-
+					handleAddAllNotification(notification);
 					break;
 				case Notification.REMOVE:
-					EObject oldValue = (EObject) notification.getOldValue();
-					if (ModelPackage.eINSTANCE.getModelElement().isInstance(
-							oldValue)) {
-						ModelElement modelElement = (ModelElement) oldValue;
-						oldValue.eAdapters().remove(this);
-						this.projectChangeObserver.modelElementRemoved(project,
-								(ModelElement) oldValue);
-					}
-
+					handleRemoveNotification(notification);
 					break;
 				case Notification.REMOVE_MANY:
-
-					List<EObject> oldValues = (List<EObject>) notification
-							.getOldValue();
-					for (EObject newElement : oldValues) {
-						if (ModelPackage.eINSTANCE.getModelElement()
-								.isInstance(newElement)) {
-							ModelElement modelElement = (ModelElement) newElement;
-							newElement.eAdapters().remove(this);
-							this.projectChangeObserver.modelElementRemoved(
-									project, modelElement);
-						}
-
-					}
+					handleRemoveAllNotification(notification);
 					break;
 				case Notification.SET:
-					Object newSetValue = notification.getNewValue();
-					Object oldSetValue = notification.getOldValue();
-					if (newSetValue != null) {
-						if (newSetValue instanceof ModelElement) {
-							this.projectChangeObserver.modelElementAdded(
-									project, (ModelElement) newSetValue);
-						}
-					} else {
-						if (oldSetValue instanceof ModelElement)
-							this.projectChangeObserver.modelElementRemoved(
-									project, (ModelElement) oldSetValue);
-					}
+					handleSetNotification(notification);
+					break;
+				default:
+					throw new IllegalStateException(
+							"Unknown containment notification received");
 				}
 
 			}
@@ -123,6 +103,76 @@ public class ProjectChangeNotifier extends AdapterImpl {
 					(ModelElement) notifier);
 		}
 
+	}
+
+	private void handleSetNotification(Notification notification) {
+		Object newSetValue = notification.getNewValue();
+		Object oldSetValue = notification.getOldValue();
+		if (newSetValue != null) {
+			if (newSetValue instanceof ModelElement) {
+				this.projectChangeObserver.modelElementAdded(project,
+						(ModelElement) newSetValue);
+			}
+		} else {
+			if (oldSetValue instanceof ModelElement) {
+				this.projectChangeObserver.modelElementRemoved(project,
+						(ModelElement) oldSetValue);
+			}
+		}
+	}
+
+	
+	private void handleRemoveAllNotification(Notification notification) {
+		//cast cannot be checked properly, flaw in EMF notification design
+		@SuppressWarnings("unchecked")
+		List<EObject> oldValues = (List<EObject>) notification.getOldValue();
+		for (EObject newElement : oldValues) {
+			if (ModelPackage.eINSTANCE.getModelElement().isInstance(newElement)) {
+				ModelElement modelElement = (ModelElement) newElement;
+				newElement.eAdapters().remove(this);
+				this.projectChangeObserver.modelElementRemoved(project,
+						modelElement);
+			}
+
+		}
+	}
+
+	private void handleRemoveNotification(Notification notification) {
+		EObject oldValue = (EObject) notification.getOldValue();
+		if (ModelPackage.eINSTANCE.getModelElement().isInstance(oldValue)) {
+			oldValue.eAdapters().remove(this);
+			this.projectChangeObserver.modelElementRemoved(project,
+					(ModelElement) oldValue);
+		}
+	}
+
+	private void handleAddAllNotification(Notification notification) {
+		//cast cannot be checked properly, flaw in EMF notification design
+		@SuppressWarnings("unchecked")
+		List<EObject> newValues = (List<EObject>) notification.getNewValue();
+		for (EObject newElement : newValues) {
+			if (ModelPackage.eINSTANCE.getModelElement().isInstance(newElement)) {
+				ModelElement modelElement = (ModelElement) newElement;
+				if (!project.contains(modelElement)) {
+					newElement.eAdapters().add(this);
+					this.projectChangeObserver.modelElementAdded(project,
+							modelElement);
+				}
+			}
+
+		}
+	}
+
+	private void handleAddNotification(Notification notification) {
+		EObject newValue = (EObject) notification.getNewValue();
+		if (ModelPackage.eINSTANCE.getModelElement().isInstance(newValue)) {
+			ModelElement modelElement = (ModelElement) newValue;
+			if (!project.contains(modelElement)) {
+				newValue.eAdapters().add(this);
+				this.projectChangeObserver.modelElementAdded(project,
+						(ModelElement) newValue);
+			}
+		}
 	}
 
 }
--- org.unicase.model/src/org/unicase/model/util/ProjectChangeObserver.java
+++ org.unicase.model/src/org/unicase/model/util/ProjectChangeObserver.java
@@ -1,14 +1,57 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * </copyright>
+ *
+ * $Id$
+ */
+
 package org.unicase.model.util;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.unicase.model.ModelElement;
 import org.unicase.model.Project;
 
+/**
+ * Observes changes to a Project. Can be registered with {@link Project}.
+ * 
+ * @author koegel
+ * 
+ */
 public interface ProjectChangeObserver {
 
-	void notify(Notification notification, Project project, ModelElement modelElement);
-	
+	/**
+	 * A notification on a model element of the project occured. See
+	 * {@link Notification} documentation for details on the notifications.
+	 * 
+	 * @param notification
+	 *            the notification
+	 * @param project
+	 *            the project
+	 * @param modelElement
+	 *            the model element the notification originates from
+	 */
+	void notify(Notification notification, Project project,
+			ModelElement modelElement);
+
+	/**
+	 * The given model element was added to the project. This means it is now in
+	 * the projects containment tree.
+	 * 
+	 * @param project
+	 *            the project
+	 * @param modelElement
+	 *            the modelElement
+	 */
 	void modelElementAdded(Project project, ModelElement modelElement);
-	
+
+	/**
+	 * The given model element was removed from the project. This means it is no
+	 * longer in the projects containment tree.
+	 * 
+	 * @param project
+	 *            the project
+	 * @param modelElement
+	 *            the modelElement
+	 */
 	void modelElementRemoved(Project project, ModelElement modelElement);
 }
