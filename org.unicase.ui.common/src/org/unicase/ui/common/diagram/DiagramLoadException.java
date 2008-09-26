50b488e4ca8324c89e27031034bb57288f59345d
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-09-26 09:02:07 +0000

    added two exceptions for diagram load and save

---
+++ org.unicase.ui.common/src/org/unicase/ui/common/diagram/DiagramLoadException.java
@@ -0,0 +1,13 @@
+package org.unicase.ui.common.diagram;
+
+public class DiagramLoadException extends Exception {
+
+	public DiagramLoadException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DiagramLoadException(String message) {
+		super(message);
+	}
+
+}
---
+++ org.unicase.ui.common/src/org/unicase/ui/common/diagram/DiagramStoreException.java
@@ -0,0 +1,13 @@
+package org.unicase.ui.common.diagram;
+
+public class DiagramStoreException extends Exception {
+
+	public DiagramStoreException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DiagramStoreException(String message) {
+		super(message);
+	}
+
+}
