c799402386d15b3bbddef40066e511dc83492068
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-10-06 08:34:22 +0000

    removed warnings

--- org.unicase.ui.navigator/src/org/unicase/ui/navigator/Activator.java
+++ org.unicase.ui.navigator/src/org/unicase/ui/navigator/Activator.java
@@ -33,12 +33,12 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
-	//BEGIN SUPRESS CATCH EXCEPTION
+	// BEGIN SUPRESS CATCH EXCEPTION
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
-	//END SUPRESS CATCH EXCEPTION
+	// END SUPRESS CATCH EXCEPTION
 
 	
 	/** 
@@ -46,12 +46,12 @@ public class Activator extends AbstractUIPlugin {
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
-	//BEGIN SUPRESS CATCH EXCEPTION
+	// BEGIN SUPRESS CATCH EXCEPTION
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
-	//END SUPRESS CATCH EXCEPTION
+	// END SUPRESS CATCH EXCEPTION
 
 	/**
 	 * Returns the shared instance.
