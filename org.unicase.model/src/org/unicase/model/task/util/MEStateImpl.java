ec85907f40235f6a328f9a4bb9dcc7b8ca6d17d2
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-10-07 12:15:31 +0000

    fixed mestate

--- org.unicase.model/src/org/unicase/model/task/util/MEStateImpl.java
+++ org.unicase.model/src/org/unicase/model/task/util/MEStateImpl.java
@@ -96,6 +96,9 @@ public class MEStateImpl implements MEState {
 	 * {@inheritDoc}
 	 */
 	public void addBlocker(ModelElement me) {
+		if(me==modelElement){
+			return;
+		}
 		if (effectiveBlocker.size() == 1) {
 			recursivlyUpdateStatus(getStatus());
 		}
@@ -113,6 +116,9 @@ public class MEStateImpl implements MEState {
 	 * {@inheritDoc}
 	 */
 	public void addOpener(ModelElement me) {
+		if(me==modelElement){
+			return;
+		}
 		effectiveOpeners.add(me);
 		if (effectiveOpeners.size() == 1) {
 			recursivlyUpdateStatus(getStatus());
@@ -191,37 +197,37 @@ public class MEStateImpl implements MEState {
 	}
 
 	private void recursivlyUpdateStatus(String status) {
-//		ENotificationImpl notificationImpl = new ENotificationImpl(
-//				(InternalEObject) modelElement, Notification.RESOLVE,
-//				ModelPackage.MODEL_ELEMENT__STATE, OPEN, OPEN);
-//
-//		modelElement.eNotify(notificationImpl);
-//
-//		ArrayList<ModelElement> opened = TaxonomyAccess.getInstance()
-//				.getOpeningLinkTaxonomy().getOpened(modelElement);
-//		ArrayList<ModelElement> blocked = TaxonomyAccess.getInstance()
-//				.getBlockingLinkTaxonomy().getBlocked(modelElement);
-//		try {
-//			if (status.equals(OPEN) | status.equals(BLOCKED)) {
-//				for (ModelElement open : opened) {
-//					open.getMEState().addOpener(modelElement);
-//				}
-//				for (ModelElement modelElement : blocked) {
-//					modelElement.getMEState().addBlocker(modelElement);
-//				}
-//			}
-//			if (status.equals(CLOSED)) {
-//				for (ModelElement open : opened) {
-//					open.getMEState().removeOpener(modelElement);
-//				}
-//				for (ModelElement modelElement : blocked) {
-//					modelElement.getMEState().removeBlocker(modelElement);
-//				}
-//			}
-//		} catch (CircularDependencyException e) {
-//			//JH: insert proper exception handlin
-//			e.printStackTrace();
-//		}
+		ENotificationImpl notificationImpl = new ENotificationImpl(
+				(InternalEObject) modelElement, Notification.RESOLVE,
+				ModelPackage.MODEL_ELEMENT__STATE, OPEN, OPEN);
+
+		modelElement.eNotify(notificationImpl);
+
+		ArrayList<ModelElement> opened = TaxonomyAccess.getInstance()
+				.getOpeningLinkTaxonomy().getOpened(modelElement);
+		ArrayList<ModelElement> blocked = TaxonomyAccess.getInstance()
+				.getBlockingLinkTaxonomy().getBlocked(modelElement);
+		try {
+			if (status.equals(OPEN) | status.equals(BLOCKED)) {
+				for (ModelElement open : opened) {
+					open.getMEState().addOpener(modelElement);
+				}
+				for (ModelElement modelElement : blocked) {
+					modelElement.getMEState().addBlocker(modelElement);
+				}
+			}
+			if (status.equals(CLOSED)) {
+				for (ModelElement open : opened) {
+					open.getMEState().removeOpener(modelElement);
+				}
+				for (ModelElement modelElement : blocked) {
+					modelElement.getMEState().removeBlocker(modelElement);
+				}
+			}
+		} catch (CircularDependencyException e) {
+			//JH: insert proper exception handlin
+			e.printStackTrace();
+		}
 
 	}
 }
