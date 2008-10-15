668efcd8efe1ac6fdfa34c65f9e3cca195fe523d
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-10-15 16:04:14 +0000

    added actions for the incredible max

---
+++ org.unicase.workspace.edit/src/org/unicase/workspace/edit/commands/ExportProjectSpaceHandler.java
@@ -0,0 +1,17 @@
+package org.unicase.workspace.edit.commands;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.PlatformUI;
+import org.unicase.workspace.ProjectSpace;
+
+public class ExportProjectSpaceHandler extends ProjectActionHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		ProjectSpace projectSpace = getProjectSpace(event);
+		MessageDialog.openInformation(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Export Projectspace", "");
+		return null;
+	}
+
+}
