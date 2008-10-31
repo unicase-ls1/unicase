72e24130aa72a662de3625424dbe0024230f3ddc
wesendon <wesendon@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-10-31 15:39:35 +0000

    working on validation basis

--- org.unicase.model/src/org/unicase/model/task/validation/ActionItemConstraint.java
+++ org.unicase.model/src/org/unicase/model/task/validation/ActionItemConstraint.java
@@ -1,8 +1,11 @@
 package org.unicase.model.task.validation;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.EMFEventType;
 import org.eclipse.emf.validation.IValidationContext;
+import org.unicase.model.task.ActionItem;
 
 /**
  * Validator for ActionItems.
@@ -17,11 +20,19 @@ public class ActionItemConstraint extends AbstractModelConstraint {
 	@Override
 	public IStatus validate(IValidationContext ctx) {
 
-		// EObject eObj = ctx.getTarget();
-		// EMFEventType eType = ctx.getEventType();
+		EObject eObj = ctx.getTarget();
+		EMFEventType eType = ctx.getEventType();
 
+		if (eType == EMFEventType.NULL) {
+			if (eObj instanceof ActionItem) {
+				if (((ActionItem) eObj).getAssignee() == null) {
+					return ctx.createFailureStatus(new Object[] { eObj.eClass()
+							.getName()
+							+ ": '" + ((ActionItem) eObj).getName() + "'" });
+				}
+			}
+		}
 		return ctx.createSuccessStatus();
 
 	}
-
 }
--- org.unicase.workspace.edit/src/org/unicase/workspace/edit/commands/ValidateHandler.java
+++ org.unicase.workspace.edit/src/org/unicase/workspace/edit/commands/ValidateHandler.java
@@ -2,6 +2,11 @@ package org.unicase.workspace.edit.commands;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
@@ -9,7 +14,6 @@ import org.eclipse.emf.validation.model.EvaluationMode;
 import org.eclipse.emf.validation.service.IBatchValidator;
 import org.eclipse.emf.validation.service.ModelValidationService;
 import org.unicase.model.validation.ValidationClientSelector;
-import org.unicase.ui.common.exceptions.DialogHandler;
 import org.unicase.workspace.ProjectSpace;
 
 /**
@@ -19,6 +23,9 @@ import org.unicase.workspace.ProjectSpace;
  * 
  */
 public class ValidateHandler extends ProjectActionHandler {
+
+	private static String markerType = "org.unicase.model.validation.marker";
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -42,15 +49,51 @@ public class ValidateHandler extends ProjectActionHandler {
 	private void validateWithoutCommand(ProjectSpace projectSpace) {
 
 		ValidationClientSelector.setRunning(true);
-		
+
 		IBatchValidator validator = (IBatchValidator) ModelValidationService
 				.getInstance().newValidator(EvaluationMode.BATCH);
 		validator.setIncludeLiveConstraints(true);
 
 		IStatus status = validator.validate(projectSpace);
-		
+
 		ValidationClientSelector.setRunning(false);
+
+		// MultiStatus multi = new MultiStatus(
+		// "org.unicase.model", 1, status.getChildren(),
+		// "Problems were found by validation", null);
+		// //
+		// try {
+		// MarkerUtil.createMarkers(status);
+		// } catch(CoreException e) {
+		// //log this ...
+		// e.printStackTrace();
+		// }
+
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IResource resource = workspace.getRoot();
 		
-		DialogHandler.showErrorDialog(status.toString());
+		try {
+			resource.deleteMarkers(markerType, true, 5);
+		} catch (CoreException e1) {
+			e1.printStackTrace();
+		}
+
+		if (status.isMultiStatus()) {
+			for (IStatus stat : status.getChildren()) {
+				System.out.println(stat.getMessage());
+				try {
+					IMarker marker = (IMarker) resource
+							.createMarker(markerType);
+					marker.setAttribute(IMarker.MESSAGE, "unicase: "
+							+ stat.getMessage());
+					marker.setAttribute(IMarker.SEVERITY,
+							IMarker.SEVERITY_WARNING);
+					System.out.println("type: " + marker.getType());
+				} catch (CoreException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 }
