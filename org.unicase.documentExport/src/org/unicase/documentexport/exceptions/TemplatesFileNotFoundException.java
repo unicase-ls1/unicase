16404e7374deca46c5ce42f0be25a31532218399
hoechts <hoechts@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-11-11 03:44:39 +0000

    this plug-in is replaced by the two plugins org.unicase.docExport and org.unciase.docExport.exportModel

--- org.unicase.documentExport/src/org/unicase/documentexport/exceptions/TemplatesFileNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package org.unicase.documentexport.exceptions;
-
-@SuppressWarnings("serial")
-public class TemplatesFileNotFoundException extends Exception {
-
-}
--- org.unicase.documentExport/src/org/unicase/documentexport/renderers/elements/URootCompositeSection.java
+++
@@ -1,5 +0,0 @@
-package org.unicase.documentexport.renderers.elements;
-
-public class URootCompositeSection extends UCompositeSection {
-
-}
