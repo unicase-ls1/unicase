cf636a75f06524469d3c73edc911284bc4e5ab0c
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-11-14 11:26:40 +0000

    removed warnings

--- org.unicase.ui.tableview/src/org/unicase/ui/taskview/BlockedElementsViewerFilter.java
+++ org.unicase.ui.tableview/src/org/unicase/ui/taskview/BlockedElementsViewerFilter.java
@@ -5,8 +5,16 @@ import org.eclipse.jface.viewers.ViewerFilter;
 import org.unicase.model.ModelElement;
 import org.unicase.model.task.util.MEState;
 
+/**
+ * Filter to filter blocked elements.
+ * 
+ * @author helming
+ * 
+ */
 public class BlockedElementsViewerFilter extends ViewerFilter {
-
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
 		if (element instanceof ModelElement) {
