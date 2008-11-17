392b8f423d35941167fae433306cf02ac3f1587f
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-11-17 14:25:22 +0000

    changed icon of deletefromdiagram menu

--- org.unicase.classDiagram/src/org/unicase/model/classDiagram/part/DeleteElementAction.java
+++ org.unicase.classDiagram/src/org/unicase/model/classDiagram/part/DeleteElementAction.java
@@ -1,8 +1,12 @@
 package org.unicase.model.classDiagram.part;
 
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
@@ -13,6 +17,7 @@ import org.eclipse.gmf.runtime.diagram.ui.commands.CommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
 import org.eclipse.gmf.runtime.diagram.ui.l10n.DiagramUIMessages;
 import org.eclipse.gmf.runtime.emf.commands.core.command.CompositeTransactionalCommand;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
@@ -38,21 +43,20 @@ public class DeleteElementAction extends AbstractDeleteFromAction {
 	}
 
 	/**
-	 * @generated
+	 * @generated NOT
 	 */
 	public void init() {
 		super.init();
 		setId(ActionIds.ACTION_DELETE_FROM_MODEL);
 		setText(DiagramUIMessages.DiagramEditor_Delete_from_Model);
 		setToolTipText(DiagramUIMessages.DiagramEditor_Delete_from_ModelToolTip);
-		ISharedImages workbenchImages = PlatformUI.getWorkbench()
-				.getSharedImages();
-		setHoverImageDescriptor(workbenchImages
-				.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
-		setImageDescriptor(workbenchImages
-				.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE));
-		setDisabledImageDescriptor(workbenchImages
-				.getImageDescriptor(ISharedImages.IMG_TOOL_DELETE_DISABLED));
+		String path = "icons/delete.gif";
+		URL url = FileLocator.find(Platform
+				.getBundle("org.unicase.ui.common"), new Path(path), null);
+		ImageDescriptor id = ImageDescriptor.createFromURL(url);
+		setHoverImageDescriptor(id);
+		setImageDescriptor(id);
+		setDisabledImageDescriptor(id);
 	}
 
 	/**
