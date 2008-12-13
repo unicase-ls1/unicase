acf0be8580e781f4b161f8e9b073ef2f3bef4777
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-12-13 14:07:26 +0000

    implemented expectedNumOfCahnges

--- org.unicase.test/src/org/unicase/test/tests/change/random/testcases/NonContainmentReferenceAddTest.java
+++ org.unicase.test/src/org/unicase/test/tests/change/random/testcases/NonContainmentReferenceAddTest.java
@@ -1,10 +1,14 @@
 package org.unicase.test.tests.change.random.testcases;
 
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.unicase.emfstore.esmodel.versioning.ChangePackage;
 import org.unicase.model.ModelElement;
+import org.unicase.model.requirement.RequirementPackage;
+import org.unicase.model.requirement.impl.StepImpl;
 import org.unicase.test.tests.change.ChangeTestHelper;
 import org.unicase.test.tests.change.random.ChangePackageTest;
 import org.unicase.ui.test.TestProjectParmeters;
@@ -23,9 +27,14 @@ public class NonContainmentReferenceAddTest extends ChangePackageTest {
 
 	private ModelElement me;
 	private EReference refToChange;
-	
-	
-	public NonContainmentReferenceAddTest(ProjectSpace testProjectSpace, String testName, TestProjectParmeters testProjParams) {
+	private ModelElement meToReference;
+	private Object oldValueOfOppositeRef;
+	private Object oldValueOfRef;
+	private boolean uniqueContainingTarget;
+	private boolean touchNotification;
+
+	public NonContainmentReferenceAddTest(ProjectSpace testProjectSpace,
+			String testName, TestProjectParmeters testProjParams) {
 		super(testProjectSpace, testName, testProjParams);
 
 	}
@@ -33,12 +42,30 @@ public class NonContainmentReferenceAddTest extends ChangePackageTest {
 	@Override
 	public void runTest() {
 
+		uniqueContainingTarget = false;
+		touchNotification = false;
+		oldValueOfRef = null;
+		oldValueOfOppositeRef = null;
+		
+		
 		me = ChangeTestHelper.getRandomME(getTestProject());
 		refToChange = ChangeTestHelper.getRandomNonContainmentRef(me);
-		
-		while(refToChange == null){
+
+		while (refToChange == null) {
 			me = ChangeTestHelper.createRandomME();
 			refToChange = ChangeTestHelper.getRandomNonContainmentRef(me);
+		}
+
+		oldValueOfRef = me.eGet(refToChange);
+		
+		meToReference = ChangeTestHelper.getRandomMEofType(getTestProject(),
+				refToChange.getEReferenceType());
+		// make a copy of old value of opposite reference for later inspection
+		// (see getExpectedNumOfChanges())
+		if (refToChange.getEOpposite() != null) {
+
+			oldValueOfOppositeRef = meToReference.eGet(refToChange
+					.getEOpposite());
 		}
 
 		TransactionalEditingDomain domain = TransactionalEditingDomain.Registry.INSTANCE
@@ -53,23 +80,77 @@ public class NonContainmentReferenceAddTest extends ChangePackageTest {
 
 		});
 	}
-	
-	private void doChangeSimpleRef(){
-		ChangeTestHelper.changeSimpleRef(me, refToChange, getTestProject());
+
+	@SuppressWarnings("unchecked")
+	private void doChangeSimpleRef() {
+
+		Object object = me.eGet(refToChange);
+		if (refToChange.isMany()) {
+			EList<EObject> eList = (EList<EObject>) object;
+			if (eList == null) {
+				throw new IllegalStateException("Null list return for feature "
+						+ refToChange.getName() + " on " + me.getName());
+			} else {
+				if (refToChange.isUnique() && eList.contains(meToReference)) {
+					uniqueContainingTarget = true;
+				}
+
+			}
+		} else {
+			if (object != null && object.equals(meToReference)) {
+				touchNotification = true;
+			}
+		}
+
+		ChangeTestHelper.changeSimpleRef(me, refToChange, meToReference);
+		
+		Object obj = new Object();
+		obj.toString();
+
 	}
 
 	public int getExpectedNumOfChanges() {
 
+		// a CreateDeleteOperation
+		// and a MultiReferenceOpertaion
+		// if refToChange or its opposite has upper bound 1 then additionally a
+		// SimpleRefOp
+				
+		if(uniqueContainingTarget || touchNotification){
+			return 0;
+		}
+		
+		if (refToChange.getUpperBound() == 1 || (refToChange.getEOpposite() != null
+				&& refToChange.getEOpposite().getUpperBound() == 1)) {
+			// why don't we test refToChange.getUpperBound() == 1 ?
+			// because even if refToChange for newly created model element has
+			// upper bound 1,
+			// its initial value is null! and therefore we do not expect a
+			// SingleRefOp for it to be created.
+			if ((refToChange.isMany() && oldValueOfOppositeRef != null) || 
+					(refToChange.getEOpposite() != null && refToChange.getEOpposite().isMany() && oldValueOfRef != null)){
+				// why do we check this?
+				// because if both refToChange and its opposite have upper bound
+				// 1, then there is just one SingleRefOp to create. Hence number
+				// of operations is just 1.
+
+				return 2;
+			}
+
+		}
+		
+		if(me instanceof StepImpl && refToChange instanceof RequirementPackage){
+			return 2;
+		}
+
 		return 1;
 	}
 
-	
-	
 	@Override
 	public boolean isSuccessful(ChangePackage changePackage) {
-		
-		return changePackage.getOperations().size() == getExpectedNumOfChanges();
+
+		boolean ret = (changePackage.getOperations().size() == getExpectedNumOfChanges());
+		return ret;
 	}
 
-	
 }
