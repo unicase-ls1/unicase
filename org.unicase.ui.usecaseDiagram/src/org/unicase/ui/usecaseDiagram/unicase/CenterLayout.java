ee1c370d89b65ef419a01d502803b51fb6c38aed
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-12-25 15:25:28 +0000

    changed comment

--- org.unicase.ui.usecaseDiagram/src/org/unicase/ui/usecaseDiagram/unicase/CenterLayout.java
+++ org.unicase.ui.usecaseDiagram/src/org/unicase/ui/usecaseDiagram/unicase/CenterLayout.java
@@ -1,13 +1,8 @@
 /*
- * Copyright (c) 2006 Borland Software Corporation
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Michael Golubev (Borland) - initial API and implementation
+ * Copyright (c) 2006 Borland Software Corporation All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html Contributors: Michael Golubev (Borland) - initial API and
+ * implementation
  */
 
 package org.unicase.ui.usecaseDiagram.unicase;
@@ -20,33 +15,38 @@ import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
 
 /**
- * @author denglerm
- * This class is uses by the usecase ellipse
+ * @author denglerm This class is uses by the usecase ellipse
  */
 
 public class CenterLayout extends StackLayout {
-		
+
 	private Rectangle clientRect;
-	/** .
-	 * The constructor
+
+	/**
+	 * . The constructor
+	 * 
 	 * @param clientRect the client rectangle
 	 */
-	public CenterLayout(Rectangle clientRect){
-		this.clientRect=clientRect;
+	public CenterLayout(Rectangle clientRect) {
+		this.clientRect = clientRect;
 	}
-	/** .
-	 * The constructor
+
+	/**
+	 * . The constructor
 	 */
-	public CenterLayout(){
-		super();		
+	public CenterLayout() {
+		super();
 	}
-	/** .
-	 * {@inheritDoc}
-	 * @see StackLayout#layout(IFigure figure)
+
+	/**
+	 * . {@inheritDoc}
+	 * 
+	 * @see org.eclipse.draw2d.StackLayout#layout(org.eclipse.draw2d.IFigure)
 	 */
+	@Override
 	public void layout(IFigure figure) {
-		
-		Rectangle r=figure.getClientArea();
+
+		Rectangle r = figure.getClientArea();
 		List children = figure.getChildren();
 		IFigure child;
 		Dimension d;
@@ -55,8 +55,8 @@ public class CenterLayout extends StackLayout {
 			d = child.getPreferredSize(r.width, r.height);
 			d.width = Math.min(d.width, r.width);
 			d.height = Math.min(d.height, r.height);
-			Rectangle childRect = new Rectangle(r.x + (r.width - d.width) / 2,
-					r.y + (r.height - d.height) / 2, d.width, d.height);
+			Rectangle childRect = new Rectangle(r.x + (r.width - d.width) / 2, r.y + (r.height - d.height) / 2,
+				d.width, d.height);
 			child.setBounds(childRect);
 		}
 	}
