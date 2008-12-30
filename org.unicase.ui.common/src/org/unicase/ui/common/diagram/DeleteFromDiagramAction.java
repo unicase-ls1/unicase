df1ce9d2c0ebbabecaa43ebd77f934582720ef39
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2008-12-30 14:41:36 +0000

    the delete key now removes MEs from diagram

--- org.unicase.ui.common/src/org/unicase/ui/common/diagram/DeleteFromDiagramAction.java
+++ org.unicase.ui.common/src/org/unicase/ui/common/diagram/DeleteFromDiagramAction.java
@@ -7,22 +7,45 @@ import org.eclipse.gef.EditPart;
 import org.eclipse.gmf.runtime.common.core.command.ICommand;
 import org.eclipse.gmf.runtime.emf.type.core.ElementTypeRegistry;
 import org.eclipse.gmf.runtime.emf.type.core.IElementType;
+import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
 import org.eclipse.gmf.runtime.notation.View;
+import org.eclipse.gmf.runtime.notation.impl.EdgeImpl;
+import org.eclipse.gmf.runtime.notation.impl.NodeImpl;
 import org.eclipse.jface.action.Action;
 import org.unicase.ui.common.commands.ActionHelper;
 import org.unicase.ui.common.commands.DeleteFromViewCommand;
 import org.unicase.workspace.WorkspaceManager;
 
+/**
+ * @author denglerm This class implements the DeleteFromDiagram Action.
+ */
 public class DeleteFromDiagramAction extends Action {
 
+	/**
+	 * This method deletes the diagram element from the diagram's elements list.
+	 * 
+	 * @see org.eclipse.jface.action#run()
+	 */
 	@Override
 	public void run() {
 		EditPart selectedElement = (EditPart) ActionHelper.getSelection();
-		DestroyElementRequest destroy = new DestroyElementRequest(WorkspaceManager.getInstance().getCurrentWorkspace()
-			.getEditingDomain(), ((View) selectedElement.getModel()).getElement(), false);
-		IElementType type = ElementTypeRegistry.getInstance().getElementType(destroy.getEditHelperContext());
-		ICommand command = (type != null) ? new DeleteFromViewCommand(destroy, selectedElement) : null;
+		ICommand command = null;
+		View view = (View) selectedElement.getModel();
+		if (view instanceof NodeImpl) {
+			DestroyElementRequest destroy = new DestroyElementRequest(WorkspaceManager.getInstance()
+				.getCurrentWorkspace().getEditingDomain(), view.getElement(), false);
+			IElementType type = ElementTypeRegistry.getInstance().getElementType(destroy.getEditHelperContext());
+			command = (type != null) ? new DeleteFromViewCommand(destroy, selectedElement) : null;
+		} else if (view instanceof EdgeImpl) {
+
+			DestroyReferenceRequest req = new DestroyReferenceRequest(((EdgeImpl) view).getSource().getElement(), null,
+				((EdgeImpl) view).getTarget().getElement(), false);
+			command = new DestroyReferenceCommand(req);
+
+		}
+
 		try {
 			OperationHistoryFactory.getOperationHistory().execute(command, new NullProgressMonitor(), null);
 		} catch (ExecutionException e) {
