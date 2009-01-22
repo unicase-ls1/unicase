9c40b95c54fbfefd0ef256f951e6013ebc298c9b
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-01-22 14:26:20 +0000

    fixed the bug: ShowInNavigator shows the first occurrence of a model element (even if it is linked and not really contained)

--- org.unicase.ui.navigator/src/org/unicase/ui/navigator/commands/ShowMEInNavigatorHandler.java
+++ org.unicase.ui.navigator/src/org/unicase/ui/navigator/commands/ShowMEInNavigatorHandler.java
@@ -3,7 +3,11 @@ package org.unicase.ui.navigator.commands;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.unicase.model.ModelElement;
 import org.unicase.ui.common.commands.ActionHelper;
 import org.unicase.ui.navigator.TreeView;
@@ -27,9 +31,26 @@ public class ShowMEInNavigatorHandler extends AbstractHandler {
 			return null;
 		}
 
-		TreeView.getTreeViewer().setSelection(new StructuredSelection(me), true);
+		TreeViewer treeViewer = TreeView.getTreeViewer();
+
+		// we could easily use the following method.
+		// but it has the problem that it shows the first occurrence of and element.
+		// for example if we have the same element somewhere else linked, and shown as a child (e.g. in
+		// ActionItemMeetingSection),
+		// it just show the first one that it finds. We want only the real containment to be shown.
+		// // TreeView.getTreeViewer().setSelection(new StructuredSelection(me), true);
+
+		EObject container = me.eContainer();
+		treeViewer.setSelection(new StructuredSelection(container));
+
+		TreeSelection treeSelection = (TreeSelection) treeViewer.getSelection();
+		if (treeSelection.getPaths().length > 0) {
+			TreePath treePath = treeSelection.getPaths()[0].createChildPath(me);
+
+			TreeSelection newTreeSeleciton = new TreeSelection(treePath);
+			treeViewer.setSelection(newTreeSeleciton, true);
+		}
 
 		return null;
 	}
-
 }
