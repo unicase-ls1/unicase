e8d6bcfb7a3e44df7dbcb08aece05791c979fad2
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-01-23 11:37:39 +0000

    -fixed a problem with dropping annotations on other model elements -added a new checkstyle header to match auto-formatting

--- org.unicase.ui.common/src/org/unicase/ui/common/dnd/dropadapters/MEDropAdapter.java
+++ org.unicase.ui.common/src/org/unicase/ui/common/dnd/dropadapters/MEDropAdapter.java
@@ -1,3 +1,9 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
 package org.unicase.ui.common.dnd.dropadapters;
 
 import java.util.Arrays;
@@ -266,7 +272,7 @@ public class MEDropAdapter {
 			if (!hasThisContainmentReference(target.eContainer(), dropee.eClass())) {
 				return false;
 			}
-		} else if (!hasThisContainmentReference(target, dropee.eClass())) {
+		} else if (!(dropee instanceof Annotation) && !hasThisContainmentReference(target, dropee.eClass())) {
 			return false;
 		}
 
