757e869cf253d32c91d31ebaf88f67f7572c818c
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-05 00:30:49 +0000

    added release number to model

--- org.unicase.model/src/org/unicase/model/ModelPackage.java
+++ org.unicase.model/src/org/unicase/model/ModelPackage.java
@@ -34,6 +34,13 @@ public interface ModelPackage extends EPackage {
 	String eNS_URI = "http://unicase.org/model";
 
 	/**
+	 * The models release number from the ecore history. Used to identify the neccessary migrotor.
+	 * 
+	 * @generated NOT
+	 */
+	int RELEASE_NUMBER = 0;
+
+	/**
 	 * The package namespace name. <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * 
 	 * @generated
