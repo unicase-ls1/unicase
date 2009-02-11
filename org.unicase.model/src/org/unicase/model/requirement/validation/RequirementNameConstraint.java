be73ccb1213cda6c9d9063c90f3555eb31cdb363
hoechts <hoechts@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-11 10:19:13 +0000

    removed diagram empty validation rule, because it is buggy. the validation class still exists, but it isn't called by an extension point.
    The model element name constraint has been moved to the requirements validation.

--- org.unicase.model/src/org/unicase/model/requirement/validation/RequirementNameConstraint.java
+++ org.unicase.model/src/org/unicase/model/requirement/validation/RequirementNameConstraint.java
@@ -3,23 +3,27 @@
  * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  */
-package org.unicase.model.validation;
+package org.unicase.model.requirement.validation;
 
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.validation.AbstractModelConstraint;
 import org.eclipse.emf.validation.EMFEventType;
 import org.eclipse.emf.validation.IValidationContext;
 import org.unicase.model.ModelElement;
+import org.unicase.model.requirement.RequirementPackage;
 import org.unicase.model.util.ValidationConstraintHelper;
 
 /**
- * Checks whether the name starts with a number.
+ * Checks whether the name starts with a number followed by a dot.
  * 
  * @author hoecht
  */
-public class ModelElementNameConstraint extends AbstractModelConstraint {
+public class RequirementNameConstraint extends AbstractModelConstraint {
 
 	/**
 	 * {@inheritDoc}
@@ -30,8 +34,7 @@ public class ModelElementNameConstraint extends AbstractModelConstraint {
 		EMFEventType eType = ctx.getEventType();
 
 		if (eType == EMFEventType.NULL) {
-			if (eObj instanceof ModelElement) {
-
+			if (isRequirement(eObj)) {
 				ModelElement me = (ModelElement) eObj;
 				if (startsWithANumber(me.getName())) {
 					EStructuralFeature errorFeature = ValidationConstraintHelper.getErrorFeatureForModelElement(
@@ -45,17 +48,35 @@ public class ModelElementNameConstraint extends AbstractModelConstraint {
 		return ctx.createSuccessStatus();
 	}
 
-	private boolean startsWithANumber(String string) {
-		if (string == null) {
-			return false;
-		}
-		for (int i = 1; i <= 9; i++) {
-			if (string.startsWith(String.valueOf(i))) {
-				return true;
+	private boolean isRequirement(EObject eObj) {
+		EList<EClassifier> classifiers = RequirementPackage.eINSTANCE.getEClassifiers();
+		for (EClassifier classifier : classifiers) {
+			if (classifier instanceof EClass) {
+				EClass eclass = (EClass) classifier;
+				if (eclass.isInstance(eObj)) {
+					return true;
+				}
 			}
 		}
 
 		return false;
 	}
 
+	private boolean startsWithANumber(String string) {
+		if (string == null) {
+			return false;
+		}
+
+		if (string.startsWith(".") || string.startsWith(")")) {
+			return true;
+		}
+
+		for (int i = 1; i <= 9; i++) {
+			if (string.startsWith(String.valueOf(i))) {
+				return startsWithANumber(string.substring(1));
+			}
+		}
+
+		return false;
+	}
 }
