ecc6ede1c03d2cfe3719018ab6268993a34f5635
shterevg <shterevg@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-17 09:16:12 +0000

    added log message to the compact changes composite

--- org.unicase.workspace.edit/src/org/unicase/workspace/edit/views/changes/CompactChangesComposite.java
+++ org.unicase.workspace.edit/src/org/unicase/workspace/edit/views/changes/CompactChangesComposite.java
@@ -7,12 +7,14 @@ package org.unicase.workspace.edit.views.changes;
 
 import java.util.List;
 
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ColumnViewerToolTipSupport;
 import org.eclipse.jface.viewers.IContentProvider;
 import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.unicase.emfstore.esmodel.versioning.ChangePackage;
+import org.unicase.emfstore.esmodel.versioning.LogMessage;
 
 /**
  * Shows the operations in a change packages grouped by model elements.
@@ -22,6 +24,7 @@ import org.unicase.emfstore.esmodel.versioning.ChangePackage;
 public class CompactChangesComposite extends AbstractChangesComposite {
 
 	private TreeViewerColumn meColumn;
+	private TreeViewerColumn opColumn;
 
 	/**
 	 * Default constructor.
@@ -62,6 +65,29 @@ public class CompactChangesComposite extends AbstractChangesComposite {
 		meColumn.getColumn().setWidth(300);
 		meColumn.getColumn().setText("ModelElement");
 		meColumn.setLabelProvider(new MENameLabelProvider(getEmfLabelProvider(), getVisualizationHelper()));
+
+		opColumn = new TreeViewerColumn(getTreeViewer(), SWT.NONE);
+		opColumn.getColumn().setWidth(300);
+		opColumn.getColumn().setText("");
+		opColumn.getColumn().setWidth(getShell().getSize().x / 2);
+		opColumn.setLabelProvider(new ColumnLabelProvider() {
+			@Override
+			public String getText(Object element) {
+				if (element instanceof ChangePackage) {
+					ChangePackage cp = (ChangePackage) element;
+					LogMessage logMessage = cp.getLogMessage();
+					if (logMessage != null) {
+						StringBuffer log = new StringBuffer();
+						log.append("Log message: ");
+						log.append(" \'");
+						log.append(logMessage.getMessage());
+						log.append("\' ");
+						return log.toString();
+					}
+				}
+				return "";
+			}
+		});
 
 		ColumnViewerToolTipSupport.enableFor(getTreeViewer());
 	}
