5b9bcca60d677b0e6c1fdd3915c9a558d8f873c3
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-20 15:15:45 +0000

    added iterator

---
+++ org.unicase.analyzer/src/analyzer/ProjectIterator.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * </copyright>
+ */
+
+package analyzer;
+
+import java.util.Iterator;
+
+/**
+ * @author koegel
+ *
+ */
+public class ProjectIterator implements Iterator<ProjectAnalysisData> {
+
+	/** 
+	 * {@inheritDoc}
+	 * @see java.util.Iterator#hasNext()
+	 */
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 * @see java.util.Iterator#next()
+	 */
+	public ProjectAnalysisData next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 * @see java.util.Iterator#remove()
+	 */
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
