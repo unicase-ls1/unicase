c72f40d7a9175fea13e06be34f41b9e9ab8b4017
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-20 15:14:17 +0000

    added project analysis data to analyzer model
    regenerated model

---
+++ org.unicase.analyzer/src/analyzer/ProjectAnalysisData.java
@@ -0,0 +1,77 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package analyzer;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.unicase.emfstore.esmodel.versioning.ChangePackage;
+
+import org.unicase.model.Project;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Project Analysis Data</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link analyzer.ProjectAnalysisData#getProjectState <em>Project State</em>}</li>
+ *   <li>{@link analyzer.ProjectAnalysisData#getChangePackages <em>Change Packages</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see analyzer.AnalyzerPackage#getProjectAnalysisData()
+ * @model
+ * @generated
+ */
+public interface ProjectAnalysisData extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Project State</b></em>' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Project State</em>' containment reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Project State</em>' containment reference.
+	 * @see #setProjectState(Project)
+	 * @see analyzer.AnalyzerPackage#getProjectAnalysisData_ProjectState()
+	 * @model containment="true"
+	 * @generated
+	 */
+	Project getProjectState();
+
+	/**
+	 * Sets the value of the '{@link analyzer.ProjectAnalysisData#getProjectState <em>Project State</em>}' containment reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Project State</em>' containment reference.
+	 * @see #getProjectState()
+	 * @generated
+	 */
+	void setProjectState(Project value);
+
+	/**
+	 * Returns the value of the '<em><b>Change Packages</b></em>' containment reference list.
+	 * The list contents are of type {@link org.unicase.emfstore.esmodel.versioning.ChangePackage}.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Change Packages</em>' containment reference list isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Change Packages</em>' containment reference list.
+	 * @see analyzer.AnalyzerPackage#getProjectAnalysisData_ChangePackages()
+	 * @model containment="true"
+	 * @generated
+	 */
+	EList<ChangePackage> getChangePackages();
+
+} // ProjectAnalysisData
---
+++ org.unicase.analyzer/src/analyzer/impl/AnalyzerFactoryImpl.java
@@ -0,0 +1,99 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package analyzer.impl;
+
+import analyzer.*;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model <b>Factory</b>.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class AnalyzerFactoryImpl extends EFactoryImpl implements AnalyzerFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static AnalyzerFactory init() {
+		try {
+			AnalyzerFactory theAnalyzerFactory = (AnalyzerFactory)EPackage.Registry.INSTANCE.getEFactory("http://unicase.org/analyzer"); 
+			if (theAnalyzerFactory != null) {
+				return theAnalyzerFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new AnalyzerFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AnalyzerFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case AnalyzerPackage.PROJECT_ANALYSIS_DATA: return createProjectAnalysisData();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ProjectAnalysisData createProjectAnalysisData() {
+		ProjectAnalysisDataImpl projectAnalysisData = new ProjectAnalysisDataImpl();
+		return projectAnalysisData;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AnalyzerPackage getAnalyzerPackage() {
+		return (AnalyzerPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static AnalyzerPackage getPackage() {
+		return AnalyzerPackage.eINSTANCE;
+	}
+
+} //AnalyzerFactoryImpl
