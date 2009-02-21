fd67b44deb9badf3bfc29c0c1297e10340ff743e
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-21 16:35:50 +0000

    added Helper class

---
+++ org.unicase.workspace/src/org/unicase/workspace/notification/provider/NotificationHelper.java
@@ -0,0 +1,35 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.workspace.notification.provider;
+
+import java.util.Date;
+import java.util.List;
+
+import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
+
+/**
+ * This class offer helper methods for notifications.
+ * 
+ * @author helming
+ */
+public final class NotificationHelper {
+
+	private NotificationHelper() {
+
+	}
+
+	/**
+	 * Returns the latest date of a list of operations.
+	 * 
+	 * @param list a list of AbstractOperations
+	 * @return the latest date.
+	 */
+	public static Date getLastDate(List<AbstractOperation> list) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
