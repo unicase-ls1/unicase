35ccd2d568a19a2fe421a7d47a488d25f1e11ef4
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-22 20:03:13 +0000

    changed ignore notification message

--- org.unicase.workspace.edit/src/org/unicase/workspace/edit/dashboard/DashboardEntry.java
+++ org.unicase.workspace.edit/src/org/unicase/workspace/edit/dashboard/DashboardEntry.java
@@ -176,8 +176,11 @@ public class DashboardEntry extends Composite {
 		close.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseDown(MouseEvent e) {
-				boolean hide = MessageDialog.openConfirm(getShell(), "Hide notification",
-					"Are you sure you are not interested in this kind of notification?");
+				boolean hide = MessageDialog
+					.openConfirm(
+						getShell(),
+						"Reject notification",
+						"Are you sure you are not interested in this notification? Your choice will be used to filter notifications you will receive in the future.");
 				if (hide) {
 					TransactionalEditingDomain domain = TransactionalEditingDomain.Registry.INSTANCE
 						.getEditingDomain("org.unicase.EditingDomain");
