56b55ee734a6b498d5b2c78b425b264202c00980
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-23 12:10:11 +0000

    removed indention formatting

--- org.unicase.docExport/src/org/unicase/docExport/docWriter/ITextRtfHeadingWriter.java
+++ org.unicase.docExport/src/org/unicase/docExport/docWriter/ITextRtfHeadingWriter.java
@@ -44,7 +44,7 @@ public class ITextRtfHeadingWriter extends ITextRtfWriter {
 		}
 
 		Paragraph paragraph = new Paragraph(child.getTitlParagraph().getText(), style);
-		paragraph.setIndentationLeft(child.getIndentionLeft() * INDENTION_WIDTH);
+		//paragraph.setIndentationLeft(child.getIndentionLeft() * INDENTION_WIDTH);
 
 		addItextObject(parent, paragraph);
 
