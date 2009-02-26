541c7a13160f560ce3ca83463e56f4f65bb73035
helming <helming@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-02-26 09:17:41 +0000

    removed warnings

--- org.unicase.ui.stem/src/org/unicase/ui/stem/views/statusview/WorkPackageTabContentProvider.java
+++ org.unicase.ui.stem/src/org/unicase/ui/stem/views/statusview/WorkPackageTabContentProvider.java
@@ -21,17 +21,33 @@ import org.unicase.model.task.WorkPackage;
  */
 
 public class WorkPackageTabContentProvider extends AdapterFactoryContentProvider {
-
+	/**
+	 * Constant for the unassigned column.
+	 */
 	public static final String UNASSIGNED = "unassigned";
+	/**
+	 * Constant for the assigned column.
+	 */
 	public static final String ASSIGNED = "assigned";
+	/**
+	 * Constant for the blocked column.
+	 */
 	public static final String BLOCKED = "blocked";
+	/**
+	 * Constant for the done column.
+	 */
 	public static final String DONE = "done";
+	/**
+	 * Column for the testing column.
+	 */
 	public static final String TESTING = "testing";
 
 	private String key;
 
 	/**
 	 * Constructor.
+	 * 
+	 * @param key determines which column is shown.
 	 */
 	public WorkPackageTabContentProvider(String key) {
 		super(new ComposedAdapterFactory(ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
@@ -55,30 +71,50 @@ public class WorkPackageTabContentProvider extends AdapterFactoryContentProvider
 				continue;
 			}
 			if (key.equals(ASSIGNED)) {
-				if (wi.getAssignee() != null && wi.getParticipants().isEmpty() && !((Checkable) wi).isChecked()
-					&& wi.getPredecessors().isEmpty()) {
-					ret.add(wi);
-				}
+				handleAssigneColumn(ret, wi);
 			} else if (key.equals(BLOCKED)) {
-				if (!wi.getPredecessors().isEmpty() && !((Checkable) wi).isChecked()) {
-					ret.add(wi);
-				}
+				handleBlockedColumn(ret, wi);
 			} else if (key.equals(TESTING)) {
-				if (!wi.getParticipants().isEmpty() && !((Checkable) wi).isChecked() && wi.getPredecessors().isEmpty()) {
-					ret.add(wi);
-				}
+				handleTestingColumn(ret, wi);
 			} else if (key.equals(DONE)) {
-				if (((Checkable) wi).isChecked()) {
-					ret.add(wi);
-				}
+				handleDoneColumn(ret, wi);
 			} else if (key.equals(UNASSIGNED)) {
-				if (!((Checkable) wi).isChecked() && wi.getAssignee() == null && wi.getParticipants().isEmpty()
-					&& wi.getPredecessors().isEmpty()) {
-					ret.add(wi);
-				}
+				handleUnassignedColumn(ret, wi);
 			}
 		}
 		return ret.toArray();
+	}
+
+	private void handleUnassignedColumn(ArrayList<WorkItem> ret, WorkItem wi) {
+		if (!((Checkable) wi).isChecked() && wi.getAssignee() == null && wi.getParticipants().isEmpty()
+			&& wi.getPredecessors().isEmpty()) {
+			ret.add(wi);
+		}
+	}
+
+	private void handleDoneColumn(ArrayList<WorkItem> ret, WorkItem wi) {
+		if (((Checkable) wi).isChecked()) {
+			ret.add(wi);
+		}
+	}
+
+	private void handleTestingColumn(ArrayList<WorkItem> ret, WorkItem wi) {
+		if (!wi.getParticipants().isEmpty() && !((Checkable) wi).isChecked() && wi.getPredecessors().isEmpty()) {
+			ret.add(wi);
+		}
+	}
+
+	private void handleBlockedColumn(ArrayList<WorkItem> ret, WorkItem wi) {
+		if (!wi.getPredecessors().isEmpty() && !((Checkable) wi).isChecked()) {
+			ret.add(wi);
+		}
+	}
+
+	private void handleAssigneColumn(ArrayList<WorkItem> ret, WorkItem wi) {
+		if (wi.getAssignee() != null && wi.getParticipants().isEmpty() && !((Checkable) wi).isChecked()
+			&& wi.getPredecessors().isEmpty()) {
+			ret.add(wi);
+		}
 	}
 
 }
