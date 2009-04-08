a2587b2ce37490473a8d807c882e919d043abe07
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-08 00:11:50 +0000

    fixed comment

--- org.unicase.ui.stem/src/org/unicase/ui/stem/Activator.java
+++ org.unicase.ui.stem/src/org/unicase/ui/stem/Activator.java
@@ -42,7 +42,7 @@ public class Activator extends AbstractUIPlugin {
 		plugin = this;
 	}
 
-	/*
+	/**
 	 * (non-Javadoc)
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
