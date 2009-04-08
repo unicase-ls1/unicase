b208363fab87270ac5a0a7d0568e34f1059ea8e7
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-08 16:32:21 +0000

    changed tests

--- org.unicase.test/src/org/unicase/test/tests/change/ChangeTestSuite.java
+++ org.unicase.test/src/org/unicase/test/tests/change/ChangeTestSuite.java
@@ -81,7 +81,7 @@ public abstract class ChangeTestSuite extends TestSuite {
 	protected Project getTestProject() {
 		if (testProject == null) {
 			System.out.println("creating test project");
-			params = new TestProjectParmeters(100, randomSeed, 10, 5, 20, 100);
+			params = new TestProjectParmeters(50, randomSeed, 5, 5, 10, 20);
 			ChangeTestHelper.setRandom(new Random(randomSeed));
 			testProject = new TestProjectGenerator(params).generateProject();
 			System.out.println("test project created");
