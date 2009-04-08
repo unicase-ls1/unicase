d707fcb4d60ced8216200539d05e116cc6ee46fd
shterevg <shterevg@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-08 15:34:38 +0000

    Initial import.

---
+++ org.unicase.dashboard.analyzer/src/org/unicase/dashboard/analyzer/commands/DashboardAnalyzerHandler.java
@@ -0,0 +1,26 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
+package org.unicase.dashboard.analyzer.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+/**
+ */
+public class DashboardAnalyzerHandler extends AbstractHandler {
+
+
+	/**
+	 * . {@inheritDoc}
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		return null;
+	}
+
+}
