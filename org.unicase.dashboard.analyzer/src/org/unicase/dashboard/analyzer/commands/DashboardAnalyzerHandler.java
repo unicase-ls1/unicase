1d6698f7f823793a70931a45e541d2b191ec04eb
shterevg <shterevg@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-09 08:44:58 +0000

    implemented notification generation and export

--- org.unicase.dashboard.analyzer/src/org/unicase/dashboard/analyzer/commands/DashboardAnalyzerHandler.java
+++ org.unicase.dashboard.analyzer/src/org/unicase/dashboard/analyzer/commands/DashboardAnalyzerHandler.java
@@ -6,19 +6,112 @@
 
 package org.unicase.dashboard.analyzer.commands;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.unicase.analyzer.ProjectAnalysisData;
+import org.unicase.analyzer.ProjectVersionIterator;
+import org.unicase.emfstore.esmodel.notification.ESNotification;
+import org.unicase.model.ModelElementId;
+import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.Usersession;
+import org.unicase.workspace.WorkspaceManager;
+import org.unicase.workspace.notification.NotificationGenerator;
 
 /**
+ * Exports the esnotifications that were generated/used in the dashboard.
  */
 public class DashboardAnalyzerHandler extends AbstractHandler {
 
-
 	/**
-	 * . {@inheritDoc}
+	 * {@inheritDoc}
 	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
+	public Object execute(ExecutionEvent executionEvent) throws ExecutionException {
+
+		ProjectSpace projectSpace = WorkspaceManager.getInstance().getCurrentWorkspace().getActiveProjectSpace();
+
+		Usersession session = projectSpace.getUsersession();
+
+		int start = 1;
+		int end = 10;
+		int step = 1;
+
+		ProjectVersionIterator iterator = new ProjectVersionIterator(session, projectSpace.getProjectId(), step, start,
+			end, "forward");
+		FileWriter fileWriter = null;
+		try {
+			FileDialog dialog = new FileDialog(Display.getCurrent().getActiveShell(), SWT.SAVE);
+			String path = dialog.open();
+			if (path == null) {
+				return null;
+			}
+
+			fileWriter = new FileWriter(path);
+			BufferedWriter writer = new BufferedWriter(fileWriter);
+
+			writer.write("Version,");
+			writer.write("NotificationId,");
+			writer.write("Provider,");
+			writer.write("Message,");
+			writer.write("User,");
+			writer.write("Date,");
+			writer.write("ModelElements");
+			writer.write("\n\n");
+
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			String[] users = { "shterevg", "super" };
+			while (iterator.hasNext()) {
+				try {
+					ProjectAnalysisData analysisData = iterator.next();
+					// System.out.println("Writing revision "+analysisData.getPrimaryVersionSpec().getIdentifier());
+					for (String user : users) {
+						List<ESNotification> notifications = NotificationGenerator.getInstance().generateNotifications(
+							analysisData.getChangePackages(), user, projectSpace);
+						for (ESNotification n : notifications) {
+							writer.write(analysisData.getPrimaryVersionSpec().getIdentifier() + "");
+							writer.write(",");
+							writer.write(n.getIdentifier());
+							writer.write(",");
+							writer.write(n.getSender());
+							writer.write(",");
+							writer.write(n.getMessage().replaceAll("\n", " "));
+							writer.write(",");
+							writer.write(user);
+							writer.write(",");
+							writer.write(dateFormat.format(n.getCreationDate()));
+							writer.write(",");
+							for (ModelElementId mid : n.getRelatedModelElements()) {
+								writer.write("{");
+								writer.write(mid.getId());
+								writer.write("|");
+								writer.write(projectSpace.getProject().getModelElement(mid).getName());
+								writer.write("|");
+								writer.write(projectSpace.getProject().getModelElement(mid).eClass().getName());
+								writer.write("}");
+							}
+							writer.write("\n\n");
+						}
+					}
+				} catch (Exception e) {
+					System.out.println(e);
+					//
+				} finally {
+					writer.flush();
+				}
+			}
+			writer.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 
 		return null;
 	}
