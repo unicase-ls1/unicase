d2a2f135556b5eb629f11a7c89ddb7d0abbfefab
shterevg <shterevg@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-09 17:15:06 +0000

    fixed class diagram icon bug

--- org.unicase.model.edit/src/org/unicase/model/diagram/provider/MEDiagramItemProvider.java
+++ org.unicase.model.edit/src/org/unicase/model/diagram/provider/MEDiagramItemProvider.java
@@ -43,16 +43,16 @@ import org.unicase.model.state.StateFactory;
 import org.unicase.model.task.TaskFactory;
 
 /**
- * This is the item provider adapter for a {@link org.unicase.model.diagram.MEDiagram} object.
- * <!-- begin-user-doc -->
+ * This is the item provider adapter for a {@link org.unicase.model.diagram.MEDiagram} object. <!-- begin-user-doc -->
  * <!-- end-user-doc -->
+ * 
  * @generated
  */
 public class MEDiagramItemProvider extends AttachmentItemProvider implements IEditingDomainItemProvider,
 	IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
 	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public MEDiagramItemProvider(AdapterFactory adapterFactory) {
@@ -60,8 +60,8 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	}
 
 	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -76,8 +76,8 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	}
 
 	/**
-	 * This adds a property descriptor for the Elements feature.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * This adds a property descriptor for the Elements feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addElementsPropertyDescriptor(Object object) {
@@ -88,8 +88,8 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	}
 
 	/**
-	 * This adds a property descriptor for the Type feature.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * This adds a property descriptor for the Type feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected void addTypePropertyDescriptor(Object object) {
@@ -103,9 +103,9 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	/**
 	 * This specifies how to implement {@link #getChildren} and is used to deduce an appropriate feature for an
 	 * {@link org.eclipse.emf.edit.command.AddCommand}, {@link org.eclipse.emf.edit.command.RemoveCommand} or
-	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}.
-	 * <!-- begin-user-doc --> <!--
+	 * {@link org.eclipse.emf.edit.command.MoveCommand} in {@link #createCommand}. <!-- begin-user-doc --> <!--
 	 * end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -119,6 +119,7 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 
 	/**
 	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -141,7 +142,7 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 		String imgStr = "full/obj16/MEDiagram";
 		DiagramType type = ((MEDiagram) object).getType();
 		if (type == DiagramType.CLASS_DIAGRAM) {
-			imgStr = "full/obj16/ClassDiagram.gif";
+			imgStr = "full/obj16/ClassDiagram.png";
 		} else if (type == DiagramType.USECASE_DIAGRAM) {
 			imgStr = "full/obj16/UsecaseDiagram.gif";
 		} else if (type == DiagramType.COMPONENT_DIAGRAM) {
@@ -188,9 +189,9 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	}
 
 	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that can be created
+	 * under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -352,9 +353,9 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	}
 
 	/**
-	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}.
-	 * <!-- begin-user-doc -->
+	 * This returns the label text for {@link org.eclipse.emf.edit.command.CreateChildCommand}. <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
@@ -373,8 +374,8 @@ public class MEDiagramItemProvider extends AttachmentItemProvider implements IEd
 	}
 
 	/**
-	 * Return the resource locator for this item provider's resources.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * Return the resource locator for this item provider's resources. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	@Override
