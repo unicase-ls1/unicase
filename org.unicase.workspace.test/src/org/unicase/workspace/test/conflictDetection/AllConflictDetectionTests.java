2ce8a2dbc3ca341b2dc764c22ffddd8b1ee51247
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-19 23:23:03 +0000

    added conflict detection test

--- org.unicase.workspace.test/src/org/unicase/workspace/test/conflictDetection/AllConflictDetectionTests.java
+++ org.unicase.workspace.test/src/org/unicase/workspace/test/conflictDetection/AllConflictDetectionTests.java
@@ -3,21 +3,18 @@
  * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
  * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  */
-
-package org.unicase.workspace.test;
+package org.unicase.workspace.test.conflictDetection;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.unicase.workspace.test.conflictDetection.AllConflictDetectionTests;
-import org.unicase.workspace.test.operations.AllOperationTests;
 
 /**
- * Runs all tests.
+ * Test Suite for running all tests of workspace.
  * 
  * @author koegel
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { AllOperationTests.class, AllConflictDetectionTests.class })
-public class AllTests {
+@Suite.SuiteClasses( { AttributeConflictTest.class })
+public class AllConflictDetectionTests {
 
 }
