4c7c5f5903098ed3ccbc2ec6501b01774decbaa6
pfeifferc <pfeifferc@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-21 11:58:38 +0000

    added a certificate selection/import dialog

--- org.unicase.ui.esbrowser/src/org/unicase/ui/esbrowser/views/NewRepositoryWizardPageOne.java
+++ org.unicase.ui.esbrowser/src/org/unicase/ui/esbrowser/views/NewRepositoryWizardPageOne.java
@@ -5,19 +5,28 @@
  */
 package org.unicase.ui.esbrowser.views;
 
+import java.util.ArrayList;
+
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.unicase.workspace.ServerInfo;
+import org.unicase.workspace.connectionmanager.CertificateStoreException;
+import org.unicase.workspace.connectionmanager.KeyStoreManager;
 
 /**
  * The main page of the wizard.
@@ -29,6 +38,7 @@ public class NewRepositoryWizardPageOne extends WizardPage {
 	private Text name;
 	private Text url;
 	private Spinner port;
+	private Text cert;
 
 	/**
 	 * Default constructor.
@@ -57,30 +67,64 @@ public class NewRepositoryWizardPageOne extends WizardPage {
 		gl.numColumns = ncol;
 		composite.setLayout(gl);
 
+		// Server Name
 		new Label(composite, SWT.NONE).setText("Name:");
 		name = new Text(composite, SWT.BORDER);
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		gd.horizontalSpan = ncol - 1;
 		name.setLayoutData(gd);
 
+		// Server URL
 		new Label(composite, SWT.NONE).setText("URL:");
 		url = new Text(composite, SWT.BORDER);
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		gd.horizontalSpan = ncol - 1;
 		url.setLayoutData(gd);
 
+		// Server Port
 		new Label(composite, SWT.NONE).setText("Port:");
 		port = new Spinner(composite, SWT.BORDER);
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		gd.horizontalSpan = ncol - 1;
 		port.setLayoutData(gd);
 		port.setValues(1099, 1, 999999, 0, 1, 10);
+		setControl(composite);
+
+		// Certificate
+		new Label(composite, SWT.NONE).setText("Certificate:");
+		cert = new Text(composite, SWT.BORDER);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = ncol - 1;
+		cert.setLayoutData(gd);
+		cert.setEditable(false);
+		cert.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
+
+		// Choose Certificate, Opens Dialogue
+		new Label(composite, SWT.NONE).setText("");
+		Button button = new Button(composite, SWT.NONE);
+		button.setText("Select certificate...");
+		button.addSelectionListener(new SelectionDialogListener());
+
+		gd = new GridData(GridData.HORIZONTAL_ALIGN_BEGINNING);
+		gd.horizontalSpan = ncol - 1;
+		button.setLayoutData(gd);
+
 		if (serverInfo.getUrl() != null) {
 			name.setText(serverInfo.getName());
 			url.setText(serverInfo.getUrl());
 			port.setSelection(serverInfo.getPort());
+			if (serverInfo.getCertificateAlias() != null) {
+				try {
+					if (KeyStoreManager.getInstance().contains(serverInfo.getCertificateAlias())) {
+						cert.setText(serverInfo.getCertificateAlias());
+					} else {
+						cert.setText("");
+					}
+				} catch (CertificateStoreException e1) {
+					cert.setText("");
+				}
+			}
 		}
-		setControl(composite);
 	}
 
 	/**
@@ -91,7 +135,7 @@ public class NewRepositoryWizardPageOne extends WizardPage {
 		if (getErrorMessage() != null) {
 			return false;
 		}
-		if (isTextNonEmpty(name) && isTextNonEmpty(url)) {
+		if (isTextNonEmpty(name) && isTextNonEmpty(url) && isTextNonEmpty(cert)) {
 			saveDataToModel();
 			return true;
 		}
@@ -112,10 +156,17 @@ public class NewRepositoryWizardPageOne extends WizardPage {
 				serverInfo.setName(name.getText());
 				serverInfo.setUrl(url.getText());
 				serverInfo.setPort(port.getSelection());
+				serverInfo.setCertificateAlias(cert.getText());
 			}
 		});
 	}
 
+	/**
+	 * Returns true if Text is not empty, i.e. not null/only space characters.
+	 * 
+	 * @param t
+	 * @return boolean
+	 */
 	private static boolean isTextNonEmpty(Text t) {
 		String s = t.getText();
 		if ((s != null) && (s.trim().length() > 0)) {
@@ -124,4 +175,48 @@ public class NewRepositoryWizardPageOne extends WizardPage {
 		return false;
 	}
 
+	/**
+	 * Listener for the selection dialog.
+	 * 
+	 * @author C
+	 */
+	class SelectionDialogListener implements SelectionListener {
+
+		/**
+		 * @param e selection event
+		 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
+		 */
+		public void widgetDefaultSelected(SelectionEvent e) {
+			// nothing to do
+		}
+
+		/**
+		 * @param e selection event
+		 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+		 */
+		public void widgetSelected(SelectionEvent e) {
+			CertificateSelectionDialog csd = new CertificateSelectionDialog(Display.getCurrent().getActiveShell(),
+				new LabelProvider() {
+					@Override
+					public String getText(Object element) {
+						if (element instanceof String) {
+							return element.toString();
+						} else {
+							return "";
+						}
+					}
+				});
+			ArrayList<String> certificates;
+			try {
+				certificates = KeyStoreManager.getInstance().getCertificates();
+				csd.setElements(certificates.toArray());
+			} catch (CertificateStoreException e1) {
+				csd.setErrorMessage(e1.getMessage());
+			}
+			csd.setBlockOnOpen(true);
+			csd.setTitle("Certificate Selection Dialogue");
+			csd.open();
+			cert.setText(csd.getCertificateAlias());
+		}
+	}
 }
