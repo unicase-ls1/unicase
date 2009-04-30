24e4a5b7dc4262cb41073ceede87c06395987e48
chodnick <chodnick@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-04-30 14:53:14 +0000

    added javadoc to make checkstyle happy

--- org.unicase.workspace/src/org/unicase/workspace/changeTracking/notification/NotificationInfo.java
+++ org.unicase.workspace/src/org/unicase/workspace/changeTracking/notification/NotificationInfo.java
@@ -1,3 +1,9 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
 package org.unicase.workspace.changeTracking.notification;
 
 import java.lang.reflect.Field;
@@ -8,26 +14,43 @@ import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EReference;
 import org.unicase.workspace.util.WorkspaceUtil;
 
+/**
+ * NotificationInfo is a type safe wrapper for EMF Notifications. It wraps a org.eclipse.emf.common.notify.Notification
+ * and implements a few additional getter methods
+ * 
+ * @author chodnick
+ */
+
 public class NotificationInfo implements Notification {
 
 	private Notification notification;
 
+	/**
+	 * The constructor needs the notification to wrap.
+	 * 
+	 * @param n the notification to wrap
+	 */
 	public NotificationInfo(Notification n) {
 		this.notification = n;
 	}
 
 	/**
-	 * the added value of this class additional descriptor and info methods
+	 * @return whether this notification signals a change of an attribute value
 	 */
-
 	public boolean isAttributeNotification() {
 		return notification.getFeature() instanceof EAttribute;
 	}
 
+	/**
+	 * @return whether this notification signals a change of a reference value
+	 */
 	public boolean isReferenceNotification() {
 		return notification.getFeature() instanceof EReference;
 	}
 
+	/**
+	 * @return the EAttribute if the notification relates to an attribute, null otherwise
+	 */
 	public EAttribute getAttribute() {
 		if (isAttributeNotification()) {
 			return (EAttribute) notification.getFeature();
@@ -35,6 +58,9 @@ public class NotificationInfo implements Notification {
 		return null;
 	}
 
+	/**
+	 * @return the EReference if the notification relates to a reference feature, null otherwise
+	 */
 	public EReference getReference() {
 		if (isReferenceNotification()) {
 			return (EReference) notification.getFeature();
@@ -42,30 +68,57 @@ public class NotificationInfo implements Notification {
 		return null;
 	}
 
+	/**
+	 * @return true if the changed feature is marked transient, false otherwise
+	 */
 	public boolean isTransient() {
 		return (isReferenceNotification() && getReference().isTransient())
 			|| (isAttributeNotification() && getAttribute().isTransient());
 	}
 
+	/**
+	 * @return true if the event is of type Notification.ADD, false otherwise
+	 */
 	public boolean isAddEvent() {
 		return this.getEventType() == Notification.ADD;
 	}
+
+	/**
+	 * @return true if the event is of type Notification.REMOVE, false otherwise
+	 */
 
 	public boolean isRemoveEvent() {
 		return this.getEventType() == Notification.REMOVE;
 	}
 
+	/**
+	 * @return true if the event is of type Notification.SET, false otherwise
+	 */
+
 	public boolean isSetEvent() {
 		return this.getEventType() == Notification.SET;
 	}
+
+	/**
+	 * @return true if the event is of type Notification.ADD_MANY, false otherwise
+	 */
 
 	public boolean isAddManyEvent() {
 		return this.getEventType() == Notification.ADD_MANY;
 	}
 
+	/**
+	 * @return true if the event is of type Notification.REMOVE_MANY, false otherwise
+	 */
+
 	public boolean isRemoveManyEvent() {
 		return this.getEventType() == Notification.REMOVE_MANY;
 	}
+
+	/**
+	 * @return true if this notification is followed by more notifications in a chain, false if this is the last
+	 *         notification of a chain
+	 */
 
 	public boolean hasNext() {
 
@@ -95,125 +148,216 @@ public class NotificationInfo implements Notification {
 	}
 
 	/**
-	 * delegate methods, use eclipse to generate them for you...
+	 * @return @see org.eclipse.emf.common.notify.Notification#getEventType()
 	 */
-
 	public int getEventType() {
 		return notification.getEventType();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getFeature()
+	 */
 	public Object getFeature() {
 		return notification.getFeature();
 	}
 
+	/**
+	 * @param expectedClass @see org.eclipse.emf.common.notify.Notification#getFeatureID(java.lang.Class)
+	 * @return @see org.eclipse.emf.common.notify.Notification#getFeatureID(java.lang.Class)
+	 */
 	public int getFeatureID(Class<?> expectedClass) {
 		return notification.getFeatureID(expectedClass);
 	}
+
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewBooleanValue()
+	 */
 
 	public boolean getNewBooleanValue() {
 		return notification.getNewBooleanValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewByteValue()
+	 */
 	public byte getNewByteValue() {
 		return notification.getNewByteValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewCharValue()
+	 */
 	public char getNewCharValue() {
 		return notification.getNewCharValue();
 	}
+
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewDoubleValue()
+	 */
 
 	public double getNewDoubleValue() {
 		return notification.getNewDoubleValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewFloatValue()
+	 */
 	public float getNewFloatValue() {
 		return notification.getNewFloatValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewIntValue()
+	 */
 	public int getNewIntValue() {
 		return notification.getNewIntValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewLongValue()
+	 */
 	public long getNewLongValue() {
 		return notification.getNewLongValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewShortValue()
+	 */
 	public short getNewShortValue() {
 		return notification.getNewShortValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewStringValue()
+	 */
 	public String getNewStringValue() {
 		return notification.getNewStringValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNewValue()
+	 */
 	public Object getNewValue() {
 		return notification.getNewValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getNotifier()
+	 */
 	public Object getNotifier() {
 		return notification.getNotifier();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldBooleanValue()
+	 */
 	public boolean getOldBooleanValue() {
 		return notification.getOldBooleanValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldByteValue()
+	 */
 	public byte getOldByteValue() {
 		return notification.getOldByteValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldCharValue()
+	 */
 	public char getOldCharValue() {
 		return notification.getOldCharValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldDoubleValue()
+	 */
 	public double getOldDoubleValue() {
 		return notification.getOldDoubleValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldFloatValue()
+	 */
 	public float getOldFloatValue() {
 		return notification.getOldFloatValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldIntValue()
+	 */
 	public int getOldIntValue() {
 		return notification.getOldIntValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldLongValue()
+	 */
 	public long getOldLongValue() {
 		return notification.getOldLongValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldShortValue()
+	 */
 	public short getOldShortValue() {
 		return notification.getOldShortValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldStringValue()
+	 */
 	public String getOldStringValue() {
 		return notification.getOldStringValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getOldValue()
+	 */
 	public Object getOldValue() {
 		return notification.getOldValue();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#getPosition()
+	 */
 	public int getPosition() {
 		return notification.getPosition();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#isReset()
+	 */
 	public boolean isReset() {
 		return notification.isReset();
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#isTouch()
+	 */
 	public boolean isTouch() {
 		return notification.isTouch();
 	}
 
+	/**
+	 * @param notification @see
+	 *            org.eclipse.emf.common.notify.Notification#merge(org.eclipse.emf.common.notify.Notification)
+	 * @return @see org.eclipse.emf.common.notify.Notification#merge(org.eclipse.emf.common.notify.Notification)
+	 */
 	public boolean merge(Notification notification) {
 		return notification.merge(notification);
 	}
 
+	/**
+	 * @return @see org.eclipse.emf.common.notify.Notification#wasSet()
+	 */
 	public boolean wasSet() {
 		return notification.wasSet();
 	}
 
+	/**
+	 * @return @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return notification.toString();
