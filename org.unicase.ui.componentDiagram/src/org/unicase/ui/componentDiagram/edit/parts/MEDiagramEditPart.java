cff0e2628699822aa8a3448588c7aba89591d2f4
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-05-02 18:22:38 +0000

    files moved

--- org.unicase.ui.componentDiagram/src/org/unicase/ui/componentDiagram/edit/parts/MEDiagramEditPart.java
+++ org.unicase.ui.componentDiagram/src/org/unicase/ui/componentDiagram/edit/parts/MEDiagramEditPart.java
@@ -6,7 +6,7 @@ import org.eclipse.gmf.runtime.notation.View;
 /**
  * @generated
  */
-public class MEDiagramEditPart extends org.unicase.ui.common.diagram.MEDiagramEditPart {
+public class MEDiagramEditPart extends org.unicase.ui.common.diagram.edit.parts.MEDiagramEditPart {
 
 	/**
 	 * @generated
