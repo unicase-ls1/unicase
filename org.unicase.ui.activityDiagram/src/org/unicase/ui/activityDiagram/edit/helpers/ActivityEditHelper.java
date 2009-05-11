b635b78d774519aaf6b465155369595904e58746
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-05-11 16:09:38 +0000

    added activitydiagram

---
+++ org.unicase.ui.activityDiagram/src/org/unicase/ui/activityDiagram/edit/helpers/ActivityEditHelper.java
@@ -0,0 +1,8 @@
+package org.unicase.ui.activityDiagram.edit.helpers;
+
+/**
+ * @generated
+ */
+public class ActivityEditHelper extends
+		org.unicase.ui.activityDiagram.edit.helpers.ModelBaseEditHelper {
+}
---
+++ org.unicase.ui.activityDiagram/src/org/unicase/ui/activityDiagram/edit/helpers/ForkEditHelper.java
@@ -0,0 +1,8 @@
+package org.unicase.ui.activityDiagram.edit.helpers;
+
+/**
+ * @generated
+ */
+public class ForkEditHelper extends
+		org.unicase.ui.activityDiagram.edit.helpers.ModelBaseEditHelper {
+}
