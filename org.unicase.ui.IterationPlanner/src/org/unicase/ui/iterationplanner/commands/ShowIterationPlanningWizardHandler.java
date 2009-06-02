7487e188f3981e613131e32e46ce48df13246fd4
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-02 12:54:35 +0000

    implemented command to call iteration planning wizard

---
+++ org.unicase.ui.IterationPlanner/src/org/unicase/ui/iterationplanner/commands/ShowIterationPlanningWizardHandler.java
@@ -0,0 +1,18 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * </copyright>
+ */
+
+package org.unicase.ui.iterationplanner.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ShowIterationPlanningWizardHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		return null;
+	}
+
+}
