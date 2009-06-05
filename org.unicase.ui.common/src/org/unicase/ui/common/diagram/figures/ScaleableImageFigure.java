30d101a52199f520c8dadf611eae345ba7540c8d
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-05 12:27:07 +0000

    added comments

--- org.unicase.ui.common/src/org/unicase/ui/common/diagram/figures/ScaleableImageFigure.java
+++ org.unicase.ui.common/src/org/unicase/ui/common/diagram/figures/ScaleableImageFigure.java
@@ -1,3 +1,8 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
 package org.unicase.ui.common.diagram.figures;
 
 import org.eclipse.core.runtime.FileLocator;
@@ -8,14 +13,33 @@ import org.eclipse.draw2d.PositionConstants;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.graphics.Image;
 
+/**
+ * @author denglerm
+ * . This is class extends the eclipse ImageFigure to allow resize
+ */
 public class ScaleableImageFigure extends ImageFigure {	
-	static Image img;
+	/**
+	 * The image.
+	 */
+	private static Image img;
 	
+	/**
+	 * . Constructor
+	 * 
+	 * @param bundle The package of the figure file
+	 * @param path the path of the figure file within the package
+	 */
 	public ScaleableImageFigure(String bundle, String path) {
- 	  super(img=ImageDescriptor.createFromURL(FileLocator.find(Platform
-				.getBundle(bundle), new Path(path), null)).createImage(),0);
+ 	  img=ImageDescriptor.createFromURL(FileLocator.find(Platform
+			.getBundle(bundle), new Path(path), null)).createImage();
+ 	  this.setImage(img);
  	  this.setAlignment(PositionConstants.NORTH_WEST);
     }
+	/**
+	 * . {@inheritDoc}
+	 * 
+	 * @see org.eclipse.draw2d.ImageFigure#getImage()
+	 */
 	@Override
 	public Image getImage()
 	{
