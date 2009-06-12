5341e7070984361435159cced6cc4d5bbb32a2d5
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-12 17:22:30 +0000

    moved class from ComponentenDiagram into common

---
+++ core/org.unicase.ui.common/src/org/unicase/ui/common/diagram/figures/BorderItemLocatorEx.java
@@ -0,0 +1,72 @@
+/**
+ * <copyright> Copyright (c) 2008 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
+package org.unicase.ui.common.diagram.figures;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
+import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.gmf.runtime.diagram.ui.figures.BorderItemLocator;
+
+/**
+ * @author denglerm This class is used to locate diagram labels centered below a diagram node
+ */
+public class BorderItemLocatorEx extends BorderItemLocator {
+
+	/**
+	 * The preferred side determines placement of figure.
+	 * 
+	 * @param parentFigure the parent figure
+	 */
+	public BorderItemLocatorEx(IFigure parentFigure) {
+		super(parentFigure);
+	}
+
+	/**
+	 * The preferred side determines placement of figure.
+	 * 
+	 * @param parentFigure the parent figure
+	 * @param preferredSide
+	 *            the preferred side of the parent figure on which to place this
+	 *            border item as defined in {@link PositionConstants}
+	 */
+	public BorderItemLocatorEx(IFigure parentFigure, int preferredSide) {
+		super(parentFigure, preferredSide);
+	}
+	
+	/**
+	 * The preferred location overrides the preferred side.
+	 * 
+	 * @param borderItem the border item to be placed at the parent figure
+	 * @param parentFigure the parent figure
+	 * @param constraint the constraint
+	 */
+	public BorderItemLocatorEx(IFigure borderItem, IFigure parentFigure, Rectangle constraint) {
+		super(borderItem, parentFigure, constraint);
+	}
+
+	/**
+	 * . {@inheritDoc}
+	 * 
+	 */
+	@Override
+	public void relocate(IFigure borderItem) {
+
+		Rectangle bounds = getParentBorder();
+		int parentFigureWidth = bounds.width;
+		int parentFigureHeight = bounds.height;
+		int parentFigureX = bounds.x;
+		int parentFigureY = bounds.y;
+		int x = parentFigureX;
+		int y = parentFigureY;
+		x += parentFigureWidth / 2;
+		y = parentFigureY + parentFigureHeight;
+		Dimension borderItemSize = getSize(borderItem);
+		Point location = new Point(x - borderItemSize.width / 2, y + getBorderItemOffset().height);
+		borderItem.setBounds(new Rectangle(location, borderItemSize));
+	}
+}
