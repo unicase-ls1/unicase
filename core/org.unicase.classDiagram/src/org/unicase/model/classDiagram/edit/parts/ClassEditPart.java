da9268ed103b80456455e2b7c8d0dabcc6217c7b
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-18 22:32:19 +0000

    layout moved

--- core/org.unicase.classDiagram/src/org/unicase/model/classDiagram/edit/parts/ClassEditPart.java
+++ core/org.unicase.classDiagram/src/org/unicase/model/classDiagram/edit/parts/ClassEditPart.java
@@ -32,8 +32,8 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
-import org.unicase.ui.common.diagram.ConfigurableRectangleFigure;
 import org.unicase.ui.common.diagram.figures.CenterLayout;
+import org.unicase.ui.common.diagram.figures.ConfigurableRectangleFigure;
 
 /**
  * @generated
