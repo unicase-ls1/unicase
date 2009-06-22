5ff6aca59f16289245434cc5c0e13aa01c5cc11d
fabisiew <fabisiew@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-22 09:24:48 +0000

    ganttTree content provider implemented

--- other/org.unicase.ui.ganttview/src/org/unicase/ui/ganttview/views/GanttTreeContentProvider.java
+++ other/org.unicase.ui.ganttview/src/org/unicase/ui/ganttview/views/GanttTreeContentProvider.java
@@ -1,7 +1,18 @@
 package org.unicase.ui.ganttview.views;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.edit.provider.ComposedAdapterFactory;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
+import org.unicase.model.ModelElement;
+import org.unicase.model.Project;
+import org.unicase.model.task.TaskPackage;
+import org.unicase.model.task.WorkItem;
+import org.unicase.model.task.WorkPackage;
 
 public class GanttTreeContentProvider extends AdapterFactoryContentProvider {
 
@@ -11,14 +22,51 @@ public class GanttTreeContentProvider extends AdapterFactoryContentProvider {
 
 	@Override
 	public Object[] getElements(Object object) {
-		// TODO Auto-generated method stub
-		return super.getElements(object);
+
+		if (object instanceof Project) {
+			EList<WorkPackage> elementsForProject = getElementsForProject(object);
+			return elementsForProject.toArray(new Object[elementsForProject.size()]);
+		} else if (object instanceof WorkPackage) {
+			Set<ModelElement> elementsForWorkPackage = getElementsForWorkPackage(object);
+			return elementsForWorkPackage.toArray(new Object[elementsForWorkPackage.size()]);
+		} else {
+			return super.getElements(object);
+		}
+	}
+
+	private EList<WorkPackage> getElementsForProject(Object object) {
+		Project project = (Project) object;
+		EList<WorkPackage> dummyList = new BasicEList<WorkPackage>();
+		return project.getAllModelElementsbyClass(TaskPackage.eINSTANCE.getWorkPackage(), dummyList);
+
 	}
 
 	@Override
 	public Object[] getChildren(Object object) {
-		// TODO Auto-generated method stub
-		return super.getChildren(object);
+		if (object instanceof WorkPackage) {
+			Set<ModelElement> elementsForWorkPackage = getElementsForWorkPackage(object);
+			return elementsForWorkPackage.toArray(new Object[elementsForWorkPackage.size()]);
+		} else {
+			return super.getChildren(object);
+		}
 	}
 
+	/**
+	 * . Returns all model elements being annotated by WorkItems contained in this WorkPackage.
+	 * 
+	 * @param object WorkPackage
+	 * @return
+	 */
+	private Set<ModelElement> getElementsForWorkPackage(Object object) {
+
+		Set<ModelElement> ret = new HashSet<ModelElement>();
+		WorkPackage workPackage = (WorkPackage) object;
+		List<WorkItem> containedWorkItems = workPackage.getAllContainedWorkItems();
+		for (WorkItem workItem : containedWorkItems) {
+			if (workItem instanceof WorkPackage) {
+				ret.add(workItem);
+			}
+		}
+		return ret;
+	}
 }
