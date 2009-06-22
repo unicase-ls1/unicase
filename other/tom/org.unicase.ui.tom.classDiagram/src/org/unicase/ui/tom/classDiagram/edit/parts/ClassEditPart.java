2228e7093fc155c6c3a2f7a8e5e2f4b89be17275
schroech <schroech@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-22 12:31:49 +0000

    Adapted tom.classDiagram to changes in ui.common package structure

--- other/tom/org.unicase.ui.tom.classDiagram/src/org/unicase/ui/tom/classDiagram/edit/parts/ClassEditPart.java
+++ other/tom/org.unicase.ui.tom.classDiagram/src/org/unicase/ui/tom/classDiagram/edit/parts/ClassEditPart.java
@@ -27,7 +27,7 @@ import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Display;
-import org.unicase.ui.common.diagram.ConfigurableRectangleFigure;
+import org.unicase.ui.common.diagram.figures.ConfigurableRectangleFigure;
 import org.unicase.ui.common.diagram.figures.CenterLayout;
 import org.unicase.ui.tom.classDiagram.edit.policies.TouchResizableShapeEditPolicy;
 
