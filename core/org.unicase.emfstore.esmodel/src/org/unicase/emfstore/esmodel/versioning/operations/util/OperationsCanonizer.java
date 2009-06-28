0c5a873fe4da1635bdab65173ccc85d227da3876
chodnick <chodnick@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-06-28 15:00:22 +0000

    canonizes attributes now

--- core/org.unicase.emfstore.esmodel/src/org/unicase/emfstore/esmodel/versioning/operations/util/OperationsCanonizer.java
+++ core/org.unicase.emfstore.esmodel/src/org/unicase/emfstore/esmodel/versioning/operations/util/OperationsCanonizer.java
@@ -9,6 +9,9 @@ package org.unicase.emfstore.esmodel.versioning.operations.util;
 import java.util.List;
 
 import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.AttributeOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.CompositeOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.CreateDeleteOperation;
 
 /**
  * Canonizes a list of operations. Removes all operations that are not necessary to achieve the same result when the
@@ -31,7 +34,51 @@ public final class OperationsCanonizer {
 	 * @param operations a list of operations (the list is order by creation time)
 	 */
 	public static void canonize(List<AbstractOperation> operations) {
-		// not doing anything, for the time being
+
+		// fold attribute operations
+		foldAttributes(operations);
 	}
 
+	private static void foldAttributes(List<AbstractOperation> operations) {
+
+		for (int i = 0; i < operations.size() - 1; i++) {
+
+			AbstractOperation opLeft = operations.get(i);
+			if (!(opLeft instanceof AttributeOperation)) {
+				continue;
+			}
+			AttributeOperation attOpLeft = (AttributeOperation) opLeft;
+
+			for (int j = i + 1; j < operations.size(); j++) {
+
+				AbstractOperation opRight = operations.get(j);
+
+				if (opRight instanceof AttributeOperation
+					&& opLeft.getModelElementId().equals(opRight.getModelElementId())) {
+
+					AttributeOperation attOpRight = (AttributeOperation) opRight;
+					if (attOpLeft.getFeatureName().equals(attOpRight.getFeatureName())) {
+						// merge opLeft and opRight in opLeft
+						attOpLeft.setNewValue(attOpRight.getNewValue());
+						operations.remove(j); // remove opRight
+						j--; // reexamine the index after removal
+					}
+
+				}
+
+				if (opRight instanceof CreateDeleteOperation || opRight instanceof CompositeOperation) {
+					break;
+				}
+
+			}
+			// if the remaining leftOp is a noop, remove it altogether
+			if ((attOpLeft.getNewValue() == null && attOpLeft.getOldValue() == null)
+				|| attOpLeft.getNewValue().equals(attOpLeft.getOldValue())) {
+				operations.remove(i);
+				i--; // reexamine the index after removal
+			}
+
+		}
+
+	}
 }
