daebd5cb575b70290b8db17daf37a1761448f22f
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-07-03 14:45:51 +0000

    changed package name

--- core/org.unicase.ui.stateDiagram/src/org/unicase/ui/stateDiagram/edit/helpers/StateEndEditHelper.java
+++
@@ -1,8 +0,0 @@
-package org.unicase.ui.stateDiagram.edit.helpers;
-
-/**
- * @generated
- */
-public class StateEndEditHelper extends
-		org.unicase.ui.stateDiagram.edit.helpers.ModelBaseEditHelper {
-}
--- core/org.unicase.ui.stateDiagram/src/org/unicase/ui/stateDiagram/edit/helpers/StateInitialEditHelper.java
+++
@@ -1,8 +0,0 @@
-package org.unicase.ui.stateDiagram.edit.helpers;
-
-/**
- * @generated
- */
-public class StateInitialEditHelper extends
-		org.unicase.ui.stateDiagram.edit.helpers.ModelBaseEditHelper {
-}
