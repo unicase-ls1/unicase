e7148921046acea02e6a1794f53b9ef9a62c3085
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-07-30 13:30:57 +0000

    implemented user can set project name on import

--- core/org.unicase.workspace.ui/src/org/unicase/workspace/ui/commands/ImportProjectHandler.java
+++ core/org.unicase.workspace.ui/src/org/unicase/workspace/ui/commands/ImportProjectHandler.java
@@ -11,12 +11,16 @@ import java.io.IOException;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.PlatformUI;
 import org.unicase.ui.common.exceptions.DialogHandler;
+import org.unicase.workspace.ProjectSpace;
 import org.unicase.workspace.Workspace;
 import org.unicase.workspace.WorkspaceManager;
 import org.unicase.workspace.util.UnicaseCommand;
@@ -39,12 +43,59 @@ public class ImportProjectHandler extends AbstractHandler {
 	 */
 	public static final String[] FILTER_EXTS = { "*.ucp", "*.*" };
 
+	private String projectName;
+
 	/**
 	 * {@inheritDoc}
 	 * 
 	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		final String absoluteFileName = showOpenFileDialog();
+		if (absoluteFileName == null) {
+			return null;
+		}
+
+		projectName = showProjectNameDialog(absoluteFileName
+				.substring(absoluteFileName.lastIndexOf(File.separator) + 1));
+		if (projectName == null) {
+			return null;
+		}
+
+		final ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+
+		new UnicaseCommand() {
+			@Override
+			protected void doRun() {
+				try {
+					progressDialog.open();
+					progressDialog.getProgressMonitor().beginTask(
+							"Import project...", 100);
+					progressDialog.getProgressMonitor().worked(10);
+					Workspace currentWorkspace = WorkspaceManager.getInstance()
+							.getCurrentWorkspace();
+					ProjectSpace projectSpace = currentWorkspace
+							.importProject(absoluteFileName);
+
+					projectSpace.setProjectName(projectName);
+
+				} catch (IOException e) {
+					DialogHandler.showExceptionDialog(e);
+				} finally {
+					progressDialog.getProgressMonitor().done();
+					progressDialog.close();
+				}
+			}
+		}.run();
+
+		MessageDialog.openInformation(null, "Import",
+				"Imported project from file: " + absoluteFileName);
+		return null;
+	}
+
+	private String showOpenFileDialog() {
 		FileDialog dialog = new FileDialog(PlatformUI.getWorkbench()
 				.getActiveWorkbenchWindow().getShell(), SWT.OPEN);
 		dialog.setFilterNames(ImportProjectHandler.FILTER_NAMES);
@@ -60,35 +111,41 @@ public class ImportProjectHandler extends AbstractHandler {
 		if (fileName.charAt(fileName.length() - 1) != File.separatorChar) {
 			stringBuilder.append(File.separatorChar);
 		}
+
 		stringBuilder.append(fileName);
-		final String absoluteFileName = stringBuilder.toString();
-		final ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(
-				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+		String absoluteFileName = stringBuilder.toString();
+		return absoluteFileName;
+	}
 
-		new UnicaseCommand() {
-			@Override
-			protected void doRun() {
-				try {
-					progressDialog.open();
-					progressDialog.getProgressMonitor().beginTask(
-							"Import project...", 100);
-					progressDialog.getProgressMonitor().worked(10);
-					Workspace currentWorkspace = WorkspaceManager.getInstance()
-							.getCurrentWorkspace();
-					currentWorkspace.importProject(absoluteFileName);
+	private String showProjectNameDialog(String initialValue) {
 
-				} catch (IOException e) {
-					DialogHandler.showExceptionDialog(e);
-				} finally {
-					progressDialog.getProgressMonitor().done();
-					progressDialog.close();
+		int extensionIndex = initialValue.length();
+		if (initialValue.contains(".")) {
+			extensionIndex = initialValue.lastIndexOf(".");
+		}
+		initialValue = initialValue.substring(0, extensionIndex);
+
+		IInputValidator inputValidator = new IInputValidator() {
+			public String isValid(String newText) {
+				if (newText == null || newText.equals("")
+						|| newText.matches("\\s*")) {
+					return "No project name provided!";
 				}
+				return null;
 			}
-		}.run();
 
-		MessageDialog.openInformation(null, "Import",
-				"Imported project from file: " + absoluteFileName);
-		return null;
+		};
+		InputDialog inputDialog = new InputDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell(), "Project Name",
+				"Please enter a name for the imported project:", initialValue,
+				inputValidator);
+
+		if (inputDialog.open() == Dialog.OK) {
+			return inputDialog.getValue();
+		} else {
+			return null;
+		}
+
 	}
 
 }
