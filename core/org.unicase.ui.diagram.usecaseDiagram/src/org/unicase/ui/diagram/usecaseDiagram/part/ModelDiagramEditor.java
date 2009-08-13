4a1536fb344343298acd6c4f71de6db8ab4cefab
denglerm <denglerm@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-08-13 18:32:49 +0000

    removed warnings in uc diagrams

--- core/org.unicase.ui.diagram.usecaseDiagram/src/org/unicase/ui/diagram/usecaseDiagram/part/ModelDiagramEditor.java
+++ core/org.unicase.ui.diagram.usecaseDiagram/src/org/unicase/ui/diagram/usecaseDiagram/part/ModelDiagramEditor.java
@@ -1,3 +1,8 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
 package org.unicase.ui.diagram.usecaseDiagram.part;
 
 import org.eclipse.core.resources.IFile;
@@ -155,8 +160,10 @@ public class ModelDiagramEditor extends
 	}
 
 	/**
-	 * @generated NOT
+	 * @generated NOT {@inheritDoc}
+	 * @see org.eclipse.gmf.runtime.diagram.ui.resources.editor.parts.DiagramDocumentEditor#isSaveAsAllowed()
 	 */
+	@Override
 	public boolean isSaveAsAllowed() {
 		return false;
 	}
