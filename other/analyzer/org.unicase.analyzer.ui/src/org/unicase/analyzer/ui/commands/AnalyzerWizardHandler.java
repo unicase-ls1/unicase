baaf2d6a4fe3e372df828f0f9aafa8d2862f7def
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-08-14 10:00:46 +0000

    removed warnings

--- other/analyzer/org.unicase.analyzer.ui/src/org/unicase/analyzer/ui/commands/AnalyzerWizardHandler.java
+++ other/analyzer/org.unicase.analyzer.ui/src/org/unicase/analyzer/ui/commands/AnalyzerWizardHandler.java
@@ -15,9 +15,6 @@ import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.unicase.analyzer.ui.wizards.ProjectAnalyzerWizard;
-import org.unicase.emfstore.exceptions.EmfStoreException;
-import org.unicase.ui.common.exceptions.DialogHandler;
-import org.unicase.workspace.ui.commands.ServerRequestCommandHandler;
 
 
 /**
