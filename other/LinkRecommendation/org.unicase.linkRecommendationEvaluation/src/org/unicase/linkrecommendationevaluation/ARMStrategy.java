5fe77305888e48d9aa0edb976bc6b4d8a51763fc
koegel <koegel@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-08-17 10:47:18 +0000

    added ARM strategy and test for the stratedy

---
+++ other/LinkRecommendation/org.unicase.linkRecommendationEvaluation/src/org/unicase/linkrecommendationevaluation/ARMStrategy.java
@@ -0,0 +1,88 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.linkrecommendationevaluation;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.unicase.analyzer.ProjectAnalysisData;
+import org.unicase.emfstore.esmodel.versioning.ChangePackage;
+import org.unicase.emfstore.esmodel.versioning.events.Event;
+import org.unicase.emfstore.esmodel.versioning.events.TraceEvent;
+import org.unicase.linkrecommendation.recommendationStrategies.RecommendationStrategy;
+import org.unicase.model.ModelElement;
+import org.unicase.model.ModelElementId;
+
+/**
+ * Recommends a link based on the previous read history based on Association Rule Mining.
+ * 
+ * @author koegel
+ */
+public class ARMStrategy implements RecommendationStrategy {
+
+	private static final String ARM_STRATEGY = "ARM Strategy";
+	private Map<String, Double> linkCountMap;
+	
+	/**
+	 * Default Constructor.
+	 */
+	public ARMStrategy() {
+		linkCountMap = new HashMap<String, Double>();
+	}
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.linkrecommendation.recommendationStrategies.RecommendationStrategy#getMatchingMap(org.unicase.model.ModelElement,
+	 *      java.util.Collection)
+	 */
+	public Map<ModelElement, Double> getMatchingMap(ModelElement base, Collection<ModelElement> elements) {
+		Map<ModelElement, Double> result = new HashMap<ModelElement, Double>();
+		String sourceId = base.getIdentifier();
+		for (ModelElement linkTarget: elements) {
+			Double linkCount = linkCountMap.get(sourceId + linkTarget.getIdentifier());
+			if (linkCount==null) {
+				linkCount = new Double(0);
+			}
+			result.put(linkTarget, linkCount);
+		}
+		return result;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.linkrecommendation.recommendationStrategies.RecommendationStrategy#getName()
+	 */
+	public String getName() {
+		return ARM_STRATEGY;
+	}
+
+	/**
+	 * Updates the data of the strategy with a new ProjectAnalysisData object.
+	 * @param data the new data
+	 */
+	public void updateStrategyData(ProjectAnalysisData data) {
+		for (ChangePackage changePackage: data.getChangePackages()) {
+			for (Event event: changePackage.getEvents()) {
+				if (event instanceof TraceEvent) {
+					TraceEvent traceEvent = (TraceEvent) event;
+					ModelElementId sourceElementId = traceEvent.getSourceElement();
+					ModelElementId targetElementId = traceEvent.getTargetElement();
+					String key = sourceElementId.getId() + targetElementId.getId();
+					Double linkCount = linkCountMap.get(key);
+					if (linkCount==null) {
+						linkCountMap.put(key, new Double(1));
+					}
+					else {
+						linkCountMap.put(key, new Double(linkCount.doubleValue() + 1));
+					}
+				}
+			}
+		}
+	}
+
+}
