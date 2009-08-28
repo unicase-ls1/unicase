406b165e594c446fd9edb4df8201073ab79ceaab
hodaie <hodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-08-28 17:59:50 +0000

    history based assignment analyzer

--- other/org.unicase.ui.IterationPlanner/src/org/unicase/ui/iterationplanner/paper/machinelearning/MachineLearningTriageAccuracyAnalyzer.java
+++ other/org.unicase.ui.IterationPlanner/src/org/unicase/ui/iterationplanner/paper/machinelearning/MachineLearningTriageAccuracyAnalyzer.java
@@ -8,11 +8,14 @@ import org.unicase.analyzer.ProjectAnalysisData;
 import org.unicase.emfstore.esmodel.versioning.ChangePackage;
 import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
 import org.unicase.emfstore.esmodel.versioning.operations.CompositeOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.MultiReferenceOperation;
 import org.unicase.emfstore.esmodel.versioning.operations.ReferenceOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.SingleReferenceOperation;
 import org.unicase.model.ModelElement;
 import org.unicase.model.ModelElementId;
 import org.unicase.model.Project;
 import org.unicase.model.bug.BugReport;
+import org.unicase.model.organization.OrganizationPackage;
 import org.unicase.model.organization.User;
 import org.unicase.model.rationale.Issue;
 import org.unicase.model.task.ActionItem;
@@ -60,17 +63,17 @@ public class MachineLearningTriageAccuracyAnalyzer implements DataAnalyzer {
 		list.add("AggregateAccuracy");
 		list.add("TotalPredictionsPerRevision");
 		list.add("CorrectPredictionsPreRevision");
-		
+
 		return list;
 	}
 
 	public List<Object> getValue(ProjectAnalysisData data) {
-		
+
 		totalPredictionsPerRevision = 0;
 		correctPredictionsPreRevision = 0;
 		int oldTotalPredictions = totalPredictions;
 		int oldCorrectPredictions = correctPredictions;
-		
+
 		List<Object> value = new ArrayList<Object>();
 		// find assignee operations
 		// first step? just fetch the data.
@@ -93,21 +96,17 @@ public class MachineLearningTriageAccuracyAnalyzer implements DataAnalyzer {
 			leafoperations.addAll(operation.getLeafOperations());
 		}
 		for (AbstractOperation operation : leafoperations) {
-			if (!(operation instanceof ReferenceOperation)) {
+			if (!isRelvant(operation)) {
 				continue;
 			}
-			ReferenceOperation refOp = (ReferenceOperation) operation;
-			if (refOp.getFeatureName().equals(
-					TaskPackage.eINSTANCE.getWorkItem_Assignee().getName())) {
 
-				Object assignee = findNewAssignee(refOp
-						.getAllInvolvedModelElements());
-				if (assignee == null) {
-					System.out.println("Assignee is NULL number: "
-							+ nullassignee);
-				}
-				predictAssigneee(clonedProject.getModelElement(refOp
-						.getModelElementId()), assignee);
+			Object assignee = getAssignee((ReferenceOperation)operation);
+			List<Object> workItems = getWorkItems((ReferenceOperation)operation);
+			if (assignee == null) {
+				System.out.println("Assignee is NULL number: " + nullassignee);
+			}
+			for (Object wi : workItems) {
+				predictAssigneee(wi, assignee);
 			}
 
 			// redraw the changes in the project
@@ -119,22 +118,76 @@ public class MachineLearningTriageAccuracyAnalyzer implements DataAnalyzer {
 		}
 
 		totalPredictionsPerRevision = totalPredictions - oldTotalPredictions;
-		correctPredictionsPreRevision = correctPredictions - oldCorrectPredictions;
-		
+		correctPredictionsPreRevision = correctPredictions
+				- oldCorrectPredictions;
+
 		// compute accuracy
-		double aggregateAccuracy = ((double)correctPredictions)/totalPredictions;
-		double accurarcyPerRevision = ((double)correctPredictionsPreRevision)/totalPredictionsPerRevision;
+		double aggregateAccuracy = ((double) correctPredictions)
+				/ totalPredictions;
+		double accurarcyPerRevision = ((double) correctPredictionsPreRevision)
+				/ totalPredictionsPerRevision;
 		value.add(data.getPrimaryVersionSpec().getIdentifier());
 		value.add(aggregateAccuracy);
 		value.add(totalPredictionsPerRevision);
 		value.add(correctPredictionsPreRevision);
-		System.out.println(value.get(0)
-				+ " ---- " + value.get(1) +  " ------- total pred: "
-				+ totalPredictions + " ------ correct pred: "
-				+ correctPredictions + " ----- totalPredictionsPerRevison " + value.get(2)  +" ----- correctPredictionsPerRevisioin " + value.get(3) + " ---- #WIs: "
-				+ meMatrix.getModelElements().size());
+		System.out.println(value.get(0) + " ---- " + value.get(1)
+				+ " ------- total pred: " + totalPredictions
+				+ " ------ correct pred: " + correctPredictions
+				+ " ----- totalPredictionsPerRevison " + value.get(2)
+				+ " ----- correctPredictionsPerRevisioin " + value.get(3)
+				+ " ---- #WIs: " + meMatrix.getModelElements().size());
 		clonedProject = (Project) EcoreUtil.copy(data.getProjectState());
 		return value;
+	}
+
+	private List<Object> getWorkItems(ReferenceOperation operation) {
+		List<Object> result = new ArrayList<Object>();
+		if(operation instanceof SingleReferenceOperation){
+			// assignee is set for work item
+			SingleReferenceOperation op = (SingleReferenceOperation) operation;
+			Object wi =clonedProject.getModelElement(op.getModelElementId());
+			result.add(wi);
+			return result;
+		}else if(operation instanceof MultiReferenceOperation){
+			// assignments is set for user
+			MultiReferenceOperation op = (MultiReferenceOperation) operation;
+			for(ModelElementId meId : op.getReferencedModelElements()){
+				result.add(clonedProject.getModelElement(meId));
+			}
+			return result;
+		}
+		return result;
+		
+	}
+
+	private Object getAssignee(ReferenceOperation operation) {
+		if(operation instanceof SingleReferenceOperation){
+			// assignee is set for work item
+			SingleReferenceOperation op = (SingleReferenceOperation) operation;
+			return clonedProject.getModelElement(op.getNewValue());
+		}else if(operation instanceof MultiReferenceOperation){
+			// assignments is set for user
+			MultiReferenceOperation op = (MultiReferenceOperation) operation;
+			return clonedProject.getModelElement(op.getModelElementId());
+		}
+		return null;
+	}
+
+	private boolean isRelvant(AbstractOperation operation) {
+		if (!(operation instanceof ReferenceOperation)) {
+			return false;
+		}
+		ReferenceOperation refOp = (ReferenceOperation) operation;
+		if (refOp.getFeatureName().equals(
+				TaskPackage.eINSTANCE.getWorkItem_Assignee().getName())) {
+			return true;
+		}
+		if (refOp.getFeatureName().equals(
+				OrganizationPackage.eINSTANCE.getOrgUnit_Assignments()
+						.getName())) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
