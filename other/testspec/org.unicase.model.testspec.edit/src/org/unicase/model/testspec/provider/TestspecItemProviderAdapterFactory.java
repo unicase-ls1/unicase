463f532746cb18eff327bef34df954687dcf3703
alexanderdelater <alexanderdelater@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-10-05 07:48:03 +0000

    generated code with fixed genmodel (fixed containment proxy setting)

--- other/testspec/org.unicase.model.testspec.edit/src/org/unicase/model/testspec/provider/TestspecItemProviderAdapterFactory.java
+++ other/testspec/org.unicase.model.testspec.edit/src/org/unicase/model/testspec/provider/TestspecItemProviderAdapterFactory.java
@@ -194,7 +194,7 @@ public class TestspecItemProviderAdapterFactory extends TestspecAdapterFactory i
 	public Object adapt(Object object, Object type) {
 		if (isFactoryForType(type)) {
 			Object adapter = super.adapt(object, type);
-			if (!(type instanceof Class) || (((Class<?>)type).isInstance(adapter))) {
+			if (!(type instanceof Class<?>) || (((Class<?>)type).isInstance(adapter))) {
 				return adapter;
 			}
 		}
