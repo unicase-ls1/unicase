27c6b4577cdb0fcaf659c6d3b1ac1f9b25afc384
alexanderdelater <alexanderdelater@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-10-05 07:47:24 +0000

    fixed containment proxy setting in genmodel

--- other/glossary/org.unicase.model.glossary.edit/src/org/unicase/model/glossary/provider/GlossaryEntryItemProvider.java
+++ other/glossary/org.unicase.model.glossary.edit/src/org/unicase/model/glossary/provider/GlossaryEntryItemProvider.java
@@ -19,6 +19,7 @@ import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
 import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.unicase.model.glossary.GlossaryEntry;
 import org.unicase.model.provider.ModelElementItemProvider;
 
 /**
