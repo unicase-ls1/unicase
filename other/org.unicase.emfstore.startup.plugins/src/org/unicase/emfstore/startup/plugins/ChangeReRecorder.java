e1ca9b51f497870d309f0a6b49a8be73951cd1df
wesendon <wesendon@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-10-08 17:04:07 +0000

    edited rerecorder

--- other/org.unicase.emfstore.startup.plugins/src/org/unicase/emfstore/startup/plugins/ChangeReRecorder.java
+++ other/org.unicase.emfstore.startup.plugins/src/org/unicase/emfstore/startup/plugins/ChangeReRecorder.java
@@ -1,13 +1,17 @@
 package org.unicase.emfstore.startup.plugins;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.ListIterator;
 
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.unicase.emfstore.esmodel.EsmodelFactory;
 import org.unicase.emfstore.esmodel.versioning.ChangePackage;
 import org.unicase.emfstore.esmodel.versioning.VersioningFactory;
 import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.CompositeOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.CreateDeleteOperation;
 import org.unicase.model.Project;
 import org.unicase.workspace.WorkspaceFactory;
 import org.unicase.workspace.impl.ProjectSpaceImpl;
@@ -35,9 +39,34 @@ public class ChangeReRecorder {
 
 	public List<AbstractOperation> convertOperations(List<AbstractOperation> operations) {
 		projectSpace.getOperations().clear();
-		projectSpace.applyOperationsWithRecording(operations, true);
+		applyOperationForOperation(operations);
 		ChangePackage changePackage = projectSpace.getLocalChangePackage(true);
 		return changePackage.getCopyOfOperations();
+	}
+
+	private void applyOperationForOperation(List<AbstractOperation> operations) {
+		int index = 0;
+		for(AbstractOperation operation : operations) {
+			Date clientDate = operation.getClientDate();
+			projectSpace.applyOperationsWithRecording(Arrays.asList(operation), true);
+			if(index >= projectSpace.getOperations().size()) {
+				continue;
+			}
+			ListIterator<AbstractOperation> listIterator = projectSpace.getOperations().listIterator(index);
+			while(listIterator.hasNext()) {
+				setDate(clientDate,listIterator.next());
+			}
+			index = projectSpace.getOperations().size();
+		}
+	}
+
+	private void setDate(Date clientDate, AbstractOperation operation) {
+		if(operation instanceof CompositeOperation) {
+			for(AbstractOperation subOperation : ((CompositeOperation) operation).getSubOperations()) {
+				setDate(clientDate, subOperation);
+			}
+		}
+		operation.setClientDate(clientDate==null?null:(Date) clientDate.clone());
 	}
 
 	public Project getProject() {
