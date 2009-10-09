960113e54c1ef9b5de854d4622c29a466cec849d
wesendon <wesendon@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-10-09 15:02:15 +0000

    removed warnings (added comments)

--- core/org.unicase.emfstore/src/org/unicase/emfstore/startup/StartupExtensionManager.java
+++ core/org.unicase.emfstore/src/org/unicase/emfstore/startup/StartupExtensionManager.java
@@ -1,3 +1,8 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
 package org.unicase.emfstore.startup;
 
 import org.eclipse.core.runtime.CoreException;
@@ -6,8 +11,18 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
 import org.unicase.emfstore.esmodel.ProjectHistory;
 
+/**
+ * This class calls the startup listeners, registered at the extension point.
+ * 
+ * @author wesendon
+ */
 public class StartupExtensionManager {
 
+	/**
+	 * Calls the startup listeners.
+	 * 
+	 * @param projects list of projects
+	 */
 	public void runOnProjects(EList<ProjectHistory> projects) {
 		IConfigurationElement[] config = Platform.getExtensionRegistry().getConfigurationElementsFor(
 			"org.unicase.emfstore.startuplistener");
