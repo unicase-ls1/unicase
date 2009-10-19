81212569e24f77302923584b5e2334027edeb734
narayan <narayan@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-10-19 11:26:06 +0000

    SWTBOT4GEF included for the UI testing. Adding the Node done.

--- test/org.unicase.test/src/org/unicase/test/uitests/SWTDiagramTest.java
+++ test/org.unicase.test/src/org/unicase/test/uitests/SWTDiagramTest.java
@@ -2,6 +2,9 @@ package org.unicase.test.uitests;
 
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
+import org.eclipse.swtbot.eclipse.gef.finder.SWTBotGefTestCase;
+import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
+import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
@@ -12,7 +15,7 @@ import org.junit.runner.RunWith;
 import org.unicase.workspace.test.SetupHelper;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
-public class SWTDiagramTest {
+public class SWTDiagramTest extends SWTBotGefTestCase {
 
 	private static SWTWorkbenchBot bot;
 
@@ -39,17 +42,71 @@ public class SWTDiagramTest {
 		bot.button("OK").click();
 
 		SWTBotView viewById = bot.activeView();
+		SWTBotTreeItem[] uninavigatoritem = viewById.bot().tree().getAllItems();
+
+		if (uninavigatoritem.length == 0) {
+
+			bot.menu("Window").menu("Show View").menu("Other...").click();
+			shell1 = bot.shell("Show View");
+			shell1.activate();
+			bot.tree().expandNode("unicase").select("EmfStore Browser");
+			bot.button("OK").click();
+
+			viewById = bot.activeView();
+			SWTBotTreeItem[] emfbrowseritem = viewById.bot().tree().getAllItems();
+			emfbrowseritem[0].doubleClick();
+			SWTBotTreeItem[] subitem = emfbrowseritem[0].getItems();
+			bot.sleep(2000);
+			subitem[0].select().contextMenu("Checkout").click().setFocus();
+
+			bot.sleep(2000);
+			bot.menu("Window").menu("Open Perspective").menu("Other...").click();
+			SWTBotShell openPerspectiveShell = bot.shell("Open Perspective");
+			openPerspectiveShell.activate();
+			bot.table().select("Unicase");
+			bot.button("OK").click();
+			// bot.viewById("Unicase Navigator").show();
+
+			bot.menu("Window").menu("Show View").menu("Other...").click();
+			shell1 = bot.shell("Show View");
+			shell1.activate();
+			bot.tree().expandNode("unicase").select("Unicase Navigator");
+			bot.button("OK").click();
+		}
+		viewById = bot.activeView();
 		SWTBotTreeItem[] items = viewById.bot().tree().getAllItems();
 		SWTBotTreeItem[] subitem = items[0].expand().getItems();
 		items = subitem[0].expand().getItems();
 		subitem = items[3].expand().getItems();
 		subitem[0].doubleClick();
 
+		bot.sleep(2000);
+
+		bot.menu("Window").menu("Navigation").menu("Activate Editor").click();
+		bot.menu("Window").menu("Show View").menu("Other...").click();
+		shell = bot.shell("Show View");
+		shell.activate();
+		bot.tree().expandNode("General").select("Palette");
+		bot.button("OK").click();
+		System.out.println(bot.activeView().getTitle());
+		SWTGefBot gefBot = new SWTGefBot();
+		gefBot.activeEditor().setFocus();
+		SWTBotGefEditor editor = gefBot.gefEditor("WalkThroughAutomaticDoor Class Diagram");
+		editor.activateTool("Class");
+		editor.mouseMoveLeftClick(50, 50);
+		gefBot.text().typeText("Test class");
+
+		bot.menu("Window").menu("Show View").menu("Other...").click();
+		shell1 = bot.shell("Show View");
+		shell1.activate();
+		bot.tree().expandNode("General").select("Properties");
+		bot.button("OK").click();
+
 	}
 
 	@AfterClass
 	public static void sleep() {
-		bot.sleep(20000);
+		bot.sleep(200000);
 	}
 
 }
