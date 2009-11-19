b5e6b2b90090b5f308d522a3125e6defe4ae2cd3
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-11-19 10:23:59 +0000

    Warnings removed

--- core/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/DashboardEditorInput.java
+++ core/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/DashboardEditorInput.java
@@ -105,6 +105,7 @@ public class DashboardEditorInput implements IEditorInput {
 	/**
 	 * {@inheritDoc}
 	 */
+	@SuppressWarnings("unchecked")
 	public Object getAdapter(Class adapter) {
 		return null;
 	}
