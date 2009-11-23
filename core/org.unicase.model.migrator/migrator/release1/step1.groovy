df4b7339fbbbf4afb492667076c945e99708caad
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-11-23 11:35:22 +0000

    Tag 0.3.43.internal UNICASE-beta client release

--- core/org.unicase.model.migrator/migrator/release1/step1.groovy
+++
@@ -1,8 +0,0 @@
-fileAttachmentClass = model.attachment.FileAttachment
-requiredOfflineAttribute = fileAttachmentClass.newEAttribute()
-requiredOfflineAttribute.name = "requiredOffline"
-eBooleanDataType = emf.EBoolean
-requiredOfflineAttribute.eType = eBooleanDataType
-commentClass = model.rationale.Comment
-nonDomainElementClass = model.NonDomainElement
-commentClass.eSuperTypes.add(nonDomainElementClass)
