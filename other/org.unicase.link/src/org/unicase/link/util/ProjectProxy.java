989956ad1cc42da3a0bf770b0810eac55d10914a
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-11-24 18:17:42 +0000

    Class for abstracting details regarding the retrievment of a project

---
+++ other/org.unicase.link/src/org/unicase/link/util/ProjectProxy.java
@@ -0,0 +1,65 @@
+package org.unicase.link.util;
+
+import java.util.Date;
+import java.util.Set;
+
+import org.unicase.emfstore.esmodel.url.ModelElementUrl;
+import org.unicase.emfstore.esmodel.url.ProjectUrlFragment;
+import org.unicase.link.handlers.EMFStoreConnection;
+import org.unicase.metamodel.ModelElement;
+import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.WorkspaceManager;
+import org.unicase.workspace.exceptions.ProjectUrlResolutionException;
+
+public class ProjectProxy {
+	
+	private static ProjectProxy projectProxy = null;
+	private EMFStoreConnection emfStoreConnection;
+	
+	public static ProjectProxy getInstance() {
+		
+		if (projectProxy == null) {
+			projectProxy = new ProjectProxy();
+		}
+		
+		return projectProxy;
+	}
+	
+	private ProjectProxy() {
+		emfStoreConnection = new EMFStoreConnection();
+	}
+
+	/**
+	 * Returns the latest project with the given URL.
+	 * 
+	 * @param projectUrl The URL of the project, for which to find the 
+	 * @return The ProjectSpace in which the project with the given URL
+	 * 		   is embedded. 
+	 */
+	public static ProjectSpace getLatestProjectSpace(ModelElementUrl url) {
+				
+		ProjectSpace currProjectSpace = null;
+				
+		try {
+			Set<ProjectSpace> set = WorkspaceManager.getInstance()
+				.getCurrentWorkspace().resolve(url.getProjectUrlFragment());
+								
+			if (!set.isEmpty()) {
+				currProjectSpace = set.iterator().next();
+						
+				for (ProjectSpace space : set) {
+					Date lastUpdated = space.getLastUpdated();
+					Date currProjectDate = currProjectSpace.getLastUpdated();
+							
+					if (lastUpdated.after(currProjectDate)) {
+						currProjectSpace = space;
+					}
+				}
+			}	
+		} catch (ProjectUrlResolutionException e) {
+			getInstance().emfStoreConnection.checkoutProjectFromServer(url);					
+		} 
+				
+		return currProjectSpace;
+	}
+}
