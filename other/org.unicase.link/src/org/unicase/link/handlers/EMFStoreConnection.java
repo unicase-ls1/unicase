6c95c0e3b8780f8990574fa0c6d73d580d5d2906
nogina.svetlana <nogina.svetlana@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-11-24 14:40:51 +0000

    login and check out from the EMFServer implemented

---
+++ other/org.unicase.link/src/org/unicase/link/handlers/EMFStoreConnection.java
@@ -0,0 +1,109 @@
+package org.unicase.link.handlers;
+
+import java.util.Set;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.ui.PlatformUI;
+import org.unicase.emfstore.esmodel.ProjectInfo;
+
+import org.unicase.ui.common.exceptions.DialogHandler;
+import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.ServerInfo;
+import org.unicase.workspace.Usersession;
+import org.unicase.workspace.WorkspaceManager;
+import org.unicase.workspace.exceptions.ServerUrlResolutionException;
+import org.unicase.workspace.util.WorkspaceUtil;
+import org.unicase.emfstore.esmodel.url.ModelElementUrl;
+
+import org.unicase.emfstore.exceptions.EmfStoreException;
+import org.unicase.workspace.ui.commands.ServerRequestCommandHandler;
+import org.unicase.workspace.ui.dialogs.LoginDialog;
+
+/**
+ * This class allows to connect to the EMFStore 
+ * and check out project.
+ * If the user is not logged in on the server, 
+ * login dialog will be opened
+ * 
+ * @author svetlana
+ *
+ */
+
+public class EMFStoreConnection {
+
+	public void checkout(final ModelElementUrl url){
+	
+	ServerRequestCommandHandler handler = new ServerRequestCommandHandler(){      
+        @Override
+        protected Object run() throws EmfStoreException {
+            checkoutProject(url);
+            return null;
+         
+        }
+     };
+     try {
+ 
+      handler.execute(new ExecutionEvent());
+   
+     } catch (ExecutionException e) {
+        DialogHandler.showErrorDialog(e.getMessage());   
+
+}
+    
+}
+
+	/**
+	 * Check out the project
+	 * @param url - Unicase url of the searched model element 
+	 */
+		
+	public  void checkoutProject(ModelElementUrl url){
+		try {
+			Set<ServerInfo> serverInfos = WorkspaceManager.getInstance()
+			.getCurrentWorkspace().resolve(url.getServerUrl());
+			
+			EList<ProjectInfo> projectInfos = null;
+			
+			for(ServerInfo server: serverInfos){
+				//get projects, stored on the server
+				projectInfos = server.getProjectInfos();
+				
+				if(projectInfos != null){
+					//search the project
+				for(ProjectInfo project: projectInfos){
+					if(project.getProjectId().getId().
+							equals(url.getProjectUrlFragment().getProjectId().getId())){
+												
+						Usersession lastSession = server.getLastUsersession();		
+						
+						if(lastSession == null || !lastSession.isLoggedIn()){
+							LoginDialog loginDialog = new LoginDialog(PlatformUI.getWorkbench()
+									.getActiveWorkbenchWindow().getShell(), lastSession,
+									server);
+							loginDialog.open();
+
+							lastSession = server.getLastUsersession();
+						}
+						ProjectSpace projectSpace = lastSession.checkout(project);
+						
+						WorkspaceUtil.logCheckout(projectSpace, projectSpace
+						.getBaseVersion());
+						
+					}
+				}
+			}
+		}
+
+		} catch (ServerUrlResolutionException e) {
+			WorkspaceUtil.logException(e.getMessage(), e);
+		} catch (EmfStoreException e) {
+			WorkspaceUtil.logException(e.getMessage(), e);
+		}
+		
+	}
+		
+	
+}
