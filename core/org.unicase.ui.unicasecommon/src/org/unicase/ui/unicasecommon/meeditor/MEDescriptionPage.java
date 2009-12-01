09e48434391601f42d72d84a59cb90e3fdbcd719
aleksandar.shterev <aleksandar.shterev@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-12-01 11:55:16 +0000

    added extensions for additional editor pages

--- core/org.unicase.ui.unicasecommon/src/org/unicase/ui/unicasecommon/meeditor/MEDescriptionPage.java
+++ core/org.unicase.ui.unicasecommon/src/org/unicase/ui/unicasecommon/meeditor/MEDescriptionPage.java
@@ -6,7 +6,6 @@
 package org.unicase.ui.unicasecommon.meeditor;
 
 import org.eclipse.emf.edit.domain.EditingDomain;
-import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.layout.GridLayoutFactory;
@@ -17,10 +16,11 @@ import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ScrolledForm;
+import org.unicase.metamodel.ModelElement;
 import org.unicase.model.ModelPackage;
 import org.unicase.model.UnicaseModelElement;
+import org.unicase.ui.meeditor.AbstractMEEditorPage;
 import org.unicase.ui.meeditor.MEEditor;
-import org.unicase.ui.meeditor.mecontrols.AbstractMEControl;
 import org.unicase.ui.meeditor.mecontrols.MERichTextControl;
 import org.unicase.workspace.Configuration;
 
@@ -29,44 +29,70 @@ import org.unicase.workspace.Configuration;
  * 
  * @author shterev
  */
-public class MEDescriptionPage extends FormPage {
+public class MEDescriptionPage extends AbstractMEEditorPage {
+
+	private static final String ID = "org.unicase.ui.unicasecommon.meeditor.descriptionpage";
+	private static final String NAME = "Description";
 
 	private UnicaseModelElement modelElement;
 	private FormToolkit toolkit;
 
 	private ScrolledForm form;
 	private Composite body;
-	private AbstractMEControl textControl;
-
-	/**
-	 * Default constructor.
-	 * 
-	 * @param editor the {@link MEEditor}
-	 * @param id the {@link FormPage#id}
-	 * @param title the title
-	 * @param editingDomain the editingDomain
-	 * @param modelElement the modelElement
-	 */
-	public MEDescriptionPage(MEEditor editor, String id, String title, EditingDomain editingDomain,
-		UnicaseModelElement modelElement) {
-		super(editor, id, title);
-		this.modelElement = modelElement;
-	}
+	private MERichTextControl textControl;
+	private Control textWidget;
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	protected void createFormContent(IManagedForm managedForm) {
-		super.createFormContent(managedForm);
+	public void init(MEEditor editor, EditingDomain editingDomain, ModelElement modelElement) {
+		if (modelElement instanceof UnicaseModelElement) {
+			this.modelElement = (UnicaseModelElement) modelElement;
+		} else {
+			throw new IllegalArgumentException("This page is valid only for UnicaseModelElements");
+		}
 
-		toolkit = this.getEditor().getToolkit();
-		form = managedForm.getForm();
-		toolkit.decorateFormHeading(form.getForm());
-		GridLayoutFactory.fillDefaults().spacing(0, 0).applyTo(form.getBody());
-		form.setText(getEditor().getTitle() + ": Description");
-		createWidget();
-		form.pack();
+		FormPage page = new FormPage(editor, ID, NAME) {
+
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			protected void createFormContent(IManagedForm managedForm) {
+				super.createFormContent(managedForm);
+
+				toolkit = this.getEditor().getToolkit();
+				form = managedForm.getForm();
+				toolkit.decorateFormHeading(form.getForm());
+				GridLayoutFactory.fillDefaults().spacing(0, 0).applyTo(form.getBody());
+				form.setText(getEditor().getTitle() + ": Description");
+				createWidget();
+				form.pack();
+			}
+
+			/**
+			 * {@inheritDoc}
+			 */
+			@Override
+			public void dispose() {
+				if (textControl != null) {
+					textControl.dispose();
+				}
+				super.dispose();
+			}
+
+			/**
+			 * {@inheritDoc} This method is added to solve the focus bug of navigator. Every time that a ME is opened in
+			 * editor, navigator has to lose focus so that its action contributions are set correctly for next time.
+			 */
+			@Override
+			public void setFocus() {
+				textWidget.setFocus();
+			}
+		};
+		setPage(page);
+
 	}
 
 	private void createWidget() {
@@ -79,30 +105,10 @@ public class MEDescriptionPage extends FormPage {
 
 		TransactionalEditingDomain domain = Configuration.getEditingDomain();
 		textControl = new MERichTextControl();
-		textControl.init((IItemPropertyDescriptor) ModelPackage.eINSTANCE.getUnicaseModelElement_Description(),
-			modelElement, domain, toolkit);
-		((MERichTextControl) textControl).setShowExpand(false);
-		Control textWidget = textControl.createControl(body, SWT.NONE);
+		textControl.init(ModelPackage.eINSTANCE.getUnicaseModelElement_Description(), modelElement, domain, toolkit);
+		(textControl).setShowExpand(false);
+		textWidget = textControl.createControl(body, SWT.NONE);
 		GridDataFactory.fillDefaults().hint(200, -1).grab(true, true).applyTo(textWidget);
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void dispose() {
-		if (textControl != null) {
-			textControl.dispose();
-		}
-		super.dispose();
-	}
-
-	/**
-	 * {@inheritDoc} This method is added to solve the focus bug of navigator. Every time that a ME is opened in editor,
-	 * navigator has to lose focus so that its action contributions are set correctly for next time.
-	 */
-	@Override
-	public void setFocus() {
-		super.setFocus();
-	}
 }
