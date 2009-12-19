ff53ee8b255c1a3123e610b899ce3892e913b1c2
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-12-19 21:38:18 +0000

    improved association label

--- core/org.unicase.model.edit/src/org/unicase/model/classes/provider/AssociationItemProvider.java
+++ core/org.unicase.model.edit/src/org/unicase/model/classes/provider/AssociationItemProvider.java
@@ -201,7 +201,7 @@ public class AssociationItemProvider extends UnicaseModelElementItemProvider imp
 	/**
 	 * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
 	 * 
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
