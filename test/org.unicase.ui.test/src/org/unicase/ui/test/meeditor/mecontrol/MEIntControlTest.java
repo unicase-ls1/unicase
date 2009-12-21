c2ee2ce26c9b9e38989430647bf2ec56cfa178c8
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-12-21 16:25:32 +0000

    MEInt Control

--- test/org.unicase.ui.test/src/org/unicase/ui/test/meeditor/mecontrol/MEIntControlTest.java
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/meeditor/mecontrol/MEIntControlTest.java
@@ -33,6 +33,62 @@ public class MEIntControlTest extends MeControlTest {
 	} 
 	
 	
+	
+	
+	@Test
+	public void testDefaultPriority() {
+		openModelElement(actionItem);
+		final int temporary = getBot().activeEditor().bot().spinnerWithLabel("Priority").getSelection();
+		UnicaseCommand unicaseCommand = new UnicaseCommand() {
+			
+			@Override
+			protected void doRun() {
+				
+				assertEquals(temporary, actionItem.getPriority());
+			}
+		};runAsnc(unicaseCommand);
+		
+	}
+	
+
+	@Test
+	public void testUpdateNeagtivePriority() {
+		openModelElement(actionItem);
+		final int selection = -999;
+		UnicaseCommand unicaseCommand = new UnicaseCommand() {
+			
+			@Override
+			protected void doRun() {
+				
+				actionItem.setPriority(selection);
+			}
+		};
+		runAsnc(unicaseCommand);
+		
+		getBot().sleep(2000);
+		int temporary = getBot().activeEditor().bot().spinnerWithLabel("Priority").getSelection();
+		assertEquals(selection, temporary);
+		
+	}
+	
+	@Test
+	public void testSetNegativePriority() {
+		openModelElement(actionItem);
+		final int selection = -1000;
+		getBot().activeEditor().bot().spinnerWithLabel("Priority").setSelection(selection);
+		getBot().sleep(2000);
+		
+		new UnicaseCommand() {
+			@Override
+			protected void doRun() {
+				assertEquals(selection, actionItem.getPriority());
+			}
+		}.run();
+	}
+	
+	
+	
+	
 	@Test
 	public void testUpdatePriorityOfActionItem() {
 		openModelElement(actionItem);
