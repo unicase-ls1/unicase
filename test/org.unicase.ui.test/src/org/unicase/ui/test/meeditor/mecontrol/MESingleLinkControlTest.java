36c967ad63cc46a1030d167dbbe10506c887d947
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-12-21 15:50:55 +0000

    SingleLink control

--- test/org.unicase.ui.test/src/org/unicase/ui/test/meeditor/mecontrol/MESingleLinkControlTest.java
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/meeditor/mecontrol/MESingleLinkControlTest.java
@@ -13,15 +13,20 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.eclipse.core.runtime.AssertionFailedException;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.forms.widgets.Hyperlink;
 import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.unicase.metamodel.Project;
+import org.unicase.model.organization.OrgUnit;
 import org.unicase.model.organization.OrganizationFactory;
 import org.unicase.model.organization.User;
 import org.unicase.model.task.ActionItem;
 import org.unicase.model.task.TaskFactory;
+import org.unicase.model.task.WorkPackage;
 import org.unicase.workspace.util.UnicaseCommand;
 import org.unicase.workspace.util.UnicaseCommandWithResult;
 
@@ -46,8 +51,154 @@ private User user;
 		}.run();
 	} 
 	
-	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testReassigning()  {
+		
+		new UnicaseCommand() {
+			
+			@Override
+			protected void doRun() {
+				OrgUnit user = OrganizationFactory.eINSTANCE.createUser();
+				user.setName("UNICASE user");
+				actionItem.setAssignee(user);
+				}
+		}.run();
+		openModelElement(actionItem);
 
+		getBot().activeEditor().bot().buttonWithLabel("Assignee",0).click(); 
+		getBot().text().setText("Joker");
+		getBot().button("OK").click();
+		
+		UnicaseCommand userfinder = new UnicaseCommand() {
+			
+			
+			@Override
+			protected void doRun() {
+				Matcher match = allOf(widgetOfType(Hyperlink.class), withRegex("Joker"));
+				final List label = getBot().getFinder().findControls(match);
+				String text = ((Hyperlink)label.get(0)).getText();
+				assertEquals("Joker", text);
+				assertEquals("Joker", actionItem.getAssignee());
+			}
+		}; runAsnc(userfinder);
+		
+			
+	}
+	
+	@Test
+	public void testActionItemWorkPackageSet() {
+		
+		openModelElement(actionItem);
+		new UnicaseCommand() {
+			
+			@Override
+			protected void doRun() {
+				Project project = actionItem.getProject();
+				 WorkPackage workpackage = TaskFactory.eINSTANCE.createWorkPackage();
+				 workpackage.setName("Sprint1");
+				 project.addModelElement(workpackage);
+				
+			}
+		}.run();
+		
+		getBot().activeEditor().bot().buttonWithLabel("Containing Workpackage", 0).click();
+		getBot().text().setText("Sprint1");
+		getBot().button("OK").click();
+		getBot().sleep(5000);
+		
+		new UnicaseCommand() {
+			
+			@Override
+			protected void doRun() {
+				assertEquals("Sprint1", actionItem.getContainingWorkpackage().getName());
+				
+			}
+		}.run();
+		
+	}
+
+	@Test
+	public void testActionItemWorkPackageUnSet() {
+			new UnicaseCommand() {
+			@Override
+			protected void doRun() {
+				Project project = actionItem.getProject();
+				 WorkPackage workpackage = TaskFactory.eINSTANCE.createWorkPackage();
+				 workpackage.setName("Sprint1");
+				 project.addModelElement(workpackage);
+				 actionItem.setContainingWorkpackage(workpackage);
+			}
+		}.run();
+		openModelElement(actionItem);
+		UnicaseCommand packageLinkFinder = new UnicaseCommand() {
+			
+			@SuppressWarnings("unchecked")
+			@Override
+			protected void doRun() {
+			Matcher match = allOf(widgetOfType(Hyperlink.class), withRegex("Sprint1"));
+			final List packageLink = getBot().getFinder().findControls(match);
+			String link = ((Hyperlink)packageLink.get(0)).getText();
+			assertEquals("Sprint1", link);
+			getBot().sleep(2000);
+			actionItem.setContainingWorkpackage(null);
+			}
+		}; runAsnc(packageLinkFinder);
+ 		
+		UnicaseCommand workpackageNullCommand = new UnicaseCommand() {
+			
+			@SuppressWarnings("unchecked")
+			@Override
+			protected void doRun() {
+				Matcher match  = allOf(widgetOfType(Label.class), withRegex("(Not Set)"));
+				List list = getBot().getFinder().findControls(match);
+			assertEquals(4, list.size());
+				
+			}
+		}; runAsnc(workpackageNullCommand);
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testAssigneeDefault()  {
+		
+		openModelElement(actionItem);
+		
+		UnicaseCommandWithResult<Matcher> labelFinderCommand = new UnicaseCommandWithResult<Matcher>() {
+			@Override
+			protected Matcher doRun() {
+				Matcher matchlabel = allOf(widgetOfType(Label.class), withRegex("(Not Set)"));
+				return matchlabel;
+			}
+		};
+		
+		Matcher labelmatcher = runAsnc(labelFinderCommand);
+		final List labelcontrols = getBot().getFinder().findControls(labelmatcher);
+		
+	
+	new UnicaseCommand() {
+			
+			@Override
+			protected void doRun() {
+				final Label label = ((Label)labelcontrols.get(1));
+				Display display = label.getDisplay();
+			
+				display.syncExec(
+					  new Runnable() {
+					    public void run(){
+					    String text = label.getText();
+					    assertEquals("(Not Set)",text);
+					    OrgUnit user = actionItem.getAssignee();
+						assertEquals(null, user);
+					    }
+					  });
+			
+					}
+		}.run();
+	}
+
+	
 	@Test
 	public void testAssigneeChange() {
 		
@@ -95,4 +246,8 @@ private User user;
 	}
 	
 	
+	
+	
+	
+
 }
