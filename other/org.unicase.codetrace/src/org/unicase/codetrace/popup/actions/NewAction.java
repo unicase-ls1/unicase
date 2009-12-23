86769f6dae0c71212b7e8e49167c849c2f1352b2
nogina.svetlana <nogina.svetlana@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2009-12-23 00:51:25 +0000

    taskmarker with an image provider created. menu item in the vertical ruler added, that creates a taskmarker at the given line

--- other/org.unicase.codetrace/src/org/unicase/codetrace/popup/actions/NewAction.java
+++ other/org.unicase.codetrace/src/org/unicase/codetrace/popup/actions/NewAction.java
@@ -1,16 +1,20 @@
 package org.unicase.codetrace.popup.actions;
 
+import java.util.HashMap;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IObjectActionDelegate;
-import org.eclipse.ui.IViewActionDelegate;
+
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IWorkbenchPart;
-import org.eclipse.ui.editors.text.TextEditor;
+
+import org.eclipse.ui.ide.ResourceUtil;
+import org.eclipse.ui.texteditor.MarkerUtilities;
+
 
 public class NewAction implements IEditorActionDelegate  {
 
@@ -22,6 +26,7 @@ public class NewAction implements IEditorActionDelegate  {
 	public void run(IAction action) {
 		System.out.println("bla");
 		
+		
 	}
 
 	/**
@@ -32,13 +37,27 @@ public class NewAction implements IEditorActionDelegate  {
 
 	public void init(IViewPart view) {
 		// TODO Auto-generated method stub
-		System.out.println("bla");
+		System.out.println("bla");	
 	}
 
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
 		// TODO Auto-generated method stub
 		System.out.println("bla");
-		((TextEditor)targetEditor).getDocumentProvider().getDocument(null);
+		IEditorInput editorInput = targetEditor.getEditorInput();
+		IResource resource = ResourceUtil.getResource(editorInput);
+		HashMap attributes = new HashMap();
+		int lineNumber = 2;
+		//TODO how to get the line number from the action?
+		MarkerUtilities.setLineNumber(attributes, lineNumber);
+	
+		try {
+			MarkerUtilities.createMarker(resource, attributes, "org.unicase.taskmarker");
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//((TextEditor)targetEditor).getDocumentProvider().getDocument(null);
 	}
 
 }
