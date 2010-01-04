0978ba0c72ff0c2f8b7334399aa0bcc6bdc564d1
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-04 12:34:40 +0000

    finished work on protocol handler for windows

--- other/org.unicase.link/src/org/unicase/link/preferences/protocolhandlers/WindowsRegisterProtocolHandler.java
+++ other/org.unicase.link/src/org/unicase/link/preferences/protocolhandlers/WindowsRegisterProtocolHandler.java
@@ -19,7 +19,7 @@ public class WindowsRegisterProtocolHandler extends AbstractRegisterProtocolHand
 			Runtime.getRuntime().exec("reg add HKCR\\unicase /ve /d \"URL:unicase Protocol\"");
 			Runtime.getRuntime().exec("reg add HKCR\\unicase /v \"URL Protocol\"");
 			Runtime.getRuntime().exec(
-				"reg add HKCR\\unicase\\Shell\\Open\\Command /ve /d " + getJavaExecutionCmd() + " %%1");
+				"reg add HKCR\\unicase\\Shell\\Open\\Command /ve /d \"" + getJavaExecutionCmd() + " %1\"");
 		} catch (IOException e) {
 			showError(e.getMessage());
 		}
