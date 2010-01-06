c531ff2716b34236fb0c788ff9631debafc61e1e
nogina.svetlana <nogina.svetlana@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-06 15:06:28 +0000

    marker creation moved to the run method line number detection implemented

--- other/org.unicase.codetrace/src/org/unicase/codetrace/popup/actions/NewAction.java
+++ other/org.unicase.codetrace/src/org/unicase/codetrace/popup/actions/NewAction.java
@@ -1,31 +1,46 @@
 package org.unicase.codetrace.popup.actions;
 
 import java.util.HashMap;
-
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-
 import org.eclipse.ui.IViewPart;
-
 import org.eclipse.ui.ide.ResourceUtil;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 import org.eclipse.ui.texteditor.MarkerUtilities;
 
 
 public class NewAction implements IEditorActionDelegate  {
 
-
+	private IEditorPart editorPart = null; 
 
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		System.out.println("bla");
 		
+		IEditorInput editorInput = editorPart.getEditorInput();
+		ISelection selection = ((AbstractTextEditor) editorPart).getSelectionProvider().getSelection();
+		
+		int lineNumber = ((ITextSelection) selection).getStartLine();
+		
+		IResource resource = ResourceUtil.getResource(editorInput);
+		HashMap attributes = new HashMap();
+		
+		
+		MarkerUtilities.setLineNumber(attributes, lineNumber + 1);
+	
+		try {
+			MarkerUtilities.createMarker(resource, attributes, "org.unicase.taskmarker");
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -41,21 +56,11 @@ public class NewAction implements IEditorActionDelegate  {
 	}
 
 	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		this.editorPart = targetEditor;
+		
 		// TODO Auto-generated method stub
 		System.out.println("bla");
-		IEditorInput editorInput = targetEditor.getEditorInput();
-		IResource resource = ResourceUtil.getResource(editorInput);
-		HashMap attributes = new HashMap();
-		int lineNumber = 2;
-		//TODO how to get the line number from the action?
-		MarkerUtilities.setLineNumber(attributes, lineNumber);
 	
-		try {
-			MarkerUtilities.createMarker(resource, attributes, "org.unicase.taskmarker");
-		} catch (CoreException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		//((TextEditor)targetEditor).getDocumentProvider().getDocument(null);
 	}
