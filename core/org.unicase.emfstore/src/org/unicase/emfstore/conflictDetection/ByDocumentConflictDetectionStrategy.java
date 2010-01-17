e6e942a82ad0cad3dc13123423ed725a6ce03743
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-17 01:07:33 +0000

    added three new conflict detection strategies

---
+++ core/org.unicase.emfstore/src/org/unicase/emfstore/conflictDetection/ByDocumentConflictDetectionStrategy.java
@@ -0,0 +1,77 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.emfstore.conflictDetection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
+import org.unicase.metamodel.ModelElement;
+import org.unicase.metamodel.ModelElementId;
+import org.unicase.metamodel.Project;
+
+/**
+ * Detects conflicts by documents.
+ * 
+ * @author koegel
+ */
+public class ByDocumentConflictDetectionStrategy implements ConflictDetectionStrategy {
+
+	private Project project;
+
+	/**
+	 * Set the project that is currently valid for the detection of the conflicts.
+	 * 
+	 * @param project the current project
+	 * @return
+	 */
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.emfstore.conflictDetection.ConflictDetectionStrategy#doConflict(org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation,
+	 *      org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation)
+	 */
+	public boolean doConflict(AbstractOperation operationA, AbstractOperation operationB) {
+		Set<ModelElementId> allInvolvedModelElementsA = operationA.getAllInvolvedModelElements();
+		Set<ModelElementId> allInvolvedModelElementsB = operationB.getAllInvolvedModelElements();
+		Set<ModelElement> allInvolvedRootElementsA = new HashSet<ModelElement>();
+		Set<ModelElement> allInvolvedRootElementsB = new HashSet<ModelElement>();
+		for (ModelElementId modelElementId : allInvolvedModelElementsA) {
+			ModelElement modelElement = project.getModelElement(modelElementId);
+			allInvolvedRootElementsA.add(getRootLevelParent(modelElement));
+		}
+		for (ModelElementId modelElementId : allInvolvedModelElementsB) {
+			ModelElement modelElement = project.getModelElement(modelElementId);
+			allInvolvedRootElementsB.add(getRootLevelParent(modelElement));
+		}
+		return allInvolvedRootElementsA.removeAll(allInvolvedRootElementsB);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.emfstore.conflictDetection.ConflictDetectionStrategy#isRequired(org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation,
+	 *      org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation)
+	 */
+	public boolean isRequired(AbstractOperation requiredOperation, AbstractOperation operation) {
+		return this.doConflict(requiredOperation, operation);
+	}
+
+	private ModelElement getRootLevelParent(ModelElement modelElement) {
+		ModelElement parent = modelElement;
+		ModelElement nextParent = modelElement.getContainerModelElement();
+		while (nextParent != null) {
+			parent = nextParent;
+			nextParent = nextParent.getContainerModelElement();
+		}
+		return parent;
+	}
+
+}
