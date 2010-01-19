04e5d2aa8f06ad58db799db9164bdb94ebb805b0
flo.schneider <flo.schneider@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-19 17:41:36 +0000

    applied unicase formatting to UserComposite and UserTabContent

--- core/org.unicase.workspace.ui/src/org/unicase/workspace/ui/views/emfstorebrowser/dialogs/admin/UserTabContent.java
+++ core/org.unicase.workspace.ui/src/org/unicase/workspace/ui/views/emfstorebrowser/dialogs/admin/UserTabContent.java
@@ -35,14 +35,12 @@ import org.unicase.workspace.ui.Activator;
 /**
  * @author gurcankarakoc, deser
  */
-public class UserTabContent extends TabContent implements
-		IPropertyChangeListener {
+public class UserTabContent extends TabContent implements IPropertyChangeListener {
 
 	/**
 	 * Action to delete a user.
 	 * 
 	 * @author koegel
-	 * 
 	 */
 	private final class DeleteUserAction extends Action {
 		private DeleteUserAction(String text) {
@@ -51,8 +49,7 @@ public class UserTabContent extends TabContent implements
 
 		@Override
 		public void run() {
-			IStructuredSelection selection = (IStructuredSelection) getTableViewer()
-					.getSelection();
+			IStructuredSelection selection = (IStructuredSelection) getTableViewer().getSelection();
 			Iterator<?> iterator = selection.iterator();
 
 			while (iterator.hasNext()) {
@@ -67,8 +64,7 @@ public class UserTabContent extends TabContent implements
 					DialogHandler.showExceptionDialog(e);
 				}
 
-				if (getForm().getCurrentInput() instanceof ACOrgUnit
-						&& getForm().getCurrentInput().equals(ou)) {
+				if (getForm().getCurrentInput() instanceof ACOrgUnit && getForm().getCurrentInput().equals(ou)) {
 					getForm().setInput(null);
 				}
 			}
@@ -77,16 +73,11 @@ public class UserTabContent extends TabContent implements
 	}
 
 	/**
-	 * @param string
-	 *            the name of tab.
-	 * @param adminBroker
-	 *            AdminBroker is needed to communicate with server.
-	 * @param frm
-	 *            used to set input to properties form and update its table
-	 *            viewer upon. deletion of OrgUnits.
+	 * @param string the name of tab.
+	 * @param adminBroker AdminBroker is needed to communicate with server.
+	 * @param frm used to set input to properties form and update its table viewer upon. deletion of OrgUnits.
 	 */
-	public UserTabContent(String string, AdminBroker adminBroker,
-			PropertiesForm frm) {
+	public UserTabContent(String string, AdminBroker adminBroker, PropertiesForm frm) {
 		super(string, adminBroker, frm);
 		this.setTab(this);
 	}
@@ -94,8 +85,7 @@ public class UserTabContent extends TabContent implements
 	/**
 	 * @see org.unicase.ui.esbrowser.dialogs.admin.TabContent#createContents(org.eclipse.swt.widgets.TabFolder)
 	 * @return Composite.
-	 * @param tabFolder
-	 *            TabFolder.
+	 * @param tabFolder TabFolder.
 	 */
 	@Override
 	protected Composite createContents(TabFolder tabFolder) {
@@ -125,14 +115,12 @@ public class UserTabContent extends TabContent implements
 
 		};
 
-		createNewUser.setImageDescriptor(Activator
-				.getImageDescriptor("icons/user.png"));
+		createNewUser.setImageDescriptor(Activator.getImageDescriptor("icons/user.png"));
 		createNewUser.setToolTipText("Create new user");
 
 		Action deleteUser = new DeleteUserAction("Delete user");
 
-		deleteUser.setImageDescriptor(Activator
-				.getImageDescriptor("icons/delete.gif"));
+		deleteUser.setImageDescriptor(Activator.getImageDescriptor("icons/delete.gif"));
 		deleteUser.setToolTipText("Delete user");
 
 		Action importOrgUnit = new AcUserImportAction(getAdminBroker());
@@ -169,8 +157,7 @@ public class UserTabContent extends TabContent implements
 			}
 
 			public Image getColumnImage(Object element, int columnIndex) {
-				return Activator.getImageDescriptor("icons/user.png")
-						.createImage();
+				return Activator.getImageDescriptor("icons/user.png").createImage();
 			}
 
 			public String getColumnText(Object element, int columnIndex) {
@@ -201,18 +188,15 @@ public class UserTabContent extends TabContent implements
 			public void dispose() {
 			}
 
-			public void inputChanged(Viewer viewer, Object oldInput,
-					Object newInput) {
+			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 			}
 		};
 	}
 
 	/**
-	 * Refresh the tableViewer after a property change. (Used e.g. after
-	 * importing users via e.g. CSV.)
+	 * Refresh the tableViewer after a property change. (Used e.g. after importing users via e.g. CSV.)
 	 * 
-	 * @param event
-	 *            The event to deal with.
+	 * @param event The event to deal with.
 	 */
 	public void propertyChange(PropertyChangeEvent event) {
 		getTableViewer().refresh();
