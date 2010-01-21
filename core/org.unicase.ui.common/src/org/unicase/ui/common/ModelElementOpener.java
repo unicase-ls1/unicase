c2863e28ea33283d81fc5fa61201577ea9fdad0a
yasmine8726 <yasmine8726@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-21 15:37:52 +0000

    branch for analyzer refactoring

--- core/org.unicase.ui.common/src/org/unicase/ui/common/ModelElementOpener.java
+++
@@ -1,8 +0,0 @@
-package org.unicase.ui.common;
-
-import org.unicase.metamodel.ModelElement;
-
-public interface ModelElementOpener {
-	public int canOpen(ModelElement modelElement);
-	public void openModelElement(ModelElement modelElement);
-}
--- core/org.unicase.ui.unicasecommon/src/org/unicase/ui/unicasecommon/diagram/figures/Snippet.java
+++
@@ -1,6 +0,0 @@
-package org.unicase.ui.unicasecommon.diagram.figures;
-
-public class Snippet {
-	public static void main(String[] args) {
-	}
-}
