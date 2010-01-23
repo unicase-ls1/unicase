4732ba8af8df448fa4b0f74a53c766814222210a
helming@in.tum.de <helming@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-23 10:27:15 +0000

    added documentation

--- core/org.unicase.ui.common/src/org/unicase/ui/common/ModelElementOpener.java
+++ core/org.unicase.ui.common/src/org/unicase/ui/common/ModelElementOpener.java
@@ -2,7 +2,26 @@ package org.unicase.ui.common;
 
 import org.unicase.metamodel.ModelElement;
 
+/**
+ * Modelelement opener offer the functionality to open a specific model element. Standard opener is the model element
+ * editor. Example for specific opener are the diagrams.
+ * 
+ * @author helming
+ */
 public interface ModelElementOpener {
+	/**
+	 * Checks whether the model element should be opened by this opener, depending on the priority. The model element
+	 * will be opened with the registered opener with the highest priority.
+	 * 
+	 * @param modelElement the model element to check
+	 * @return
+	 */
 	public int canOpen(ModelElement modelElement);
+
+	/**
+	 * The action to open the model element.
+	 * 
+	 * @param modelElement the model element to open
+	 */
 	public void openModelElement(ModelElement modelElement);
 }
