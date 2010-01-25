4bef24de69f8505ba029754c28b0e9b2fce54709
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-25 22:24:41 +0000

    - updated linux script to check whether url handler has been registered - new version of startup jar

--- other/org.unicase.link_feature/lib/registerUnicaseProtocolHandler.sh
+++ other/org.unicase.link_feature/lib/registerUnicaseProtocolHandler.sh
@@ -1,4 +1,36 @@
 #! /bin/sh
-gconftool-2 -t string -s /desktop/gnome/url-handlers/unicase/command "$1"
-gconftool-2 -t bool -s /desktop/gnome/url-handlers/unicase/enabled true
-gconftool-2 -t bool -s /desktop/gnome/url-handlers/unicase/needs_terminal false 
+
+# for unsetting use: gconftool-2 -u <id>
+
+enabled=$(gconftool-2 -g /desktop/gnome/url-handlers/unicase/enabled)
+needs_terminal=$(gconftool-2 -g /desktop/gnome/url-handlers/unicase/needs_terminal)
+command=$(gconftool-2 -g /desktop/gnome/url-handlers/unicase/command)
+
+registerURLHandler() {
+	gconftool-2 -t string -s /desktop/gnome/url-handlers/unicase/command "$1 %s $2"
+	gconftool-2 -t bool -s /desktop/gnome/url-handlers/unicase/enabled true
+	gconftool-2 -t bool -s /desktop/gnome/url-handlers/unicase/needs_terminal false
+}
+
+checkURLHandler() {
+	if [ "$enabled" != "true" ]
+ 		then exit 1
+	fi;
+
+	if [ "$needs_terminal" != "false" ]
+ 		then exit 2
+	fi;
+
+	if [ "$command" = "" ]
+ 		then exit 3
+	fi;
+	
+	exit 0
+}
+
+if [ "$1" = "check" ] 
+	then checkURLHandler
+elif [ "$1" = "register" ]
+	then registerURLHandler "$2" "$3"
+fi
+	 
