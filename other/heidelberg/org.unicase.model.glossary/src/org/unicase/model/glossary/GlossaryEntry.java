d6a7e124c056498ba93399968594dfbf3e545384
adelater <adelater@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-25 13:05:27 +0000

    Initial Import for new Glossary Plugin

---
+++ other/heidelberg/org.unicase.model.glossary/src/org/unicase/model/glossary/GlossaryEntry.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.unicase.model.glossary;
+
+import org.unicase.model.UnicaseModelElement;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Entry</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.unicase.model.glossary.GlossaryPackage#getGlossaryEntry()
+ * @model
+ * @generated
+ */
+public interface GlossaryEntry extends UnicaseModelElement {
+} // GlossaryEntry
