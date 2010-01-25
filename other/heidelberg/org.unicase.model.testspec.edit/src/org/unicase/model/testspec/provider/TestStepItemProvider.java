9ea307a6dfc8cdad164d19eb5dac10a226ea9d62
adelater <adelater@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-01-25 13:10:49 +0000

    Initial Import for new Test Specification Plugin (edit)

---
+++ other/heidelberg/org.unicase.model.testspec.edit/src/org/unicase/model/testspec/provider/TestStepItemProvider.java
@@ -0,0 +1,207 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.unicase.model.testspec.provider;
+
+
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.common.util.ResourceLocator;
+
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+
+import org.unicase.model.provider.UnicaseModelElementItemProvider;
+
+import org.unicase.model.testspec.TestStep;
+import org.unicase.model.testspec.TestspecPackage;
+
+/**
+ * This is the item provider adapter for a {@link org.unicase.model.testspec.TestStep} object.
+ * <!-- begin-user-doc -->
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class TestStepItemProvider
+	extends UnicaseModelElementItemProvider
+	implements
+		IEditingDomainItemProvider,
+		IStructuredItemContentProvider,
+		ITreeItemContentProvider,
+		IItemLabelProvider,
+		IItemPropertySource {
+	/**
+     * This constructs an instance from a factory and a notifier.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	public TestStepItemProvider(AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
+
+	/**
+     * This returns the property descriptors for the adapted class.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+        if (itemPropertyDescriptors == null) {
+            super.getPropertyDescriptors(object);
+
+            addExceptionPropertyDescriptor(object);
+            addInputPropertyDescriptor(object);
+            addOutputPropertyDescriptor(object);
+        }
+        return itemPropertyDescriptors;
+    }
+
+	/**
+	 * This adds a property descriptor for the Exception feature.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	protected void addExceptionPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add
+			(createItemPropertyDescriptor
+				(((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+				 getResourceLocator(),
+				 getString("_UI_TestStep_exception_feature"),
+				 getString("_UI_PropertyDescriptor_description", "_UI_TestStep_exception_feature", "_UI_TestStep_type"),
+				 TestspecPackage.Literals.TEST_STEP__EXCEPTION,
+				 true,
+				 true,
+				 false,
+				 ItemPropertyDescriptor.GENERIC_VALUE_IMAGE,
+				 null,
+				 null));
+	}
+
+	/**
+     * This adds a property descriptor for the Input feature.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	protected void addInputPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add
+            (createItemPropertyDescriptor
+                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                 getResourceLocator(),
+                 getString("_UI_TestStep_input_feature"),
+                 getString("_UI_PropertyDescriptor_description", "_UI_TestStep_input_feature", "_UI_TestStep_type"),
+                 TestspecPackage.Literals.TEST_STEP__INPUT,
+                 true,
+                 false,
+                 false,
+                 null,
+                 null,
+                 null));
+    }
+
+	/**
+     * This adds a property descriptor for the Output feature.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	protected void addOutputPropertyDescriptor(Object object) {
+        itemPropertyDescriptors.add
+            (createItemPropertyDescriptor
+                (((ComposeableAdapterFactory)adapterFactory).getRootAdapterFactory(),
+                 getResourceLocator(),
+                 getString("_UI_TestStep_output_feature"),
+                 getString("_UI_PropertyDescriptor_description", "_UI_TestStep_output_feature", "_UI_TestStep_type"),
+                 TestspecPackage.Literals.TEST_STEP__OUTPUT,
+                 true,
+                 false,
+                 false,
+                 null,
+                 null,
+                 null));
+    }
+
+	/**
+     * This returns TestStep.gif.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	@Override
+	public Object getImage(Object object) {
+        return overlayImage(object, getResourceLocator().getImage("full/obj16/TestStep"));
+    }
+
+	/**
+	 * This returns the label text for the adapted class.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT
+	 */
+	@Override
+	public String getText(Object object) {
+	    return super.getText(object);
+	}
+
+	/**
+     * This handles model notifications by calling {@link #updateChildren} to update any cached
+     * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	@Override
+	public void notifyChanged(Notification notification) {
+        updateChildren(notification);
+
+        switch (notification.getFeatureID(TestStep.class)) {
+            case TestspecPackage.TEST_STEP__EXCEPTION:
+            case TestspecPackage.TEST_STEP__INPUT:
+            case TestspecPackage.TEST_STEP__OUTPUT:
+                fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+                return;
+        }
+        super.notifyChanged(notification);
+    }
+
+	/**
+     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
+     * that can be created under this object.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+        super.collectNewChildDescriptors(newChildDescriptors, object);
+    }
+
+	/**
+     * Return the resource locator for this item provider's resources.
+     * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+     * @generated
+     */
+	@Override
+	public ResourceLocator getResourceLocator() {
+        return TestspecEditPlugin.INSTANCE;
+    }
+
+}
