dd0efc2cf716e04d7ed57755dba8296418a47312
kameliya.terzieva <kameliya.terzieva@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-02-02 18:45:43 +0000

    cleaned code

--- other/org.unicase.trace/src/org/unicase/codetrace/ui/AttachCodeLocation.java
+++ other/org.unicase.trace/src/org/unicase/codetrace/ui/AttachCodeLocation.java
@@ -1,12 +1,16 @@
 package org.unicase.codetrace.ui;
 
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * Search active project and give all his model elements in user dialog
+ * @author kterzieva
+ * @author snogina
+ * @author jfinis
+ * 
+ *
+ */
 
 import org.eclipse.jface.window.Window;
-import org.unicase.metamodel.ModelElement;
 import org.unicase.metamodel.Project;
-import org.unicase.metamodel.provider.ShortLabelProvider;
 import org.unicase.model.UnicaseModelElement;
 import org.unicase.workspace.ProjectSpace;
 import org.unicase.workspace.WorkspaceManager;
@@ -26,8 +30,6 @@ public class AttachCodeLocation {
 	}
 
 	public static UnicaseModelElement showUserDialog() {
-
-		//ShortLabelProvider shortLabelProvider = new ShortLabelProvider();
 		ChooseModelElementDialog cmed = new ChooseModelElementDialog(
 				getActiveProject(), "Choose Model Element for code location!");
 		if (cmed.open() == Window.OK) {
