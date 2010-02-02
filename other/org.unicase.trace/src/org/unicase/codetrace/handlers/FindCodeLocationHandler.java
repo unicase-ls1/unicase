342437cce20abc2ac15bf6daa4a60bcf760e4033
nogina.svetlana <nogina.svetlana@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-02-02 21:49:12 +0000

    menu item "Get to code location" in the Navigator context menu added. The handler implemented.

---
+++ other/org.unicase.trace/src/org/unicase/codetrace/handlers/FindCodeLocationHandler.java
@@ -0,0 +1,58 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.codetrace.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.unicase.codetrace.tracer.FoundLocation;
+import org.unicase.codetrace.tracer.LocationFinder;
+import org.unicase.codetrace.ui.TaskLocationLoader;
+import org.unicase.metamodel.ModelElement;
+import org.unicase.model.trace.CodeLocation;
+import org.unicase.ui.common.util.ActionHelper;
+
+/**
+ * This handler changes current perspective to the java perspective 
+ * and shows the code location in the editor. 
+ * @author snogina
+ *
+ */
+public class FindCodeLocationHandler extends AbstractHandler {
+
+	/***
+	 * . ({@inheritDoc})
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+
+		ModelElement me = ActionHelper.getModelElement(event);
+		if (me instanceof CodeLocation) {
+			LocationFinder finder = LocationFinder.getInstance();
+			FoundLocation location = finder.find((CodeLocation)me);
+			
+			//change current perspective to the java perspective
+			IWorkbench workbench = PlatformUI.getWorkbench();
+			try {
+				workbench.showPerspective("org.eclipse.jdt.ui.JavaPerspective", 
+				workbench.getActiveWorkbenchWindow());
+			} catch (WorkbenchException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			IWorkbenchPage page  = workbench.getActiveWorkbenchWindow().getActivePage();
+		
+			TaskLocationLoader.openTaskLocation(location.getFile(), location.getLineNumber(), page);
+		}
+
+		return null;
+	}
+
+}
--- other/org.unicase.trace/src/org/unicase/codetrace/ui/TaskLocationLoader.java
+++ other/org.unicase.trace/src/org/unicase/codetrace/ui/TaskLocationLoader.java
@@ -1,3 +1,8 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
 package org.unicase.codetrace.ui;
 
 import java.util.HashMap;
@@ -5,31 +10,29 @@ import java.util.HashMap;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.editors.text.EditorsUI;
 import org.eclipse.ui.ide.IDE;
 
 public class TaskLocationLoader {
 	
 	public static void openTaskLocation(IFile file, int lineNumber, IWorkbenchPage page){
-	//	IFile file = <choose the file to open>;
-	//	IWorkbenchPage page = <the page to open the editor in>;
+	
 		HashMap map = new HashMap();
 		map.put(IMarker.LINE_NUMBER, lineNumber);
-	
+		
 		IMarker marker;
 		try {
 			marker = file.createMarker(IMarker.TEXT);
 			marker.setAttributes(map);
-			IDE.openEditor(page, marker); // gotoMarker(editor, marker); // .openEditor(marker, file); //3.0 API
+			IDE.openEditor(page, marker); 
 			marker.delete();
 
 		} catch (CoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		   //page.openEditor(marker); //2.1 API
-	
+		}	
 		
 	}
 
