f7db5d2b094678b6e050ed4665191c1a9404ad65
fatihulusoy <fatihulusoy@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-02-27 13:46:57 +0000

    Extension view can be created now.

--- other/org.unicase.ui.web.projectview/src/org/unicase/ui/web/projectview/ProjectView.java
+++ other/org.unicase.ui.web.projectview/src/org/unicase/ui/web/projectview/ProjectView.java
@@ -14,7 +14,7 @@ import org.unicase.ui.web.views.ProjectAwareView;
 
 public class ProjectView extends ProjectAwareView {
 	
-	public static final String ID = "org.unicase.ui.web.projectview.ProjectView";
+	public static final String ID = "org.unicase.ui.web.perspectives.defaultPerspective.projectview";
 	
 	private WorkItemsTab workItemsTab;
 	private TeamListTab teamListTab;
