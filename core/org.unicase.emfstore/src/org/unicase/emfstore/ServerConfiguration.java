afb3b09cb118aed3c1c3d85f3a9b521ea33e625c
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-03-02 10:20:57 +0000

    default port for EMFStore release changed

--- core/org.unicase.emfstore/src/org/unicase/emfstore/ServerConfiguration.java
+++ core/org.unicase.emfstore/src/org/unicase/emfstore/ServerConfiguration.java
@@ -60,7 +60,7 @@ public final class ServerConfiguration {
 	/**
 	 * Default port for XML RPC.
 	 */
-	public static final String XML_RPC_PORT_DEFAULT = "443";
+	public static final String XML_RPC_PORT_DEFAULT = "8443";
 
 	/**
 	 * Default port for XML RPC if running developer version. This port differs to the normal default port due to port
