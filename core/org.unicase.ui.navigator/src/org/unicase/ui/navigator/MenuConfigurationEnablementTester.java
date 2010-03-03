33964480c4d4052a462fbeeffc94d674392ce3b7
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-03-03 18:06:08 +0000

    added extension point for menuconfiguration of navigator added property tester for menuconfiguration

---
+++ core/org.unicase.ui.navigator/src/org/unicase/ui/navigator/MenuConfigurationEnablementTester.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
+package org.unicase.ui.navigator;
+
+import org.eclipse.core.expressions.PropertyTester;
+import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.Usersession;
+import org.unicase.workspace.util.UnicaseCommandWithResult;
+
+/**
+ * Property tester to test if a project is Shared with a server already.
+ * 
+ * @author koegel
+ */
+public class MenuConfigurationEnablementTester extends PropertyTester {
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.core.expressions.IPropertyTester#test(java.lang.Object,
+	 *      java.lang.String, java.lang.Object[], java.lang.Object)
+	 */
+	public boolean test(Object receiver, String property, Object[] args,
+			final Object expectedValue) {
+		if (receiver instanceof ProjectSpace
+				&& expectedValue instanceof Boolean) {
+			final ProjectSpace projectSpace = (ProjectSpace) receiver;
+			UnicaseCommandWithResult<Boolean> command = new UnicaseCommandWithResult<Boolean>() {
+				@Override
+				protected Boolean doRun() {
+					Usersession usersession = projectSpace.getUsersession();
+					Boolean isShared = new Boolean(usersession != null);
+					return isShared.equals(expectedValue);
+				}
+			};
+			return command.run();
+		}
+		return false;
+	}
+
+}
