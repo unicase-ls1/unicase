639022c1cba24d9d3577dcc4a4fd32965a6d887e
fgxdx85 <fgxdx85@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-03-09 12:17:07 +0000

    v7 of cut / paste: cuts and pastes ModelElements in LeafSections, also stores string and picture representations of ME in clipboard

--- other/org.unicase.cutpaste/src/org/unicase/cutpaste/handlers/PasteHandler.java
+++ other/org.unicase.cutpaste/src/org/unicase/cutpaste/handlers/PasteHandler.java
@@ -4,12 +4,17 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.unicase.metamodel.ModelElement;
 import org.unicase.ui.common.util.ActionHelper;
+import org.unicase.workspace.util.UnicaseCommand;
 
 public class PasteHandler extends AbstractHandler {
 
@@ -24,86 +29,116 @@ public class PasteHandler extends AbstractHandler {
 		clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 		transferable = clipboard.getContents(null); 
 
-		canPasteString();
-		canPasteImage();
-		canPasteModelElement();
+		if(	! (canPasteString(false) &&	canPasteImage(false) &&	canPasteModelElement(false) )) // if no errors occur, show no dialog boxes
+			{canPasteString(true); 	canPasteImage(true); 	canPasteModelElement(true);}
+		
 		paste (meTarget);
-	
+
 		return null;
 	}
-
-
-	public void paste (ModelElement meTarget){
-
+	
+	public void paste (final ModelElement meTarget){
+		
 		try{
 			meSource = (ModelElement) transferable.getTransferData(new DataFlavor(org.unicase.metamodel.ModelElement.class, "UnicaseModelElement"));
 			}
-			catch (Exception e){e.printStackTrace();
+		catch (Exception e){
+			e.printStackTrace();
 			}
-		meSource.eSet(meSource.eContainingFeature(), meTarget.eContainingFeature());
-	}
+		final EReference theRef = getTargetRef(meTarget, meSource);
+		
+		new UnicaseCommand() {
+			@Override
+			protected void doRun() {
+				meSource.eSet(theRef.getEOpposite(), meTarget);
+				}
+			}.run();
+			}
 	
-	public void canPasteString(){
+	public boolean canPasteString(boolean b){
 		try{
 			if (transferable.isDataFlavorSupported(DataFlavor.stringFlavor))
-				{
+				{ if (b)
 				MessageDialog.openInformation(
 					null,
 					"w@iglt info_box",
 					"Can paste String.");
+				return true;
 				}
 			else {
 				MessageDialog.openInformation(
 					null,
 					"w@iglt info_box",
 					"Cannot paste String.");
+				return false;
 			}
 				
 			}
 			catch (Exception e){
 				e.printStackTrace();
 			}
+		return false;
 	}
-	public void canPasteImage(){
+	public boolean canPasteImage(boolean b){
 		try{
 			if (transferable.isDataFlavorSupported(DataFlavor.imageFlavor))
-				{
+				{ if (b)
 				MessageDialog.openInformation(
 					null,
 					"w@iglt info_box",
 					"Can paste Image.");
+				return true;
 				}
 			else {
 				MessageDialog.openInformation(
 					null,
 					"w@iglt info_box",
 					"Cannot paste Image.");
+				return false;
 			}
 				
 			}
 			catch (Exception e){
 				e.printStackTrace();
 			}
+			return false;
 	}
-	public void canPasteModelElement(){
+	public boolean canPasteModelElement(boolean b){
 		try{
 			if (transferable.isDataFlavorSupported(new DataFlavor(org.unicase.metamodel.ModelElement.class, "UnicaseModelElement")))
-				{
+				{ if (b)
 				MessageDialog.openInformation(
 					null,
 					"w@iglt info_box",
 					"Can paste ME.");
+				return true;
 				}
 			else {
 				MessageDialog.openInformation(
 					null,
 					"w@iglt info_box",
 					"Cannot paste ME.");
+				return true;
 			}
-				
 			}
 			catch (Exception e){
 				e.printStackTrace();
 			}
+			return false;
+	}
+	private EReference getTargetRef(EObject targetContainer, ModelElement me) {
+
+		List<EReference> refs = targetContainer.eClass().getEAllContainments();
+		for (EReference ref : refs) {
+			if (ref.isContainer()) {
+				continue;
+			}
+			if (ref.getEReferenceType().equals(me.eClass())
+				|| ref.getEReferenceType().isSuperTypeOf(me.eClass())) {
+				return ref;
+			}
+		}
+		return null;
+
 	}
 }
