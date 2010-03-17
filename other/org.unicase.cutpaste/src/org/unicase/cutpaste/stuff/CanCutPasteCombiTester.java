c30f0585fbdff6e9d88c9558b60d0345600644ed
fgxdx85 <fgxdx85@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-03-17 09:01:13 +0000

    v11.004 of cut/paste: added canCutTest, combined in CanCutPasteCombiTester

--- other/org.unicase.cutpaste/src/org/unicase/cutpaste/stuff/CanCutPasteCombiTester.java
+++ other/org.unicase.cutpaste/src/org/unicase/cutpaste/stuff/CanCutPasteCombiTester.java
@@ -11,13 +11,15 @@ import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.unicase.model.UnicaseModelElement;
+import org.unicase.model.document.CompositeSection;
 
 /**
  * This property tester checks if the paste handler is active.
  * 
  * @author weiglt
  */
-public class CanPasteTester extends PropertyTester {
+public class CanCutPasteCombiTester extends PropertyTester {
 
 	private Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 	private Transferable transferable;
@@ -29,10 +31,21 @@ public class CanPasteTester extends PropertyTester {
 	 *      java.lang.Object)
 	 */
 	public boolean test(Object receiver, String property, Object[] args, final Object expectedValue) {
-		transferable = clipboard.getContents(null);
-		return (transferable.isDataFlavorSupported(new DataFlavor(org.unicase.metamodel.ModelElement.class,
-			"ModelElement")))
-			&& (transferable.isDataFlavorSupported(new DataFlavor(org.unicase.workspace.CompositeOperationHandle.class,
-				"CompositeOperationHandle")));
+		if (property.equals("canPaste")) {
+			transferable = clipboard.getContents(null);
+			return (transferable.isDataFlavorSupported(new DataFlavor(org.unicase.metamodel.ModelElement.class,
+				"ModelElement")))
+				&& (transferable.isDataFlavorSupported(new DataFlavor(
+					org.unicase.workspace.CompositeOperationHandle.class, "CompositeOperationHandle")));
+		} else if (property.equals("canCut")) {
+			System.out.println(((org.eclipse.jface.viewers.TreeSelection) receiver).getFirstElement());
+			if ((((org.eclipse.jface.viewers.TreeSelection) receiver).getFirstElement() instanceof UnicaseModelElement)
+				&& !(((org.eclipse.jface.viewers.TreeSelection) receiver).getFirstElement() instanceof CompositeSection)) {
+				return true;
+			}
+			return false;
+		} else {
+			return false;
+		}
 	}
 }
