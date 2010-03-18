329b36a6bd793b6f720f4c767a7a82cd261b8643
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-03-18 19:05:14 +0000

    added validation plugin to trunk

---
+++ core/org.unicase.ui.validation/src/org/unicase/ui/validation/ValidationListener.java
@@ -0,0 +1,37 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ui.validation;
+
+import org.eclipse.emf.validation.service.IValidationListener;
+import org.eclipse.emf.validation.service.ValidationEvent;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.unicase.ui.common.exceptions.DialogHandler;
+
+/**
+ * Validation listener for the table.
+ * 
+ * @author wesendonk
+ */
+public class ValidationListener implements IValidationListener {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void validationOccurred(ValidationEvent event) {
+		IWorkbenchPage page = PlatformUI.getWorkbench()
+		.getActiveWorkbenchWindow().getActivePage();
+		ValidationView validationView = null;
+		try {
+			validationView = (ValidationView) page
+			.showView("org.unicase.ui.validation.validationView");
+		} catch (PartInitException e) {
+			DialogHandler.showExceptionDialog(e);
+		}
+		validationView.updateTable(event.getValidationResults());
+	}
+}
