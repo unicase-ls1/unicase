ff2ed0cfc2a6140f89f5873ce620d03e944a36ec
fgxdx85 <fgxdx85@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-03-22 06:38:28 +0000

    v5 of the ecoreLoader, no recursive function yet

--- other/org.unicase.ecoreLoader/src/org/unicase/ecoreloader/handlers/EcoreLoader.java
+++ other/org.unicase.ecoreLoader/src/org/unicase/ecoreloader/handlers/EcoreLoader.java
@@ -23,6 +23,7 @@ import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.PlatformUI;
 import org.unicase.model.classes.Attribute;
 import org.unicase.model.classes.Class;
+import org.unicase.model.classes.Method;
 import org.unicase.model.classes.Package;
 import org.unicase.model.document.LeafSection;
 import org.unicase.ui.common.util.ActionHelper;
@@ -41,6 +42,8 @@ import org.xml.sax.SAXException;
  */
 
 // Flo: geh erstmal davon aus dass da Klassen, Packages, Attribute und Operationen drin stehen
+
+// note: Method seems not to be exported by "generate ecore"
 
 public final class EcoreLoader extends AbstractHandler {
 
@@ -72,8 +75,9 @@ public final class EcoreLoader extends AbstractHandler {
 		final String absoluteFileName = showOpenFileDialog();
 
 		if (absoluteFileName == null) {
-			System.out.println("Loading Error.");
+			return null;
 		}
+
 		FileReader fileIn = null;
 		try {
 			fileIn = new FileReader(absoluteFileName);
@@ -96,96 +100,32 @@ public final class EcoreLoader extends AbstractHandler {
 		// maybe an alternative:
 		// org.eclipse.emf.ecore.xmi.util.XMLProcessor p = new org.eclipse.emf.ecore.xmi.util.XMLProcessor();
 
+		File file = null;
+		DocumentBuilder theDocBuilder = null;
 		try {
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder theDocBuilder = dbf.newDocumentBuilder();
-			File file = new File(absoluteFileName);
+			theDocBuilder = dbf.newDocumentBuilder();
+			file = new File(absoluteFileName);
 
-			if (file.exists()) {
-				Document doc = theDocBuilder.parse(file);
-				Element eDoc = doc.getDocumentElement();
-
-				System.out.println("Root element: " + eDoc.getNodeName() + '\n');
-				packageName = eDoc.getAttribute("name");
-
-				NodeList eClassifiersList = eDoc.getElementsByTagName("eClassifiers");
-
-				System.out.println("Number of Elements detected: " + eClassifiersList.getLength() + '\n');
-
-				Package p = null;
-
-				if (eClassifiersList != null && eClassifiersList.getLength() > 0) { // Create Package
-
-					p = new UnicaseCommandWithParameterAndResult<Package, Package>() {
-						@Override
-						protected Package doRun(Package p) {
-							p = org.unicase.model.classes.ClassesFactory.eINSTANCE.createPackage();
-							p.setName(packageName);
-							leafsection.getModelElements().add(p);
-							return p;
-						}
-					}.run(p);
-
-					for (int i = 0; i < eClassifiersList.getLength(); i++) { // Create Package Contents
-
-						Node packageContent = eClassifiersList.item(i);
-						final String packageElement = packageContent.getAttributes().getNamedItem("xsi:type")
-							.getNodeValue().substring(6);
-						final String packageElementName = packageContent.getAttributes().getNamedItem("name")
-							.getNodeValue();
-						System.out.print("Detected new Element " + packageElement + " " + packageElementName);
-
-						if (packageElement.equals("EClass")) {
-							Class c = new UnicaseCommandWithParameterAndResult<Class, Package>() {
-								@Override
-								protected Class doRun(Package p) {
-									Class c = null;
-									c = org.unicase.model.classes.ClassesFactory.eINSTANCE.createClass();
-									c.setName(packageElementName);
-									c.setParentPackage(p);
-
-									return c;
-								}
-							}.run(p);
-
-							if (packageContent.getNodeType() == Node.ELEMENT_NODE) {
-
-								Element e = (Element) packageContent;
-								NodeList eStructuralFeatureList = e.getElementsByTagName("eStructuralFeatures");
-								System.out.println(" has " + eStructuralFeatureList.getLength() + " attributes:");
-
-								for (int j = 0; j < eStructuralFeatureList.getLength(); j++) { // Create Contents
-									// Attributes
-
-									NamedNodeMap map = eStructuralFeatureList.item(j).getAttributes();
-
-									final String contentElement = map.getNamedItem("xsi:type").getNodeValue()
-										.substring(6);
-									final String contentElementName = map.getNamedItem("name").getNodeValue();
-
-									System.out.print("  " + contentElement + contentElementName);
-
-									if (contentElement.equals("EAttribute")) {
-										Attribute a = new UnicaseCommandWithParameterAndResult<Attribute, Class>() {
-											@Override
-											protected Attribute doRun(Class c) {
-												Attribute a = null;
-												a = org.unicase.model.classes.ClassesFactory.eINSTANCE
-													.createAttribute();
-												a.setName(contentElementName);
-												a.setDefiningClass(c);
-												return a;
-											}
-										}.run(c);
-									}
-								}
-								System.out.println('\n');
-							}
-						}
-					}
-				}
-
+			if (!file.exists()) {
+				return null;
 			}
+
+			Document doc = theDocBuilder.parse(file);
+			Element root = doc.getDocumentElement();
+
+			System.out.println("Root element: " + root.getNodeName() + '\n');
+			packageName = root.getAttribute("name");
+
+			Package p = createPackage(leafsection, packageName);
+
+			Node temp = root.getFirstChild(), temp2 = temp;
+			do {
+				if (temp2.getNodeName().equals("eClassifiers")) {
+					resourceCreator(temp2, p);
+				}
+				temp2 = temp2.getNextSibling();
+			} while (!temp.isSameNode(temp2));
 
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace();
@@ -194,8 +134,44 @@ public final class EcoreLoader extends AbstractHandler {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
 		return null;
+	}
+
+	private void resourceCreator(Node n, Package p) {
+
+		Node packageContent = n;
+		final String packageElement = n.getAttributes().getNamedItem("xsi:type").getNodeValue().substring(6);
+		final String packageElementName = n.getAttributes().getNamedItem("name").getNodeValue();
+		System.out.print("Detected new Element " + packageElement + " " + packageElementName);
+
+		if (packageElement.equals("EClass")) {
+
+			Class c = createClass(p, packageElementName);
+
+			if (packageContent.getNodeType() == Node.ELEMENT_NODE) {
+
+				Element e = (Element) packageContent;
+				NodeList eStructuralFeatureList = e.getElementsByTagName("eStructuralFeatures");
+				System.out.println(" has " + eStructuralFeatureList.getLength() + " attributes:");
+
+				for (int j = 0; j < eStructuralFeatureList.getLength(); j++) { // Create Attributes
+
+					NamedNodeMap map = eStructuralFeatureList.item(j).getAttributes();
+					final String contentElement = map.getNamedItem("xsi:type").getNodeValue().substring(6);
+					final String contentElementName = map.getNamedItem("name").getNodeValue();
+
+					System.out.print("  " + contentElement + contentElementName);
+
+					if (contentElement.equals("EAttribute")) {
+						createAttribute(c, contentElementName);
+					} else if (contentElement.equals("Method")) {
+						createMethod(c, contentElementName);
+					}
+				}
+				System.out.println('\n');
+			}
+		}
+
 	}
 
 	private String showOpenFileDialog() {
@@ -210,5 +186,62 @@ public final class EcoreLoader extends AbstractHandler {
 		final File file = new File(dialog.getFilterPath(), dialog.getFileName());
 
 		return file.getAbsolutePath();
+	}
+
+	private Package createPackage(final LeafSection leafsection, final String packageName) {
+		Package p = null;
+		p = new UnicaseCommandWithParameterAndResult<Package, Package>() {
+
+			@Override
+			protected Package doRun(Package p) {
+				p = org.unicase.model.classes.ClassesFactory.eINSTANCE.createPackage();
+				p.setName(packageName);
+				leafsection.getModelElements().add(p);
+				return p;
+			}
+		}.run(p);
+		return p;
+	}
+
+	private Class createClass(final Package p, final String packageElementName) {
+		Class c = new UnicaseCommandWithParameterAndResult<Class, Package>() {
+			@Override
+			protected Class doRun(Package p) {
+				Class c = null;
+				c = org.unicase.model.classes.ClassesFactory.eINSTANCE.createClass();
+				c.setName(packageElementName);
+				c.setParentPackage(p);
+				return c;
+			}
+		}.run(p);
+		return c;
+	}
+
+	private Attribute createAttribute(final Class c, final String contentElementName) {
+		Attribute a = new UnicaseCommandWithParameterAndResult<Attribute, Class>() {
+			@Override
+			protected Attribute doRun(Class c) {
+				Attribute a = null;
+				a = org.unicase.model.classes.ClassesFactory.eINSTANCE.createAttribute();
+				a.setName(contentElementName);
+				a.setDefiningClass(c);
+				return a;
+			}
+		}.run(c);
+		return a;
+	}
+
+	private Method createMethod(final Class c, final String contentElementName) {
+		Method m = new UnicaseCommandWithParameterAndResult<Method, Class>() {
+			@Override
+			protected Method doRun(Class c) {
+				Method m = null;
+				m = org.unicase.model.classes.ClassesFactory.eINSTANCE.createMethod();
+				m.setName(contentElementName);
+				m.setDefiningClass(c);
+				return m;
+			}
+		}.run(c);
+		return m;
 	}
 }
