079beef20f0a69052d1c4ec0da0846f1b4afa292
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-04-21 14:00:59 +0000

    Regenerated diagram codes

---
+++ other/GMFDiagramMigrationTemp/org.unicase.docExport/src/org/unicase/docExport/exceptions/DocumentExportException.java
@@ -0,0 +1,39 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.docExport.exceptions;
+
+/**
+ * The exception for any failure during the export of a DocWriter.
+ * 
+ * @see DocWriter
+ * @author Sebastian Hoecht
+ */
+public class DocumentExportException extends Exception {
+
+	private static final long serialVersionUID = -3769960630478314443L;
+
+	/**
+	 * @param message the message of the exception
+	 * @param ex the exception which caused this exception
+	 */
+	public DocumentExportException(String message, Exception ex) {
+		super(message, ex);
+	}
+
+	/**
+	 * @param ex the exception which caused this exception
+	 */
+	public DocumentExportException(Exception ex) {
+		super(ex);
+	}
+
+	/**
+	 * Constructor.
+	 */
+	public DocumentExportException() {
+	}
+
+}
