560458df9915ffeee458ec9987001604563377e9
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-04-21 13:50:55 +0000

    Regenerated diagram codes

---
+++ other/GMFDiagramMigrationTemp/org.unicase.model/src/org/unicase/model/attachment/impl/AttachmentPackageImpl.java
@@ -0,0 +1,452 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.attachment.impl;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EEnum;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EPackageImpl;
+import org.eclipse.gmf.runtime.notation.NotationPackage;
+import org.unicase.emfstore.esmodel.EsmodelPackage;
+import org.unicase.model.ModelPackage;
+import org.unicase.model.activity.ActivityPackage;
+import org.unicase.model.activity.impl.ActivityPackageImpl;
+import org.unicase.model.attachment.AttachmentFactory;
+import org.unicase.model.attachment.AttachmentPackage;
+import org.unicase.model.attachment.FileAttachment;
+import org.unicase.model.attachment.FileAttachmentType;
+import org.unicase.model.attachment.UrlAttachment;
+import org.unicase.model.bug.BugPackage;
+import org.unicase.model.bug.impl.BugPackageImpl;
+import org.unicase.model.change.ChangePackage;
+import org.unicase.model.change.impl.ChangePackageImpl;
+import org.unicase.model.classes.ClassesPackage;
+import org.unicase.model.classes.impl.ClassesPackageImpl;
+import org.unicase.model.component.ComponentPackage;
+import org.unicase.model.component.impl.ComponentPackageImpl;
+import org.unicase.model.diagram.DiagramPackage;
+import org.unicase.model.diagram.impl.DiagramPackageImpl;
+import org.unicase.model.document.DocumentPackage;
+import org.unicase.model.document.impl.DocumentPackageImpl;
+import org.unicase.model.impl.ModelPackageImpl;
+import org.unicase.model.meeting.MeetingPackage;
+import org.unicase.model.meeting.impl.MeetingPackageImpl;
+import org.unicase.model.organization.OrganizationPackage;
+import org.unicase.model.organization.impl.OrganizationPackageImpl;
+import org.unicase.model.profile.ProfilePackage;
+import org.unicase.model.profile.impl.ProfilePackageImpl;
+import org.unicase.model.rationale.RationalePackage;
+import org.unicase.model.rationale.impl.RationalePackageImpl;
+import org.unicase.model.requirement.RequirementPackage;
+import org.unicase.model.requirement.impl.RequirementPackageImpl;
+import org.unicase.model.state.StatePackage;
+import org.unicase.model.state.impl.StatePackageImpl;
+import org.unicase.model.task.TaskPackage;
+import org.unicase.model.task.impl.TaskPackageImpl;
+import org.unicase.model.util.UtilPackage;
+import org.unicase.model.util.impl.UtilPackageImpl;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model <b>Package</b>. <!-- end-user-doc -->
+ * @generated
+ */
+public class AttachmentPackageImpl extends EPackageImpl implements AttachmentPackage {
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private EClass urlAttachmentEClass = null;
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private EClass fileAttachmentEClass = null;
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private EEnum fileAttachmentTypeEEnum = null;
+
+	/**
+	 * Creates an instance of the model <b>Package</b>, registered with {@link org.eclipse.emf.ecore.EPackage.Registry
+	 * EPackage.Registry} by the package package URI value.
+	 * <p>
+	 * Note: the correct way to create the package is via the static factory method {@link #init init()}, which also
+	 * performs initialization of the package, or returns the registered package, if one already exists. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @see org.eclipse.emf.ecore.EPackage.Registry
+	 * @see org.unicase.model.attachment.AttachmentPackage#eNS_URI
+	 * @see #init()
+	 * @generated
+	 */
+	private AttachmentPackageImpl() {
+		super(eNS_URI, AttachmentFactory.eINSTANCE);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private static boolean isInited = false;
+
+	/**
+	 * Creates, registers, and initializes the <b>Package</b> for this model, and for any others upon which it depends.
+	 * 
+	 * <p>This method is used to initialize {@link AttachmentPackage#eINSTANCE} when that field is accessed.
+	 * Clients should not invoke it directly. Instead, they should simply access that field to obtain the package.
+	 * <!-- begin-user-doc
+	 * --> <!-- end-user-doc -->
+	 * @see #eNS_URI
+	 * @see #createPackageContents()
+	 * @see #initializePackageContents()
+	 * @generated
+	 */
+	public static AttachmentPackage init() {
+		if (isInited)
+			return (AttachmentPackage) EPackage.Registry.INSTANCE.getEPackage(AttachmentPackage.eNS_URI);
+
+		// Obtain or create and register package
+		AttachmentPackageImpl theAttachmentPackage = (AttachmentPackageImpl) (EPackage.Registry.INSTANCE.get(eNS_URI) instanceof AttachmentPackageImpl ? EPackage.Registry.INSTANCE
+			.get(eNS_URI)
+			: new AttachmentPackageImpl());
+
+		isInited = true;
+
+		// Initialize simple dependencies
+		EsmodelPackage.eINSTANCE.eClass();
+		NotationPackage.eINSTANCE.eClass();
+
+		// Obtain or create and register interdependencies
+		ModelPackageImpl theModelPackage = (ModelPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(ModelPackage.eNS_URI) instanceof ModelPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(ModelPackage.eNS_URI) : ModelPackage.eINSTANCE);
+		OrganizationPackageImpl theOrganizationPackage = (OrganizationPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(OrganizationPackage.eNS_URI) instanceof OrganizationPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(OrganizationPackage.eNS_URI) : OrganizationPackage.eINSTANCE);
+		TaskPackageImpl theTaskPackage = (TaskPackageImpl) (EPackage.Registry.INSTANCE.getEPackage(TaskPackage.eNS_URI) instanceof TaskPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(TaskPackage.eNS_URI)
+			: TaskPackage.eINSTANCE);
+		DiagramPackageImpl theDiagramPackage = (DiagramPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(DiagramPackage.eNS_URI) instanceof DiagramPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(DiagramPackage.eNS_URI) : DiagramPackage.eINSTANCE);
+		ClassesPackageImpl theClassesPackage = (ClassesPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(ClassesPackage.eNS_URI) instanceof ClassesPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(ClassesPackage.eNS_URI) : ClassesPackage.eINSTANCE);
+		DocumentPackageImpl theDocumentPackage = (DocumentPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(DocumentPackage.eNS_URI) instanceof DocumentPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(DocumentPackage.eNS_URI) : DocumentPackage.eINSTANCE);
+		RequirementPackageImpl theRequirementPackage = (RequirementPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(RequirementPackage.eNS_URI) instanceof RequirementPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(RequirementPackage.eNS_URI) : RequirementPackage.eINSTANCE);
+		RationalePackageImpl theRationalePackage = (RationalePackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(RationalePackage.eNS_URI) instanceof RationalePackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(RationalePackage.eNS_URI) : RationalePackage.eINSTANCE);
+		ChangePackageImpl theChangePackage = (ChangePackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(ChangePackage.eNS_URI) instanceof ChangePackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(ChangePackage.eNS_URI) : ChangePackage.eINSTANCE);
+		BugPackageImpl theBugPackage = (BugPackageImpl) (EPackage.Registry.INSTANCE.getEPackage(BugPackage.eNS_URI) instanceof BugPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(BugPackage.eNS_URI)
+			: BugPackage.eINSTANCE);
+		ComponentPackageImpl theComponentPackage = (ComponentPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(ComponentPackage.eNS_URI) instanceof ComponentPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(ComponentPackage.eNS_URI) : ComponentPackage.eINSTANCE);
+		MeetingPackageImpl theMeetingPackage = (MeetingPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(MeetingPackage.eNS_URI) instanceof MeetingPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(MeetingPackage.eNS_URI) : MeetingPackage.eINSTANCE);
+		StatePackageImpl theStatePackage = (StatePackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(StatePackage.eNS_URI) instanceof StatePackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(StatePackage.eNS_URI) : StatePackage.eINSTANCE);
+		ProfilePackageImpl theProfilePackage = (ProfilePackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(ProfilePackage.eNS_URI) instanceof ProfilePackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(ProfilePackage.eNS_URI) : ProfilePackage.eINSTANCE);
+		UtilPackageImpl theUtilPackage = (UtilPackageImpl) (EPackage.Registry.INSTANCE.getEPackage(UtilPackage.eNS_URI) instanceof UtilPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(UtilPackage.eNS_URI)
+			: UtilPackage.eINSTANCE);
+		ActivityPackageImpl theActivityPackage = (ActivityPackageImpl) (EPackage.Registry.INSTANCE
+			.getEPackage(ActivityPackage.eNS_URI) instanceof ActivityPackageImpl ? EPackage.Registry.INSTANCE
+			.getEPackage(ActivityPackage.eNS_URI) : ActivityPackage.eINSTANCE);
+
+		// Create package meta-data objects
+		theAttachmentPackage.createPackageContents();
+		theModelPackage.createPackageContents();
+		theOrganizationPackage.createPackageContents();
+		theTaskPackage.createPackageContents();
+		theDiagramPackage.createPackageContents();
+		theClassesPackage.createPackageContents();
+		theDocumentPackage.createPackageContents();
+		theRequirementPackage.createPackageContents();
+		theRationalePackage.createPackageContents();
+		theChangePackage.createPackageContents();
+		theBugPackage.createPackageContents();
+		theComponentPackage.createPackageContents();
+		theMeetingPackage.createPackageContents();
+		theStatePackage.createPackageContents();
+		theProfilePackage.createPackageContents();
+		theUtilPackage.createPackageContents();
+		theActivityPackage.createPackageContents();
+
+		// Initialize created meta-data
+		theAttachmentPackage.initializePackageContents();
+		theModelPackage.initializePackageContents();
+		theOrganizationPackage.initializePackageContents();
+		theTaskPackage.initializePackageContents();
+		theDiagramPackage.initializePackageContents();
+		theClassesPackage.initializePackageContents();
+		theDocumentPackage.initializePackageContents();
+		theRequirementPackage.initializePackageContents();
+		theRationalePackage.initializePackageContents();
+		theChangePackage.initializePackageContents();
+		theBugPackage.initializePackageContents();
+		theComponentPackage.initializePackageContents();
+		theMeetingPackage.initializePackageContents();
+		theStatePackage.initializePackageContents();
+		theProfilePackage.initializePackageContents();
+		theUtilPackage.initializePackageContents();
+		theActivityPackage.initializePackageContents();
+
+		// Mark meta-data to indicate it can't be changed
+		theAttachmentPackage.freeze();
+
+		// Update the registry and return the package
+		EPackage.Registry.INSTANCE.put(AttachmentPackage.eNS_URI, theAttachmentPackage);
+		return theAttachmentPackage;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EClass getUrlAttachment() {
+		return urlAttachmentEClass;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getUrlAttachment_Url() {
+		return (EAttribute) urlAttachmentEClass.getEStructuralFeatures().get(0);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EClass getFileAttachment() {
+		return fileAttachmentEClass;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_FileName() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(0);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_FileHash() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(1);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_FileID() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(2);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_FileSize() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(3);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_RequiredOffline() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(4);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_FileType() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(5);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_Downloading() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(7);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EAttribute getFileAttachment_Uploading() {
+		return (EAttribute) fileAttachmentEClass.getEStructuralFeatures().get(6);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public EEnum getFileAttachmentType() {
+		return fileAttachmentTypeEEnum;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public AttachmentFactory getAttachmentFactory() {
+		return (AttachmentFactory) getEFactoryInstance();
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private boolean isCreated = false;
+
+	/**
+	 * Creates the meta-model objects for the package.  This method is
+	 * guarded to have no affect on any invocation but its first.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void createPackageContents() {
+		if (isCreated)
+			return;
+		isCreated = true;
+
+		// Create classes and their features
+		urlAttachmentEClass = createEClass(URL_ATTACHMENT);
+		createEAttribute(urlAttachmentEClass, URL_ATTACHMENT__URL);
+
+		fileAttachmentEClass = createEClass(FILE_ATTACHMENT);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__FILE_NAME);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__FILE_HASH);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__FILE_ID);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__FILE_SIZE);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__REQUIRED_OFFLINE);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__FILE_TYPE);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__UPLOADING);
+		createEAttribute(fileAttachmentEClass, FILE_ATTACHMENT__DOWNLOADING);
+
+		// Create enums
+		fileAttachmentTypeEEnum = createEEnum(FILE_ATTACHMENT_TYPE);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	private boolean isInitialized = false;
+
+	/**
+	 * Complete the initialization of the package and its meta-model.  This
+	 * method is guarded to have no affect on any invocation but its first.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void initializePackageContents() {
+		if (isInitialized)
+			return;
+		isInitialized = true;
+
+		// Initialize package
+		setName(eNAME);
+		setNsPrefix(eNS_PREFIX);
+		setNsURI(eNS_URI);
+
+		// Obtain other dependent packages
+		ModelPackage theModelPackage = (ModelPackage) EPackage.Registry.INSTANCE.getEPackage(ModelPackage.eNS_URI);
+
+		// Create type parameters
+
+		// Set bounds for type parameters
+
+		// Add supertypes to classes
+		urlAttachmentEClass.getESuperTypes().add(theModelPackage.getAttachment());
+		fileAttachmentEClass.getESuperTypes().add(theModelPackage.getAttachment());
+
+		// Initialize classes and features; add operations and parameters
+		initEClass(urlAttachmentEClass, UrlAttachment.class, "UrlAttachment", !IS_ABSTRACT, !IS_INTERFACE,
+			IS_GENERATED_INSTANCE_CLASS);
+		initEAttribute(getUrlAttachment_Url(), ecorePackage.getEString(), "url", null, 0, 1, UrlAttachment.class,
+			!IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE, !IS_DERIVED, IS_ORDERED);
+
+		initEClass(fileAttachmentEClass, FileAttachment.class, "FileAttachment", !IS_ABSTRACT, !IS_INTERFACE,
+			IS_GENERATED_INSTANCE_CLASS);
+		initEAttribute(getFileAttachment_FileName(), ecorePackage.getEString(), "fileName", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_FileHash(), ecorePackage.getEString(), "fileHash", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_FileID(), ecorePackage.getEString(), "fileID", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_FileSize(), ecorePackage.getELong(), "fileSize", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_RequiredOffline(), ecorePackage.getEBoolean(), "requiredOffline", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_FileType(), this.getFileAttachmentType(), "fileType", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_Uploading(), ecorePackage.getEBoolean(), "uploading", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+		initEAttribute(getFileAttachment_Downloading(), ecorePackage.getEBoolean(), "downloading", null, 0, 1,
+			FileAttachment.class, !IS_TRANSIENT, !IS_VOLATILE, IS_CHANGEABLE, !IS_UNSETTABLE, !IS_ID, IS_UNIQUE,
+			!IS_DERIVED, IS_ORDERED);
+
+		// Initialize enums and add enum literals
+		initEEnum(fileAttachmentTypeEEnum, FileAttachmentType.class, "FileAttachmentType");
+		addEEnumLiteral(fileAttachmentTypeEEnum, FileAttachmentType.BINARY);
+		addEEnumLiteral(fileAttachmentTypeEEnum, FileAttachmentType.IMAGE);
+		addEEnumLiteral(fileAttachmentTypeEEnum, FileAttachmentType.AUDIO);
+		addEEnumLiteral(fileAttachmentTypeEEnum, FileAttachmentType.VIDEO);
+
+		// Create annotations
+		// org.unicase.ui.meeditor
+		createOrgAnnotations();
+	}
+
+	/**
+	 * Initializes the annotations for <b>org.unicase.ui.meeditor</b>.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void createOrgAnnotations() {
+		String source = "org.unicase.ui.meeditor";
+		addAnnotation(getFileAttachment_FileName(), source, new String[] { "priority", "2", "position", "left" });
+		addAnnotation(getFileAttachment_FileHash(), source, new String[] { "priority", "2", "position", "left" });
+		addAnnotation(getFileAttachment_FileID(), source, new String[] { "priority", "2", "position", "left" });
+		addAnnotation(getFileAttachment_FileSize(), source, new String[] { "priority", "2", "position", "left" });
+	}
+
+} // AttachmentPackageImpl
