de2d4e4870ce4582428bea3bd665e8b8f474e13a
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-05-17 16:47:01 +0000

    Modified delete-model-elment-handler to test EMF deletecommand

--- org.unicase.ui.common/src/org/unicase/ui/common/handler/DeleteModelelementHandler.java
+++ org.unicase.ui.common/src/org/unicase/ui/common/handler/DeleteModelelementHandler.java
@@ -8,6 +8,11 @@ package org.unicase.ui.common.handler;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.change.impl.EObjectToChangesMapEntryImpl;
+import org.eclipse.emf.edit.command.CutToClipboardCommand;
+import org.eclipse.emf.edit.command.DeleteCommand;
+import org.eclipse.emf.edit.command.PasteFromClipboardCommand;
 import org.unicase.metamodel.ModelElement;
 import org.unicase.ui.common.commands.DeleteModelElementCommand;
 import org.unicase.ui.common.util.ActionHelper;
@@ -23,19 +28,41 @@ public class DeleteModelelementHandler extends AbstractHandler {
 	 * . {@inheritDoc}
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		ModelElement me = ActionHelper.getModelElement(event);
-		if (me == null) {
-			return null;
-		}
-
-
-		deleteModelElement(me);
-
+		
+		//Paste command from emf edit
+		
+	Object toValidate = ActionHelper.getModelElement(event);
+//		org.eclipse.emf.edit.command.PasteFromClipboardCommand dc3 = (PasteFromClipboardCommand) org.eclipse.emf.edit.command.PasteFromClipboardCommand.create(org.unicase.workspace.Configuration.getEditingDomain(),
+//			toValidate,toValidate);
+//		org.unicase.workspace.Configuration.getEditingDomain()
+//				.getCommandStack().execute(dc3);
+		
+		
+		
+		//delete command from emf edit
+		
+		 org.eclipse.emf.edit.command.DeleteCommand dc2 = (DeleteCommand)
+		 org.eclipse.emf.edit.command.DeleteCommand
+		 .create(org.unicase.workspace.Configuration.getEditingDomain(),
+		 toValidate);
+		 org.unicase.workspace.Configuration.getEditingDomain()
+		 .getCommandStack().execute(dc2);
+//		
+//		ModelElement me = ActionHelper.getModelElement(event);
+//		if (me == null) {
+//			return null;
+//		}
+//
+//
+//		deleteModelElement(me);
+//
+//return null;
+//	}
+//
+//	private void deleteModelElement(final ModelElement me) {
+//		new DeleteModelElementCommand(me).run();
+//	}
 		return null;
-	}
 
-	private void deleteModelElement(final ModelElement me) {
-		new DeleteModelElementCommand(me).run();
-	}
-
+}
 }
