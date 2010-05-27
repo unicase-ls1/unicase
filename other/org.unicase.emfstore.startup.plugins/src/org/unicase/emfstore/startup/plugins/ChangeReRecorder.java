d83fffc8ca9f9eaa90fbf3adcca572a67c49bdce
wesendon@in.tum.de <wesendon@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-05-27 16:17:23 +0000

    work in progress

--- other/org.unicase.emfstore.startup.plugins/src/org/unicase/emfstore/startup/plugins/ChangeReRecorder.java
+++ other/org.unicase.emfstore.startup.plugins/src/org/unicase/emfstore/startup/plugins/ChangeReRecorder.java
@@ -16,6 +16,7 @@ import org.unicase.emfstore.esmodel.versioning.ChangePackage;
 import org.unicase.emfstore.esmodel.versioning.VersioningFactory;
 import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
 import org.unicase.emfstore.esmodel.versioning.operations.CompositeOperation;
+import org.unicase.emfstore.esmodel.versioning.operations.OperationsFactory;
 import org.unicase.metamodel.Project;
 import org.unicase.workspace.WorkspaceFactory;
 import org.unicase.workspace.impl.ProjectSpaceImpl;
@@ -32,17 +33,21 @@ public class ChangeReRecorder {
 	/**
 	 * Initialises the transiet projecstapce which is needed for rerecording.
 	 * 
-	 * @param project initial project
+	 * @param project
+	 *            initial project
 	 */
 	public void init(Project project) {
 		if (project == null) {
 			throw new IllegalStateException();
 		}
-		projectSpace = (ProjectSpaceImpl) WorkspaceFactory.eINSTANCE.createProjectSpace();
-		projectSpace.setBaseVersion(VersioningFactory.eINSTANCE.createPrimaryVersionSpec());
+		projectSpace = (ProjectSpaceImpl) WorkspaceFactory.eINSTANCE
+				.createProjectSpace();
+		projectSpace.setBaseVersion(VersioningFactory.eINSTANCE
+				.createPrimaryVersionSpec());
 		projectSpace.setIdentifier("testProjectSpace");
 		projectSpace.setLastUpdated(new Date());
-		projectSpace.setLocalOperations(WorkspaceFactory.eINSTANCE.createOperationComposite());
+		projectSpace.setLocalOperations(WorkspaceFactory.eINSTANCE
+				.createOperationComposite());
 		projectSpace.setProjectDescription("ps description");
 		projectSpace.setProjectId(EsmodelFactory.eINSTANCE.createProjectId());
 		projectSpace.setProjectName("ps name");
@@ -54,10 +59,12 @@ public class ChangeReRecorder {
 	/**
 	 * Converts the operations through rerecording.
 	 * 
-	 * @param operations list of operations
+	 * @param operations
+	 *            list of operations
 	 * @return new operations
 	 */
-	public List<AbstractOperation> convertOperations(List<AbstractOperation> operations) {
+	public List<AbstractOperation> convertOperations(
+			List<AbstractOperation> operations) {
 		projectSpace.getOperations().clear();
 		applyOperationForOperation(operations);
 		ChangePackage changePackage = projectSpace.getLocalChangePackage(true);
@@ -66,31 +73,74 @@ public class ChangeReRecorder {
 
 	private void applyOperationForOperation(List<AbstractOperation> operations) {
 		int index = 0;
-		for(AbstractOperation operation : operations) {
+		for (AbstractOperation operation : operations) {
 			Date clientDate = operation.getClientDate();
-			projectSpace.applyOperationsWithRecording(Arrays.asList(operation), true);
-			if(index >= projectSpace.getOperations().size()) {
+			projectSpace.applyOperationsWithRecording(Arrays.asList(operation),
+					true);
+			if (index >= projectSpace.getOperations().size()) {
 				continue;
 			}
-			ListIterator<AbstractOperation> listIterator = projectSpace.getOperations().listIterator(index);
-			while(listIterator.hasNext()) {
-				setDate(clientDate,listIterator.next());
+			ListIterator<AbstractOperation> listIterator = projectSpace
+					.getOperations().listIterator(index);
+
+//			listIterator = handleCompositeOperation(index, operation,
+//					listIterator);
+
+			while (listIterator.hasNext()) {
+				setDate(clientDate, listIterator.next());
 			}
 			index = projectSpace.getOperations().size();
 		}
 	}
 
+	@SuppressWarnings("unused")
+	private ListIterator<AbstractOperation> handleCompositeOperation(int index,
+			AbstractOperation operation,
+			ListIterator<AbstractOperation> listIterator) {
+		
+		if (operation instanceof CompositeOperation
+				&& ((CompositeOperation) operation).getMainOperation() == null) {
+			CompositeOperation oldComp = (CompositeOperation) operation;
+
+			CompositeOperation composite = OperationsFactory.eINSTANCE
+					.createCompositeOperation();
+			composite.setAccepted(oldComp.isAccepted());
+			composite.setCompositeDescription(oldComp.getDescription());
+			composite.setCompositeName(oldComp.getName());
+			composite.setModelElementId(null);
+			
+			while (listIterator.hasNext()) {
+				AbstractOperation abstractOperation = listIterator.next();
+				composite.getSubOperations().add(abstractOperation);
+				listIterator.remove();
+			}
+			if (composite.getSubOperations().size() > 0) {
+				AbstractOperation mainOperation = composite.getSubOperations()
+						.get(composite.getSubOperations().size() - 1);
+				composite.setMainOperation(mainOperation);
+			} else {
+				//TODO
+			}
+
+			listIterator = projectSpace.getOperations().listIterator(index);
+		}
+		return listIterator;
+	}
+
 	private void setDate(Date clientDate, AbstractOperation operation) {
-		if(operation instanceof CompositeOperation) {
-			for(AbstractOperation subOperation : ((CompositeOperation) operation).getSubOperations()) {
+		if (operation instanceof CompositeOperation) {
+			for (AbstractOperation subOperation : ((CompositeOperation) operation)
+					.getSubOperations()) {
 				setDate(clientDate, subOperation);
 			}
 		}
-		operation.setClientDate(clientDate==null?null:(Date) clientDate.clone());
+		operation.setClientDate(clientDate == null ? null : (Date) clientDate
+				.clone());
 	}
 
 	/**
 	 * Returns a copy of the project.
+	 * 
 	 * @return project
 	 */
 	public Project getProject() {
