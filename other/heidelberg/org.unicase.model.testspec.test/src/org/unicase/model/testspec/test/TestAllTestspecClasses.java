4bd8ab740c46ca3b8881e2260fa065345bdaf811
connect2nitesh <connect2nitesh@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-05-28 12:33:39 +0000

    Trunk has been branched to test build-server configuration!

--- other/heidelberg/org.unicase.model.testspec.test/src/org/unicase/model/testspec/test/TestAllTestspecClasses.java
+++
@@ -1,9 +0,0 @@
-package org.unicase.model.testspec.test;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses( { TestTestCase.class, TestTestStep.class })
-public class TestAllTestspecClasses {
-}
