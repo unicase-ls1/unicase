4e8493e87b20a10f940e6f4e89998b85fd7c55b2
hodamng <hodamng@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-09 10:27:58 +0000

    updating classes

--- test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/AllNavigatorTests.java
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/AllNavigatorTests.java
@@ -3,8 +3,7 @@ package org.unicase.ui.test.navigator;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { CreateProjectTest.class, DeleteProjectTest.class, ImportProjectTest.class })
+@Suite.SuiteClasses( { CreateProjectUITest.class, DeleteProjectTest.class, ImportProjectTest.class })
 public class AllNavigatorTests {
 }
