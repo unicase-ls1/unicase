f4e7fd0f99ce767dd48f9de20f29e6a57e3f421c
hodamng <hodamng@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-10 08:19:30 +0000

    updated the allnavigator tests with new ones

--- test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/AllNavigatorTests.java
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/AllNavigatorTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { CreateProjectUITest.class, DeleteProjectTest.class, ImportProjectTest.class })
+@Suite.SuiteClasses( { CreateProjectUITest.class, DeleteProjectUITest.class, ImportProjectTest.class })
 public class AllNavigatorTests {
 }
