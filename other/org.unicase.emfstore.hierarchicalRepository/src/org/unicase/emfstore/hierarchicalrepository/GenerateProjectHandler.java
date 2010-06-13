8c078ec7271e15c29230f249c56c8f84a96b1e21
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-13 12:44:40 +0000

    added more realistic project generation

--- other/org.unicase.emfstore.hierarchicalRepository/src/org/unicase/emfstore/hierarchicalrepository/GenerateProjectHandler.java
+++ other/org.unicase.emfstore.hierarchicalRepository/src/org/unicase/emfstore/hierarchicalrepository/GenerateProjectHandler.java
@@ -11,9 +11,10 @@ import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.unicase.metamodel.Project;
+import org.unicase.metamodel.util.ModelUtil;
 import org.unicase.workspace.ProjectSpace;
 import org.unicase.workspace.WorkspaceManager;
-import org.unicase.workspace.util.UnicaseCommand;
+import org.unicase.workspace.util.UnicaseCommandWithParameterAndResult;
 
 public class GenerateProjectHandler extends  AbstractHandler {
 
@@ -23,12 +24,12 @@ public class GenerateProjectHandler extends  AbstractHandler {
 		.getCurrentWorkspace().getActiveProjectSpace();
 
 		final Project project = projSpace.getProject();
-		final Library library = LibraryFactory.eINSTANCE.createLibrary();
+		Library rootlibrary = LibraryFactory.eINSTANCE.createLibrary();
 		
 		
 		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
 		InputDialog inputDialog = new InputDialog(shell,
-				"Model Site", "Enter the required site:", "", null);
+				"Model Site", "Enter the required total size:", "", null);
 		if (inputDialog.open() != Window.OK) {
 			return null;
 		}
@@ -41,18 +42,61 @@ public class GenerateProjectHandler extends  AbstractHandler {
 			return null;
 		}
 		
-		for (int i = 0; i < size; i++) {
-			library.getBooks().add((LibraryFactory.eINSTANCE.createBook()));
+		InputDialog inputDialog2 = new InputDialog(shell,
+				"Model Site", "Enter the required branching factor:", "", null);
+		if (inputDialog2.open() != Window.OK) {
+			return null;
 		}
-		new UnicaseCommand() {
-			
-			@Override
-			protected void doRun() {
-				project.addModelElement(library);
-			}
-		}.run();
+		int factor = 0;
+		try {
+			factor = Integer.parseInt(inputDialog2.getValue());
+		} catch (NumberFormatException e) {
+			MessageDialog.openError(shell, "Invalid input",
+					"A numerical value was expected!");
+			return null;
+		}
 		
+		
+		
+		//create a struture with given branching factor and total size
+		
+		int remainder = size;
+		while (remainder>factor) {
+			remainder = remainder / factor;
+			//increase existing instance by factor "factor"
+			Library newRoot = LibraryFactory.eINSTANCE.createLibrary();
+			
+			for (int i=0; i<factor-1; i++) {
+				newRoot.getIncludedLibraries().add(ModelUtil.clone(rootlibrary));
+			}
+			newRoot.getIncludedLibraries().add(rootlibrary);
+			rootlibrary = newRoot;
+		}
+		
+		//increase existing instance by remainder
+		Library newRoot = LibraryFactory.eINSTANCE.createLibrary();
+		
+		for (int i=0; i<remainder-1; i++) {
+			newRoot.getIncludedLibraries().add(ModelUtil.clone(rootlibrary));
+		}
+		newRoot.getIncludedLibraries().add(rootlibrary);
+		rootlibrary = newRoot;
+		
+		new UnicaseCommandWithParameterAndResult<Project, Library>() {
+
+			@Override
+			protected Project doRun(Library parameter) {
+				project.addModelElement(parameter);
+				return project;
+			}
+		}.run(rootlibrary);
+		
+		
+		int actualSize = project.getAllModelElements().size();
+		
+		MessageDialog.openInformation(null, "Generation completed.", "Generated " + actualSize + " model elements");
 		return null;
+		
 	}
 
 }
