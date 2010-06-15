b09774e99b3abfab6528da14a35cbc7af70b7e35
helming@in.tum.de <helming@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-15 22:47:36 +0000

    added example

---
+++ developer/CodeExamples/exampleModel.editor/src/examplemodel/editor/ExampleInput.java
@@ -0,0 +1,24 @@
+package examplemodel.editor;
+
+import org.eclipse.emf.common.ui.URIEditorInput;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.unicase.metamodel.ModelElement;
+
+public class ExampleInput extends URIEditorInput {
+
+
+	private final ModelElement modelelement;
+
+	public ExampleInput(ModelElement modelelement) {
+		super(EcoreUtil.getURI(modelelement));
+		this.modelelement = modelelement;
+		
+	}
+
+	public ModelElement getModelelement() {
+		// TODO Auto-generated method stub
+		return modelelement;
+	}
+
+}
