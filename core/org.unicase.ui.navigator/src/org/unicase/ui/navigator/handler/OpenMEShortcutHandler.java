dc1cee03b1809d9d93c53318f00575d0a7bcb692
helming@in.tum.de <helming@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-22 13:18:57 +0000

    refactored open shortcut handler

--- core/org.unicase.ui.navigator/src/org/unicase/ui/navigator/handler/OpenMEShortcutHandler.java
+++ core/org.unicase.ui.navigator/src/org/unicase/ui/navigator/handler/OpenMEShortcutHandler.java
@@ -9,11 +9,11 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
-import org.unicase.metamodel.ModelElement;
 import org.unicase.ui.common.util.ActionHelper;
 import org.unicase.ui.navigator.Activator;
 import org.unicase.ui.navigator.NoWorkspaceException;
@@ -56,8 +56,8 @@ public class OpenMEShortcutHandler extends AbstractHandler implements IHandler {
 			if (dialog.open() == Window.OK) {
 				Object[] result = dialog.getResult();
 
-				if (result.length == 1 && result[0] instanceof ModelElement) {
-					ModelElement modelElement = (ModelElement) result[0];
+				if (result.length == 1 && result[0] instanceof EObject) {
+					EObject modelElement = (EObject) result[0];
 					ActionHelper.openModelElement(modelElement, "org.unicase.ui.OpenMEShortcut");
 				}
 			}
