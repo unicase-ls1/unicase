511010919ef92e057302d65d353408239ac68900
kumar@fueseschi.de <kumar@fueseschi.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-23 10:29:28 +0000

    refactoring

--- other/EmailNotifier/org.unicase.proxyclient.notifier/src/org/unicase/proxyclient/notifier/email/Mailer.java
+++ other/EmailNotifier/org.unicase.proxyclient.notifier/src/org/unicase/proxyclient/notifier/email/Mailer.java
@@ -14,7 +14,6 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import javax.security.auth.login.Configuration;
 
 /**
  * Mail Service.
@@ -22,46 +21,55 @@ import javax.security.auth.login.Configuration;
  * @author fuesescc
  */
 public class Mailer extends Authenticator {
-	
+
 	private MailerInfo mailerInfo;
 	private Session session;
 
+	/**
+	 * Constructor for Mailer.
+	 * 
+	 * @param mailerInfo Values like host, port, username etc. are contained in the MailerInfo object and are necessary
+	 *            for the Mailer class to work.
+	 */
 	public Mailer(MailerInfo mailerInfo) {
 		this.mailerInfo = mailerInfo;
-		
+
 		String host = mailerInfo.getHost();
 		int port = mailerInfo.getPort();
 		boolean useSSL = mailerInfo.useSSL();
-		
-		Properties props = new Properties();
 
+		Properties props = new Properties();
 		props.put("mail.transport.protocol", "smtp");
 		props.put("mail.smtp.host", host);
 		props.put("mail.smtp.port", port);
 		props.put("mail.smtp.auth", "true");
-		// props.put("mail.smtps.socketFactory.port", "995");
-		// props.put("mail.smtps.socketFactory.class",
-		// "javax.net.ssl.SSLSocketFactory");
-		// props.put("mail.smtps.socketFactory.fallback", "false");
-		// props.put("mail.smtp.ssl.checkserveridentity", "true");
 		props.put("mail.smtp.ssl.enable", useSSL);
 		props.put("mail.mime.charset", "UTF-8");
 		props.put("mail.smtp.quitwait", "false");
 
 		session = Session.getInstance(props, this);
-
-		session.setDebug(true);
 	}
 
+	/**
+	 * 
+	 * @return 
+	 */
 	protected PasswordAuthentication getPasswordAuthentication() {
 		String user = mailerInfo.getUsername();
 		String password = mailerInfo.getPassword();
-		
+
 		return new PasswordAuthentication(user, password);
 	}
-	
-	public boolean send(String to, String subject, String msg) {
 
+	/**
+	 * Method to send emails.
+	 * 
+	 * @param to email address of the receiver.
+	 * @param subject Subject of message.
+	 * @param msg Message body.
+	 * @return boolean value that indicates whether the email has been sent or not.
+	 */
+	public boolean send(String to, String subject, String msg) {
 		try {
 			MimeMessage message = new MimeMessage(session);
 			message.setHeader("Content-Type", "UTF-8");
@@ -69,16 +77,12 @@ public class Mailer extends Authenticator {
 			message.setFrom(new InternetAddress(mailerInfo.getSender(), true));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 			message.setSubject(subject);
-			// set message content, type, and encoding, and send the message
-			//message.setContent(msg, "text/plain; charset=UTF-8");
 			message.setContent(msg, "text/html; charset=UTF-8");
 			Transport.send(message);
 			return true;
-			
+
 		} catch (Exception e) {
-			// TODO: handle exception
-			System.err.println( e.getMessage() );
-			return  false;
+			return false;
 		}
 
 	}
