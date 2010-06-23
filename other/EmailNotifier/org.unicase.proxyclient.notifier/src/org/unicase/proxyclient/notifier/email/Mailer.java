332bc471fc218f4485c98be022701fe1e6fc5f66
kumar@fueseschi.de <kumar@fueseschi.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-06-23 10:46:39 +0000

    refactoring

--- other/EmailNotifier/org.unicase.proxyclient.notifier/src/org/unicase/proxyclient/notifier/email/Mailer.java
+++ other/EmailNotifier/org.unicase.proxyclient.notifier/src/org/unicase/proxyclient/notifier/email/Mailer.java
@@ -12,14 +12,10 @@ import javax.mail.IllegalWriteException;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
-import javax.mail.SendFailedException;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-
-import org.unicase.proxyclient.notifier.client.NotificationProxyClientException;
 
 /**
  * Mail Service.
