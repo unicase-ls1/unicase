e8c5ac64ff50c963e899206ab6fe2f812e411a3f
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-08-16 21:58:36 +0000

    removed warnings

--- core/org.unicase.ui.common/src/org/unicase/ui/common/commands/ECPCommand.java
+++ core/org.unicase.ui.common/src/org/unicase/ui/common/commands/ECPCommand.java
@@ -1,3 +1,8 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
 package org.unicase.ui.common.commands;
 
 import org.eclipse.emf.ecore.EObject;
@@ -5,6 +10,11 @@ import org.eclipse.emf.edit.command.ChangeCommand;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.domain.EditingDomain;
 
+/**
+ * Command capable of recording changes on a model element.
+ * 
+ * @author emueller
+ */
 public abstract class ECPCommand extends ChangeCommand {
 
 	private EObject eObject;
@@ -12,7 +22,7 @@ public abstract class ECPCommand extends ChangeCommand {
 	/**
 	 * Constructor.
 	 * 
-	 * @param eObject
+	 * @param eObject the model element whose changes one is interested in
 	 */
 	public ECPCommand(EObject eObject) {
 		super(eObject);
