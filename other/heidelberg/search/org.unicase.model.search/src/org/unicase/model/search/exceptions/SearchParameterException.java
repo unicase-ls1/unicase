acf354c2d68ee4848eff1357001a09a61aef918e
adelater@gmail.com <adelater@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-09-07 06:13:25 +0000

    Initial

---
+++ other/heidelberg/search/org.unicase.model.search/src/org/unicase/model/search/exceptions/SearchParameterException.java
@@ -0,0 +1,11 @@
+package org.unicase.model.search.exceptions;
+
+public class SearchParameterException extends Exception {
+	
+	private static final long serialVersionUID = 9210713134666978236L;
+
+	public SearchParameterException(String message) {
+		super(message);
+	}
+
+}
