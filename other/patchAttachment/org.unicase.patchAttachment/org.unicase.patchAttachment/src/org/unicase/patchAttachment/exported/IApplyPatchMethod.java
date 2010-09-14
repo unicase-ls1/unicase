cfbae15325df31ba5ec4f02373b762d6f48dd336
jan_finis@gmx.de <jan_finis@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-09-14 17:44:13 +0000

    deleted wrongly added subproject

--- other/patchAttachment/org.unicase.patchAttachment/org.unicase.patchAttachment/src/org/unicase/patchAttachment/exported/IApplyPatchMethod.java
+++
@@ -1,9 +0,0 @@
-package org.unicase.patchAttachment.exported;
-
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IStorage;
-
-public interface IApplyPatchMethod {
-
-	void applyPatch(IStorage patch, IResource target);
-}
