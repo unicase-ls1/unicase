7a6ea4939b49aac301fd732ee7a21ea87b52fc0c
pfeiffec@in.tum.de <pfeiffec@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-09-23 10:34:59 +0000

    ba work

---
+++ carl/org.unicase.ui.refactoring/src/org/unicase/ui/refactoring/specific/strategies/dialogs/wizards/AIEmbodiesFrRefactoringWizard.java
@@ -0,0 +1,34 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
+package org.unicase.ui.refactoring.specific.strategies.dialogs.wizards;
+
+import org.unicase.ui.refactoring.specific.strategies.dialogs.wizards.pages.AIEmbodiesFRPage1;
+import org.unicase.ui.refactoring.specific.strategies.dialogs.wizards.pages.AIEmbodiesFRPage2;
+import org.unicase.ui.refactoring.templates.AbstractRefactoringStrategy;
+import org.unicase.ui.refactoring.templates.dialogs.wizards.AbstractRefactoringWizard;
+
+/**
+ * @author pfeifferc
+ */
+public class AIEmbodiesFrRefactoringWizard extends AbstractRefactoringWizard {
+
+	/**
+	 * @param abstractRefactoringStrategy the
+	 */
+	public AIEmbodiesFrRefactoringWizard(AbstractRefactoringStrategy abstractRefactoringStrategy) {
+		super(abstractRefactoringStrategy);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void addPages() {
+		addPage(new AIEmbodiesFRPage1("page_1", this));
+		addPage(new AIEmbodiesFRPage2("page_2", this));
+	}
+}
