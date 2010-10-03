e71e81109a9a3936bdf123f8759c133a03e5f030
koegel@headstrong.de <koegel@headstrong.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-10-03 18:18:46 +0000

    add missing developer info to branch

---
+++ developer/CodeExamples/exampleModel/src/library/LibraryBase.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package library;
+
+import org.unicase.metamodel.ModelElement;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object '
+ * <em><b>Base</b></em>'. <!-- end-user-doc -->
+ * 
+ * 
+ * @see library.LibraryPackage#getLibraryBase()
+ * @model abstract="true"
+ * @extends ModelElement
+ * @generated
+ */
+public interface LibraryBase extends ModelElement {
+} // LibraryBase
---
+++ developer/CodeExamples/exampleModel/src/library/impl/LibraryBaseImpl.java
@@ -0,0 +1,44 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package library.impl;
+
+import library.LibraryBase;
+import library.LibraryPackage;
+
+import org.eclipse.emf.ecore.EClass;
+import org.unicase.metamodel.impl.ModelElementImpl;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model object '
+ * <em><b>Base</b></em>'. <!-- end-user-doc -->
+ * <p>
+ * </p>
+ * 
+ * @generated
+ */
+public abstract class LibraryBaseImpl extends ModelElementImpl implements
+		LibraryBase {
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected LibraryBaseImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return LibraryPackage.Literals.LIBRARY_BASE;
+	}
+
+} // LibraryBaseImpl
