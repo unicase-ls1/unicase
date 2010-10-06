e13896d19a01897f68a0a7e9f04316f70b4868d9
helming@in.tum.de <helming@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-10-06 14:07:38 +0000

    added to tag

---
+++ 0.6.3.nightly/org.unicase.ecpemfstorebridge/src/org/unicase/ecpemfstorebridge/EMFStoreECPProject.java
@@ -0,0 +1,213 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ecpemfstorebridge;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.swt.widgets.Display;
+import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
+import org.unicase.metamodel.AssociationClassElement;
+import org.unicase.metamodel.ModelElement;
+import org.unicase.metamodel.NonDomainElement;
+import org.unicase.metamodel.Project;
+import org.unicase.metamodel.util.ProjectChangeObserver;
+import org.unicase.ui.common.ECPAssociationClassElement;
+import org.unicase.ui.common.MetaModelElementContext;
+import org.unicase.ui.navigator.workSpaceModel.ECPProject;
+import org.unicase.ui.navigator.workSpaceModel.impl.ECPProjectImpl;
+import org.unicase.workspace.Configuration;
+import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.observers.SimpleOperationListener;
+
+/**
+ * ECPproject for the EMFStore.
+ * 
+ * @author helming
+ */
+public class EMFStoreECPProject extends ECPProjectImpl implements ECPProject, ProjectChangeObserver {
+
+	private final ProjectSpace projectSpace;
+	private SimpleOperationListener simpleOperationListener;
+
+	/**
+	 * Default constructor.
+	 * 
+	 * @param projectSpace the projectspace
+	 */
+	public EMFStoreECPProject(ProjectSpace projectSpace) {
+		this.projectSpace = projectSpace;
+		setRootObject(projectSpace);
+		simpleOperationListener = new SimpleOperationListener() {
+
+			@Override
+			public void operationPerformed(AbstractOperation operation) {
+				Display.getDefault().asyncExec(new Runnable() {
+
+					public void run() {
+						projectChanged();
+					}
+
+				});
+			}
+
+		};
+		projectSpace.addOperationListener(simpleOperationListener);
+		projectSpace.getProject().addProjectChangeObserver(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.common.ECPModelelementContext#getAllModelElements()
+	 */
+	public Collection<EObject> getAllModelElements() {
+		ArrayList<EObject> ret = new ArrayList<EObject>();
+		ret.addAll(projectSpace.getProject().getAllModelElements());
+		return ret;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.common.ECPModelelementContext#getEditingDomain()
+	 */
+	public EditingDomain getEditingDomain() {
+		return Configuration.getEditingDomain();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#contains(org.eclipse.emf.ecore.EObject)
+	 */
+	public boolean contains(EObject eObject) {
+		return (((ModelElement) eObject).getProject().equals(projectSpace.getProject()));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#getAllModelElement()
+	 */
+	public Collection<EObject> getAllModelElement() {
+		Collection<EObject> ret = new BasicEList<EObject>();
+		ret.addAll(projectSpace.getProject().getAllModelElements());
+		return ret;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#getAllModelElementsbyClass(org.eclipse.emf.ecore.EClass,
+	 *      org.eclipse.emf.common.util.BasicEList)
+	 */
+	public Collection<EObject> getAllModelElementsbyClass(EClass clazz, BasicEList<EObject> basicEList) {
+		Collection<EObject> ret = new BasicEList<EObject>();
+		ret.addAll(projectSpace.getProject().getAllModelElementsbyClass(clazz, new BasicEList<ModelElement>()));
+		return ret;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#getMetaModelElementContext()
+	 */
+	public MetaModelElementContext getMetaModelElementContext() {
+		return EMFStoreMetaModelElementContext.getInstance();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#isNonDomainElement(org.eclipse.emf.ecore.EObject)
+	 */
+	public boolean isNonDomainElement(EObject eObject) {
+		return (eObject instanceof NonDomainElement);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#dispose()
+	 */
+	public void dispose() {
+		projectSpace.removeOperationListener(simpleOperationListener);
+		projectSpace.getProject().removeProjectChangeObserver(this);
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.util.ProjectChangeObserver#modelElementAdded(org.unicase.metamodel.Project,
+	 *      org.unicase.metamodel.ModelElement)
+	 */
+	public void modelElementAdded(Project project, ModelElement modelElement) {
+		// Do nothing
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.util.ProjectChangeObserver#modelElementRemoved(org.unicase.metamodel.Project,
+	 *      org.unicase.metamodel.ModelElement)
+	 */
+	public void modelElementRemoved(Project project, ModelElement modelElement) {
+		super.modelelementDeleted(modelElement);
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.util.ProjectChangeObserver#notify(org.eclipse.emf.common.notify.Notification,
+	 *      org.unicase.metamodel.Project, org.unicase.metamodel.ModelElement)
+	 */
+	public void notify(Notification notification, Project project, ModelElement modelElement) {
+		// Do nothing
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.util.ProjectChangeObserver#projectDeleted(org.unicase.metamodel.Project)
+	 */
+	public void projectDeleted(Project project) {
+		super.projectDeleted();
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#isAssociationClassElement(org.eclipse.emf.ecore.EObject)
+	 */
+	public boolean isAssociationClassElement(EObject eObject) {
+		return (eObject instanceof AssociationClassElement);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.navigator.workSpaceModel.ECPProject#getAssociationClassElement(org.eclipse.emf.ecore.EObject)
+	 */
+	public ECPAssociationClassElement getAssociationClassElement(EObject eObject) {
+		if (isAssociationClassElement(eObject)) {
+			AssociationClassElement ace = (AssociationClassElement) eObject;
+			return new ECPAssociationClassElement(ace.getSourceFeature(), ace.getTargetFeature(), ace
+				.getAssociationFeatures());
+		}
+		return null;
+	}
+}
