eeff7de75595526cd09558767aec00e9c5b09b32
hodamng <hodamng@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-10-07 16:15:10 +0000

    edited the test case and fixed the bug in the update method

--- test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/ShareProjectUITest.java
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/ShareProjectUITest.java
@@ -1,6 +1,12 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
 package org.unicase.ui.test.navigator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,6 +30,11 @@ import org.unicase.workspace.WorkspaceManager;
 import org.unicase.workspace.test.SetupHelper;
 import org.unicase.workspace.util.UnicaseCommand;
 
+/**
+ * Class to test if a local project (i.e. located on your workspace only) is correctly shared on the UNICASE Server or
+ * not. Also it tests if the created project appears on the UNICASE Navigator as shared or not.
+ */
+
 public class ShareProjectUITest extends MEEditorTest {
 
 	private ProjectSpace projectSpace;
@@ -38,7 +49,7 @@ public class ShareProjectUITest extends MEEditorTest {
 	}
 
 	/**
-	 * This method shares the project progrmaticaly and then tests if its reflected on the UI or not.
+	 * This method shares the project programaticaly and then tests if its reflected on the UI or not.
 	 */
 	@Test
 	public void updateProjectUpdate() {
@@ -56,7 +67,8 @@ public class ShareProjectUITest extends MEEditorTest {
 				Usersession usersession = UITestCommon.createUsersession(user1);
 				try {
 					usersession.logIn();
-					projectSpace.shareProject(usersession);
+					projectSpace.shareProject(usersession); // the project has been shared programaticaly using the
+					// command shareProject();
 				} catch (AccessControlException e1) {
 
 				} catch (EmfStoreException e1) {
@@ -71,7 +83,8 @@ public class ShareProjectUITest extends MEEditorTest {
 		UITestCommon.openView(getBot(), "Unicase", "Unicase Navigator");
 
 		SWTBotTreeItem[] checktext = getBot().activeView().bot().tree().getAllItems();
-		Pattern p = Pattern.compile("@");
+		Pattern p = Pattern.compile("@"); // this is to indicate that the project is being shared is reflected on the UI
+		// ,as it will have a version number that starts with @
 
 		Matcher m = p.matcher(checktext[0].getText());
 		assertEquals(true, m.find());
@@ -79,26 +92,42 @@ public class ShareProjectUITest extends MEEditorTest {
 	}
 
 	/**
-	 * This method shares the project from the UI and then tests if it was shared on the server.
+	 * This method shares the project from the UI and then tests if it was shared on the server or not.
 	 */
 	@Test
 	public void updateProjectChange() {
 		UITestCommon.openView(getBot(), "Unicase", "Unicase Navigator");
 
+		// The project is shared using the UI functionalities
 		SWTBotTreeItem[] checktext = getBot().activeView().bot().tree().getAllItems();
 		checktext[0].select().contextMenu("Share  Project").click();// the text of the context menu contains double
-		// space for some reason
+		// spaces
 		getBot().sleep(1000);
 		SWTBotShell openPerspectiveShell = getBot().shell("Select Usersession");
 		openPerspectiveShell.activate();
-		getBot().table().select(0);
+		getBot().table().select(0);// selects the user session (in this example there is only one user)
 		getBot().sleep(1000);
 		getBot().button("OK").click();
 		openPerspectiveShell = getBot().shell("Authentication required");
 		openPerspectiveShell.activate();
-		getBot().text(1).typeText("jo");
-		System.out.print("------->" + getBot().text(1).getText());
-		getBot().sleep(10000);
+		getBot().text(1).typeText("super"); // enters the user password to connect to the server
+		getBot().button("OK").click();
+		getBot().sleep(1000);
+		getBot().button("OK").click();
+		getBot().sleep(1000);
+
+		new UnicaseCommand() {
+
+			@Override
+			protected void doRun() {
+				Workspace currentWorkspace = WorkspaceManager.getInstance().getCurrentWorkspace();
+				projectSpace = currentWorkspace.getActiveProjectSpace();
+				assertNotNull(projectSpace.getBaseVersion()); // if the project dosn't have a base version, i.e. its
+				// null. Then, it was not shared on the server and thus the test fails
+			}
+		}.run();
+		getBot().sleep(1000);
+
 	}
 
 	/**
