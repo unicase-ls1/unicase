4babe70dcaa0fe372ed2f869f55e0f18ae51c77d
pfeiffec@in.tum.de <pfeiffec@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-10-10 23:55:18 +0000

    ba carl2

---
+++ carl2/org.unicase.model/src/org/unicase/model/classes/Package.java
@@ -0,0 +1,71 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.classes;
+
+import org.eclipse.emf.common.util.EList;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object ' <em><b>Package</b></em>'. <!-- end-user-doc -->
+ * <p>
+ * The following features are supported:
+ * <ul>
+ * <li>{@link org.unicase.model.classes.Package#getContainedPackageElements <em>Contained Package Elements</em>}</li>
+ * <li>{@link org.unicase.model.classes.Package#getFacadeClass <em>Facade Class</em>}</li>
+ * </ul>
+ * </p>
+ * 
+ * @see org.unicase.model.classes.ClassesPackage#getPackage()
+ * @model
+ * @generated
+ */
+public interface Package extends PackageElement {
+	/**
+	 * Returns the value of the '<em><b>Contained Package Elements</b></em>' containment reference list. The list
+	 * contents are of type {@link org.unicase.model.classes.PackageElement}. It is bidirectional and its opposite is '
+	 * {@link org.unicase.model.classes.PackageElement#getParentPackage <em>Parent Package</em>}'. <!-- begin-user-doc
+	 * -->
+	 * <p>
+	 * If the meaning of the '<em>Contained Package Elements</em>' containment reference list isn't clear, there really
+	 * should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Contained Package Elements</em>' containment reference list.
+	 * @see org.unicase.model.classes.ClassesPackage#getPackage_ContainedPackageElements()
+	 * @see org.unicase.model.classes.PackageElement#getParentPackage
+	 * @model opposite="parentPackage" containment="true" resolveProxies="true"
+	 *        annotation="org.unicase.ui.meeditor priority='9.5' position='right'"
+	 * @generated
+	 */
+	EList<PackageElement> getContainedPackageElements();
+
+	/**
+	 * Returns the value of the '<em><b>Facade Class</b></em>' reference. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Facade Class</em>' reference isn't clear, there really should be more of a description
+	 * here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Facade Class</em>' reference.
+	 * @see #setFacadeClass(org.unicase.model.classes.Class)
+	 * @see org.unicase.model.classes.ClassesPackage#getPackage_FacadeClass()
+	 * @model annotation="org.unicase.ui.meeditor priority='20.0' position='left'"
+	 * @generated
+	 */
+	org.unicase.model.classes.Class getFacadeClass();
+
+	/**
+	 * Sets the value of the '{@link org.unicase.model.classes.Package#getFacadeClass <em>Facade Class</em>}' reference.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value the new value of the '<em>Facade Class</em>' reference.
+	 * @see #getFacadeClass()
+	 * @generated
+	 */
+	void setFacadeClass(org.unicase.model.classes.Class value);
+
+} // Package
---
+++ carl2/org.unicase.model/src/org/unicase/model/rationale/Solution.java
@@ -0,0 +1,71 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.rationale;
+
+import org.eclipse.emf.common.util.EList;
+import org.unicase.metamodel.NonDomainElement;
+import org.unicase.model.UnicaseModelElement;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object ' <em><b>Solution</b></em>'. <!-- end-user-doc -->
+ * <p>
+ * The following features are supported:
+ * <ul>
+ * <li>{@link org.unicase.model.rationale.Solution#getUnderlyingProposals <em>Underlying Proposals</em>}</li>
+ * <li>{@link org.unicase.model.rationale.Solution#getIssue <em>Issue</em>}</li>
+ * </ul>
+ * </p>
+ * 
+ * @see org.unicase.model.rationale.RationalePackage#getSolution()
+ * @model
+ * @generated
+ */
+public interface Solution extends UnicaseModelElement, NonDomainElement {
+	/**
+	 * Returns the value of the '<em><b>Underlying Proposals</b></em>' reference list. The list contents are of type
+	 * {@link org.unicase.model.rationale.Proposal}. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Underlying Proposals</em>' reference list isn't clear, there really should be more of
+	 * a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Underlying Proposals</em>' reference list.
+	 * @see org.unicase.model.rationale.RationalePackage#getSolution_UnderlyingProposals()
+	 * @model annotation="org.unicase.ui.meeditor priority='10.0' position='right'"
+	 * @generated
+	 */
+	EList<Proposal> getUnderlyingProposals();
+
+	/**
+	 * Returns the value of the '<em><b>Issue</b></em>' container reference. It is bidirectional and its opposite is '
+	 * {@link org.unicase.model.rationale.Issue#getSolution <em>Solution</em>}'. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Issue</em>' container reference isn't clear, there really should be more of a
+	 * description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Issue</em>' container reference.
+	 * @see #setIssue(Issue)
+	 * @see org.unicase.model.rationale.RationalePackage#getSolution_Issue()
+	 * @see org.unicase.model.rationale.Issue#getSolution
+	 * @model opposite="solution" transient="false" annotation="org.unicase.ui.meeditor priority='10.0' position='left'"
+	 * @generated
+	 */
+	Issue getIssue();
+
+	/**
+	 * Sets the value of the '{@link org.unicase.model.rationale.Solution#getIssue <em>Issue</em>}' container reference.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value the new value of the '<em>Issue</em>' container reference.
+	 * @see #getIssue()
+	 * @generated
+	 */
+	void setIssue(Issue value);
+
+} // Solution
