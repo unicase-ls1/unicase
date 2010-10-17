2bd3973e0023aa2ea805680d21a3fe22804f894c
yasmine8726 <yasmine8726@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-10-17 11:54:22 +0000

    tried to create 2 different users on the server for ui test

--- test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/ShareProjectUITest.java
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/ShareProjectUITest.java
@@ -5,29 +5,33 @@
  */
 package org.unicase.ui.test.navigator;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.unicase.emfstore.exceptions.AccessControlException;
+import org.unicase.emfstore.ServerConfiguration;
+import org.unicase.emfstore.esmodel.SessionId;
+import org.unicase.emfstore.esmodel.accesscontrol.ACOrgUnitId;
+import org.unicase.emfstore.esmodel.accesscontrol.roles.RolesPackage;
 import org.unicase.emfstore.exceptions.EmfStoreException;
-import org.unicase.metamodel.Project;
-import org.unicase.model.organization.OrganizationFactory;
-import org.unicase.model.organization.User;
+import org.unicase.metamodel.MetamodelFactory;
 import org.unicase.ui.test.UITestCommon;
 import org.unicase.ui.test.meeditor.MEEditorTest;
+import org.unicase.workspace.Configuration;
 import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.ServerInfo;
 import org.unicase.workspace.Usersession;
 import org.unicase.workspace.Workspace;
+import org.unicase.workspace.WorkspaceFactory;
 import org.unicase.workspace.WorkspaceManager;
+import org.unicase.workspace.connectionmanager.ConnectionManager;
+import org.unicase.workspace.connectionmanager.KeyStoreManager;
+import org.unicase.workspace.impl.WorkspaceImpl;
 import org.unicase.workspace.test.SetupHelper;
+import org.unicase.workspace.test.server.ServerTests;
 import org.unicase.workspace.util.UnicaseCommand;
 
 /**
@@ -38,6 +42,10 @@ import org.unicase.workspace.util.UnicaseCommand;
 public class ShareProjectUITest extends MEEditorTest {
 
 	private ProjectSpace projectSpace;
+	private static ConnectionManager connectionManager;
+	private static SessionId sessionId;
+
+	private Usersession usersession;
 
 	/**
 	 * Setup the environment for testing by starting the server.
@@ -45,7 +53,48 @@ public class ShareProjectUITest extends MEEditorTest {
 	@BeforeClass
 	public static void beforeClass() {
 
+		ServerConfiguration.setTesting(true);
+		SetupHelper.addUserFileToServer(false);
 		SetupHelper.startSever();
+		connectionManager = WorkspaceManager.getInstance().getConnectionManager();
+		try {
+			login(SetupHelper.getServerInfo());
+		} catch (EmfStoreException e) {
+			// TODO Auto-generated catch block
+			// Do NOT catch all Exceptions ("catch (Exception e)")
+			// Log AND handle Exceptions if possible
+			//
+			// You can just uncomment one of the lines below to log an exception:
+			// logException will show the logged excpetion to the user
+			// ModelUtil.logException(e);
+			// ModelUtil.logException("YOUR MESSAGE HERE", e);
+			// logWarning will only add the message to the error log
+			// ModelUtil.logWarning("YOUR MESSAGE HERE", e);
+			// ModelUtil.logWarning("YOUR MESSAGE HERE");
+			//			
+			// If handling is not possible declare and rethrow Exception
+		}
+	}
+
+	/**
+	 * @param serverInfo serverinfo
+	 * @throws EmfStoreException in case of failure
+	 */
+	protected static void login(ServerInfo serverInfo) throws EmfStoreException {
+		sessionId = login(serverInfo, "super", "super");
+		WorkspaceManager.getInstance().getAdminConnectionManager().initConnection(serverInfo, sessionId);
+	}
+
+	/**
+	 * @param serverInfo serverinfo
+	 * @param username username
+	 * @param password password
+	 * @return sessionId
+	 * @throws EmfStoreException in case of failure
+	 */
+	protected static SessionId login(ServerInfo serverInfo, String username, String password) throws EmfStoreException {
+		return connectionManager.logIn(username, KeyStoreManager.getInstance().encrypt(password, serverInfo),
+			serverInfo, Configuration.getClientVersion());
 	}
 
 	/**
@@ -53,42 +102,132 @@ public class ShareProjectUITest extends MEEditorTest {
 	 */
 	@Test
 	public void updateProjectUpdate() {
-		UITestCommon.openView(getBot(), "Unicase", "Navigator");
+		// UITestCommon.openView(getBot(), "Unicase", "Navigator");
 		new UnicaseCommand() {
 
 			@Override
 			protected void doRun() {
-				Workspace currentWorkspace = WorkspaceManager.getInstance().getCurrentWorkspace();
-				projectSpace = currentWorkspace.getActiveProjectSpace();
-				Project project = projectSpace.getProject();
-
-				User user1 = OrganizationFactory.eINSTANCE.createUser();
-				user1.setName("super");
-				project.addModelElement(user1);
-				Usersession usersession = UITestCommon.createUsersession(user1, "super");
 				try {
-					usersession.logIn();
-					projectSpace.shareProject(usersession); // the project has been shared programaticaly using the
-					// command shareProject();
-				} catch (AccessControlException e1) {
+					ProjectSpace projectSpace = WorkspaceFactory.eINSTANCE.createProjectSpace();
+					projectSpace.setProject(MetamodelFactory.eINSTANCE.createProject());
+					projectSpace.setProjectName("Testproject");
+					projectSpace.setProjectDescription("Test description");
+					projectSpace.setLocalOperations(WorkspaceFactory.eINSTANCE.createOperationComposite());
 
-				} catch (EmfStoreException e1) {
+					Workspace workSpace = WorkspaceManager.getInstance().getCurrentWorkspace();
+					projectSpace.initResources(workSpace.eResource().getResourceSet());
+
+					((WorkspaceImpl) workSpace).addProjectSpace(projectSpace);
+					workSpace.save();
+
+					if (usersession == null) {
+						usersession = WorkspaceFactory.eINSTANCE.createUsersession();
+						usersession.setServerInfo(SetupHelper.getServerInfo());
+						usersession.setUsername("super");
+						usersession.setPassword("super");
+						WorkspaceManager.getInstance().getCurrentWorkspace().getUsersessions().add(usersession);
+					}
+					try {
+						if (!usersession.isLoggedIn()) {
+							usersession.logIn();
+						}
+
+						projectSpace.shareProject(usersession);
+					} catch (EmfStoreException e) {
+						e.printStackTrace();
+					}
+					ACOrgUnitId orgUnitId = SetupHelper.createUserOnServer(sessionId, "projectadmin");
+					SetupHelper.setUsersRole(sessionId, orgUnitId, RolesPackage.eINSTANCE.getProjectAdminRole(),
+						projectSpace.getProjectId());
+					// ServerTests.getConnectionManager().logout(ServerTests.getSessionId());
+					ServerTests.getConnectionManager().logIn("projectadmin",
+						KeyStoreManager.getInstance().encrypt("foo", SetupHelper.getServerInfo()),
+						SetupHelper.getServerInfo(), Configuration.getClientVersion());
+
+				} catch (EmfStoreException e) {
 
 				}
+				// Workspace currentWorkspace = WorkspaceManager.getInstance().getCurrentWorkspace();
+				// projectSpace = currentWorkspace.getActiveProjectSpace();
+				// Usersession usersession = getProjectSpace().getUsersession();
+				// if (usersession == null) {
+				// usersession = WorkspaceFactory.eINSTANCE.createUsersession();
+				// ServerInfo serverInfo = SetupHelper.getServerInfo();
+				// usersession.setServerInfo(serverInfo);
+				// usersession.setUsername("super");
+				// usersession.setPassword("super");
+				// }
+				//
+				// if (!usersession.isLoggedIn()) {
+				// try {
+				// usersession.logIn();
+				// WorkspaceManager.getInstance().getAdminConnectionManager().initConnection(
+				// SetupHelper.getServerInfo(), usersession.getSessionId());
+				// } catch (AccessControlException e) {
+				// e.printStackTrace();
+				// } catch (EmfStoreException e) {
+				// e.printStackTrace();
+				// }
+				// }
+				//
+				// try {
+				// ACOrgUnitId orgUnitId = SetupHelper.createUserOnServer(usersession.getSessionId(), "foo");
+				// SetupHelper.setUsersRole(usersession.getSessionId(), orgUnitId, RolesPackage.eINSTANCE
+				// .getServerAdmin(), projectSpace.getProjectId());
+				// } catch (EmfStoreException e1) {
+				//
+				// }
+				//
+				// try {
+				// usersession.logout();
+				// } catch (EmfStoreException e1) {
+				//
+				// }
+				// usersession.setServerInfo(SetupHelper.getServerInfo());
+				// usersession.setUsername("foo");
+				// usersession.setPassword("foo");
+				//
+				// if (!usersession.isLoggedIn()) {
+				// try {
+				// usersession.logIn();
+				// } catch (AccessControlException e) {
+				// e.printStackTrace();
+				// } catch (EmfStoreException e) {
+				// e.printStackTrace();
+				// }
+				// }
+				// Workspace currentWorkspace = WorkspaceManager.getInstance().getCurrentWorkspace();
+				// projectSpace = currentWorkspace.getActiveProjectSpace();
+				// Project project = projectSpace.getProject();
+				//
+				// User user1 = OrganizationFactory.eINSTANCE.createUser();
+				// user1.setName("super");
+				// project.addModelElement(user1);
+				// Usersession usersession = UITestCommon.createUsersession(user1, "super");
+				// try {
+				// usersession.logIn();
+				// projectSpace.shareProject(usersession); // the project has been shared programaticaly using the
+				// // command shareProject();
+				// } catch (AccessControlException e1) {
+				//
+				// } catch (EmfStoreException e1) {
+				//
+				// }
 
 			}
 		}.run();
 
 		getBot().sleep(3000);
 
-		UITestCommon.openView(getBot(), "Unicase", "Navigator");
-
-		SWTBotTreeItem[] checktext = getBot().activeView().bot().tree().getAllItems();
-		Pattern p = Pattern.compile("@"); // this is to indicate that the project is being shared is reflected on the UI
-		// ,as it will have a version number that starts with @
-
-		Matcher m = p.matcher(checktext[0].getText());
-		assertEquals(true, m.find());
+		// UITestCommon.openView(getBot(), "Unicase", "Navigator");
+		//
+		// SWTBotTreeItem[] checktext = getBot().activeView().bot().tree().getAllItems();
+		// Pattern p = Pattern.compile("@"); // this is to indicate that the project is being shared is reflected on the
+		// UI
+		// // ,as it will have a version number that starts with @
+		//
+		// Matcher m = p.matcher(checktext[0].getText());
+		// assertEquals(true, m.find());
 
 	}
 
