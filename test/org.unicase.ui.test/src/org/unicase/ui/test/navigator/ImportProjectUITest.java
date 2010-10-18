f267771ff5eb6757acefde965ac3205100150d46
hodamng <hodamng@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-10-18 12:37:20 +0000

    new imp. of the importing test case

---
+++ test/org.unicase.ui.test/src/org/unicase/ui/test/navigator/ImportProjectUITest.java
@@ -0,0 +1,15 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
+package org.unicase.ui.test.navigator;
+
+/**
+ * Class to test if a project is is updated correctly in a way that the project is successfully synchronized between the
+ * UI and the UNICASE repository .
+ */
+public class ImportProjectUITest {
+
+}
