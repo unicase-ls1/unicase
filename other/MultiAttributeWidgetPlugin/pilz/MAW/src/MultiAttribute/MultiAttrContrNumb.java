3e84524ac9431f016f5741fb4b631664c9245857
worldwidetobi@gmx.de <worldwidetobi@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-05 15:54:47 +0000

    5.11.2010 16:56 final version #1

--- other/MultiAttributeWidgetPlugin/pilz/MAW/src/MultiAttribute/MultiAttrContrNumb.java
+++ other/MultiAttributeWidgetPlugin/pilz/MAW/src/MultiAttribute/MultiAttrContrNumb.java
@@ -1,11 +1,9 @@
 package MultiAttribute;
 
-import java.awt.GridBagLayout;
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
-import javax.swing.Icon;
 import javax.swing.JOptionPane;
 
 import org.eclipse.emf.ecore.EObject;
@@ -13,14 +11,10 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.impl.ETypedElementImpl;
 import org.eclipse.emf.ecore.util.EDataTypeEList;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
-import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
-import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -38,7 +32,6 @@ public class MultiAttrContrNumb extends AbstractMEControl {
 	}
 	@Override
 	public int canRender(IItemPropertyDescriptor arg0, EObject arg1) {
-		// TODO Auto-generated method stub
 		Object feature = arg0.getFeature(arg1);
 		ETypedElementImpl attr = null;
 		if (feature instanceof ETypedElementImpl){
@@ -61,17 +54,23 @@ public class MultiAttrContrNumb extends AbstractMEControl {
 		gridLayout.numColumns = 1;
 		composite.setLayout(gridLayout);
 		
+		//list you actually work on
 		final List listCtrl = new List(composite, SWT.SINGLE | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		
+		// feature
 		final EStructuralFeature feature = (EStructuralFeature) getItemPropertyDescriptor().getFeature(getModelElement());
+		
+		//list containing Long numbers
 		final EDataTypeEList<Long> data = (EDataTypeEList<Long>) getModelElement().eGet(feature);
 		for (Long i : data) {
 			listCtrl.add(i.toString());
 		}
 		
+		//Add-Button
 		Button btnAdd = new Button(composite, SWT.PUSH);
 		btnAdd.setText("+");
 		btnAdd.addSelectionListener(new SelectionAdapter() {
+			
 			@Override
 			public void widgetSelected(SelectionEvent e){
 				
@@ -79,46 +78,46 @@ public class MultiAttrContrNumb extends AbstractMEControl {
 					
 					@Override
 					protected void doRun() {
+						
+						//determine if a number was entered or not 
 						boolean validInput = false;
 						while(validInput == false){
-							String input = JOptionPane.showInputDialog("Please enter a Number");
-//							InputDialog inputDialog = new InputDialog(null, "New Number", "Enter new number here", null, null);
-//							inputDialog.setBlockOnOpen(true);
-//							inputDialog.open();
-//							String input = inputDialog.getValue();
 							
-						  
+							//Input window
+							String input = JOptionPane.showInputDialog("Please enter a Number");
 							try {
-								if(input == null){
-									return;
+									//if pressing cancel or close
+									if(input == null)
+									{
+										return;
+									}
+									else
+									{
+										
+										//check if given input is a Long or not
+										Long i = Long.valueOf(input);
+										listCtrl.add(i.toString());
+										data.addUnique(i);
+										validInput = true;
+									}
 								}
-								else{
-								Long i = Long.valueOf(input);
-								listCtrl.add(i.toString());
-								data.addUnique(i);
-								validInput = true;
-								}
-							}
 							
 							catch(Exception e) {
-//								String[] label = new String[1];
-//								label[0] = "OK";
-//								MessageDialog md = new MessageDialog(null, "False Entry" , null, "Not a Number!", MessageDialog.ERROR, label , 1);
-//								md.setBlockOnOpen(true);
-//								md.open();
-								JOptionPane.showMessageDialog(null, "Not a Number!", "False Entry", JOptionPane.ERROR_MESSAGE);
+									
+									//if it is not a Long/Integer than error message
+									JOptionPane.showMessageDialog(null, "Not a Number!", "False Entry", JOptionPane.ERROR_MESSAGE);
 							}
-						
 						}	
 					}
 				}.run();
 			}
-			
 		});
 		
+		//Edit-Button
 		Button btnEdit = new Button(composite, SWT.PUSH);
 		btnEdit.setText("...");
 		btnEdit.addSelectionListener(new SelectionAdapter() {
+			
 			@Override
 			public void widgetSelected(SelectionEvent e){
 				
@@ -126,38 +125,49 @@ public class MultiAttrContrNumb extends AbstractMEControl {
 					
 					@Override
 					protected void doRun() {
-						// TODO Auto-generated method stub
+						
+						//determine if a number was entered or not
 						boolean validInput = false;
 						int SelectedIndex = listCtrl.getSelectionIndex();
 						while(validInput == false){
+							
+							//Input window
 							String input = JOptionPane.showInputDialog("Please Edit...", listCtrl.getItem(SelectedIndex).toString());
-							try{
-								if (input == null){
-									return;
+							try
+								{
+									//if pressing cancel or close
+									if (input == null){
+										return;
+									}
+									
+									//check if given input is a Long or not
+									Long i = Long.valueOf(input);
+									
+									//throw old value out and put new one in
+									listCtrl.remove(SelectedIndex);
+									listCtrl.add(i.toString(), SelectedIndex);
+									data.remove(SelectedIndex);
+									data.addUnique(SelectedIndex, i);
+									
+									//exit while loop
+									validInput = true;
 								}
-								Long i = Long.valueOf(input);
-							    listCtrl.remove(SelectedIndex);
-								listCtrl.add(i.toString(), SelectedIndex);
-							    data.remove(SelectedIndex);
-							    data.addUnique(SelectedIndex, i);
-							    validInput = true;
-							}
-							    
 							catch (Exception e){
+								
+								//if it is not a Long/Integer than error message
 								JOptionPane.showMessageDialog(null, "Not a Number!", "False Entry", JOptionPane.ERROR_MESSAGE);
 							}
-							 
 						}
-						
 					}
 				}.run();
 			}
-			
 		});
 		
+		//Delete-Button
 		Button btnDelete = new Button(composite, SWT.PUSH);
 		btnDelete.setText("-");
 		btnDelete.addSelectionListener(new SelectionAdapter() {
+			
 			@Override
 			public void widgetSelected(SelectionEvent e){
 				
@@ -165,20 +175,19 @@ public class MultiAttrContrNumb extends AbstractMEControl {
 					
 					@Override
 					protected void doRun() {
-						// TODO Auto-generated method stub
+								
+								//delete selected value
 								int SelectedIndex = listCtrl.getSelectionIndex();
 								listCtrl.remove(SelectedIndex);
 								data.remove(SelectedIndex);
 							
 							
-						}
-						
-					}.run();
-				}
-			
-			
+					}
+				}.run();
+			}
 		});
 		
+		//Sort-Button
 		Button btnSort = new Button(composite, SWT.PUSH);
 		btnSort.setText("Sort");
 		btnSort.addSelectionListener(new SelectionAdapter() {
@@ -190,58 +199,41 @@ public class MultiAttrContrNumb extends AbstractMEControl {
 					
 					@Override
 					protected void doRun() {
-						// TODO Auto-generated method stub
+							
+							//copy everything in an arrayList to be able to sort the entries
 							ArrayList<Long> al = new ArrayList<Long>();
-							for(int i = 0; i < data.size(); i++){
+							for(int i = 0; i < data.size(); i++)
+							{
 								Long output = data.basicGet(i);
 								al.add(output);
 							}
+							
+							//empty data and listCtrl to have no doubles in the lists
 							data.removeAll(al);
 							listCtrl.removeAll();							
+							
+							//sort
 							Collections.sort(al);
 							
-							for (Long i : al){
+							//copy the sorted arrayList back into data and listCtrl
+							for (Long i : al)
+							{
 								listCtrl.add(i.toString());
 								data.addUnique(i);
 							}
-							}
-					
-							
-						
-						
-					}.run();
-				
+						}
+				}.run();
 			}
-			
 		});
 		
-		
+		//create widget
 		GridDataFactory.fillDefaults().span(4, 1).hint(40, 80).applyTo(listCtrl);
 		GridDataFactory.fillDefaults().applyTo(btnAdd);
 		GridDataFactory.fillDefaults().applyTo(btnEdit);
 		GridDataFactory.fillDefaults().applyTo(btnDelete);
 		GridDataFactory.fillDefaults().applyTo(btnSort);
-		/*
-		GridDataFactory.generate(btnAdd, 1, 1);
-		GridDataFactory.generate(btnEdit, 1, 1);
-		GridDataFactory.generate(btnDelete, 1, 1);
-		GridDataFactory.generate(btnSort, 1, 1);
-		*/
 		gridLayout.makeColumnsEqualWidth = true;
-//		GridBagLayout gridBagLayout = new GridBagLayout();
-//		double[] doublelayout = new double[2];
-//		doublelayout[0] = 5.0d;
-//		doublelayout[1] = 2.0d;
-//		int[] intlayout = new int[2];
-//		intlayout[0] = 5;
-//		intlayout[1] = 2;
-//		gridBagLayout.columnWeights = doublelayout;
-//		gridBagLayout.columnWidths = intlayout;
-//		gridBagLayout.rowHeights = intlayout;
-//		gridBagLayout.rowWeights = doublelayout;
-		
-		
-		
+				
 		return composite;
 	
 	}
--- other/MultiAttributeWidgetPlugin/pilz/MAW/src/MultiAttribute/MultiAttributeControl.java
+++ other/MultiAttributeWidgetPlugin/pilz/MAW/src/MultiAttribute/MultiAttributeControl.java
@@ -1,35 +1,23 @@
 package MultiAttribute;
 
-import java.security.AlgorithmParameterGenerator;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import javax.swing.JOptionPane;
-import javax.swing.text.html.HTMLDocument.HTMLReader.IsindexAction;
-
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.impl.EAttributeImpl;
 import org.eclipse.emf.ecore.impl.ETypedElementImpl;
 import org.eclipse.emf.ecore.util.EDataTypeEList;
-import org.eclipse.emf.ecore.util.EDataTypeUniqueEList;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Dialog;
 import org.eclipse.swt.widgets.List;
-import org.eclipse.swt.widgets.MessageBox;
 import org.unicase.ui.common.commands.ECPCommand;
 import org.unicase.ui.meeditor.mecontrols.AbstractMEControl;
 
@@ -37,14 +25,13 @@ public class MultiAttributeControl extends AbstractMEControl {
 
 	private static final int PRIORITY = 2;
 	public MultiAttributeControl() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public int canRender(IItemPropertyDescriptor arg0, EObject arg1) {
 		Object feature = arg0.getFeature(arg0);
 		ETypedElementImpl attr = null;
-		
 		if (feature instanceof ETypedElementImpl) {
 			 attr = (ETypedElementImpl) feature;
 			 if (attr.getUpperBound() == -1) {
@@ -58,33 +45,25 @@ public class MultiAttributeControl extends AbstractMEControl {
 
 	@Override
 	protected Control createControl(Composite parent, int style) {
-		// TODO Auto-generated method stub
 		Composite composite = getToolkit().createComposite(parent, style);
 		GridLayout gridLayout = new GridLayout();
 		gridLayout.numColumns = 4;
 		composite.setLayout(gridLayout);
+		//list you actually work on
 		final List listCtrl = new List(composite, SWT.SINGLE | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		//feature
 		final EStructuralFeature feature = (EStructuralFeature) getItemPropertyDescriptor().getFeature(getModelElement());
+		//list containing Strings
 		final EDataTypeEList<String> data = (EDataTypeEList<String>) getModelElement().eGet(feature);
 		for (Object obj : data) {
 			listCtrl.add(obj.toString());
 		}
-		/*
-		listCtrl.add("Test1");
-		listCtrl.add("Test2");
-		listCtrl.add("Test3");
-		*/
-		/*
-		GridData gridData = new GridData();
-		gridData.verticalSpan = 4;
-		gridData.heightHint = 200;
-		gridData.widthHint = 100;
-		listCtrl.setLayoutData(gridData);
-		*/
+		
+		//Add-Button
 		Button btnAdd = new Button(composite, SWT.PUSH);
 		btnAdd.setText("+");
-		
 		btnAdd.addSelectionListener(new SelectionAdapter() {
+			
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				
@@ -92,111 +71,115 @@ public class MultiAttributeControl extends AbstractMEControl {
 					
 					@Override
 					protected void doRun() {
-						// TODO Auto-generated method stub
+						// open window to let user enter a String
 						InputDialog dlg = new InputDialog(null, "New Middlename", "Enter new middlename here", null, null);
 						dlg.setBlockOnOpen(true);
 						dlg.open();
 						String input = dlg.getValue();
+						//add String to listCtrl and data
 						listCtrl.add(input);
-//						Object test = (Object) input;
 						data.addUnique(input);
-						//getModelElement().eSet(feature, data);
 					};
 				}.run();
 			}
 		});
 		
+		//Edit-Button
 		Button btnEdit = new Button(composite, SWT.PUSH);
 		btnEdit.setText("...");
-		
 		btnEdit.addSelectionListener(new SelectionAdapter(){
-		@Override
-		public void widgetSelected(SelectionEvent e){
-			
-			new ECPCommand(getModelElement()) {
-				
-				@Override
-				protected void doRun() {
-					// TODO Auto-generated method stub
-					
-					//data.get(index)
-					int SelectedIndex = listCtrl.getSelectionIndex();
-					InputDialog dlg = new InputDialog(null, "Edit Middlename", "Edit middlename here", listCtrl.getItem(SelectedIndex) , null);
-					data.remove(listCtrl.getItem(SelectedIndex));
-					dlg.setBlockOnOpen(true);
-					dlg.open();
-					String input = dlg.getValue();
-					if (input.isEmpty()){
-						return;
-					}
-					else
-					//listCtrl.setItem(listCtrl.getSelectionIndex(), input);
-					listCtrl.remove(SelectedIndex);
-					listCtrl.add(input, SelectedIndex);
-					
-					data.remove(listCtrl.getItem(SelectedIndex));
-					data.addUnique(SelectedIndex, input);
-		
-				}
-			}.run();
-		}
-		});
-		
-		
-		Button btnDelete = new Button(composite, SWT.PUSH);
-		btnDelete.setText("-");
-		btnDelete.addSelectionListener(new SelectionAdapter() {
+	
 			@Override
-			public void widgetSelected(SelectionEvent e) {
-				
+			public void widgetSelected(SelectionEvent e){
+			
 				new ECPCommand(getModelElement()) {
-					
+				
 					@Override
 					protected void doRun() {
-						// TODO Auto-generated method stub
-					
 						int SelectedIndex = listCtrl.getSelectionIndex();
-						listCtrl.remove(SelectedIndex);
-						data.remove(SelectedIndex);
-					}
+						// open window to let user edit a value
+						InputDialog dlg = new InputDialog(null, "Edit Middlename", "Edit middlename here", listCtrl.getItem(SelectedIndex) , null);
+						//remove selected entry from data
+						data.remove(listCtrl.getItem(SelectedIndex));
+						dlg.setBlockOnOpen(true);
+						dlg.open();
+						String input = dlg.getValue();
+						//if pressing cancel or close to ensure that the edited entry stays in the list 
+						if (input.isEmpty()){
+							return;
+						}
+						else
+							//edit value of the chosen entry
+							listCtrl.remove(SelectedIndex);
+							listCtrl.add(input, SelectedIndex);
+							data.remove(listCtrl.getItem(SelectedIndex));
+							data.addUnique(SelectedIndex, input);
+						}
 				}.run();
-				
 			}
 		});
 		
+		//Delete-Button
+		Button btnDelete = new Button(composite, SWT.PUSH);
+		btnDelete.setText("-");
+		btnDelete.addSelectionListener(new SelectionAdapter() {
+			
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					
+					new ECPCommand(getModelElement()) {
+					
+						@Override
+						protected void doRun() {
+							//delete selected value
+							int SelectedIndex = listCtrl.getSelectionIndex();
+							listCtrl.remove(SelectedIndex);
+							data.remove(SelectedIndex);
+						}
+					}.run();
+				}
+		});
+		
+		//Sort-Button
 		Button btnSort = new Button(composite, SWT.PUSH);
 		btnSort.setText("Sort");
 		btnSort.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e){
+			
+				@Override
+				public void widgetSelected(SelectionEvent e){
 				
-				new ECPCommand(getModelElement()) {
+					new ECPCommand(getModelElement()) {
 					
-					
-					
-					@Override
-					protected void doRun() {
-						// new list with strings to sort, because data cannot be sorted easily
-						ArrayList<String> al = new ArrayList<String>();
+						@Override
+						protected void doRun() {
+							// new list with Strings to sort, because data cannot be sorted easily
+							ArrayList<String> al = new ArrayList<String>();
 						
-						// add all strings from data to new list
-						for(int i = 0; i < data.size(); i++){
-							al.add(data.get(i));
+							// add all strings from data to new list
+							for(int i = 0; i < data.size(); i++){
+								al.add(data.get(i));
+							}
+							
+							// remove all the unsorted Strings
+							data.removeAll(al); 
+							
+							// remove all visible Strings
+							listCtrl.removeAll(); 
+						
+							//sort list => list is sorted
+							Collections.sort(al); 			
+							
+							//copy the sorted arrayList back into data and listCtrl
+							for(String s: al) {
+								listCtrl.add(s);
+								data.addUnique(s);
+							}
 						}
-						data.removeAll(al); // remove all the unsorted strings
-						listCtrl.removeAll(); // remove all visible strings
-						
-						Collections.sort(al); //sort list => list is sorted				
-						
-						for(String s: al) {
-							listCtrl.add(s);
-							data.addUnique(s);
-						}
+					}.run();
 				}
-			}.run();
-		}
 		});
 		
+		//create widget
 		GridDataFactory.fillDefaults().span(4, 2).hint(SWT.DEFAULT, SWT.DEFAULT).applyTo(listCtrl);
 		GridDataFactory.generate(btnAdd, 1, 1);
 		GridDataFactory.generate(btnEdit, 1, 1);
