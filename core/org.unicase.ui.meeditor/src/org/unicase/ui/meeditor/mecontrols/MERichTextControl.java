19366b46090727d050ca12d7d8dade980f3f79f0
helming@in.tum.de <helming@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-08 18:15:02 +0000

    replaced richtext widget with plain text field (commited for bleibinhaus)

--- core/org.unicase.ui.meeditor/src/org/unicase/ui/meeditor/mecontrols/MERichTextControl.java
+++ core/org.unicase.ui.meeditor/src/org/unicase/ui/meeditor/mecontrols/MERichTextControl.java
@@ -12,23 +12,17 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.TextViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.Bullet;
-import org.eclipse.swt.custom.StyleRange;
-import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.FocusAdapter;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.GlyphMetrics;
-import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.ui.IEditorPart;
@@ -43,29 +37,27 @@ import org.unicase.ui.meeditor.MEEditor;
  * @author helming
  */
 public class MERichTextControl extends AbstractMEControl {
-
 	private EAttribute attribute;
 
 	private AdapterImpl eAdapter;
 
-	private final String bulletString = "  \u2022 ";
-
 	private static final int PRIORITY = 2;
 
-	protected Control createControl(Composite parent, int style, Composite c, TextViewer t) {
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.meeditor.mecontrols.AbstractMEControl#createControl(org.eclipse.swt.widgets.Composite, int)
+	 */
+	@Override
+	public Control createControl(Composite parent, int style) {
 		Object feature = getItemPropertyDescriptor().getFeature(getModelElement());
 		this.attribute = (EAttribute) feature;
-		if (c == null) {
-			composite = getToolkit().createComposite(parent, style);
-
-		} else {
-			composite = c;
-		}
+		composite = getToolkit().createComposite(parent, style);
 		composite.setBackgroundMode(SWT.INHERIT_FORCE);
 		composite.setLayout(new GridLayout());
 
 		createToolBar();
-		createStyledText(t);
+		createText();
 		eAdapter = new AdapterImpl() {
 			@Override
 			public void notifyChanged(Notification msg) {
@@ -83,43 +75,17 @@ public class MERichTextControl extends AbstractMEControl {
 		return composite;
 	}
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Control createControl(Composite parent, int style) {
-		return createControl(parent, style, null, null);
-	}
-
 	private Composite composite;
 
 	private ToolBar toolBar;
 
-	private StyledText text;
-
-	private TextViewer viewer;
-
-	/**
-	 * Returns the viewer.
-	 * 
-	 * @return the {@link TextViewer}
-	 */
-	public TextViewer getViewer() {
-		return viewer;
-	}
-
 	private boolean shoudShowExpand;
 
-	private void createStyledText(TextViewer t) {
+	private Text text;
 
-		if (t == null) {
-			viewer = new TextViewer(composite, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
-			viewer.setDocument(new Document());
-		} else {
-			viewer = t;
-		}
+	private void createText() {
+		text = new Text(composite, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
 
-		text = viewer.getTextWidget();
 		text.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_WHITE));
 		text.setSize(10, 100);
 		text.addFocusListener(new FocusAdapter() {
@@ -142,16 +108,6 @@ public class MERichTextControl extends AbstractMEControl {
 
 	private void createToolBar() {
 		toolBar = new ToolBar(composite, SWT.NULL);
-		// final ToolItem bulletsItem = new ToolItem(toolBar, SWT.PUSH);
-		// ImageDescriptor descriptor = Activator.getImageDescriptor("icons/bullet.jpg");
-		// bulletsItem.setImage(descriptor.createImage());
-		// bulletsItem.addSelectionListener(new SelectionAdapter() {
-		//
-		// @Override
-		// public void widgetSelected(SelectionEvent event) {
-		// toggleBullet();
-		// }
-		// });
 
 		if (shoudShowExpand) {
 			ToolItem textItem = new ToolItem(toolBar, SWT.PUSH);
@@ -171,66 +127,6 @@ public class MERichTextControl extends AbstractMEControl {
 		}
 	}
 
-	@SuppressWarnings("unused")
-	private void toggleBullet() {
-		int size = text.getText().length();
-		StringBuilder newText = new StringBuilder();
-		int newOffset = 0;
-		if (!text.getSelectionText().equals("")) {
-			Point selectionRange = text.getSelectionRange();
-			int start = selectionRange.x;
-			int length = selectionRange.y;
-
-			String before = "";
-			if (start > 0) {
-				before = text.getText(0, start - 1);
-			}
-			String target = text.getSelectionText();
-
-			String after = "";
-			if (start + length < size) {
-				after = text.getText(start + length, size - 1);
-			}
-			newText.append(before);
-			if (!target.contains(bulletString)) {
-				newText.append("\n");
-				newText.append(bulletString);
-				newText.append(target);
-				newOffset = newText.length();
-				newText.append("\n");
-			} else {
-				newText.append(target.replaceFirst(bulletString, ""));
-				newOffset = newText.length();
-			}
-			newText.append(after);
-		} else {
-			int pos = text.getCaretOffset();
-			int line = text.getLineAtOffset(pos);
-			String target = text.getLine(line);
-			String before = "";
-			int start = text.getOffsetAtLine(line);
-			if (start > 0) {
-				before = text.getText(0, start - 1);
-			}
-			String after = "";
-			if (line + 1 < text.getLineCount()) {
-				start = text.getOffsetAtLine(line + 1);
-				int end = size - 1;
-				if (start < end) {
-					after = text.getText(start, end);
-				}
-			}
-			newText.append(before);
-			newText.append(bulletString);
-			newText.append(target);
-			newText.append("\n");
-			newOffset = newText.length();
-			newText.append(after);
-		}
-		viewer.getDocument().set(newText.toString());
-		text.setCaretOffset(newOffset);
-	}
-
 	/**
 	 * Sets if the expand toolbar button should be shown.
 	 * 
@@ -245,17 +141,6 @@ public class MERichTextControl extends AbstractMEControl {
 	 */
 	public ToolBar getToolbar() {
 		return toolBar;
-	}
-
-	/**
-	 * Removes bullets.
-	 * 
-	 * @param line The startline
-	 * @param count The number of lines
-	 */
-	protected void unbullet(int line, int count) {
-		text.setLineBullet(line + count - 1, 1, null);
-
 	}
 
 	private void save() {
@@ -282,26 +167,7 @@ public class MERichTextControl extends AbstractMEControl {
 			}
 		}.run();
 		txt = value.toString();
-		viewer.getDocument().set(txt);
-	}
-
-	/**
-	 * Adds bullets.
-	 * 
-	 * @param line The start line
-	 * @param count The number of lines
-	 */
-	protected void bullet(int line, int count) {
-
-		StyleRange style0 = new StyleRange();
-		style0.metrics = new GlyphMetrics(0, 0, 40);
-		Bullet bullet0 = new Bullet(style0);
-		for (int i = 0; i < count; i++) {
-
-			text.setLineBullet(line + count - 1, 1, bullet0);
-
-		}
-
+		text.setText(txt);
 	}
 
 	/**
@@ -320,6 +186,12 @@ public class MERichTextControl extends AbstractMEControl {
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.TOP).hint(250, 150).grab(true, false).applyTo(composite);
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.ui.meeditor.mecontrols.AbstractMEControl#canRender(org.eclipse.emf.edit.provider.IItemPropertyDescriptor,
+	 *      org.eclipse.emf.ecore.EObject)
+	 */
 	@Override
 	public int canRender(IItemPropertyDescriptor itemPropertyDescriptor, EObject modelElement) {
 		Object feature = itemPropertyDescriptor.getFeature(modelElement);
