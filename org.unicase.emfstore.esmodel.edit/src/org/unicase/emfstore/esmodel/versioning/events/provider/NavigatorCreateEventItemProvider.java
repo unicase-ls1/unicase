4ce6ca35936c5fb3f6ca5f577513f85e98890504
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-21 21:20:06 +0000

    creating a branch for zardoshts BA

---
+++ org.unicase.emfstore.esmodel.edit/src/org/unicase/emfstore/esmodel/versioning/events/provider/NavigatorCreateEventItemProvider.java
@@ -0,0 +1,154 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.emfstore.esmodel.versioning.events.provider;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import org.unicase.emfstore.esmodel.versioning.events.EventsPackage;
+import org.unicase.emfstore.esmodel.versioning.events.NavigatorCreateEvent;
+
+/**
+ * This is the item provider adapter for a {@link org.unicase.emfstore.esmodel.versioning.events.NavigatorCreateEvent}
+ * object. <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
+ * @generated
+ */
+public class NavigatorCreateEventItemProvider extends EventItemProvider implements IEditingDomainItemProvider,
+	IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public NavigatorCreateEventItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addCreatedElementPropertyDescriptor(object);
+			addSourceSectionPropertyDescriptor(object);
+			addDynamicPropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Created Element feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected void addCreatedElementPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory)
+			.getRootAdapterFactory(), getResourceLocator(),
+			getString("_UI_NavigatorCreateEvent_createdElement_feature"), getString(
+				"_UI_PropertyDescriptor_description", "_UI_NavigatorCreateEvent_createdElement_feature",
+				"_UI_NavigatorCreateEvent_type"), EventsPackage.Literals.NAVIGATOR_CREATE_EVENT__CREATED_ELEMENT, true,
+			false, true, null, null, null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Source Section feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected void addSourceSectionPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory)
+			.getRootAdapterFactory(), getResourceLocator(),
+			getString("_UI_NavigatorCreateEvent_sourceSection_feature"), getString(
+				"_UI_PropertyDescriptor_description", "_UI_NavigatorCreateEvent_sourceSection_feature",
+				"_UI_NavigatorCreateEvent_type"), EventsPackage.Literals.NAVIGATOR_CREATE_EVENT__SOURCE_SECTION, true,
+			false, true, null, null, null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Dynamic feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected void addDynamicPropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory)
+			.getRootAdapterFactory(), getResourceLocator(), getString("_UI_NavigatorCreateEvent_dynamic_feature"),
+			getString("_UI_PropertyDescriptor_description", "_UI_NavigatorCreateEvent_dynamic_feature",
+				"_UI_NavigatorCreateEvent_type"), EventsPackage.Literals.NAVIGATOR_CREATE_EVENT__DYNAMIC, true, false,
+			false, ItemPropertyDescriptor.BOOLEAN_VALUE_IMAGE, null, null));
+	}
+
+	/**
+	 * This returns NavigatorCreateEvent.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/NavigatorCreateEvent"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		Date labelValue = ((NavigatorCreateEvent) object).getTimestamp();
+		String label = labelValue == null ? null : labelValue.toString();
+		return label == null || label.length() == 0 ? getString("_UI_NavigatorCreateEvent_type")
+			: getString("_UI_NavigatorCreateEvent_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached children and by creating
+	 * a viewer notification, which it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(NavigatorCreateEvent.class)) {
+		case EventsPackage.NAVIGATOR_CREATE_EVENT__DYNAMIC:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+			return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that can be created
+	 * under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+}
