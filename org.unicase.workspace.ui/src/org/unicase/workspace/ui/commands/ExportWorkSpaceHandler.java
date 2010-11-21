53ce10862d1de51823fd0c4d144b13b98f9aa70d
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-21 21:24:34 +0000

    creating a branch for zardoshts BA

---
+++ org.unicase.workspace.ui/src/org/unicase/workspace/ui/commands/ExportWorkSpaceHandler.java
@@ -0,0 +1,81 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.workspace.ui.commands;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.ui.PlatformUI;
+import org.unicase.ui.common.exceptions.DialogHandler;
+import org.unicase.ui.common.util.PreferenceHelper;
+import org.unicase.workspace.WorkspaceManager;
+import org.unicase.workspace.util.UnicaseCommand;
+
+/**
+ * Exports the current workspace.
+ * 
+ * @author helming
+ */
+public class ExportWorkSpaceHandler extends AbstractHandler {
+
+	private static final String EXPORT_WORKSPACE_PATH = "org.unicase.workspace.ui.exportWorkSpacePath";
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
+	 */
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		FileDialog dialog = new FileDialog(PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getShell(), SWT.SAVE);
+		dialog.setFilterNames(ExportChangesHandler.FILTER_NAMES);
+		dialog.setFilterExtensions(ExportChangesHandler.FILTER_EXTS);
+		String initialPath = PreferenceHelper.getPreference(
+				EXPORT_WORKSPACE_PATH, System.getProperty("user.home"));
+		dialog.setFilterPath(initialPath);
+		dialog.setOverwrite(true);
+		String fn = dialog.open();
+		if (fn == null) {
+			return null;
+		}
+
+		final File file = new File(fn);
+		PreferenceHelper.setPreference(EXPORT_WORKSPACE_PATH, file.getParent());
+
+		final ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+
+		new UnicaseCommand() {
+			@Override
+			protected void doRun() {
+				try {
+					progressDialog.open();
+					progressDialog.getProgressMonitor().beginTask(
+							"Export workspace...", 100);
+					progressDialog.getProgressMonitor().worked(10);
+					WorkspaceManager.getInstance().getCurrentWorkspace()
+							.exportWorkSpace(file.getAbsolutePath());
+				} catch (IOException e) {
+					DialogHandler.showExceptionDialog(e);
+				} finally {
+					progressDialog.getProgressMonitor().done();
+					progressDialog.close();
+				}
+			}
+		}.run();
+		MessageDialog.openInformation(null, "Export",
+				"Exported workspace to file " + file.getName());
+		return null;
+	}
+
+}
