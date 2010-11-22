fcfd78fef871383c657bd4b274cdb9bbb26ab742
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-22 21:25:56 +0000

    creating a branch for task triage paper

---
+++ org.unicase.ui.validation/src/org/unicase/ui/validation/filter/FilterTableViewer.java
@@ -0,0 +1,64 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+
+package org.unicase.ui.validation.filter;
+
+import java.util.ArrayList;
+
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * @author pfeifferc
+ */
+public class FilterTableViewer extends TableViewer {
+
+	/**
+	 * @param parent the
+	 * @param style the
+	 */
+	public FilterTableViewer(Composite parent, int style) {
+		super(parent, style);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.jface.viewers.StructuredViewer#getFilteredChildren(java.lang.Object)
+	 */
+	@Override
+	protected Object[] getFilteredChildren(Object parent) {
+		return filter(getRawChildren(parent));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.jface.viewers.StructuredViewer#filter(java.lang.Object[])
+	 */
+	@Override
+	protected Object[] filter(Object[] elements) {
+		if (getFilters() != null) {
+			ArrayList<Object> filtered = new ArrayList<Object>(elements.length);
+			Object root = getRoot();
+			for (int i = 0; i < elements.length; i++) {
+				boolean add = true;
+				for (int j = 0; j < getFilters().length; j++) {
+					add = (getFilters()[j]).select(this, root, elements[i]);
+					if (add) {
+						break;
+					}
+				}
+				if (add) {
+					filtered.add(elements[i]);
+				}
+			}
+			return filtered.toArray();
+		}
+		return elements;
+	}
+
+}
---
+++ org.unicase.ui.validation/src/org/unicase/ui/validation/providers/DescriptionLabelProvider.java
@@ -0,0 +1,30 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ui.validation.providers;
+
+import org.eclipse.emf.validation.model.IConstraintStatus;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+
+/**
+ * LbaleProvider for the Description Column.
+ * 
+ * @author helming
+ */
+public class DescriptionLabelProvider extends ColumnLabelProvider {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getText(Object element) {
+		if (element instanceof IConstraintStatus) {
+			IConstraintStatus constraint = (IConstraintStatus) element;
+			return constraint.getMessage();
+		}
+		return super.getText(element);
+	}
+
+}
