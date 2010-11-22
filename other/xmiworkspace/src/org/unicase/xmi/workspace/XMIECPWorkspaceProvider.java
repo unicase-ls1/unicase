f22b5ebe39336246d51bf1fe343777993f3c68f2
mattimaieris <mattimaieris@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-22 16:39:23 +0000

    Added an autogenerated comment into the workspaceprovider class of the xmi-workspace plugin.

--- other/xmiworkspace/src/org/unicase/xmi/workspace/XMIECPWorkspaceProvider.java
+++ other/xmiworkspace/src/org/unicase/xmi/workspace/XMIECPWorkspaceProvider.java
@@ -5,6 +5,11 @@ import org.unicase.ui.navigator.workSpaceModel.util.ECPWorkspaceProvider;
 
 public class XMIECPWorkspaceProvider implements ECPWorkspaceProvider {
 
+	/** 
+     * {@inheritDoc} 
+     * 
+     * @see org.unicase.ui.navigator.workSpaceModel.util.ECPWorkspaceProvider#getECPWorkspace() 
+     */
 	@Override
 	public ECPWorkspace getECPWorkspace() {
 		return new XMIECPWorkspace();
