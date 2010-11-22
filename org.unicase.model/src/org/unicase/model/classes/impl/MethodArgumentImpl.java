895349b49b69e6ffc86e8117e03ae0e60331997e
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-22 21:23:33 +0000

    creating a branch for task triage paper

---
+++ org.unicase.model/src/org/unicase/model/classes/impl/MethodArgumentImpl.java
@@ -0,0 +1,434 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.classes.impl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.unicase.model.classes.ArgumentDirectionType;
+import org.unicase.model.classes.ClassesPackage;
+import org.unicase.model.classes.MethodArgument;
+import org.unicase.model.impl.UnicaseModelElementImpl;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model object ' <em><b>Method Argument</b></em>'. <!-- end-user-doc
+ * -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ * <li>{@link org.unicase.model.classes.impl.MethodArgumentImpl#getType <em>Type</em>}</li>
+ * <li>{@link org.unicase.model.classes.impl.MethodArgumentImpl#getDirection <em>Direction</em>}</li>
+ * <li>{@link org.unicase.model.classes.impl.MethodArgumentImpl#getDefaultValue <em>Default Value</em>}</li>
+ * <li>{@link org.unicase.model.classes.impl.MethodArgumentImpl#getSignature <em>Signature</em>}</li>
+ * <li>{@link org.unicase.model.classes.impl.MethodArgumentImpl#getLabel <em>Label</em>}</li>
+ * </ul>
+ * </p>
+ * 
+ * @generated
+ */
+public class MethodArgumentImpl extends UnicaseModelElementImpl implements MethodArgument {
+	/**
+	 * The default value of the '{@link #getType() <em>Type</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @see #getType()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String TYPE_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getType() <em>Type</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @see #getType()
+	 * @generated
+	 * @ordered
+	 */
+	protected String type = TYPE_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getDirection() <em>Direction</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #getDirection()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final ArgumentDirectionType DIRECTION_EDEFAULT = ArgumentDirectionType.UNDEFINED;
+
+	/**
+	 * The cached value of the '{@link #getDirection() <em>Direction</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #getDirection()
+	 * @generated
+	 * @ordered
+	 */
+	protected ArgumentDirectionType direction = DIRECTION_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getDefaultValue() <em>Default Value</em>}' attribute. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #getDefaultValue()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String DEFAULT_VALUE_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getDefaultValue() <em>Default Value</em>}' attribute. <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * 
+	 * @see #getDefaultValue()
+	 * @generated
+	 * @ordered
+	 */
+	protected String defaultValue = DEFAULT_VALUE_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getSignature() <em>Signature</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #getSignature()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String SIGNATURE_EDEFAULT = null;
+
+	/**
+	 * The default value of the '{@link #getLabel() <em>Label</em>}' attribute. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @see #getLabel()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String LABEL_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getLabel() <em>Label</em>}' attribute. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @see #getLabel()
+	 * @generated
+	 * @ordered
+	 */
+	protected String label = LABEL_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected MethodArgumentImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return ClassesPackage.Literals.METHOD_ARGUMENT;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public void setType(String newType) {
+		String oldType = type;
+		type = newType;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ClassesPackage.METHOD_ARGUMENT__TYPE, oldType, type));
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public ArgumentDirectionType getDirection() {
+		return direction;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public void setDirection(ArgumentDirectionType newDirection) {
+		ArgumentDirectionType oldDirection = direction;
+		direction = newDirection == null ? DIRECTION_EDEFAULT : newDirection;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ClassesPackage.METHOD_ARGUMENT__DIRECTION,
+				oldDirection, direction));
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public void setDefaultValue(String newDefaultValue) {
+		String oldDefaultValue = defaultValue;
+		defaultValue = newDefaultValue;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, ClassesPackage.METHOD_ARGUMENT__DEFAULT_VALUE,
+				oldDefaultValue, defaultValue));
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
+	 * @return the signature
+	 */
+	public String getSignature() {
+		String signature = "";
+
+		if (this.getDirection() != ArgumentDirectionType.UNDEFINED) {
+			signature += this.getDirection().getLiteral();
+		}
+
+		if (this.getName() != null) {
+			if (signature.length() > 0) {
+				signature += " ";
+			}
+			signature += this.getName();
+		}
+
+		if (this.getType() != null) {
+			if (signature.length() > 0) {
+				signature += " ";
+			}
+			signature += ": ";
+			signature += this.getType();
+		}
+
+		if (this.getDefaultValue() != null) {
+			if (signature.length() > 0) {
+				signature += " ";
+			}
+			signature += "= ";
+			signature += this.getDefaultValue();
+		}
+
+		return signature;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> If the label can not be parsed it is a string field. otherwise this returns the
+	 * signature. <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
+	 * @return The displayable label.
+	 */
+	public String getLabel() {
+		if (label != null) {
+			return label;
+		} else {
+			return this.getSignature();
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc --> Sets and tries to parse the label. If it can be parsed, label attribute stays null and
+	 * signature is used instead. <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
+	 * @param newLabel the input string which will be parsed.
+	 */
+	public void setLabel(String newLabel) {
+		String oldLabel = label;
+		label = newLabel;
+
+		String validSignatureRegExp;
+
+		validSignatureRegExp = "";
+		validSignatureRegExp += "\\s*(in|out|inout)?\\s*"; // group1 ->
+		// direction
+		validSignatureRegExp += "(\\w+)\\s*"; // group2 -> name
+		validSignatureRegExp += "(?::\\s*(\\w+))?\\s*"; // group3 -> type
+		validSignatureRegExp += "(?:=\\s*(\\w+))?\\s*"; // group4 ->
+		// defaultValue
+
+		Pattern p = Pattern.compile(validSignatureRegExp);
+		Matcher m = p.matcher(newLabel);
+		boolean b = m.matches();
+
+		if (b) {
+			String directionLiteral = m.group(1);
+			if (directionLiteral != null && directionLiteral.length() > 0) {
+				this.setDirection(ArgumentDirectionType.get(directionLiteral));
+			} else {
+				this.setDirection(ArgumentDirectionType.UNDEFINED);
+			}
+
+			String nameString = m.group(2);
+			if (nameString != null && nameString.length() > 0) {
+				this.setName(nameString);
+			} else {
+				this.setName("Unnamed");
+			}
+
+			String typeString = m.group(3);
+			this.setType(typeString);
+
+			String defaultValueString = m.group(4);
+			this.setDefaultValue(defaultValueString);
+
+			label = null;
+		}
+
+		if (eNotificationRequired()) {
+			eNotify(new ENotificationImpl(this, Notification.SET, ClassesPackage.METHOD_ARGUMENT__LABEL, oldLabel,
+				label));
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc --> .<!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+		case ClassesPackage.METHOD_ARGUMENT__TYPE:
+			return getType();
+		case ClassesPackage.METHOD_ARGUMENT__DIRECTION:
+			return getDirection();
+		case ClassesPackage.METHOD_ARGUMENT__DEFAULT_VALUE:
+			return getDefaultValue();
+		case ClassesPackage.METHOD_ARGUMENT__SIGNATURE:
+			return getSignature();
+		case ClassesPackage.METHOD_ARGUMENT__LABEL:
+			return getLabel();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+		case ClassesPackage.METHOD_ARGUMENT__TYPE:
+			setType((String) newValue);
+			return;
+		case ClassesPackage.METHOD_ARGUMENT__DIRECTION:
+			setDirection((ArgumentDirectionType) newValue);
+			return;
+		case ClassesPackage.METHOD_ARGUMENT__DEFAULT_VALUE:
+			setDefaultValue((String) newValue);
+			return;
+		case ClassesPackage.METHOD_ARGUMENT__LABEL:
+			setLabel((String) newValue);
+			return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+		case ClassesPackage.METHOD_ARGUMENT__TYPE:
+			setType(TYPE_EDEFAULT);
+			return;
+		case ClassesPackage.METHOD_ARGUMENT__DIRECTION:
+			setDirection(DIRECTION_EDEFAULT);
+			return;
+		case ClassesPackage.METHOD_ARGUMENT__DEFAULT_VALUE:
+			setDefaultValue(DEFAULT_VALUE_EDEFAULT);
+			return;
+		case ClassesPackage.METHOD_ARGUMENT__LABEL:
+			setLabel(LABEL_EDEFAULT);
+			return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+		case ClassesPackage.METHOD_ARGUMENT__TYPE:
+			return TYPE_EDEFAULT == null ? type != null : !TYPE_EDEFAULT.equals(type);
+		case ClassesPackage.METHOD_ARGUMENT__DIRECTION:
+			return direction != DIRECTION_EDEFAULT;
+		case ClassesPackage.METHOD_ARGUMENT__DEFAULT_VALUE:
+			return DEFAULT_VALUE_EDEFAULT == null ? defaultValue != null : !DEFAULT_VALUE_EDEFAULT.equals(defaultValue);
+		case ClassesPackage.METHOD_ARGUMENT__SIGNATURE:
+			return SIGNATURE_EDEFAULT == null ? getSignature() != null : !SIGNATURE_EDEFAULT.equals(getSignature());
+		case ClassesPackage.METHOD_ARGUMENT__LABEL:
+			return LABEL_EDEFAULT == null ? label != null : !LABEL_EDEFAULT.equals(label);
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy())
+			return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (type: ");
+		result.append(type);
+		result.append(", direction: ");
+		result.append(direction);
+		result.append(", defaultValue: ");
+		result.append(defaultValue);
+		result.append(", label: ");
+		result.append(label);
+		result.append(')');
+		return result.toString();
+	}
+
+} // MethodArgumentImpl
---
+++ org.unicase.model/src/org/unicase/model/requirement/Step.java
@@ -0,0 +1,134 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.requirement;
+
+import org.unicase.metamodel.NonDomainElement;
+import org.unicase.model.UnicaseModelElement;
+
+/**
+ * <!-- begin-user-doc --> A representation of the model object ' <em><b>Step</b></em>'. <!-- end-user-doc -->
+ * <p>
+ * The following features are supported:
+ * <ul>
+ * <li>{@link org.unicase.model.requirement.Step#isUserStep <em>User Step</em>}</li>
+ * <li>{@link org.unicase.model.requirement.Step#getIncludedUseCase <em>Included Use Case</em>}</li>
+ * <li>{@link org.unicase.model.requirement.Step#getIncludedSystemFunction <em>Included System Function</em>}</li>
+ * <li>{@link org.unicase.model.requirement.Step#getUseCase <em>Use Case</em>}</li>
+ * </ul>
+ * </p>
+ * 
+ * @see org.unicase.model.requirement.RequirementPackage#getStep()
+ * @model
+ * @generated
+ */
+public interface Step extends UnicaseModelElement, NonDomainElement {
+	/**
+	 * Returns the value of the '<em><b>User Step</b></em>' attribute. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>User Step</em>' attribute isn't clear, there really should be more of a description
+	 * here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>User Step</em>' attribute.
+	 * @see #setUserStep(boolean)
+	 * @see org.unicase.model.requirement.RequirementPackage#getStep_UserStep()
+	 * @model
+	 * @generated
+	 */
+	boolean isUserStep();
+
+	/**
+	 * Sets the value of the '{@link org.unicase.model.requirement.Step#isUserStep <em>User Step</em>}' attribute. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value the new value of the '<em>User Step</em>' attribute.
+	 * @see #isUserStep()
+	 * @generated
+	 */
+	void setUserStep(boolean value);
+
+	/**
+	 * Returns the value of the '<em><b>Included Use Case</b></em>' reference. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Included Use Case</em>' reference isn't clear, there really should be more of a
+	 * description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Included Use Case</em>' reference.
+	 * @see #setIncludedUseCase(UseCase)
+	 * @see org.unicase.model.requirement.RequirementPackage#getStep_IncludedUseCase()
+	 * @model keys="identifier"
+	 * @generated
+	 */
+	UseCase getIncludedUseCase();
+
+	/**
+	 * Sets the value of the '{@link org.unicase.model.requirement.Step#getIncludedUseCase <em>Included Use Case</em>}'
+	 * reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value the new value of the '<em>Included Use Case</em>' reference.
+	 * @see #getIncludedUseCase()
+	 * @generated
+	 */
+	void setIncludedUseCase(UseCase value);
+
+	/**
+	 * Returns the value of the '<em><b>Included System Function</b></em>' reference. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Included System Function</em>' reference isn't clear, there really should be more of a
+	 * description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Included System Function</em>' reference.
+	 * @see #setIncludedSystemFunction(SystemFunction)
+	 * @see org.unicase.model.requirement.RequirementPackage#getStep_IncludedSystemFunction()
+	 * @model keys="identifier"
+	 * @generated
+	 */
+	SystemFunction getIncludedSystemFunction();
+
+	/**
+	 * Sets the value of the '{@link org.unicase.model.requirement.Step#getIncludedSystemFunction
+	 * <em>Included System Function</em>}' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value the new value of the '<em>Included System Function</em>' reference.
+	 * @see #getIncludedSystemFunction()
+	 * @generated
+	 */
+	void setIncludedSystemFunction(SystemFunction value);
+
+	/**
+	 * Returns the value of the '<em><b>Use Case</b></em>' container reference. It is bidirectional and its opposite is
+	 * '{@link org.unicase.model.requirement.UseCase#getUseCaseSteps <em>Use Case Steps</em>}'. <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Use Case</em>' container reference isn't clear, there really should be more of a
+	 * description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * 
+	 * @return the value of the '<em>Use Case</em>' container reference.
+	 * @see #setUseCase(UseCase)
+	 * @see org.unicase.model.requirement.RequirementPackage#getStep_UseCase()
+	 * @see org.unicase.model.requirement.UseCase#getUseCaseSteps
+	 * @model opposite="useCaseSteps" keys="identifier" transient="false"
+	 * @generated
+	 */
+	UseCase getUseCase();
+
+	/**
+	 * Sets the value of the '{@link org.unicase.model.requirement.Step#getUseCase <em>Use Case</em>}' container
+	 * reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @param value the new value of the '<em>Use Case</em>' container reference.
+	 * @see #getUseCase()
+	 * @generated
+	 */
+	void setUseCase(UseCase value);
+
+} // Step
