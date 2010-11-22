78bcbc21e55564cdb9f1796999d87262c43f87bb
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-22 21:23:19 +0000

    creating a branch for task triage paper

---
+++ org.unicase.metamodel/src/org/unicase/metamodel/impl/ProjectImpl.java
@@ -0,0 +1,611 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.metamodel.impl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.InternalEObject;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+import org.unicase.metamodel.MetamodelPackage;
+import org.unicase.metamodel.ModelElement;
+import org.unicase.metamodel.ModelElementId;
+import org.unicase.metamodel.Project;
+import org.unicase.metamodel.util.ModelUtil;
+import org.unicase.metamodel.util.ProjectChangeNotifier;
+import org.unicase.metamodel.util.ProjectChangeObserver;
+
+/**
+ * @author koegel, naughton
+ * @generated
+ */
+public class ProjectImpl extends EObjectImpl implements Project {
+	/**
+	 * The cached value of the '{@link #getModelElements() <em>Model Elements</em>}' containment reference list. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @see #getModelElements()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<ModelElement> modelElements;
+	/**
+	 * The cached value of the '{@link #getCutElements() <em>Cut Elements</em>}' containment reference list. <!--
+	 * begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @see #getCutElements()
+	 * @generated
+	 * @ordered
+	 */
+	protected EList<ModelElement> cutElements;
+	private Map<ModelElementId, ModelElement> modelElementCache;
+	private List<ProjectChangeObserver> observers;
+	private boolean isNotifiying;
+	private Set<ProjectChangeObserver> exceptionThrowingObservers;
+	private Set<ProjectChangeObserver> observersToRemove;
+	private Set<ProjectChangeObserver> undetachableObservers;
+
+	// begin of custom code
+	/**
+	 * Constructor. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated NOT
+	 */
+	protected ProjectImpl() {
+		super();
+		observers = new ArrayList<ProjectChangeObserver>();
+		isNotifiying = false;
+		exceptionThrowingObservers = new HashSet<ProjectChangeObserver>();
+		observersToRemove = new HashSet<ProjectChangeObserver>();
+		undetachableObservers = new HashSet<ProjectChangeObserver>();
+	}
+
+	// end of custom code
+
+	/**
+	 * <!-- begin-user-doc --> .<!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return MetamodelPackage.Literals.PROJECT;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public EList<ModelElement> getModelElements() {
+		if (modelElements == null) {
+			modelElements = new EObjectContainmentEList.Resolving<ModelElement>(ModelElement.class, this,
+				MetamodelPackage.PROJECT__MODEL_ELEMENTS);
+		}
+		return modelElements;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public EList<ModelElement> getCutElements() {
+		if (cutElements == null) {
+			cutElements = new EObjectContainmentEList.Resolving<ModelElement>(ModelElement.class, this,
+				MetamodelPackage.PROJECT__CUT_ELEMENTS);
+		}
+		return cutElements;
+	}
+
+	// begin of custom code
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#addModelElement(org.unicase.model.ModelElement)
+	 * @generated NOT
+	 */
+	public void addModelElement(ModelElement modelElement) {
+		this.getModelElements().add(modelElement);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#getAllModelElements()
+	 * @generated NOT
+	 */
+	public EList<ModelElement> getAllModelElements() {
+		return this.getAllModelElementsbyClass(MetamodelPackage.eINSTANCE.getModelElement(),
+			new BasicEList<ModelElement>());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#getAllModelElementsbyClass(org.eclipse.emf.ecore.EClass)
+	 * @generated NOT
+	 */
+	public <T extends ModelElement> EList<T> getAllModelElementsbyClass(EClass modelElementClass, EList<T> list) {
+		return getAllModelElementsbyClass(modelElementClass, list, true);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#getAllModelElementsbyClass(org.eclipse.emf.ecore.EClass)
+	 * @generated NOT
+	 */
+	// two casts below are guarded by initial sanity check and if statement
+	@SuppressWarnings("unchecked")
+	public <T extends ModelElement> EList<T> getAllModelElementsbyClass(EClass modelElementClass, EList<T> list,
+		Boolean subclasses) {
+
+		// sanity check
+		if (!MetamodelPackage.eINSTANCE.getModelElement().isSuperTypeOf(modelElementClass)) {
+			return list;
+		}
+
+		if (subclasses) {
+			for (ModelElementId modelElementId : getModelElementsFromCache().keySet()) {
+				ModelElement modelElement = this.getModelElement(modelElementId);
+				if (modelElementClass.isInstance(modelElement)) {
+					list.add((T) modelElement);
+				}
+			}
+		} else {
+			for (ModelElementId modelElementId : getModelElementsFromCache().keySet()) {
+				ModelElement modelElement = this.getModelElement(modelElementId);
+				if (modelElement.eClass() == modelElementClass) {
+					list.add((T) modelElement);
+				}
+			}
+		}
+
+		return list;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#getModelElementsByClass(org.eclipse.emf.ecore.EClass)
+	 * @generated NOT
+	 */
+	// cast below is guarded by sanity check
+	@SuppressWarnings("unchecked")
+	public <T extends ModelElement> EList<T> getModelElementsByClass(EClass modelElementClass, EList<T> list) {
+
+		if (!MetamodelPackage.eINSTANCE.getModelElement().isSuperTypeOf(modelElementClass)) {
+			return list;
+		}
+		for (ModelElement modelElement : this.getModelElements()) {
+			if (modelElementClass.isInstance(modelElement)) {
+				list.add((T) modelElement);
+			}
+		}
+		return list;
+	}
+
+	// end of custom code
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
+		switch (featureID) {
+		case MetamodelPackage.PROJECT__MODEL_ELEMENTS:
+			return ((InternalEList<?>) getModelElements()).basicRemove(otherEnd, msgs);
+		case MetamodelPackage.PROJECT__CUT_ELEMENTS:
+			return ((InternalEList<?>) getCutElements()).basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+		case MetamodelPackage.PROJECT__MODEL_ELEMENTS:
+			return getModelElements();
+		case MetamodelPackage.PROJECT__CUT_ELEMENTS:
+			return getCutElements();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+		case MetamodelPackage.PROJECT__MODEL_ELEMENTS:
+			getModelElements().clear();
+			getModelElements().addAll((Collection<? extends ModelElement>) newValue);
+			return;
+		case MetamodelPackage.PROJECT__CUT_ELEMENTS:
+			getCutElements().clear();
+			getCutElements().addAll((Collection<? extends ModelElement>) newValue);
+			return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+		case MetamodelPackage.PROJECT__MODEL_ELEMENTS:
+			getModelElements().clear();
+			return;
+		case MetamodelPackage.PROJECT__CUT_ELEMENTS:
+			getCutElements().clear();
+			return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+		case MetamodelPackage.PROJECT__MODEL_ELEMENTS:
+			return modelElements != null && !modelElements.isEmpty();
+		case MetamodelPackage.PROJECT__CUT_ELEMENTS:
+			return cutElements != null && !cutElements.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
+
+	// begin of custom code
+
+	/**
+	 * this methods implements the adapter interface which is needed by the navigator.
+	 * 
+	 * @param adapter the adapter class
+	 * @return the adapter
+	 * @author shterev
+	 */
+	@SuppressWarnings("unchecked")
+	public Object getAdapter(Class adapter) {
+		return Platform.getAdapterManager().getAdapter(this, adapter);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @generated NOT
+	 * @see org.unicase.metamodel.Project#contains(org.unicase.model.ModelElement)
+	 */
+	public boolean contains(ModelElement modelElement) {
+		return contains(modelElement.getModelElementId());
+	}
+
+	/**
+	 * Get the model element cache.
+	 * 
+	 * @return the cache map
+	 */
+	private Map<ModelElementId, ModelElement> getModelElementsFromCache() {
+		initCacheAndNotifier();
+		return modelElementCache;
+	}
+
+	private void initCacheAndNotifier() {
+		if (modelElementCache == null) {
+			// init cache
+			modelElementCache = new HashMap<ModelElementId, ModelElement>();
+			TreeIterator<EObject> allContents = this.eAllContents();
+			while (allContents.hasNext()) {
+				EObject next = allContents.next();
+				if (MetamodelPackage.eINSTANCE.getModelElement().isInstance(next)) {
+					ModelElement modelElement = (ModelElement) next;
+					modelElementCache.put(modelElement.getModelElementId(), modelElement);
+				}
+			}
+			// projectChangeNotifier = new ProjectChangeNotifier(this);
+			new ProjectChangeNotifier(this);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.model.util.ProjectChangeObserver#modelElementAdded(org.unicase.metamodel.Project,
+	 *      org.unicase.model.ModelElement)
+	 */
+	public void handleEMFModelElementAdded(final Project project, final ModelElement modelElement) {
+		if (this.modelElementCache.containsKey(modelElement.getModelElementId())) {
+			throw new IllegalStateException("ModelElement is already in the project!");
+		}
+		// checkForCrossReferences(modelElement);
+		addModelElementAndChildrenToCache(modelElement);
+		ProjectChangeObserverNotificationCommand command = new ProjectChangeObserverNotificationCommand() {
+			public void run(ProjectChangeObserver projectChangeObserver) {
+				projectChangeObserver.modelElementAdded(project, modelElement);
+			}
+		};
+		notifyProjectChangeObservers(command);
+	}
+
+	private void notifyProjectChangeObservers(ProjectChangeObserverNotificationCommand command) {
+		isNotifiying = true;
+		for (ProjectChangeObserver projectChangeObserver : this.observers) {
+			try {
+				command.run(projectChangeObserver);
+				// BEGIN SUPRESS CATCH EXCEPTION
+			} catch (RuntimeException ex) {
+				// END SUPRESS CATCH EXCEPTION
+				if (exceptionThrowingObservers.contains(projectChangeObserver)) {
+					if (!undetachableObservers.contains(projectChangeObserver)) {
+						observersToRemove.add(projectChangeObserver);
+						ModelUtil.logException(
+							"Project Change Observer threw an exception again, it has been detached, UI may not update now: "
+								+ projectChangeObserver.getClass().getName(), ex);
+					} else {
+						ModelUtil.logException(
+							"Project Change Observer threw an exception again, but it will not be detached."
+								+ projectChangeObserver.getClass().getName(), ex);
+					}
+				} else {
+					exceptionThrowingObservers.add(projectChangeObserver);
+					ModelUtil.logWarning("Project Change Observer threw an exception: "
+						+ projectChangeObserver.getClass().getName(), ex);
+				}
+
+			}
+		}
+		isNotifiying = false;
+		for (ProjectChangeObserver observer : this.observersToRemove) {
+			removeProjectChangeObserver(observer);
+		}
+		this.observersToRemove.clear();
+	}
+
+	private void addModelElementAndChildrenToCache(ModelElement modelElement) {
+		this.modelElementCache.put(modelElement.getModelElementId(), modelElement);
+		for (ModelElement child : modelElement.getAllContainedModelElements()) {
+			this.modelElementCache.put(child.getModelElementId(), child);
+		}
+	}
+
+	private void removeModelElementAndChildrenFromCache(ModelElement modelElement) {
+		this.modelElementCache.remove(modelElement.getModelElementId());
+		for (ModelElement child : modelElement.getAllContainedModelElements()) {
+			this.modelElementCache.remove(child.getModelElementId());
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.model.util.ProjectChangeObserver#notify(org.eclipse.emf.common.notify.Notification,
+	 *      org.unicase.metamodel.Project, org.unicase.model.ModelElement)
+	 */
+	public void handleEMFNotification(final Notification notification, final Project project,
+		final ModelElement modelElement) {
+		ProjectChangeObserverNotificationCommand command = new ProjectChangeObserverNotificationCommand() {
+			public void run(ProjectChangeObserver projectChangeObserver) {
+				projectChangeObserver.notify(notification, project, modelElement);
+			}
+		};
+		notifyProjectChangeObservers(command);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#contains(org.unicase.metamodel.ModelElementId)
+	 */
+	public boolean contains(ModelElementId modelElementId) {
+		return this.getModelElementsFromCache().containsKey(modelElementId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#getModelElement(org.unicase.metamodel.ModelElementId)
+	 */
+	public ModelElement getModelElement(ModelElementId modelElementId) {
+		return this.getModelElementsFromCache().get(modelElementId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#addProjectChangeObserver(org.unicase.model.util.ProjectChangeObserver)
+	 */
+	public void addProjectChangeObserver(ProjectChangeObserver projectChangeObserver) {
+		initCacheAndNotifier();
+		this.observers.add(projectChangeObserver);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#removeProjectChangeObserver(org.unicase.model.util.ProjectChangeObserver)
+	 */
+	public void removeProjectChangeObserver(ProjectChangeObserver projectChangeObserver) {
+		if (isNotifiying) {
+			observersToRemove.add(projectChangeObserver);
+			return;
+		}
+		this.observers.remove(projectChangeObserver);
+		exceptionThrowingObservers.remove(projectChangeObserver);
+		undetachableObservers.remove(projectChangeObserver);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#containsInstance(org.unicase.model.ModelElement)
+	 */
+	public boolean containsInstance(ModelElement modelElement) {
+		ModelElementId modelElementId = modelElement.getModelElementId();
+		if (!this.contains(modelElementId)) {
+			return false;
+		}
+		ModelElement element = this.getModelElementsFromCache().get(modelElementId);
+		return element == modelElement;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#deleteModelElement(org.unicase.model.ModelElement)
+	 */
+	public void deleteModelElement(final ModelElement modelElement) {
+		if (!this.contains(modelElement)) {
+			throw new IllegalArgumentException("Cannot delete a model element that is not contained in this project.");
+		}
+
+		deleteOutgoingCrossReferences(modelElement);
+		deleteIncomingCrossReferences(modelElement);
+
+		for (ModelElement child : modelElement.getAllContainedModelElements()) {
+			deleteOutgoingCrossReferences(child);
+			deleteIncomingCrossReferences(child);
+		}
+
+		// remove containment
+		ModelElement containerModelElement = modelElement.getContainerModelElement();
+		if (containerModelElement == null) {
+			this.getModelElements().remove(modelElement);
+		} else {
+			EReference containmentFeature = modelElement.eContainmentFeature();
+			if (containmentFeature.isMany()) {
+				EList<?> containmentList = (EList<?>) containerModelElement.eGet(containmentFeature);
+				containmentList.remove(modelElement);
+			} else {
+				containerModelElement.eSet(containmentFeature, null);
+			}
+		}
+	}
+
+	private void deleteOutgoingCrossReferences(ModelElement modelElement) {
+		// delete all non containment cross references to other elments
+		for (EReference reference : modelElement.eClass().getEAllReferences()) {
+			EClassifier eType = reference.getEType();
+			if (reference.isContainer() || reference.isContainment() || !reference.isChangeable()) {
+				continue;
+			}
+
+			if (eType instanceof EClass && MetamodelPackage.eINSTANCE.getModelElement().isSuperTypeOf((EClass) eType)) {
+				modelElement.eUnset(reference);
+			}
+		}
+	}
+
+	private void deleteIncomingCrossReferences(ModelElement modelElement) {
+		// delete all non containment cross references from other elements in the project
+		for (ModelElement otherModelElement : this.getAllModelElements()) {
+			for (ModelElement otherElementOpposite : otherModelElement.getLinkedModelElements()) {
+				if (otherElementOpposite == modelElement) {
+					EList<EReference> references = otherModelElement.eClass().getEAllReferences();
+					for (EReference reference : references) {
+						if (!reference.isContainment() && !reference.isContainer()
+							&& isCorrespondingReference(modelElement, otherModelElement, reference)) {
+							if (reference.isMany()) {
+								((EList<?>) otherModelElement.eGet(reference)).remove(modelElement);
+							} else {
+								otherModelElement.eUnset(reference);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private boolean isCorrespondingReference(ModelElement modelElement, ModelElement otherModelElement,
+		EReference reference) {
+		if (reference.isMany()) {
+			if (otherModelElement.eGet(reference) == null) {
+				return false;
+			}
+			return ((List<?>) otherModelElement.eGet(reference)).contains(modelElement);
+		} else {
+			return modelElement.equals(otherModelElement.eGet(reference));
+		}
+	}
+
+	/**
+	 * Make a project change observer undetachable.
+	 * 
+	 * @param observer the observer
+	 */
+	public void setUndetachable(ProjectChangeObserver observer) {
+		undetachableObservers.add(observer);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.unicase.metamodel.Project#delete()
+	 */
+	public void delete() {
+		final Project project = this;
+		ProjectChangeObserverNotificationCommand command = new ProjectChangeObserverNotificationCommand() {
+			public void run(ProjectChangeObserver projectChangeObserver) {
+				projectChangeObserver.projectDeleted(project);
+			}
+		};
+		notifyProjectChangeObservers(command);
+	}
+
+	/**
+	 * Handle the removal of an element from the containment hierachy.
+	 * 
+	 * @param projectImpl the project
+	 * @param modelElement the model element
+	 */
+	public void handleEMFModelElementRemoved(final ProjectImpl projectImpl, final ModelElement modelElement) {
+		this.removeModelElementAndChildrenFromCache(modelElement);
+		ProjectChangeObserverNotificationCommand command = new ProjectChangeObserverNotificationCommand() {
+			public void run(ProjectChangeObserver projectChangeObserver) {
+				projectChangeObserver.modelElementRemoved(projectImpl, modelElement);
+			}
+		};
+		notifyProjectChangeObservers(command);
+
+	}
+
+}
