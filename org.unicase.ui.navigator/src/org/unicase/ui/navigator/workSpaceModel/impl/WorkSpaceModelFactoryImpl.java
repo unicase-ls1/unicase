3308da1833515d4680fe8cb95308de307a46995e
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-11-22 21:25:14 +0000

    creating a branch for task triage paper

---
+++ org.unicase.ui.navigator/src/org/unicase/ui/navigator/workSpaceModel/impl/WorkSpaceModelFactoryImpl.java
@@ -0,0 +1,89 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ui.navigator.workSpaceModel.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.impl.EFactoryImpl;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.unicase.ui.navigator.workSpaceModel.ECPWorkspace;
+import org.unicase.ui.navigator.workSpaceModel.WorkSpaceModelFactory;
+import org.unicase.ui.navigator.workSpaceModel.WorkSpaceModelPackage;
+
+/**
+ * <!-- begin-user-doc --> An implementation of the model <b>Factory</b>. <!-- end-user-doc -->
+ * @generated
+ */
+public class WorkSpaceModelFactoryImpl extends EFactoryImpl implements WorkSpaceModelFactory {
+	/**
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static WorkSpaceModelFactory init() {
+		try {
+			WorkSpaceModelFactory theWorkSpaceModelFactory = (WorkSpaceModelFactory)EPackage.Registry.INSTANCE.getEFactory("http://unicase.org/workspaceModel"); 
+			if (theWorkSpaceModelFactory != null) {
+				return theWorkSpaceModelFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new WorkSpaceModelFactoryImpl();
+	}
+
+	/**
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public WorkSpaceModelFactoryImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public EObject create(EClass eClass) {
+		switch (eClass.getClassifierID()) {
+			case WorkSpaceModelPackage.ECP_WORKSPACE: return createECPWorkspace();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ECPWorkspace createECPWorkspace() {
+		ECPWorkspaceImpl ecpWorkspace = new ECPWorkspaceImpl();
+		return ecpWorkspace;
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @generated
+	 */
+	public WorkSpaceModelPackage getWorkSpaceModelPackage() {
+		return (WorkSpaceModelPackage)getEPackage();
+	}
+
+	/**
+	 * <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * @deprecated
+	 * @generated
+	 */
+	@Deprecated
+	public static WorkSpaceModelPackage getPackage() {
+		return WorkSpaceModelPackage.eINSTANCE;
+	}
+
+} // WorkSpaceModelFactoryImpl
