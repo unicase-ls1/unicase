e09f01073a01368c1a3b7ed47e57f252fc795b3e
wesendon@in.tum.de <wesendon@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-12-09 15:01:06 +0000

    Added comments

--- core/org.unicase.ecp.model/src/org/unicase/ecp/model/workSpaceModel/ECPProject.java
+++ core/org.unicase.ecp.model/src/org/unicase/ecp/model/workSpaceModel/ECPProject.java
@@ -13,14 +13,19 @@ import org.eclipse.emf.ecore.EObject;
 import org.unicase.ecp.model.ECPModelelementContext;
 
 /**
- * <!-- begin-user-doc --> A representation of the model object '<em><b>ECP Project</b></em>'.
+ * <!-- begin-user-doc --> A representation of the model object '
+ * <em><b>ECP Project</b></em>'.
  * 
  * @implements ECPModelelementContext <!-- end-user-doc -->
  *             <p>
  *             The following features are supported:
  *             <ul>
- *             <li>{@link org.unicase.ecp.model.workSpaceModel.ECPProject#getWorkspace <em>Workspace</em>}</li>
- *             <li>{@link org.unicase.ecp.model.workSpaceModel.ECPProject#getRootObject <em>Root Object</em>}</li>
+ *             <li>
+ *             {@link org.unicase.ecp.model.workSpaceModel.ECPProject#getWorkspace
+ *             <em>Workspace</em>}</li>
+ *             <li>
+ *             {@link org.unicase.ecp.model.workSpaceModel.ECPProject#getRootObject
+ *             <em>Root Object</em>}</li>
  *             </ul>
  *             </p>
  * @see org.unicase.ecp.model.workSpaceModel.WorkSpaceModelPackage#getECPProject()
@@ -30,12 +35,13 @@ import org.unicase.ecp.model.ECPModelelementContext;
 public interface ECPProject extends EObject, ECPModelelementContext {
 
 	/**
-	 * Returns the value of the '<em><b>Workspace</b></em>' container reference. It is bidirectional and its opposite is
-	 * '{@link org.unicase.ecp.model.workSpaceModel.ECPWorkspace#getProjects <em>Projects</em>}'. <!-- begin-user-doc
-	 * -->
+	 * Returns the value of the '<em><b>Workspace</b></em>' container reference.
+	 * It is bidirectional and its opposite is '
+	 * {@link org.unicase.ecp.model.workSpaceModel.ECPWorkspace#getProjects
+	 * <em>Projects</em>}'. <!-- begin-user-doc -->
 	 * <p>
-	 * If the meaning of the '<em>Workspace</em>' container reference isn't clear, there really should be more of a
-	 * description here...
+	 * If the meaning of the '<em>Workspace</em>' container reference isn't
+	 * clear, there really should be more of a description here...
 	 * </p>
 	 * <!-- end-user-doc -->
 	 * 
@@ -49,21 +55,54 @@ public interface ECPProject extends EObject, ECPModelelementContext {
 	ECPWorkspace getWorkspace();
 
 	/**
-	 * Sets the value of the '{@link org.unicase.ecp.model.workSpaceModel.ECPProject#getWorkspace <em>Workspace</em>}
-	 * ' container reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * Sets the value of the '
+	 * {@link org.unicase.ecp.model.workSpaceModel.ECPProject#getWorkspace
+	 * <em>Workspace</em>} ' container reference. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
 	 * 
-	 * @param value the new value of the '<em>Workspace</em>' container reference.
+	 * @param value
+	 *            the new value of the '<em>Workspace</em>' container reference.
 	 * @see #getWorkspace()
 	 * @generated
 	 */
 	void setWorkspace(ECPWorkspace value);
 
+	/**
+	 * Checks whether an EObject is contained in the project.
+	 * 
+	 * @param eObject
+	 *            EObject
+	 * @return true if is contained
+	 */
 	boolean contains(EObject eObject);
 
+	/**
+	 * Returns all model elements contained in the project. Don't add elements
+	 * to this collection, instead use {@link #addModelElementToRoot(EObject)}
+	 * or attach it as a child of an element of this collection.
+	 * 
+	 * 
+	 * @return all model elements
+	 */
 	Collection<EObject> getAllModelElement();
 
-	Collection<EObject> getAllModelElementsbyClass(EClass clazz, BasicEList<EObject> basicEList);
+	/**
+	 * Returns all elements contained in the project of the specified class type or its subtypes.
+	 * 
+	 * @param clazz specified class type
+	 * @param basicEList list containing the results
+	 * TODO
+	 * @return resulting list (is the same as the basicEList parameter, API will be changed)
+	 */
+	Collection<EObject> getAllModelElementsbyClass(EClass clazz,
+			BasicEList<EObject> basicEList);
 
+	/**
+	 * Specifies whether an element is a NonDomainElement. If the underlying layer doesn't support NDE, return true.
+	 * 
+	 * @param eObject object of test
+	 * @return true if is NonDomainElement
+	 */
 	boolean isNonDomainElement(EObject eObject);
 
 	void dispose();
@@ -83,10 +122,13 @@ public interface ECPProject extends EObject, ECPModelelementContext {
 	EObject getRootObject();
 
 	/**
-	 * Sets the value of the '{@link org.unicase.ecp.model.workSpaceModel.ECPProject#getRootObject
-	 * <em>Root Object</em>}' reference. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * Sets the value of the '
+	 * {@link org.unicase.ecp.model.workSpaceModel.ECPProject#getRootObject
+	 * <em>Root Object</em>}' reference. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
 	 * 
-	 * @param value the new value of the '<em>Root Object</em>' reference.
+	 * @param value
+	 *            the new value of the '<em>Root Object</em>' reference.
 	 * @see #getRootObject()
 	 * @generated
 	 */
