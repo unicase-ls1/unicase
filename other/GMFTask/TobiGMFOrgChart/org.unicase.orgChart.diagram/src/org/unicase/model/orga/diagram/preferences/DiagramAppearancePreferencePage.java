bf133f84155ab62078818d513c48cbf60811a68b
worldwidetobi@gmx.de <worldwidetobi@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-12-10 19:00:03 +0000

    Working OrgChartDiagram

---
+++ other/GMFTask/TobiGMFOrgChart/org.unicase.orgChart.diagram/src/org/unicase/model/orga/diagram/preferences/DiagramAppearancePreferencePage.java
@@ -0,0 +1,18 @@
+package org.unicase.model.orga.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.AppearancePreferencePage;
+import org.unicase.model.orga.diagram.part.OrgaDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramAppearancePreferencePage extends AppearancePreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramAppearancePreferencePage() {
+		setPreferenceStore(OrgaDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
---
+++ other/GMFTask/TobiGMFOrgChart/org.unicase.orgChart.diagram/src/org/unicase/model/orga/diagram/preferences/DiagramRulersAndGridPreferencePage.java
@@ -0,0 +1,18 @@
+package org.unicase.model.orga.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.RulerGridPreferencePage;
+import org.unicase.model.orga.diagram.part.OrgaDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramRulersAndGridPreferencePage extends RulerGridPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramRulersAndGridPreferencePage() {
+		setPreferenceStore(OrgaDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
