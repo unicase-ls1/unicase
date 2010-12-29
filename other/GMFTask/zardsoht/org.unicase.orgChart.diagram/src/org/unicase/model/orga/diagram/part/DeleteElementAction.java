b85d47441489e381374c85bc8673d3163c80b702
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2010-12-29 20:15:46 +0000

    updating diagram

---
+++ other/GMFTask/zardsoht/org.unicase.orgChart.diagram/src/org/unicase/model/orga/diagram/part/DeleteElementAction.java
@@ -0,0 +1,91 @@
+package org.unicase.model.orga.diagram.part;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.UnexecutableCommand;
+import org.eclipse.gmf.runtime.diagram.ui.actions.AbstractDeleteFromAction;
+import org.eclipse.gmf.runtime.diagram.ui.actions.ActionIds;
+import org.eclipse.gmf.runtime.diagram.ui.commands.CommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.l10n.DiagramUIMessages;
+import org.eclipse.gmf.runtime.emf.commands.core.command.CompositeTransactionalCommand;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+
+/**
+ * @generated
+ */
+public class DeleteElementAction extends AbstractDeleteFromAction {
+
+	/**
+	 * @generated
+	 */
+	public DeleteElementAction(IWorkbenchPart part) {
+		super(part);
+	}
+
+	/**
+	 * @generated
+	 */
+	public DeleteElementAction(IWorkbenchPage workbenchPage) {
+		super(workbenchPage);
+	}
+
+	/**
+	 * @generated
+	 */
+
+	public void init() {
+		super.init();
+		setId(ActionIds.ACTION_DELETE_FROM_MODEL);
+		setText(DiagramUIMessages.DiagramEditor_Delete_from_Model);
+		setToolTipText(DiagramUIMessages.DiagramEditor_Delete_from_ModelToolTip);
+		String path = "icons/delete.gif";
+		java.net.URL url = FileLocator.find(Platform
+				.getBundle("org.unicase.ui.unicasecommon"), new Path(path),
+				null);
+		ImageDescriptor id = ImageDescriptor.createFromURL(url);
+		setHoverImageDescriptor(id);
+		setImageDescriptor(id);
+		setDisabledImageDescriptor(id);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected String getCommandLabel() {
+		return DiagramUIMessages.DiagramEditor_Delete_from_Model;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected Command getCommand(Request request) {
+		List operationSet = getOperationSet();
+		if (operationSet.isEmpty()) {
+			return UnexecutableCommand.INSTANCE;
+		}
+		Iterator editParts = operationSet.iterator();
+		CompositeTransactionalCommand command = new CompositeTransactionalCommand(
+				getEditingDomain(), getCommandLabel());
+		while (editParts.hasNext()) {
+			EditPart editPart = (EditPart) editParts.next();
+			Command curCommand = editPart.getCommand(request);
+			if (curCommand != null) {
+				command.compose(new CommandProxy(curCommand));
+			}
+		}
+		if (command.isEmpty() || command.size() != operationSet.size()) {
+			return UnexecutableCommand.INSTANCE;
+		}
+		return new ICommandProxy(command);
+	}
+}
---
+++ other/GMFTask/zardsoht/org.unicase.orgChart.diagram/src/org/unicase/model/orga/diagram/preferences/DiagramGeneralPreferencePage.java
@@ -0,0 +1,18 @@
+package org.unicase.model.orga.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.DiagramsPreferencePage;
+import org.unicase.model.orga.diagram.part.OrgaDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramGeneralPreferencePage extends DiagramsPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramGeneralPreferencePage() {
+		setPreferenceStore(OrgaDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
