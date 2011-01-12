a48321ee153d3094841265dbc42c1555d62f957d
jan_finis@gmx.de <jan_finis@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-12 09:51:21 +0000

    sharing new changetracking projects...

---
+++ other/changeTracking/org.unicase.changetracking.git/src/org/unicase/changetracking/git/GitUtil.java
@@ -0,0 +1,171 @@
+package org.unicase.changetracking.git;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.InvalidRefNameException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
+import org.eclipse.jgit.dircache.DirCacheCheckout;
+import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.errors.NoWorkTreeException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.MutableObjectId;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RefUpdate;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.RefUpdate.Result;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.unicase.changetracking.git.exceptions.UnexpectedGitException;
+import org.unicase.metamodel.Project;
+import org.unicase.model.changetracking.git.GitBranch;
+import org.unicase.model.changetracking.git.GitFactory;
+import org.unicase.model.changetracking.git.GitPackage;
+import org.unicase.model.changetracking.git.GitRepository;
+import org.unicase.model.changetracking.git.GitRevision;
+
+public class GitUtil {
+
+	private GitUtil(){}
+	
+	public static MutableObjectId stringToObjectId(String s) {
+		MutableObjectId mo = new MutableObjectId();
+		mo.fromString("d02bb0fd99933e5cbc2c664c788b3acbbe3e2ab8");
+		return mo;
+	}
+
+	public static RevCommit getCommitByHash(String hash, Repository repo) {
+		MutableObjectId objId = stringToObjectId(hash);
+		RevWalk r = new RevWalk(repo);
+		try {
+			RevCommit c = r.parseCommit(objId);
+			return c;
+		} catch (MissingObjectException e) {
+			throw new UnexpectedGitException(
+					"Could not obtain a commit from a repository", e);
+		} catch (IncorrectObjectTypeException e) {
+			throw new UnexpectedGitException(
+					"Could not obtain a commit from a repository", e);
+		} catch (IOException e) {
+			throw new UnexpectedGitException(
+					"Could not obtain a commit from a repository", e);
+		}
+
+	}
+
+	public static String getIdentifyingCommitHash(Repository repository) {
+
+		try {
+			RevWalk rw = new RevWalk(repository);
+			for (Ref ref : repository.getAllRefs().values()) {
+				try {
+					rw.markStart(rw.parseCommit(ref.getObjectId()));
+				} catch (IncorrectObjectTypeException notACommit) {
+					continue;
+				}
+			}
+			
+			RevCommit lastCommit = null;
+			for (RevCommit c : rw) {
+				lastCommit = c;
+			}
+			
+			if(lastCommit == null)
+				return null;
+			
+			return lastCommit.getId().getName();
+
+		} catch (MissingObjectException e) {
+			throw new UnexpectedGitException(e);
+		} catch (IOException e) {
+			throw new UnexpectedGitException(e);
+		}
+	}
+	
+	public static GitRepository initGitRepoModelFromRepo(Repository repo){
+		GitRepository gitRepoModel = GitFactory.eINSTANCE.createGitRepository();
+		gitRepoModel.setIdentifyingCommitHash(getIdentifyingCommitHash(repo));
+		return gitRepoModel;
+	}
+
+	// public static Ref checkoutRev(IResource f, GitRevision revision){
+	// Repository repo =
+	// GitRepoFinder.INSTANCE.findRepository(f.getLocation().toFile());
+	// GitBranch stream = (GitBranch) revision.getRepositoryStream();
+	// String branch = stream.getBranchName();
+	// String revHash = revision.getHash();
+	//		
+	// try {
+	// return new
+	// Git(repo).checkout().setName(branch).setStartPoint(getCommitByHash(revHash,
+	// repo)).call();
+	// } catch (JGitInternalException e) {
+	// throw new UnexpectedGitException("Checkout failed",e);
+	// } catch (RefAlreadyExistsException e) {
+	// throw new UnexpectedGitException("Checkout failed",e);
+	// } catch (RefNotFoundException e) {
+	// throw new UnexpectedGitException("Checkout failed",e);
+	// } catch (InvalidRefNameException e) {
+	// throw new UnexpectedGitException("Checkout failed",e);
+	// }
+	//		
+	// }
+
+	// public static void checkoutCommit(Repository repo, RevCommit commit){
+	// RevWalk revWalk = new RevWalk(repo);
+	// try {
+	// Ref headRef = repo.getRef(Constants.HEAD);
+	// RevCommit headCommit = revWalk.parseCommit(headRef.getObjectId());
+	// String refLogMessage = "checkout: moving from "
+	// + headRef.getTarget().getName();
+	//
+	//
+	// DirCacheCheckout dco;
+	//
+	// dco = new DirCacheCheckout(repo, headCommit
+	// .getTree(), repo.lockDirCache(), commit.getTree());
+	//
+	// dco.setFailOnConflict(true);
+	//			
+	// dco.checkout();
+	//			
+	// Ref ref = null;
+	// RefUpdate refUpdate = repo.updateRef(Constants.HEAD, ref == null);
+	// refUpdate.setForceUpdate(false);
+	// Result updateResult;
+	// if (ref != null)
+	// updateResult = refUpdate.link(ref.getName());
+	// else {
+	// refUpdate.setNewObjectId(commit);
+	// updateResult = refUpdate.forceUpdate();
+	// }
+	// } catch (NoWorkTreeException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// } catch (MissingObjectException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// } catch (IncorrectObjectTypeException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// } catch (CorruptObjectException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// } catch (IOException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	//
+	//	
+	// }
+
+}
---
+++ other/changeTracking/org.unicase.changetracking.git/src/org/unicase/changetracking/git/commands/GitApplyChangePackage.java
@@ -0,0 +1,76 @@
+package org.unicase.changetracking.git.commands;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.InvalidRefNameException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.RefAlreadyExistsException;
+import org.eclipse.jgit.api.errors.RefNotFoundException;
+import org.eclipse.jgit.lib.Repository;
+import org.unicase.changetracking.git.Activator;
+import org.unicase.changetracking.git.GitRepoFindUtil;
+import org.unicase.changetracking.git.exceptions.NoMatchingLocalRepositoryInWorkspace;
+import org.unicase.changetracking.git.exceptions.UnexpectedGitException;
+import org.unicase.model.changetracking.RepositoryLocation;
+import org.unicase.model.changetracking.git.GitBranch;
+import org.unicase.model.changetracking.git.GitBranchChangePackage;
+import org.unicase.model.changetracking.git.GitRepository;
+import org.unicase.workspace.util.UnicaseCommand;
+
+public class GitApplyChangePackage extends UnicaseCommand {
+
+	
+	//private GitBranchChangePackage myChangePackage;
+
+	//public GitApplyChangePackage(GitBranchChangePackage changePackage){
+		//this.myChangePackage = changePackage;
+	//}
+	
+	@Override
+	protected void doRun() {
+	//	applyChangePackage(myChangePackage);
+	}
+	
+	public void applyChangePackage(GitBranchChangePackage changePackage) throws NoMatchingLocalRepositoryInWorkspace {
+		//1. Retrieve and check necessary data
+		GitBranch branch = changePackage.getBranch();
+		if(branch == null){
+			throw new UnexpectedGitException("The change package has no associated branch");
+		}
+		
+		RepositoryLocation repoLocationModel = branch.getLocation();
+		if(repoLocationModel == null){
+			throw new UnexpectedGitException("The git branch associated with the change package has no repository location.");
+		}
+		if(!(repoLocationModel instanceof GitRepository)){
+			throw new UnexpectedGitException("The repository associated with the git branch is not a git repository.");
+		}
+		GitRepository gitRepoLocation = (GitRepository) repoLocationModel;
+		
+		//1. Find a local repo matching the repo of the change package.
+		Repository repo = GitRepoFindUtil.findAssociatedLocalRepo(gitRepoLocation);
+		if(repo == null){
+			throw new NoMatchingLocalRepositoryInWorkspace("Found no local repository associated with this git repository. Clone from it first.");
+		}
+		
+		//2. Pull the branch from remote
+		//TODO: The pull
+		
+		//3. Checkout the branch
+		try {
+			new Git(repo).checkout().setName(branch.getBranchName()).call();
+		} catch (JGitInternalException e) {
+			throw new UnexpectedGitException(e);
+		} catch (RefAlreadyExistsException e) {
+			throw new UnexpectedGitException(e);
+		} catch (RefNotFoundException e) {
+			throw new UnexpectedGitException(e);
+		} catch (InvalidRefNameException e) {
+			throw new UnexpectedGitException(e);
+		}
+		
+		
+	}
+	
+
+
+}
