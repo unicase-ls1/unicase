f8b6f12701c187707577c4b795f2dfc4986cdc6e
hongkhoanquach <hongkhoanquach@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-15 17:57:54 +0000

    - refactored code

--- other/org.unicase.unicase.projectgenerator2/src/org/unicase/unicase/projectgenerator2/CreateExampleProjectHandler.java
+++ other/org.unicase.unicase.projectgenerator2/src/org/unicase/unicase/projectgenerator2/CreateExampleProjectHandler.java
@@ -20,6 +20,12 @@ public class CreateExampleProjectHandler  extends AbstractHandler implements IHa
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 				
 		final String modelKey = "http://unicase.org/model";
+		String key2 = "http://www.eclipse.org/emf/2004/Ecore2Ecore";
+		String key3 = "http:///org/eclipse/example/library.ecore";
+		
+//		for (EPackage p:ProjectGeneratorUtil.getAllRootModelPackages()) {
+//			System.out.println(p.getNsURI());
+//		}
 		
 		new UnicaseCommand() {
 			@Override
@@ -27,12 +33,10 @@ public class CreateExampleProjectHandler  extends AbstractHandler implements IHa
 				final Project project = WorkspaceManager.getInstance().getCurrentWorkspace().createLocalProject("Generated Project", "Generated").getProject();
 				final EPackage pckge = ProjectGeneratorUtil.getModelPackage(modelKey);
 				ProjectGeneratorConfiguration config = new ProjectGeneratorConfiguration(pckge, project, 5, 3);
-				new ProjectGeneratorImpl().generateModel(config);
+				ProjectGeneratorImpl.generateModel(config);
 			}
 		}.run(false);
-		
-		
-	
+
 		return null;
 	}
 }
