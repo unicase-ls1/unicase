e04c5192afd2b9f1aac884ab3ecb9008cfb3866d
markus1987@gmx.de <markus1987@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-15 12:23:44 +0000

    Changed Property Tester and command "resolve" - still doesn't work

--- other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/DeletedResourceHandler.java
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/DeletedResourceHandler.java
@@ -1,15 +1,11 @@
 package org.unicase.xmi.commands;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
-public class DeletedResourceHandler extends AbstractHandler {
+public class DeletedResourceHandler extends XmiAbstractHandler {
 
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+	public Object execute(ExecutionEvent event) throws ExecutionException {		
 		return null;
 	}
-
-
 }
--- other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ProjectStatusTester.java
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ProjectStatusTester.java
@@ -1,11 +1,19 @@
 package org.unicase.xmi.commands;
 
 import org.eclipse.core.expressions.PropertyTester;
+import org.unicase.ecp.model.workSpaceModel.ECPProject;
+import org.unicase.xmi.workspace.XmiUtil.PROJECT_STATUS;
+import org.unicase.xmi.xmiworkspacestructure.XMIECPFileProject;
 
 public class ProjectStatusTester extends PropertyTester{
 	
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		if (receiver instanceof ECPProject && expectedValue instanceof PROJECT_STATUS){
+			XMIECPFileProject project = (XMIECPFileProject) receiver;
+			if (project.getProjectStatus() == PROJECT_STATUS.FAILED){
+				return true;
+			}
+		}
 		return true;
 	}
-
 }
