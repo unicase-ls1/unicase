ca2488f92af7ee4e4701fc266fe3126d93f38f7c
hongkhoanquach <hongkhoanquach@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-16 21:56:46 +0000

    memory leak: set status to open

--- other/core_branch_memory_leak/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/widgets/AbstractDashboardWidget.java
+++ other/core_branch_memory_leak/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/widgets/AbstractDashboardWidget.java
@@ -85,9 +85,9 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 	public AbstractDashboardWidget() {
 		display = Display.getCurrent();
 		bg = new Color(display, 233, 244, 255);
-		labelProvider = new AdapterFactoryLabelProvider(
-				new ComposedAdapterFactory(
-						ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
+		// hkq: open
+		labelProvider = new AdapterFactoryLabelProvider(new ComposedAdapterFactory(
+			ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
 
 		title = "";
 	}
@@ -95,8 +95,7 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 	/**
 	 * Sets the dashboard for this widget.
 	 * 
-	 * @param dashboard
-	 *            the dashboard.
+	 * @param dashboard the dashboard.
 	 */
 	public void setDashboard(DashboardPage dashboard) {
 		this.dashboard = dashboard;
@@ -105,16 +104,14 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 	/**
 	 * Creates the widget in the given parent.
 	 * 
-	 * @param parent
-	 *            the parent
+	 * @param parent the parent
 	 * @return the widget composite
 	 */
 	public Composite createWidget(final Composite parent) {
 		composite = new Composite(parent, SWT.NONE);
 
 		composite.setBackgroundMode(SWT.INHERIT_FORCE);
-		GridLayoutFactory.fillDefaults().numColumns(2).extendedMargins(12, 12,
-				47, 5).applyTo(composite);
+		GridLayoutFactory.fillDefaults().numColumns(2).extendedMargins(12, 12, 47, 5).applyTo(composite);
 
 		createContentPanel();
 		createToolbar();
@@ -165,8 +162,8 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 		gc.drawString(buffer, x + 2, y);
 		y += titleMetrics.getHeight() + 2;
 		gc.drawLine(x - 3, y, width - 13, y);
-		GridLayoutFactory.fillDefaults().numColumns(2).extendedMargins(12, 12,
-				25 + lines * titleMetrics.getHeight(), 5).applyTo(composite);
+		GridLayoutFactory.fillDefaults().numColumns(2)
+			.extendedMargins(12, 12, 25 + lines * titleMetrics.getHeight(), 5).applyTo(composite);
 	}
 
 	/**
@@ -180,33 +177,23 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 		sysLayout.marginRight = 0;
 		systemToolbar.setLayout(sysLayout);
 
-		DashboardToolbarAction down = new DashboardToolbarAction(systemToolbar,
-				"down.png");
+		DashboardToolbarAction down = new DashboardToolbarAction(systemToolbar, "down.png");
 		down.setToolTipText("Move widget down");
-		final PositionAdapter upListener = new PositionAdapter(
-				PositionAdapter.DOWN);
-		final PositionAdapter downListener = new PositionAdapter(
-				PositionAdapter.UP);
+		final PositionAdapter upListener = new PositionAdapter(PositionAdapter.DOWN);
+		final PositionAdapter downListener = new PositionAdapter(PositionAdapter.UP);
 		down.addMouseListener(upListener);
-		DashboardToolbarAction up = new DashboardToolbarAction(systemToolbar,
-				"up.png");
+		DashboardToolbarAction up = new DashboardToolbarAction(systemToolbar, "up.png");
 		up.setToolTipText("Move widget up");
 		up.addMouseListener(downListener);
 
-		DashboardToolbarAction settings = new DashboardToolbarAction(
-				systemToolbar, "cog.png");
+		DashboardToolbarAction settings = new DashboardToolbarAction(systemToolbar, "cog.png");
 		settings.setToolTipText("Settings");
 		settings.addHyperlinkListener(new HyperlinkAdapter() {
 			@Override
 			public void linkActivated(HyperlinkEvent e) {
-				PreferenceDialog propertyDialog = PreferencesUtil
-						.createPropertyDialogOn(
-								Display.getCurrent().getActiveShell(),
-								WorkspaceManager.getInstance()
-										.getCurrentWorkspace()
-										.getActiveProjectSpace().getProject(),
-								"org.unicase.workspace.ui.dashboardWidgetProperties",
-								null, null);
+				PreferenceDialog propertyDialog = PreferencesUtil.createPropertyDialogOn(Display.getCurrent()
+					.getActiveShell(), WorkspaceManager.getInstance().getCurrentWorkspace().getActiveProjectSpace()
+					.getProject(), "org.unicase.workspace.ui.dashboardWidgetProperties", null, null);
 				propertyDialog.open();
 			}
 		});
@@ -217,8 +204,7 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 	 */
 	protected void createContentPanel() {
 		contentPanel = new Composite(composite, SWT.NONE);
-		GridDataFactory.fillDefaults().span(2, 1).hint(150, SWT.DEFAULT).grab(
-				true, false).applyTo(contentPanel);
+		GridDataFactory.fillDefaults().span(2, 1).hint(150, SWT.DEFAULT).grab(true, false).applyTo(contentPanel);
 	}
 
 	/**
@@ -281,8 +267,7 @@ public abstract class AbstractDashboardWidget implements PaintListener {
 	}
 
 	/**
-	 * @param title
-	 *            the new title.
+	 * @param title the new title.
 	 */
 	protected void setTitle(String title) {
 		this.title = title;
