bbf4296426a456799eef1dc30bcb3e500a7a7963
hongkhoanquach <hongkhoanquach@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-17 23:47:00 +0000

    added labelprovider.dispose() in dispose method

--- other/core_branch_memory_leak/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/DashboardNotificationEntry.java
+++ other/core_branch_memory_leak/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/DashboardNotificationEntry.java
@@ -212,6 +212,7 @@ public class DashboardNotificationEntry extends AbstractDashboardEntry {
 		}
 		super.dispose();
 		((IDisposable) labelProvider.getAdapterFactory()).dispose();
+		labelProvider.dispose();
 	}
 
 	/**
