af7ba06c0b55d493d75544f0e3f56517cd6e598d
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-18 17:53:27 +0000

    Renamed plugin and packages

--- other/org.unicase.unicase.projectgenerator2/src/org/unicase/unicase/modelgenerator/CreateExampleProjectHandler.java
+++ other/org.unicase.unicase.projectgenerator2/src/org/unicase/unicase/modelgenerator/CreateExampleProjectHandler.java
@@ -1,5 +1,5 @@
 
-package org.unicase.unicase.projectgenerator2;
+package org.unicase.unicase.modelgenerator;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -7,9 +7,9 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
 import org.eclipse.emf.ecore.EPackage;
 import org.unicase.metamodel.Project;
-import org.unicase.projectgenerator2.ProjectGeneratorImpl;
-import org.unicase.projectgenerator2.util.ProjectGeneratorConfiguration;
-import org.unicase.projectgenerator2.util.ProjectGeneratorUtil;
+import org.unicase.modelgenerator.ModelGenerator;
+import org.unicase.modelgenerator.util.ModelGeneratorConfiguration;
+import org.unicase.modelgenerator.util.ModelGeneratorUtil;
 import org.unicase.workspace.WorkspaceManager;
 import org.unicase.workspace.util.UnicaseCommand;
 
@@ -20,20 +20,14 @@ public class CreateExampleProjectHandler  extends AbstractHandler implements IHa
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 				
 		final String modelKey = "http://unicase.org/model";
-		String key2 = "http://www.eclipse.org/emf/2004/Ecore2Ecore";
-		String key3 = "http:///org/eclipse/example/library.ecore";
-		
-//		for (EPackage p:ProjectGeneratorUtil.getAllRootModelPackages()) {
-//			System.out.println(p.getNsURI());
-//		}
 		
 		new UnicaseCommand() {
 			@Override
 			protected void doRun() {
 				final Project project = WorkspaceManager.getInstance().getCurrentWorkspace().createLocalProject("Generated Project", "Generated").getProject();
-				final EPackage pckge = ProjectGeneratorUtil.getModelPackage(modelKey);
-				ProjectGeneratorConfiguration config = new ProjectGeneratorConfiguration(pckge, project, 5, 3);
-				ProjectGeneratorImpl.generateModel(config);
+				final EPackage pckge = ModelGeneratorUtil.getModelPackage(modelKey);
+				ModelGeneratorConfiguration config = new ModelGeneratorConfiguration(pckge, project, 5, 3);
+				ModelGenerator.generateModel(config);
 			}
 		}.run(false);
 
