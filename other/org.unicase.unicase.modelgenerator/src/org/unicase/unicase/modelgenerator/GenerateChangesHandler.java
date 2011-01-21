84cbceafe81f0f575da87a0231a7811d405e4520
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-21 22:10:36 +0000

    Reworked Handlers, especially GenerateChangesHandler Added JavaDoc

--- other/org.unicase.unicase.modelgenerator/src/org/unicase/unicase/modelgenerator/GenerateChangesHandler.java
+++ other/org.unicase.unicase.modelgenerator/src/org/unicase/unicase/modelgenerator/GenerateChangesHandler.java
@@ -4,38 +4,60 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
-import org.unicase.metamodel.Project;
 import org.unicase.modelchanger.ModelChanger;
 import org.unicase.modelgenerator.common.ModelGeneratorConfiguration;
-import org.unicase.modelgenerator.common.ModelGeneratorUtil;
 import org.unicase.workspace.ProjectSpace;
-import org.unicase.workspace.WorkspaceManager;
 import org.unicase.workspace.util.UnicaseCommand;
 
+/**
+ * Handler for the "Generate Changes" context menu command.
+ * The command is only available if an EObject is selected.
+ * If the selected EObject is a ProjectSpace, the contained
+ * Project is used as the root, otherwise the EObject itself
+ * will be the root.
+ */
 public class GenerateChangesHandler extends AbstractHandler {
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
-		final String modelKey = "http://unicase.org/model";
-		
 		ISelection selection = HandlerUtil.getCurrentSelection(event);
-		final IStructuredSelection strucSel = (IStructuredSelection)selection;
-		final ProjectSpace ps = (ProjectSpace) strucSel.getFirstElement();
-		
+		final EObject rootObject = validateSelection(selection);
 		
 		new UnicaseCommand() {
 			@Override
 			protected void doRun() {
-				final EPackage pckge = ModelGeneratorUtil.getEPackage(modelKey);
-				ModelGeneratorConfiguration config = new ModelGeneratorConfiguration(pckge, ps.getProject(), 5, 2);
+				ModelGeneratorConfiguration config = new ModelGeneratorConfiguration(null, rootObject);
 				ModelChanger.generateChanges(config);
 			}
 		}.run(false);
 
 		return null;
+	}
+	
+	/**
+	 * Validates selection in terms of checking what's selected:<br>
+	 * - throws IllegalArgumentException if no EObject is selected
+	 * - if a ProjectSpace is selected, returns the contained Project
+	 * - if any other EObject is selected, returns this EObject
+	 * 
+	 * @param selection the selection made in the navigator
+	 * @return the valid EObject
+	 */
+	private EObject validateSelection(ISelection selection) {
+		if(selection != null && selection instanceof IStructuredSelection) {
+			IStructuredSelection strucSel = (IStructuredSelection) selection;
+			Object selectedElement = strucSel.getFirstElement();
+			if(selectedElement instanceof ProjectSpace)
+				return ((ProjectSpace) selectedElement).getProject();
+			else if(selectedElement instanceof EObject)
+				return (EObject) selectedElement;
+			else throw new IllegalArgumentException("Selected Element is no EObject!");
+		} else throw new IllegalArgumentException("Nothing is selected!");
 	}
 }
--- other/org.unicase.unicase.modelgenerator/src/org/unicase/unicase/modelgenerator/GenerateProjectHandler.java
+++ other/org.unicase.unicase.modelgenerator/src/org/unicase/unicase/modelgenerator/GenerateProjectHandler.java
@@ -4,38 +4,50 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.unicase.metamodel.Project;
-import org.unicase.metamodel.util.ModelUtil;
 import org.unicase.modelgenerator.ModelGenerator;
 import org.unicase.modelgenerator.common.ModelGeneratorConfiguration;
 import org.unicase.modelgenerator.common.ModelGeneratorUtil;
 import org.unicase.workspace.WorkspaceManager;
 import org.unicase.workspace.util.UnicaseCommand;
 
-
-
+/**
+ * Handler for the "Generate Project" context menu command.
+ * The command is available no matter what is selected.
+ * This handler creates a project and generates children from
+ * the package defined in <code>MODEL_KEY</code>, where width
+ * and depth are defined in <code>WIDTH</code> and <code>DEPTH</code>.
+ */
 public class GenerateProjectHandler extends AbstractHandler implements IHandler {
+	
+	/**
+	 * The NsURI of the EPackage to generate EObjects from.
+	 */
+	private final String MODEL_KEY = "http://unicase.org/model";
+	
+	/**
+	 * The maximum number of children for each EObject.
+	 */
+	private final int WIDTH = 3;
+	
+	/**
+	 * The maximum hierarchy depth of the project.
+	 */
+	private final int DEPTH = 3;
 
+	/**
+	 * ({@inheritDoc})
+	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 				
-		final String modelKey = "http://unicase.org/model";
-		
 		new UnicaseCommand() {
 			@Override
 			protected void doRun() {
 				final Project project = WorkspaceManager.getInstance().getCurrentWorkspace().createLocalProject("Generated Project", "Generated").getProject();
-				final EPackage pckge = ModelGeneratorUtil.getEPackage(modelKey);
-				EObject rootObject = project;
-//				for(EClass ec : ModelGeneratorUtil.getAllModelElementEClasses(pckge))
-//					if(ec.getName().equals("CompositeSection"))
-//						rootObject = EcoreUtil.create(ec);
-				ModelGeneratorConfiguration config = new ModelGeneratorConfiguration(pckge, rootObject);
+				final EPackage pckge = ModelGeneratorUtil.getEPackage(MODEL_KEY);
+				ModelGeneratorConfiguration config = new ModelGeneratorConfiguration(pckge, project, WIDTH, DEPTH);
 				ModelGenerator.generateModel(config);
-//				project.addModelElement(rootObject);
 			}
 		}.run(false);
 
