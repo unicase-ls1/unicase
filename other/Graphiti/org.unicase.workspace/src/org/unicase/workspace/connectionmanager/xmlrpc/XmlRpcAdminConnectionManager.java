b5695b4d6c957b97c7eca26dee8459fec2fb1d3c
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-26 22:26:44 +0000

    Initial import.

---
+++ other/Graphiti/org.unicase.workspace/src/org/unicase/workspace/connectionmanager/xmlrpc/XmlRpcAdminConnectionManager.java
@@ -0,0 +1,188 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.workspace.connectionmanager.xmlrpc;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.unicase.emfstore.connection.xmlrpc.XmlRpcAdminConnectionHander;
+import org.unicase.emfstore.esmodel.ProjectId;
+import org.unicase.emfstore.esmodel.ProjectInfo;
+import org.unicase.emfstore.esmodel.SessionId;
+import org.unicase.emfstore.esmodel.accesscontrol.ACGroup;
+import org.unicase.emfstore.esmodel.accesscontrol.ACOrgUnit;
+import org.unicase.emfstore.esmodel.accesscontrol.ACOrgUnitId;
+import org.unicase.emfstore.esmodel.accesscontrol.ACUser;
+import org.unicase.emfstore.esmodel.accesscontrol.roles.Role;
+import org.unicase.emfstore.exceptions.ConnectionException;
+import org.unicase.emfstore.exceptions.EmfStoreException;
+import org.unicase.workspace.ServerInfo;
+import org.unicase.workspace.connectionmanager.AbstractConnectionManager;
+import org.unicase.workspace.connectionmanager.AdminConnectionManager;
+
+/**
+ * connection manager for adminemfstore.
+ * 
+ * @author wesendon
+ */
+public class XmlRpcAdminConnectionManager extends AbstractConnectionManager<XmlRpcClientManager> implements
+	AdminConnectionManager {
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void initConnection(ServerInfo serverInfo, SessionId id) throws ConnectionException {
+		XmlRpcClientManager clientManager = new XmlRpcClientManager(XmlRpcAdminConnectionHander.ADMINEMFSTORE);
+		clientManager.initConnection(serverInfo);
+		addConnectionProxy(id, clientManager);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void addMember(SessionId sessionId, ACOrgUnitId group, ACOrgUnitId member) throws EmfStoreException {
+		getConnectionProxy(sessionId).call("addMember", sessionId, group, member);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void addParticipant(SessionId sessionId, ProjectId projectId, ACOrgUnitId participant)
+		throws EmfStoreException {
+		getConnectionProxy(sessionId).call("addParticipant", sessionId, projectId, participant);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void changeOrgUnit(SessionId sessionId, ACOrgUnitId orgUnitId, String name, String description)
+		throws EmfStoreException {
+		getConnectionProxy(sessionId).call("changeOrgUnit", sessionId, orgUnitId, name, description);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void changeRole(SessionId sessionId, ProjectId projectId, ACOrgUnitId orgUnit, EClass role)
+		throws EmfStoreException {
+		getConnectionProxy(sessionId).call("changeRole", sessionId, projectId, orgUnit, role);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ACOrgUnitId createGroup(SessionId sessionId, String name) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithResult("createGroup", ACOrgUnitId.class, sessionId, name);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ACOrgUnitId createUser(SessionId sessionId, String name) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithResult("createUser", ACOrgUnitId.class, sessionId, name);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void deleteGroup(SessionId sessionId, ACOrgUnitId group) throws EmfStoreException {
+		getConnectionProxy(sessionId).call("deleteGroup", sessionId, group);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void deleteUser(SessionId sessionId, ACOrgUnitId user) throws EmfStoreException {
+		getConnectionProxy(sessionId).call("deleteUser", sessionId, user);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ACGroup> getGroups(SessionId sessionId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getGroups", ACGroup.class, sessionId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ACGroup> getGroups(SessionId sessionId, ACOrgUnitId user) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getGroups", ACGroup.class, sessionId, user);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ACOrgUnit> getMembers(SessionId sessionId, ACOrgUnitId groupId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getMembers", ACOrgUnit.class, sessionId, groupId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ACOrgUnit getOrgUnit(SessionId sessionId, ACOrgUnitId orgUnitId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithResult("getOrgUnit", ACOrgUnit.class, sessionId, orgUnitId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ACOrgUnit> getOrgUnits(SessionId sessionId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getOrgUnits", ACOrgUnit.class, sessionId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ACOrgUnit> getParticipants(SessionId sessionId, ProjectId projectId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getParticipants", ACOrgUnit.class, sessionId,
+			projectId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ProjectInfo> getProjectInfos(SessionId sessionId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getProjectInfos", ProjectInfo.class, sessionId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Role getRole(SessionId sessionId, ProjectId projectId, ACOrgUnitId orgUnit) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithResult("getRole", Role.class, sessionId, projectId, orgUnit);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<ACUser> getUsers(SessionId sessionId) throws EmfStoreException {
+		return getConnectionProxy(sessionId).callWithListResult("getUsers", ACUser.class, sessionId);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void removeGroup(SessionId sessionId, ACOrgUnitId user, ACOrgUnitId group) throws EmfStoreException {
+		getConnectionProxy(sessionId).call("removeGroup", sessionId, user, group);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void removeMember(SessionId sessionId, ACOrgUnitId group, ACOrgUnitId member) throws EmfStoreException {
+		getConnectionProxy(sessionId).call("removeMember", sessionId, group, member);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void removeParticipant(SessionId sessionId, ProjectId projectId, ACOrgUnitId participant)
+		throws EmfStoreException {
+		getConnectionProxy(sessionId).call("removeParticipant", sessionId, projectId, participant);
+	}
+
+}
---
+++ other/Graphiti/org.unicase.workspace/src/org/unicase/workspace/connectionmanager/xmlrpc/XmlRpcClientManager.java
@@ -0,0 +1,165 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.workspace.connectionmanager.xmlrpc;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.client.XmlRpcClient;
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
+import org.apache.xmlrpc.client.XmlRpcSun15HttpTransportFactory;
+import org.unicase.emfstore.connection.xmlrpc.util.EObjectTypeFactory;
+import org.unicase.emfstore.exceptions.ConnectionException;
+import org.unicase.emfstore.exceptions.EmfStoreException;
+import org.unicase.metamodel.util.SerializationException;
+import org.unicase.workspace.ServerInfo;
+import org.unicase.workspace.connectionmanager.ConnectionManager;
+import org.unicase.workspace.connectionmanager.KeyStoreManager;
+import org.unicase.workspace.exceptions.CertificateStoreException;
+import org.xml.sax.SAXException;
+
+/**
+ * Manager for XML RPC server calls.
+ * 
+ * @author wesendon
+ */
+public class XmlRpcClientManager {
+
+	/**
+	 * Default constructor.
+	 * 
+	 * @param serverInterface name of interface
+	 */
+	public XmlRpcClientManager(String serverInterface) {
+		this.serverInterface = serverInterface;
+	}
+
+	private String serverInterface;
+	private XmlRpcClient client;
+
+	/**
+	 * Initializes the connection.
+	 * 
+	 * @param serverInfo server info
+	 * @throws ConnectionException in case of failure
+	 */
+	public void initConnection(ServerInfo serverInfo) throws ConnectionException {
+		try {
+			XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+			config.setServerURL(createURL(serverInfo));
+			config.setEnabledForExceptions(true);
+			config.setEnabledForExtensions(true);
+			config.setConnectionTimeout(60 * 1000);
+			config.setReplyTimeout(60 * 1000);
+
+			client = new XmlRpcClient();
+			client.setTypeFactory(new EObjectTypeFactory(client));
+
+			XmlRpcSun15HttpTransportFactory factory = new XmlRpcSun15HttpTransportFactory(client);
+
+			try {
+				factory.setSSLSocketFactory(KeyStoreManager.getInstance().getSSLContext().getSocketFactory());
+			} catch (CertificateStoreException e) {
+				throw new ConnectionException("Couldn't load certificate", e);
+			}
+			client.setTransportFactory(factory);
+
+			client.setConfig(config);
+
+			// } catch (XmlRpcException e) {
+			// throw new ConnectionException("", e);
+		} catch (MalformedURLException e) {
+			throw new ConnectionException("Malformed Url or Port", e);
+		}
+	}
+
+	private URL createURL(ServerInfo serverInfo) throws MalformedURLException {
+		checkUrl(serverInfo.getUrl());
+		return new URL("https", serverInfo.getUrl(), serverInfo.getPort(), "xmlrpc");
+	}
+
+	private void checkUrl(String url) throws MalformedURLException {
+		if (url != null && !url.equals("")) {
+			if (!(url.contains(":") || url.contains("/"))) {
+				return;
+			}
+		}
+		throw new MalformedURLException();
+	}
+
+	/**
+	 * Executes a server call with return value.
+	 * 
+	 * @param <T> return type
+	 * @param methodName method name
+	 * @param returnType return type
+	 * @param parameters parameters
+	 * @return returned object from server
+	 * @throws EmfStoreException in case of failure
+	 */
+	public <T> T callWithResult(String methodName, Class<T> returnType, Object... parameters) throws EmfStoreException {
+		return executeCall(methodName, returnType, parameters);
+	}
+
+	/**
+	 * Executes a server call with list return value.
+	 * 
+	 * @param <T> return type
+	 * @param methodName method name
+	 * @param returnType list return type
+	 * @param parameters parameters
+	 * @return list return type
+	 * @throws EmfStoreException in case of failure
+	 */
+	@SuppressWarnings("unchecked")
+	public <T> List<T> callWithListResult(String methodName, Class<T> returnType, Object... parameters)
+		throws EmfStoreException {
+		List<T> result = new ArrayList<T>();
+		Object[] callResult = executeCall(methodName, Object[].class, parameters);
+		if (callResult == null) {
+			return result;
+		}
+		for (Object obj : callResult) {
+			result.add((T) obj);
+		}
+		return result;
+	}
+
+	/**
+	 * Executes a server call without return value.
+	 * 
+	 * @param methodName method name
+	 * @param parameters parameters
+	 * @throws EmfStoreException in case of failure
+	 */
+	public void call(String methodName, Object... parameters) throws EmfStoreException {
+		executeCall(methodName, null, parameters);
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> T executeCall(String methodName, Class<T> returnType, Object[] params) throws EmfStoreException {
+		if (client == null) {
+			throw new ConnectionException(ConnectionManager.REMOTE);
+		}
+		try {
+			return (T) client.execute(serverInterface + "." + methodName, params);
+		} catch (XmlRpcException e) {
+			if (e.getCause() instanceof EmfStoreException) {
+				throw ((EmfStoreException) e.getCause());
+			} else if (e.linkedException instanceof SAXException
+				&& ((SAXException) e.linkedException).getException() instanceof SerializationException) {
+				SerializationException serialE = (SerializationException) ((SAXException) e.linkedException)
+					.getException();
+				throw new org.unicase.emfstore.exceptions.SerializationException(serialE);
+			} else {
+				throw new ConnectionException(ConnectionManager.REMOTE, e);
+			}
+		}
+	}
+}
