09a6d3cf10e3a1af4dd26c6044629181da84ba76
zhodaie <zhodaie@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-26 22:21:01 +0000

    Initial import.

---
+++ other/Graphiti/org.unicase.model/src/org/unicase/model/diagram/impl/OrgChartDiagramImpl.java
@@ -0,0 +1,41 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.diagram.impl;
+
+import org.eclipse.emf.ecore.EClass;
+import org.unicase.model.diagram.DiagramPackage;
+import org.unicase.model.diagram.OrgChartDiagram;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Org Chart Diagram</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class OrgChartDiagramImpl extends GDiagramImpl implements OrgChartDiagram {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected OrgChartDiagramImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return DiagramPackage.Literals.ORG_CHART_DIAGRAM;
+	}
+
+} //OrgChartDiagramImpl
