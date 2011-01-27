3f3c26bb3eb3ddca9431a34a3c67053d012a97dd
chrizero@z-corp-online.de <chrizero@z-corp-online.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-27 13:40:43 +0000

    BowlingExample: commit matchupgenerator (unimplemented)

---
+++ developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.matchupgenerator/src/org/unicase/bowlingexample/matchupgenerator/MatchupGeneratorHandler.java
@@ -0,0 +1,14 @@
+package org.unicase.bowlingexample.matchupgenerator;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class MatchupGeneratorHandler extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
