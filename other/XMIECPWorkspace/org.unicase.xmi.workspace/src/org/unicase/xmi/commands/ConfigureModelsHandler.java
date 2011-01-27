c90594dd6837cd6cec5e941a524514e43cac4ef8
markus1987@gmx.de <markus1987@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-27 13:29:54 +0000

    Configure Models Dialog and Handler added (not implemented)

---
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ConfigureModelsHandler.java
@@ -0,0 +1,14 @@
+package org.unicase.xmi.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ConfigureModelsHandler extends AbstractHandler{
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/views/ConfigureModelsDialog.java
@@ -0,0 +1,13 @@
+package org.unicase.xmi.views;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class ConfigureModelsDialog extends TitleAreaDialog {
+
+	public ConfigureModelsDialog(Shell parentShell) {
+		super(parentShell);
+		// TODO Auto-generated constructor stub
+	}
+
+}
