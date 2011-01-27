0a9ff041d9ad87594e41fb20b6b6d02773db4fae
markus1987@gmx.de <markus1987@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-27 13:39:20 +0000

    Started to implement ConfigureModels Handler and Dialog

--- other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ConfigureModelsHandler.java
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ConfigureModelsHandler.java
@@ -1,14 +1,26 @@
 package org.unicase.xmi.commands;
 
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.PlatformUI;
+import org.unicase.xmi.views.ConfigureModelsDialog;
 
 public class ConfigureModelsHandler extends AbstractHandler{
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		// open dialog
+		ConfigureModelsDialog dialog = new ConfigureModelsDialog(PlatformUI
+			.getWorkbench().getDisplay().getActiveShell(), this);
+		
+		
 		return null;
+	}
+	
+	public void setModelList(List<String> list){
+		
 	}
 
 }
--- other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/views/ConfigureModelsDialog.java
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/views/ConfigureModelsDialog.java
@@ -2,10 +2,11 @@ package org.unicase.xmi.views;
 
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.unicase.xmi.commands.ConfigureModelsHandler;
 
 public class ConfigureModelsDialog extends TitleAreaDialog {
 
-	public ConfigureModelsDialog(Shell parentShell) {
+	public ConfigureModelsDialog(Shell parentShell, ConfigureModelsHandler handler) {
 		super(parentShell);
 		// TODO Auto-generated constructor stub
 	}
