90e379369e32a21c84a8d30c984d1ee6c4ea4304
worldwidetobi@gmx.de <worldwidetobi@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-27 23:24:37 +0000

    Initial import.

---
+++ developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.navigatorcustomization/src/org/eclipse/bowlingmodel/navigatorcustomisation/MEDropAdaptorTournament.java
@@ -0,0 +1,28 @@
+package org.eclipse.bowlingmodel.navigatorcustomisation;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.unicase.ui.common.dnd.MEDropAdapter;
+
+import bowling.BowlingPackage;
+import bowling.Playerlist;
+import bowling.Tournament;
+
+public class MEDropAdaptorTournament extends MEDropAdapter {
+
+	@Override
+	public EClass isDropAdapterfor() {
+		// TODO Auto-generated method stub
+		return BowlingPackage.eINSTANCE.getTournament();
+	}
+
+	public void drop(DropTargetEvent event, EObject target, List<EObject> source){
+		if(source.get(0) instanceof Playerlist | target instanceof Tournament){
+			Playerlist playerlist = (Playerlist) source.get(0);
+			((Tournament) target).setPlayerlist(playerlist);
+		}
+	}
+}
