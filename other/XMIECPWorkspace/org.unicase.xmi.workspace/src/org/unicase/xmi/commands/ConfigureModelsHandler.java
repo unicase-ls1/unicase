27bd1d09b99638d9a10727371240d01d09e795d9
markus1987@gmx.de <markus1987@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-27 14:37:55 +0000

    Implemented ConfigureModelsHandler added rightclickmenu entry

--- other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ConfigureModelsHandler.java
+++ other/XMIECPWorkspace/org.unicase.xmi.workspace/src/org/unicase/xmi/commands/ConfigureModelsHandler.java
@@ -5,21 +5,55 @@ import java.util.List;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.window.Window;
 import org.eclipse.ui.PlatformUI;
+import org.unicase.ecp.model.ECPWorkspaceManager;
+import org.unicase.ecp.model.MetaModelElementContext;
+import org.unicase.ecp.model.NoWorkspaceException;
+import org.unicase.xmi.exceptions.XMIWorkspaceException;
 import org.unicase.xmi.views.ConfigureModelsDialog;
+import org.unicase.xmi.workspace.XMIECPWorkspace;
+import org.unicase.xmi.workspace.XMIMetaModelElementContext;
+import org.unicase.xmi.xmiworkspacestructure.XMIECPFileProject;
 
+/**
+ * Handles the user's choice to add or remove a model from a FileProject
+ * @author Markus, Matti
+ *
+ */
 public class ConfigureModelsHandler extends AbstractHandler{
 
+	private List<String> list;
+
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		
+		//determine an open project
+		XMIECPFileProject project = null;
+		try {
+			project = (XMIECPFileProject) ECPWorkspaceManager.getInstance().getWorkSpace().getActiveProject();
+		} catch (NoWorkspaceException e) {
+			new XMIWorkspaceException("No Workspace available.", e);
+		}
+		
 		// open dialog
 		ConfigureModelsDialog dialog = new ConfigureModelsDialog(PlatformUI
-			.getWorkbench().getDisplay().getActiveShell(), this);
+			.getWorkbench().getDisplay().getActiveShell(), this, project);
 		
+		//work with the result of the dialog to add or remove models from FileProject
+		if (dialog.open() == Window.OK){
+			XMIMetaModelElementContext context = (XMIMetaModelElementContext) project.getMetaModelElementContext();
+			
+			//Add all selected models
+			for(String s : list){
+				context.addModel(s);
+			}
+		}
 		
 		return null;
 	}
 	
 	public void setModelList(List<String> list){
+		this.list = list;
 		
 	}
 
