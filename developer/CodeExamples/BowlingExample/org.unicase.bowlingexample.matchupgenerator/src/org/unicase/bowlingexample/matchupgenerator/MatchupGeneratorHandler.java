9934262f09de6e8f7f0261ec31d1d28c25372961
chrizero@z-corp-online.de <chrizero@z-corp-online.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-27 14:31:53 +0000

    BowlingExample: MatchupGenerator updated, still not working, no algorithm implemented

--- developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.matchupgenerator/src/org/unicase/bowlingexample/matchupgenerator/MatchupGeneratorHandler.java
+++ developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.matchupgenerator/src/org/unicase/bowlingexample/matchupgenerator/MatchupGeneratorHandler.java
@@ -1,13 +1,49 @@
 package org.unicase.bowlingexample.matchupgenerator;
 
+import javax.swing.JOptionPane;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecore.EObject;
+import org.unicase.ui.common.util.ActionHelper;
+import org.unicase.ui.navigator.TreeView;
+
+import bowling.BowlingFactory;
+import bowling.Matchup;
+import bowling.Tournament;
 
 public class MatchupGeneratorHandler extends AbstractHandler {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		// get EObject
+		EObject eObject = ActionHelper.getSelection(event);
+		if (!(eObject instanceof Tournament)) {
+			return null;
+		}
+		// cast
+		final Tournament t = (Tournament) eObject;
+		// check playerlist
+		if (t.getPlayerlist() == null) {
+			JOptionPane.showMessageDialog(null, "No playerlist available.",
+					"Generate Matchups", JOptionPane.INFORMATION_MESSAGE);
+			return null;
+		}
+		// check existing matchups
+		if (!t.getMatchup().isEmpty()) {
+			// ask user if matchups shall be deleted
+			int userInput = JOptionPane.showConfirmDialog(null, "Do you really want to delete all existent matchups and create new ones?",
+					"Generate Matchups", JOptionPane.YES_NO_OPTION);
+			if (userInput == JOptionPane.NO_OPTION) {
+				return null;
+			}
+		}
+		// implement - which command?
+		t.getMatchup().clear();
+		Matchup m = BowlingFactory.eINSTANCE.createMatchup();
+		m.setTournament(t);		
+		
+		TreeView.getTreeViewer().refresh();
 		return null;
 	}
 
