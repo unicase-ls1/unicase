6a77891f019e07f96647f1275dcee6362f6bbb39
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-01-29 17:14:41 +0000

    Moved GUI to its own plugin.

--- other/org.unicase.modelgenerator/src/org/unicase/modelgenerator/IGuiListener.java
+++
@@ -1,7 +0,0 @@
-package org.unicase.modelgenerator;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-
-public interface IGuiListener {
-	public void runInCommand(ModelGeneratorGUI gui, IProgressMonitor monitor);
-}
