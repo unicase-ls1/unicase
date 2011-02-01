caa73e235c6b35ec0bdd77b337e869cc72f67af9
cunoa@in.tum.de <cunoa@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-02-01 09:50:35 +0000

    initial import

---
+++ other/GMFTask/Andrea/org.unicase.orgChart.diagram/src/org/unicase/model/orga/diagram/preferences/DiagramPrintingPreferencePage.java
@@ -0,0 +1,18 @@
+package org.unicase.model.orga.diagram.preferences;
+
+import org.eclipse.gmf.runtime.diagram.ui.preferences.PrintingPreferencePage;
+import org.unicase.model.orga.diagram.part.OrgaDiagramEditorPlugin;
+
+/**
+ * @generated
+ */
+public class DiagramPrintingPreferencePage extends PrintingPreferencePage {
+
+	/**
+	 * @generated
+	 */
+	public DiagramPrintingPreferencePage() {
+		setPreferenceStore(OrgaDiagramEditorPlugin.getInstance()
+				.getPreferenceStore());
+	}
+}
