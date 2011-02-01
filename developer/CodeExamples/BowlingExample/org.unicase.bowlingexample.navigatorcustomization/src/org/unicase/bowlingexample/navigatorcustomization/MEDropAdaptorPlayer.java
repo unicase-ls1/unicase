45668b6306e5080c87de0fa2b1354c102e69acf5
worldwidetobi@gmx.de <worldwidetobi@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-02-01 22:12:21 +0000

    custom page and dnd features "running again"

---
+++ developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.navigatorcustomization/src/org/unicase/bowlingexample/navigatorcustomization/MEDropAdaptorPlayer.java
@@ -0,0 +1,27 @@
+package org.unicase.bowlingexample.navigatorcustomization;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.dnd.DropTargetEvent;
+import org.unicase.ui.common.dnd.MEDropAdapter;
+
+import bowling.BowlingPackage;
+import bowling.Game;
+import bowling.Player;
+
+public class MEDropAdaptorPlayer extends MEDropAdapter {
+
+	@Override
+	public EClass isDropAdapterfor() {
+		return BowlingPackage.eINSTANCE.getPlayer();
+	}
+	
+	public void drop(DropTargetEvent event, EObject target, List<EObject> source){
+		if(source.get(0) instanceof Game | target instanceof Player){
+			((Game)source.get(0)).setPlayer((Player)target);
+		}
+	}
+
+}
