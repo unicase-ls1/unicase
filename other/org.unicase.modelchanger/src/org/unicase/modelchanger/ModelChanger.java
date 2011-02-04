df387a43dd07ab76c77112e7559369510ebebe51
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-02-04 15:48:48 +0000

    Major Refactoring: Created ModelChangerHelper to divide work Added possibility to pass monitor to Changer, for ProgressBar use Fixes in JavaDoc

--- other/org.unicase.modelchanger/src/org/unicase/modelchanger/ModelChanger.java
+++ other/org.unicase.modelchanger/src/org/unicase/modelchanger/ModelChanger.java
@@ -1,19 +1,16 @@
 package org.unicase.modelchanger;
 
-import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 
-import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.unicase.modelgenerator.common.attribute.AttributeHandler;
 import org.unicase.modelgenerator.common.ModelGeneratorUtil;
 
 /**
@@ -27,23 +24,6 @@ import org.unicase.modelgenerator.common.ModelGeneratorUtil;
  * @see #generateChanges(EObject, long, boolean)
  */
 public class ModelChanger {
-	
-	/**
-	 * Random-object to compute random values for deleting EObjects
-	 * and setting their attributes and references.
-	 */
-	private static Random random;
-	
-	/**
-	 * All EObjects including the root and all its direct and indirect contents,
-	 * after deleting random EObjects.
-	 */
-	private static Map<EClass, List<EObject>> allObjectsByEClass;
-	
-	/**
-	 * A set of RuntimeExceptions that occurred during the last changing process.
-	 */
-	private static Set<RuntimeException> exceptionLog;
 	
 	/**
 	 * Private constructor.
@@ -60,7 +40,7 @@ public class ModelChanger {
 	 * 
 	 * @param rootObject the root EObject
 	 * @see #generateChanges(EObject, long, boolean)
-	 * @see #generateChanges()
+	 * @see #generateChanges(EObject, long, boolean, IProgressMonitor)
 	 */
 	public static void generateChanges(EObject rootObject) {
 		generateChanges(rootObject, System.currentTimeMillis(), false);
@@ -73,39 +53,56 @@ public class ModelChanger {
 	 * Changing includes deleting EObjects and their children and replacing all set
 	 * attributes and references (no containment-references) with new values. 
 	 * 
-	 * @param root the root EObject
+	 * @param rootObject the root EObject
 	 * @param seed value to determine randomness
 	 * @param ignoreAndLog should exceptions be ignored and added to the log?
-	 * @see #generateChanges()
+	 * @see #generateChanges(EObject, long, boolean, IProgressMonitor)
 	 */
-	public static void generateChanges(EObject root, long seed, boolean ignoreAndLog) {
-		random = new Random(seed);
-		AttributeHandler.setRandom(random);
-		Set<EObject> allChildren = new LinkedHashSet<EObject>();
-		EObject rootObject = root;
-		TreeIterator<EObject> allContents = rootObject.eAllContents();
-		while(allContents.hasNext()) {
-			allChildren.add(allContents.next());
-		}
+	public static void generateChanges(EObject rootObject, long seed, boolean ignoreAndLog) {
+		ModelChangerHelper.init(seed, ignoreAndLog);
+		Set<EObject> allChildren = ModelChangerHelper.getAllChildren(rootObject); 
 		deleteRandomEObjects(allChildren);
-		allObjectsByEClass = ModelGeneratorUtil.getAllClassesAndObjects(rootObject);
-		generateChanges(ignoreAndLog);
+		changeAttributesAndReferences(rootObject);
 	}
 	
 	/**
+	 * Generates changes for the specified root and all its direct and indirect contents
+	 * using <code>seed</code> to determine random values and <code>ignoreAndLog</code>
+	 * to decide whether occurring RuntimeExceptions shall be thrown or logged only.
+	 * Changing includes deleting EObjects and their children and replacing all set
+	 * attributes and references (no containment-references) with new values.
+	 * Using the <code>monitor</code>, a progress bar is shown during the changing process,
+	 * also making it possible to cancel it.
+	 * 
+	 * @param rootObject the root EObject
+	 * @param seed value to determine randomness
+	 * @param ignoreAndLog should exceptions be ignored and added to the log?
+	 * @param monitor the monitor displaying the progress bar
+	 * @see #generateChanges(EObject, long, boolean)
+	 */
+	public static void generateChanges(EObject rootObject, long seed, boolean ignoreAndLog, IProgressMonitor monitor) {
+		ModelChangerHelper.init(seed, ignoreAndLog);
+		monitor.beginTask("Changing process", ModelChangerHelper.getAmountOfWork());
+		Set<EObject> allChildren = ModelChangerHelper.getAllChildren(rootObject);
+		deleteRandomEObjects(allChildren, monitor);
+		changeAttributesAndReferences(rootObject, monitor);
+	}
+
+	/**
 	 * Randomly deletes EObjects and their children from a set of EObjects.
 	 * <code>allChildren</code> shouldn't contain the rootObject, so the
-	 * root (and therefore ALL existing EObjects) doesn't get deleted.
+	 * root (and therefore ALL contained EObjects) doesn't get deleted.
 	 * 
 	 * @param allChildren set of EObjects from which EObjects are selected to be deleted
 	 * @see #deleteAllChildren(Set)
+	 * @see #deleteRandomEObjects(Set, IProgressMonitor)
 	 */
 	private static void deleteRandomEObjects(Set<EObject> allChildren) {
 		Set<EObject> deletedChildren = new LinkedHashSet<EObject>();
 		for(EObject eObject : allChildren) {
 			if(deletedChildren.contains(eObject))
 				continue;
-			if(random.nextDouble() < 0.1) {
+			if(ModelChangerHelper.randomDelete()) {
 				Set<EObject> contentCopy = new LinkedHashSet<EObject>(eObject.eContents());
 				deletedChildren.addAll(deleteAllChildren(contentCopy));
 				EcoreUtil.delete(eObject);
@@ -133,22 +130,82 @@ public class ModelChanger {
 	}
 
 	/**
-	 * Performs changes upon every EObject (the root and all its direct and indirect contents).
-	 * Changing includes replacing attribute and reference values.
-	 * Deleting EObjects is performed before this operation.
+	 * Changes all attributes and references for every child (direct and indirect)
+	 * of <code>root</code>.
 	 * 
-	 * @param ignoreAndLog should exceptions be ignored and added to the log?
-	 * @see #generateChanges(EObject)
-	 * @see #generateChanges(EObject, long, boolean)
+	 * @param root the root EObject of the hierarchy that shall be changed
+	 * @see #changeAttributesAndReferences(EObject, IProgressMonitor)
+	 * @see #changeEObjectAttributes(EObject)
+	 * @see #changeEObjectReferences(EObject, Map)
 	 */
-	private static void generateChanges(boolean ignoreAndLog) {
+	private static void changeAttributesAndReferences(EObject root) {
+		Map<EClass, List<EObject>> allObjectsByEClass = ModelGeneratorUtil.getAllClassesAndObjects(root);
 		for(EClass eClass : allObjectsByEClass.keySet()) {
 			for(EObject eObject : allObjectsByEClass.get(eClass)) {
-				changeEObjectAttributes(eObject, ignoreAndLog);
-				changeEObjectReferences(eObject, ignoreAndLog);
+				changeEObjectAttributes(eObject);
+				changeEObjectReferences(eObject, allObjectsByEClass);
 			}
 		}
 	}
+	
+
+	/**
+	 * Randomly deletes EObjects and their children from a set of EObjects.
+	 * <code>allChildren</code> shouldn't contain the rootObject, so the
+	 * root (and therefore ALL contained EObjects) doesn't get deleted.
+	 * The <code>monitor</code> shows the progress of the whole changing process,
+	 * also making it possible to cancel it.
+	 * 
+	 * @param allChildren set of EObjects from which EObjects are selected to be deleted
+	 * @param monitor the monitor displaying the progress bar
+	 * @see #deleteAllChildren(Set)
+	 */
+	private static void deleteRandomEObjects(Set<EObject> allChildren, IProgressMonitor monitor) {
+		Set<EObject> deletedChildren = new LinkedHashSet<EObject>();
+		for(EObject eObject : allChildren) {
+			if(monitor.isCanceled())
+				return;
+			if(deletedChildren.contains(eObject)) {
+				monitor.worked(1);
+				continue;
+			}
+			if(ModelChangerHelper.randomDelete()) {
+				Set<EObject> contentCopy = new LinkedHashSet<EObject>(eObject.eContents());
+				deletedChildren.addAll(deleteAllChildren(contentCopy));
+				EcoreUtil.delete(eObject);
+				monitor.worked(1);
+			}
+		}
+		monitor.worked(deletedChildren.size());
+	}
+	
+	/**
+	 * Changes all attributes and references for every child (direct and indirect)
+	 * of <code>root</code>. The <code>monitor</code> shows the progress of the 
+	 * whole changing process, also making it possible to cancel it.
+	 * 
+	 * @param root the root EObject of the hierarchy that shall be changed
+	 * @param monitor the monitor displaying the progress bar
+	 * @see #changeAttributesAndReferences(EObject)
+	 * @see #changeEObjectAttributes(EObject)
+	 * @see #changeEObjectReferences(EObject, Map)
+	 */
+	private static void changeAttributesAndReferences(EObject root, IProgressMonitor monitor) {
+		Map<EClass, List<EObject>> allObjectsByEClass = ModelGeneratorUtil.getAllClassesAndObjects(root);
+		for(EClass eClass : allObjectsByEClass.keySet()) {
+			if(monitor.isCanceled())
+				return;
+			for(EObject eObject : allObjectsByEClass.get(eClass)) {
+				if(monitor.isCanceled())
+					return;
+				changeEObjectAttributes(eObject);
+				monitor.worked(1);
+				changeEObjectReferences(eObject, allObjectsByEClass);
+				monitor.worked(1);
+			}
+		}
+	}
+
 	
 	/**
 	 * Sets all of <code>eObject</code>'s attributes to new values, discarding old ones.
@@ -156,18 +213,14 @@ public class ModelChanger {
 	 * Single-valued attributes are simply replaced.
 	 * 
 	 * @param eObject the EObject to change attributes for
-	 * @param ignoreAndLog should exceptions be ignored and added to the log?
-	 * @see ModelGeneratorUtil#removePerCommand(EObject, org.eclipse.emf.ecore.EStructuralFeature, Collection, Set, boolean)
-	 * @see ModelGeneratorUtil#setEObjectAttributes(EObject, AttributeHandler, Set, boolean)
+	 * @see ModelChangerHelper#clear(EObject, org.eclipse.emf.ecore.EStructuralFeature)
+	 * @see ModelChangerHelper#setEObjectAttributes(EObject)
 	 */
-	private static void changeEObjectAttributes(EObject eObject, boolean ignoreAndLog) {
+	private static void changeEObjectAttributes(EObject eObject) {
 		for(EAttribute attribute : eObject.eClass().getEAllAttributes()) {
-			if(attribute.isMany() && eObject.eIsSet(attribute)) {
-				ModelGeneratorUtil.removePerCommand(eObject, attribute, (Collection<?>) eObject.eGet(attribute),
-					exceptionLog, ignoreAndLog);
-			}
+			ModelChangerHelper.clear(eObject, attribute);
 		}
-		ModelGeneratorUtil.setEObjectAttributes(eObject, exceptionLog, ignoreAndLog);
+		ModelChangerHelper.setEObjectAttributes(eObject);
 	}
 
 	/**
@@ -176,20 +229,15 @@ public class ModelChanger {
 	 * Single-valued references are simply replaced.
 	 * 
 	 * @param eObject the EObject to change references for
-	 * @param ignoreAndLog should exceptions be ignored and added to the log?
-	 * @see ModelGeneratorUtil#removePerCommand(EObject, org.eclipse.emf.ecore.EStructuralFeature, Collection, Set, boolean)
-	 * @see ModelGeneratorUtil#setReference(EObject, EClass, EReference, Random, Set, boolean, Map)
+	 * @see ModelChangerHelper#clear(EObject, org.eclipse.emf.ecore.EStructuralFeature)
+	 * @see ModelChangerHelper#setReference(EObject, EClass, EReference, Map)
 	 */
-	private static void changeEObjectReferences(EObject eObject, boolean ignoreAndLog) {
+	private static void changeEObjectReferences(EObject eObject, Map<EClass, List<EObject>> allObjectsByEClass) {
 		for(EReference reference : ModelGeneratorUtil.getValidReferences(eObject)) {
-			if(reference.isMany() && eObject.eIsSet(reference)) {
-				ModelGeneratorUtil.removePerCommand(eObject, reference, (Collection<?>) eObject.eGet(reference),
-					exceptionLog, ignoreAndLog);
-			}
+			ModelChangerHelper.clear(eObject, reference);
 			for(EClass nextReferenceClass : ModelGeneratorUtil.getReferenceClasses(reference, allObjectsByEClass.keySet())) {
 				if(allObjectsByEClass.containsKey(nextReferenceClass)) {
-					ModelGeneratorUtil.setReference(eObject, nextReferenceClass, reference, random,
-						exceptionLog, ignoreAndLog, allObjectsByEClass);
+					ModelChangerHelper.setReference(eObject, nextReferenceClass, reference, allObjectsByEClass);
 				}
 			}
 		}
@@ -203,6 +251,6 @@ public class ModelChanger {
 	 * @return a set of RuntimeExceptions that occurred during the last changing process
 	 */
 	public static Set<RuntimeException> getLog() {
-		return exceptionLog;
+		return ModelChangerHelper.getExceptionLog();
 	}
 }
---
+++ other/org.unicase.modelchanger/src/org/unicase/modelchanger/ModelChangerHelper.java
@@ -0,0 +1,172 @@
+package org.unicase.modelchanger;
+
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.unicase.modelgenerator.common.ModelGeneratorUtil;
+import org.unicase.modelgenerator.common.attribute.AttributeHandler;
+
+/**
+ * Helper class that takes work from <code>ModelChanger</code>.
+ * With the help of this class, <code>ModelChanger</code> only 
+ * has to be aware of the current root EObject. All methods
+ * should be accessed in a static way and <code>init()</code>
+ * has to be called every time before starting a changing process.
+ * 
+ * @see ModelChanger
+ * @see #init(long, boolean)
+ */
+class ModelChangerHelper {
+	
+	/**
+	 * Random-object to compute random values for deleting EObjects
+	 * and setting their attributes and references.
+	 */	
+	private static Random random;
+	
+	/**
+	 * The amount of work in units as an integer, used by the 
+	 * <code>ModelChanger</code>'s monitor to show the correct
+	 * progress in the progress bar.
+	 * 
+	 * @see #getAmountOfWork()
+	 */
+	private static int amountOfWork;
+	
+	/**
+	 * Specifies whether exceptions should be caught and logged during the changing
+	 * process. Initialized during {@link #init(long, boolean)}.
+	 */
+	private static boolean ignoreAndLog;
+	
+	/**
+	 * A set of RuntimeExceptions that occurred during the last changing process.
+	 */
+	private static Set<RuntimeException> exceptionLog;
+	
+	/**
+	 * Private Constructor.
+	 */
+	private ModelChangerHelper() {
+		// all methods should be accessed in a static way
+	}
+
+	/**
+	 * Initializes the helper to prepare it for the next changing process.
+	 * All private fields get new values, old ones are discarded.
+	 * This also initializes <code>AttributeHandler</code>.
+	 * 
+	 * @param seed the seed value used to generate random values
+	 * @param ignoreExceptions should exceptions be ignored and logged?
+	 * @see AttributeHandler#setRandom(Random)
+	 */
+	protected static void init(long seed, boolean ignoreExceptions) {
+		random = new Random(seed);
+		amountOfWork = 0;
+		ignoreAndLog = ignoreExceptions;
+		exceptionLog = new LinkedHashSet<RuntimeException>();
+		AttributeHandler.setRandom(random);
+	}
+
+	/**
+	 * Retrieves all children, direct and indirect, and returns them
+	 * as a set. The <code>amountOfWork</code> for the progress monitor
+	 * is also computed in this method. Every EObject is counted three times:
+	 * Once for the deletion process, once for changing its attributes
+	 * and once for changing its references.
+	 * 
+	 * @param root the root EObject of the hierarchy
+	 * @return all direct and indirect contents of <code>root</code>
+	 */
+	protected static Set<EObject> getAllChildren(EObject root) {
+		TreeIterator<EObject> allContents = root.eAllContents();
+		Set<EObject> result = new LinkedHashSet<EObject>();
+		while(allContents.hasNext()) {
+			result .add(allContents.next());
+			amountOfWork += 3;
+		}
+		return result;
+	}
+	
+	/**
+	 * Returns the amount of work, that was computed before when
+	 * obtaining all children with {@link #getAllChildren}.
+	 * 
+	 * @return the amount of work in units as an integer
+	 * @see #amountOfWork
+	 * @see #getAllChildren
+	 */
+	protected static int getAmountOfWork() {
+		return amountOfWork;
+	}
+
+	/**
+	 * Sets a reference using {@link ModelGeneratorUtil#setReference}.
+	 * 
+	 * @param eObject the EObject to set the reference for
+	 * @param referenceClass the EClass of EObjects that shall be referenced
+	 * @param reference the EReference that shall be set
+	 * @param allEObjects all possible EObjects that can be referenced
+	 * @see ModelGeneratorUtil#setReference(EObject, EClass, EReference, Random, Set, boolean, Map)
+	 */
+	protected static void setReference(EObject eObject, EClass referenceClass, EReference reference,
+		Map<EClass, List<EObject>> allEObjects) {
+		ModelGeneratorUtil.setReference(eObject, referenceClass, reference, random,
+			exceptionLog, ignoreAndLog, allEObjects);
+	}
+
+	/**
+	 * Clears an EObject's feature, that is removes all objects from a many-valued
+	 * feature. If the target is single-valued, nothing is done.
+	 * 
+	 * @param eObject the EObject to clear the feature for
+	 * @param feature the EStructuralFeature that shall be cleared
+	 */
+	protected static void clear(EObject eObject, EStructuralFeature feature) {
+		if(feature.isMany() && eObject.eIsSet(feature)) {
+			ModelGeneratorUtil.removePerCommand(eObject, feature, (Collection<?>) eObject.eGet(feature),
+				exceptionLog, ignoreAndLog);
+		}
+		
+	}
+
+	/**
+	 * Sets attributes of an EObject using {@link ModelGeneratorUtil#setEObjectAttributes()}.
+	 * 
+	 * @param eObject the EObject to set attributes for
+	 * @see ModelGeneratorUtil#setEObjectAttributes(EObject, Set, boolean)
+	 */
+	protected static void setEObjectAttributes(EObject eObject) {
+		ModelGeneratorUtil.setEObjectAttributes(eObject, exceptionLog, ignoreAndLog);
+	}
+
+	/**
+	 * @return a random value, that specifies whether an EObject shall
+	 * be deleted or not
+	 */
+	protected static boolean randomDelete() {
+		return random.nextDouble() < 0.1;
+	}
+	
+	/**
+	 * Returns the Exception-Log of the <code>ModelChanger</code>.
+	 * The log is cleared after every {@link #init}-call, i.e. before every changing process.
+	 * The log will be empty if no RuntimeException occurred or <code>ignoreAndLog</code>
+	 * was set to <code>false</code> in <code>init</code>.
+	 * 
+	 * @return a set of RuntimeExceptions that occurred during the last changing process
+	 */
+	protected static Set<RuntimeException> getExceptionLog() {
+		return exceptionLog;
+	}
+
+}
