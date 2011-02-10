151a6b92072ef14891771aaacb4d63c39b9ed97b
worldwidetobi@gmx.de <worldwidetobi@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-02-10 19:41:10 +0000

    New Page final

--- developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.editorcustiomization/src/org/unicase/bowlingexample/editorcustiomization/NewPage.java
+++ developer/CodeExamples/BowlingExample/org.unicase.bowlingexample.editorcustiomization/src/org/unicase/bowlingexample/editorcustiomization/NewPage.java
@@ -6,6 +6,9 @@ import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.IManagedForm;
 import org.eclipse.ui.forms.editor.FormPage;
@@ -16,16 +19,18 @@ import org.unicase.ui.meeditor.MEFormPage;
 
 import bowling.Tournament;
 
+//import org.unicase.ui.meeditor.multiattribute.StringAttributeControl;
+
 public class NewPage extends AbstractMEEditorPage {
 
 	private static final String ID = "myID";
 	private static final String NAME = "Custom Page";
-	public Tournament tournament;
+	private Tournament tournament;
 
 	private ScrolledForm form;
 	private Composite composite;
+	Text text;
 	GridLayout gridLayout = new GridLayout(1, true);
-	
 	
 	public NewPage() {
 		super();
@@ -34,6 +39,7 @@ public class NewPage extends AbstractMEEditorPage {
 	@Override
 	public FormPage createPage(MEEditor editor, EditingDomain arg1, EObject arg2) {
 		
+		//create special page for tournament otherwise create empty page
 		if (arg2 instanceof Tournament){
 			tournament = (Tournament) arg2;
 		MEFormPage page = new MEFormPage(editor, ID, NAME) {
@@ -56,6 +62,7 @@ public class NewPage extends AbstractMEEditorPage {
 		return page;
 		}
 		else {
+			//empty page
 			MEFormPage page = new MEFormPage(editor, ID, NAME) {};
 			page.setParentMEPage(this);
 			return page;
@@ -69,27 +76,59 @@ public class NewPage extends AbstractMEEditorPage {
 		
 		composite = new Composite(form.getBody(), SWT.NONE);
 		composite.setLayout(gridLayout);
-		
-		Text text = new Text(composite, SWT.MULTI);
+		text = new Text(composite, SWT.MULTI);
 		text.setEditable(false);
-//		text.setText("text\ntest");
-		if(tournament.getTitle() != null){
-			text.setText(tournament.getTitle());
-		}
-//		else if(tournament.getTitle() != null && tournament.getMatchup() != null){
-//			text.setText(tournament.getTitle()+"\n"+ tournament.getMatchup().toString());
-//		}
-//		else if(tournament.getTitle() != null && tournament.getMatchup() != null && tournament.getPlayerlist() != null){
-//			text.setText(tournament.getTitle()+"\n"+ tournament.getMatchup().toString()+"\n"+ tournament.getPlayerlist().toString());
-//		}
-		else{
-			text.setText("No information for this Tournament set!\n Please edit!");
-		}
 		
-//		text.setText(tournament.getTitle()+"\n"+
-//					 tournament.getMatchup().toString()+"\n"+
-//					 tournament.getPlayerlist().toString());
+		rebuildText();
 		
 	}
 
+	//Summary of the tournament printed on the second page. Tournament title, type, all matchups, and title of playerlist 
+	private void rebuildText() {
+		try{
+		String description = "";
+		
+		if (tournament.getTitle() == null){
+			text.setText("No information for this Tournament set!\nPlease edit!");
+		}
+		else {
+		if(tournament.getTitle() != null){
+			description = "Title: " + tournament.getTitle() + "\nType: " + tournament.getType() + "\n";
+			text.setText(description);
+		}
+		if(tournament.getMatchup() != null){
+			for (int i = 0; i <= tournament.getMatchup().size()-1; i++ ){
+			description += "\nMatchup " + (i+1) + ": " + tournament.getMatchup().get(i).getGames().get(0).getPlayer().getLastname()+ 
+			" vs. " + tournament.getMatchup().get(i).getGames().get(1).getPlayer().getLastname();
+			}
+			text.setText(description);
+			
+		}
+		if(tournament.getPlayerlist() != null){
+			description += "\n\nPlayerlist: "+ tournament.getPlayerlist().getName();
+			text.setText(description);
+		}
+		
+		}
+		}catch(Exception e){
+			//catching exception that is thrown if a matchup only contains one game and opening
+			//a window that asks the user to complete all matchups before using the page 
+			Thread messageThread = new Thread("Message"){
+				public void run(){
+			
+			final Display display = new Display();
+			Shell shell = new Shell(display);
+			MessageBox ms = new MessageBox(shell, SWT.ICON_ERROR);
+			ms.setText("Sorry!");
+			ms.setMessage("Please set up all the Matchups with two Games!");
+			ms.open();
+			display.dispose();
+			
+				}
+			
+			};
+			messageThread.start();
+			
+		}
+	}
 }
