1125ea85c03a55342786f904d74ad57333adfd09
hongkhoanquach <hongkhoanquach@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-02-28 02:13:35 +0000

    removed comment

--- other/core_branch_memory_leak/org.unicase.ui.navigator/src/org/unicase/ui/navigator/commands/DynamicContainmentCommands.java
+++ other/core_branch_memory_leak/org.unicase.ui.navigator/src/org/unicase/ui/navigator/commands/DynamicContainmentCommands.java
@@ -42,7 +42,7 @@ public class DynamicContainmentCommands extends CompoundContributionItem {
 
 	private AdapterFactoryLabelProvider labelProvider = new AdapterFactoryLabelProvider(new ComposedAdapterFactory(
 		ComposedAdapterFactory.Descriptor.Registry.INSTANCE));
-	// hkq: done
+	
 
 	private static final String COMMAND_ID = "org.unicase.ui.navigator.createContaiment";
 	private EObject selectedME;
