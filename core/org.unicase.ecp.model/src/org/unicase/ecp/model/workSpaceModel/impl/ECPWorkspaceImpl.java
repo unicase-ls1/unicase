07d15aaa462ed3d2a4010a06400b80d3f8c90321
jonas.helming@gmail.com <jonas.helming@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-03-08 10:01:13 +0000

    removed unused import

--- core/org.unicase.ecp.model/src/org/unicase/ecp/model/workSpaceModel/impl/ECPWorkspaceImpl.java
+++ core/org.unicase.ecp.model/src/org/unicase/ecp/model/workSpaceModel/impl/ECPWorkspaceImpl.java
@@ -15,7 +15,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
 import org.eclipse.emf.ecore.impl.EObjectImpl;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EObjectContainmentWithInverseEList;
 import org.eclipse.emf.ecore.util.InternalEList;
 import org.eclipse.emf.edit.domain.EditingDomain;
