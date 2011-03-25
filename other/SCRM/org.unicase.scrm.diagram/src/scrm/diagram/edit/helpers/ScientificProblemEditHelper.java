ad90947309291a9f2408975a5ca596b99b2ad08e
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-03-25 17:01:36 +0000

    Replaced these plug-ins with newly shared plug-ins due to SVN Errors.

--- other/SCRM/org.unicase.scrm.diagram/src/scrm/diagram/edit/helpers/ScientificProblemEditHelper.java
+++
@@ -1,7 +0,0 @@
-package scrm.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ScientificProblemEditHelper extends ScrmBaseEditHelper {
-}
--- other/SCRM/org.unicase.scrm.diagram/src/scrm/diagram/providers/ElementInitializers.java
+++
@@ -1,8 +0,0 @@
-package scrm.diagram.providers;
-
-/**
- * @generated
- */
-public class ElementInitializers {
-
-}
