b7ec17aaa96ec234701d40f54466923d4866760c
edgar.m.mueller@gmail.com <edgar.m.mueller@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-03-26 20:26:00 +0000

    renamed packages

---
+++ emfstore/org.unicase.emfstore.esmodel.edit/src/org/eclipse/emf/emfstore/server/model/provider/AbstractOperationCustomLabelProvider.java
@@ -0,0 +1,46 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.eclipse.emf.emfstore.server.model.provider;
+
+import org.unicase.emfstore.esmodel.versioning.operations.AbstractOperation;
+
+/**
+ * Interface for a custom LabelProvider for {@link AbstractOperation}s.
+ * 
+ * @author Michael Kagel
+ */
+public interface AbstractOperationCustomLabelProvider {
+
+	/**
+	 * Constant for render priority meaning this provider does not want to render the given element.
+	 */
+	int CANNOT_RENDER = 0;
+
+	/**
+	 * Returns the description of an operation.
+	 * 
+	 * @param operation for description
+	 * @return The description of the operation
+	 */
+	String getDescription(AbstractOperation operation);
+
+	/**
+	 * Returns the image of an operation.
+	 * 
+	 * @param operation for image
+	 * @return The image of the operation
+	 */
+	Object getImage(AbstractOperation operation);
+
+	/**
+	 * Checks if this provider can render the given operation.
+	 * 
+	 * @param operation which should be checked.
+	 * @return a priority for rendering this element. The higher the priority the more likely this provider will render
+	 *         the element. Returning 0 means this provider does not want to render this element.
+	 */
+	int canRender(AbstractOperation operation);
+}
---
+++ emfstore/org.unicase.emfstore.esmodel.edit/src/org/eclipse/emf/emfstore/server/model/versioning/events/provider/ExceptionEventItemProvider.java
@@ -0,0 +1,180 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.eclipse.emf.emfstore.server.model.versioning.events.provider;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
+import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
+import org.eclipse.emf.edit.provider.IItemLabelProvider;
+import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
+import org.eclipse.emf.edit.provider.IStructuredItemContentProvider;
+import org.eclipse.emf.edit.provider.ITreeItemContentProvider;
+import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
+import org.eclipse.emf.edit.provider.ViewerNotification;
+import org.unicase.emfstore.esmodel.versioning.events.EventsPackage;
+import org.unicase.emfstore.esmodel.versioning.events.ExceptionEvent;
+
+/**
+ * This is the item provider adapter for a {@link org.unicase.emfstore.esmodel.versioning.events.ExceptionEvent} object.
+ * <!-- begin-user-doc --> <!-- end-user-doc -->
+ * 
+ * @generated
+ */
+public class ExceptionEventItemProvider extends EventItemProvider implements IEditingDomainItemProvider,
+	IStructuredItemContentProvider, ITreeItemContentProvider, IItemLabelProvider, IItemPropertySource {
+	/**
+	 * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	public ExceptionEventItemProvider(AdapterFactory adapterFactory) {
+		super(adapterFactory);
+	}
+
+	/**
+	 * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+		if (itemPropertyDescriptors == null) {
+			super.getPropertyDescriptors(object);
+
+			addExceptionTitlePropertyDescriptor(object);
+			addExceptionStackTracePropertyDescriptor(object);
+			addExceptionCauseTitlePropertyDescriptor(object);
+			addExceptionCauseStackTracePropertyDescriptor(object);
+		}
+		return itemPropertyDescriptors;
+	}
+
+	/**
+	 * This adds a property descriptor for the Exception Title feature. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected void addExceptionTitlePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(
+			((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+			getResourceLocator(),
+			getString("_UI_ExceptionEvent_ExceptionTitle_feature"),
+			getString("_UI_PropertyDescriptor_description", "_UI_ExceptionEvent_ExceptionTitle_feature",
+				"_UI_ExceptionEvent_type"), EventsPackage.Literals.EXCEPTION_EVENT__EXCEPTION_TITLE, true, false,
+			false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Exception Stack Trace feature. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	protected void addExceptionStackTracePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(
+			((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+			getResourceLocator(),
+			getString("_UI_ExceptionEvent_ExceptionStackTrace_feature"),
+			getString("_UI_PropertyDescriptor_description", "_UI_ExceptionEvent_ExceptionStackTrace_feature",
+				"_UI_ExceptionEvent_type"), EventsPackage.Literals.EXCEPTION_EVENT__EXCEPTION_STACK_TRACE, true, false,
+			false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Exception Cause Title feature. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	protected void addExceptionCauseTitlePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(
+			((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+			getResourceLocator(),
+			getString("_UI_ExceptionEvent_ExceptionCauseTitle_feature"),
+			getString("_UI_PropertyDescriptor_description", "_UI_ExceptionEvent_ExceptionCauseTitle_feature",
+				"_UI_ExceptionEvent_type"), EventsPackage.Literals.EXCEPTION_EVENT__EXCEPTION_CAUSE_TITLE, true, false,
+			false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+	}
+
+	/**
+	 * This adds a property descriptor for the Exception Cause Stack Trace feature. <!-- begin-user-doc --> <!--
+	 * end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	protected void addExceptionCauseStackTracePropertyDescriptor(Object object) {
+		itemPropertyDescriptors.add(createItemPropertyDescriptor(
+			((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+			getResourceLocator(),
+			getString("_UI_ExceptionEvent_ExceptionCauseStackTrace_feature"),
+			getString("_UI_PropertyDescriptor_description", "_UI_ExceptionEvent_ExceptionCauseStackTrace_feature",
+				"_UI_ExceptionEvent_type"), EventsPackage.Literals.EXCEPTION_EVENT__EXCEPTION_CAUSE_STACK_TRACE, true,
+			false, false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+	}
+
+	/**
+	 * This returns ExceptionEvent.gif. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public Object getImage(Object object) {
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/ExceptionEvent"));
+	}
+
+	/**
+	 * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public String getText(Object object) {
+		Date labelValue = ((ExceptionEvent) object).getTimestamp();
+		String label = labelValue == null ? null : labelValue.toString();
+		return label == null || label.length() == 0 ? getString("_UI_ExceptionEvent_type")
+			: getString("_UI_ExceptionEvent_type") + " " + label;
+	}
+
+	/**
+	 * This handles model notifications by calling {@link #updateChildren} to update any cached children and by creating
+	 * a viewer notification, which it passes to {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!-- end-user-doc
+	 * -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	public void notifyChanged(Notification notification) {
+		updateChildren(notification);
+
+		switch (notification.getFeatureID(ExceptionEvent.class)) {
+		case EventsPackage.EXCEPTION_EVENT__EXCEPTION_TITLE:
+		case EventsPackage.EXCEPTION_EVENT__EXCEPTION_STACK_TRACE:
+		case EventsPackage.EXCEPTION_EVENT__EXCEPTION_CAUSE_TITLE:
+		case EventsPackage.EXCEPTION_EVENT__EXCEPTION_CAUSE_STACK_TRACE:
+			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+			return;
+		}
+		super.notifyChanged(notification);
+	}
+
+	/**
+	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that can be created
+	 * under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
+	 * @generated
+	 */
+	@Override
+	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+		super.collectNewChildDescriptors(newChildDescriptors, object);
+	}
+
+}
