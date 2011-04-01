0dbb05866300804b109bd3d1cc2f5bc7f78031be
edgar.m.mueller@gmail.com <edgar.m.mueller@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-04-01 11:09:43 +0000

    renamed packages

---
+++ ecp/org.eclipse.emf.ecp.common.model/src/org/eclipse/emf/ecp/model/AbstractECPModelElementContext.java
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2011 Chair for Applied Software Engineering, Technische Universitaet Muenchen.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ ******************************************************************************/
+package org.eclipse.emf.ecp.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * The context of a certain {@link EObject}. 
+ * Subclasses shall call modelelementDeleted and contextDeleted.
+ * 
+ * @author helming
+ */
+public abstract class AbstractECPModelElementContext implements ECPModelelementContext {
+
+	private Set<ModelElementContextListener> modelElementContextListeners = new HashSet<ModelElementContextListener>();
+
+	/**
+	 * Adds a {@link ModelElementContextListener}.
+	 * 
+	 * @param modelElementContextListener the {@link ModelElementContextListener}
+	 */
+	public void addModelElementContextListener(ModelElementContextListener modelElementContextListener) {
+		modelElementContextListeners.add(modelElementContextListener);
+	}
+
+	/**
+	 * Removes a {@link ModelElementContextListener}.
+	 * 
+	 * @param modelElementContextListener the {@link ModelElementContextListener}
+	 */
+	public void removeModelElementContextListener(ModelElementContextListener modelElementContextListener) {
+		modelElementContextListeners.remove(modelElementContextListener);
+	}
+
+	/**
+	 * Call if a model element is deleted.
+	 */
+	protected void modelElementDeleted(EObject deleted) {
+		for (ModelElementContextListener modelElementContextListener : modelElementContextListeners) {
+			modelElementContextListener.onModelElementDeleted(deleted);
+		}
+	}
+}
