9a17639566a0f9b1ede52b29811e7752fdac8425
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-04-06 07:54:18 +0000

    updated header and checkstyle file

--- ecp/org.eclipse.emf.ecp.common.util/src/org/eclipse/emf/ecp/common/util/Activator.java
+++ ecp/org.eclipse.emf.ecp.common.util/src/org/eclipse/emf/ecp/common/util/Activator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008-2011 Chair for Applied Software Engineering, 
+ * Copyright (c) 2008-2011 Chair for Applied Software Engineering,
  * Technische Universitaet Muenchen.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
