e2630625513bfc172d80f9bc7c972e875e9f0b30
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-05-17 09:18:44 +0000

    Created tag for EMFStore IP Review process with fixes.

--- other/ModelGenerator/org.unicase.modelgenerator.ui/src/org/unicase/modelgenerator/ui/IGUIListener.java
+++
@@ -1,8 +0,0 @@
-package org.unicase.modelgenerator.ui;
-
-import org.eclipse.core.runtime.IProgressMonitor;
-
-public interface IGUIListener {
-	public void runInCommand(ModelGeneratorGUI gui, IProgressMonitor monitor);
-}
-
--- other/memory_leak_branch_patches/org.unicase.model.migrator/migrator/release7/step5.groovy
+++
@@ -1,8 +0,0 @@
-modelElementClass = metamodel.ModelElement
-identifiableElementClass = metamodel.IdentifiableElement
-modelElementClass.eSuperTypes.remove(identifiableElementClass)
-
-for(element in model.UnicaseModelElement.allInstances) {
-	def id = element.unset(identifiableElementClass.identifier)
-	element.setUuid(id)
-}
