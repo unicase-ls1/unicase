4e2e71efe37cb0c45fbc2982b5cca6c6a3772d9b
birgit.engelmann@googlemail.com <birgit.engelmann@googlemail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-05-21 13:05:47 +0000

    created initial version of adding action items and bug reports under a saved modelelement over properties

--- other/LABCourseSS11/core/org.unicase.ui.toolbarbutton/src/org/unicase/ui/toolbarbutton/CreateActionItemHandler.java
+++ other/LABCourseSS11/core/org.unicase.ui.toolbarbutton/src/org/unicase/ui/toolbarbutton/CreateActionItemHandler.java
@@ -1,14 +1,22 @@
 
 package org.unicase.ui.toolbarbutton;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.emf.ecore.EObject;
 import org.unicase.ui.util.ActionHelper;
 import org.unicase.workspace.ProjectSpace;
+import org.unicase.workspace.preferences.PreferenceManager;
 import org.unicase.workspace.util.UnicaseCommand;
+import org.unicase.emfstore.esmodel.accesscontrol.OrgUnitProperty;
+import org.unicase.metamodel.ModelElementId;
 import org.unicase.metamodel.Project;
+import org.unicase.model.document.LeafSection;
 import org.unicase.model.task.*;
 
 
@@ -34,10 +42,24 @@ public class CreateActionItemHandler extends AbstractHandler {
 		new UnicaseCommand() {
 			@Override
 			protected void doRun() {
-				ActionItem item = TaskFactory.eINSTANCE.createActionItem();
-				Project project = projectSpace.getProject();
-				project.addModelElement(item);
 				
+				//for testing: (setting a workpackage as property)
+				Project p = projectSpace.getProject();
+				Set<EObject> modelElements = p.getAllModelElements();
+				ModelElementId element = null;
+				for(EObject modelElement: modelElements){
+					if(modelElement instanceof WorkPackage){
+						element = ((WorkPackage)modelElement).getModelElementId();
+						break;
+					}
+				}
+
+				if(element != null){
+					PreferenceManager.INSTANCE.setProperty(projectSpace, ShortcutActionKey.USERLOCATION, new EObject[]{element});
+				//end of testing part
+					ActionItem item = TaskFactory.eINSTANCE.createActionItem();
+					TaskCreator.addTask(projectSpace, ShortcutActionKey.USERLOCATION, item);
+				}
 			}
 		}.run(true);
 
---
+++ other/LABCourseSS11/core/org.unicase.ui.toolbarbutton/src/org/unicase/ui/toolbarbutton/ShortcutActionKey.java
@@ -0,0 +1,11 @@
+package org.unicase.ui.toolbarbutton;
+
+import org.unicase.workspace.preferences.PropertyKey;
+
+public enum ShortcutActionKey implements PropertyKey {
+	
+	GROUPLOCATION,
+	
+	USERLOCATION,
+
+}
