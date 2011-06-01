29140ccff558fd1560d09d0da8c1726e407ece9e
edgar.m.mueller <edgar.m.mueller@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-06-01 14:32:33 +0000

    created branch

---
+++ org.eclipse.emf.emfstore.server.backchannel/src/org/eclipse/emf/emfstore/server/backchannel/BachchannelInitiator.java
@@ -0,0 +1,51 @@
+package org.eclipse.emf.emfstore.server.backchannel;
+
+import java.util.Set;
+
+import org.eclipse.emf.emfstore.common.model.util.ModelUtil;
+import org.eclipse.emf.emfstore.server.EmfStoreInterface;
+import org.eclipse.emf.emfstore.server.accesscontrol.AccessControlImpl;
+import org.eclipse.emf.emfstore.server.backchannel.connection.server.RMIBackchannelConnectionHandler;
+import org.eclipse.emf.emfstore.server.connection.ConnectionHandler;
+import org.eclipse.emf.emfstore.server.exceptions.EmfStoreException;
+import org.eclipse.emf.emfstore.server.exceptions.FatalEmfStoreException;
+import org.eclipse.emf.emfstore.server.model.ServerSpace;
+import org.eclipse.emf.emfstore.server.startup.PostStartupListener;
+
+/**
+ * Poststart plugin for emstore which initiates the backchannel component. This
+ * allows clients to register to the server and get push events.
+ * 
+ * @author wesendon
+ */
+public class BachchannelInitiator implements PostStartupListener {
+
+	/**
+	 * Default constructor.
+	 */
+	public BachchannelInitiator() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void postStartUp(ServerSpace serverspace, AccessControlImpl accessControl,
+		Set<ConnectionHandler<? extends EmfStoreInterface>> connectionHandlers) {
+
+		try {
+			BackchannelInterface backchannelImpl = new BackchannelImpl(serverspace, accessControl);
+			RMIBackchannelConnectionHandler connectionHandler = new RMIBackchannelConnectionHandler();
+			connectionHandler.init(backchannelImpl, accessControl);
+
+			connectionHandlers.add(connectionHandler);
+
+			ModelUtil.logInfo("Backchannel initiated successfully.");
+
+		} catch (FatalEmfStoreException e) {
+			ModelUtil.logException("Couldn't initiate bachchannel.", e);
+		} catch (EmfStoreException e) {
+			ModelUtil.logException("Couldn't initiate bachchannel.", e);
+		}
+	}
+
+}
---
+++ org.eclipse.emf.emfstore.server.backchannel/src/org/eclipse/emf/emfstore/server/backchannel/BackchannelConfiguration.java
@@ -0,0 +1,59 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.eclipse.emf.emfstore.server.backchannel;
+
+import java.util.Properties;
+
+import org.eclipse.emf.emfstore.server.ServerConfiguration;
+
+/**
+ * Configuration file for the backchannel.
+ * 
+ * @author wesendon
+ */
+public final class BackchannelConfiguration {
+
+	private BackchannelConfiguration() {
+	}
+
+	/**
+	 * RMI port property.
+	 */
+	public static final String BACKCHANNEL_RMI_PORT = "emfstore.backchannel.rmi.port";
+
+	/**
+	 * Default value for rmi port property.
+	 */
+	public static final String BACKCHANNEL_RMI_PORT_DEFAULT = "3000";
+
+	/**
+	 * Returns the properties, using {@link ServerConfiguration}.
+	 * 
+	 * @return java properties
+	 */
+	public static Properties getProperties() {
+		return ServerConfiguration.getProperties();
+	}
+
+	/**
+	 * Gets a integer property.
+	 * 
+	 * @param propertyName propertyname
+	 * @param defaultProperty default property
+	 * @return property value
+	 */
+	public static int getNumberProperty(String propertyName, String defaultProperty) {
+		String property = getProperties().getProperty(propertyName);
+		try {
+			if (property != null) {
+				return Integer.parseInt(property);
+			}
+		} catch (NumberFormatException e) {
+		}
+		return Integer.parseInt(defaultProperty);
+	}
+
+}
