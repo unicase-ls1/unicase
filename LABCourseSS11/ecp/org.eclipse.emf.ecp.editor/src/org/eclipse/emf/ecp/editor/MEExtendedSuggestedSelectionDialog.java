b5283d7695660ddb191b517799ab4eddb8b986ec
birgit.engelmann@googlemail.com <birgit.engelmann@googlemail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-06-06 11:24:53 +0000

    added javadocs

--- LABCourseSS11/ecp/org.eclipse.emf.ecp.editor/src/org/eclipse/emf/ecp/editor/MEExtendedSuggestedSelectionDialog.java
+++ LABCourseSS11/ecp/org.eclipse.emf.ecp.editor/src/org/eclipse/emf/ecp/editor/MEExtendedSuggestedSelectionDialog.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2011 Chair for Applied Software Engineering,
+ * Technische Universitaet Muenchen.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ ******************************************************************************/
 package org.eclipse.emf.ecp.editor;
 
 import java.util.Collection;
@@ -5,19 +15,32 @@ import java.util.Collection;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.List;
-import org.eclipse.swt.widgets.Text;
 
+/**
+ * This dialog represents the possibility to select an element from a list where the list is sorted and additional
+ * information can be provided. In addition first a filter can be selected for the items displayed in the list.
+ * 
+ * @author Birgit Engelmann
+ */
 public class MEExtendedSuggestedSelectionDialog extends
 		MESuggestedSelectionDialog {
 	
-	Combo contexts;
+	private Combo contexts;
 	
+	/**
+	 * The constructor.
+	 * 
+	 * @param title The title of the dialog
+	 * @param message the message displayed
+	 * @param blockOnOpen block
+	 * @param elements The elements, which can be selected.
+	 * @param baseElement The element, to which the selection is made and to which other elements are compared.
+	 * @param reference the reference for which this is used
+	 */
 	public MEExtendedSuggestedSelectionDialog(String title, String message,
 			boolean blockOnOpen, EObject baseElement, EReference reference,
 			Collection<EObject> elements) {
