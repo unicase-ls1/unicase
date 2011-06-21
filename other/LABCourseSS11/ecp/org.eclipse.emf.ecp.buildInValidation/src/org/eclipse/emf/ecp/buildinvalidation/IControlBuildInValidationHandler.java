6a661473a6c0efb268b971687dcb2f6590d5cab9
franziska.haunolder@gmail.com <franziska.haunolder@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-06-21 08:32:03 +0000

    New BuildInValidation Plugin

---
+++ other/LABCourseSS11/ecp/org.eclipse.emf.ecp.buildInValidation/src/org/eclipse/emf/ecp/buildinvalidation/IControlBuildInValidationHandler.java
@@ -0,0 +1,9 @@
+package org.eclipse.emf.ecp.buildinvalidation;
+
+import org.eclipse.emf.common.util.Diagnostic;
+
+public interface IControlBuildInValidationHandler {
+
+		public void handleValidation(Diagnostic diagnostic);
+
+}
--- other/LABCourseSS11/ecp/org.eclipse.emf.ecp.buildInValidation/src/org/eclipse/emf/ecp/buildinvalidation/controlHandler/IControlBuildInValidationHandler.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.emf.ecp.buildinvalidation.controlHandler;
-
-import org.eclipse.emf.common.util.Diagnostic;
-
-public interface IControlBuildInValidationHandler {
-
-	public void handleValidation(Diagnostic diagnostic);
-}
