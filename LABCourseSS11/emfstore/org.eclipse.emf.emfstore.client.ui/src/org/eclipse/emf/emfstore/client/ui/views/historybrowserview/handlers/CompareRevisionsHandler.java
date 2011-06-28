81cf0aa2da5454880eee40744c6f5e297207d00b
birgit.engelmann@googlemail.com <birgit.engelmann@googlemail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-06-28 22:51:14 +0000

    alternative request of revision

--- LABCourseSS11/emfstore/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/historybrowserview/handlers/CompareRevisionsHandler.java
+++ LABCourseSS11/emfstore/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/historybrowserview/handlers/CompareRevisionsHandler.java
@@ -13,11 +13,15 @@ package org.eclipse.emf.emfstore.client.ui.views.historybrowserview.handlers;
 import java.util.List;
 
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.emf.ecp.common.util.DialogHandler;
 import org.eclipse.emf.emfstore.client.model.ProjectSpace;
+import org.eclipse.emf.emfstore.client.model.WorkspaceManager;
+import org.eclipse.emf.emfstore.client.ui.commands.ServerRequestCommandHandler;
 import org.eclipse.emf.emfstore.client.ui.views.historybrowserview.HistoryBrowserView;
 import org.eclipse.emf.emfstore.client.ui.views.historybrowserview.HistoryCompare;
-import org.eclipse.emf.emfstore.common.model.Project;
 import org.eclipse.emf.emfstore.common.model.util.ModelUtil;
+import org.eclipse.emf.emfstore.server.exceptions.EmfStoreException;
 import org.eclipse.emf.emfstore.server.model.versioning.HistoryInfo;
 import org.eclipse.emf.emfstore.server.model.versioning.PrimaryVersionSpec;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -45,85 +49,84 @@ public class CompareRevisionsHandler extends AbstractHistoryViewHandler {
 		IStructuredSelection object = (IStructuredSelection) activeWorkbenchWindow
 				.getSelectionService().getSelection();
 		List list = object.toList();
-		Project project1 = null;
-		Project project2 = null;
-		ProjectSpace projectSpace1 = null;
-		ProjectSpace projectSpace2 = null;
+		// Project project1 = null;
+		// Project project2 = null;
+		// ProjectSpace projectSpace1 = null;
+		// ProjectSpace projectSpace2 = null;
 
 		if (list.size() == 2) {
-			for (int i = 0; i < list.size(); i++) {
-				TreeNode element = (TreeNode) list.get(i);
-				HistoryInfo historyInfo = (HistoryInfo) element.getValue();
-				final PrimaryVersionSpec versionSpec = ModelUtil
-						.clone(historyInfo.getPrimerySpec());
-				final ProjectSpace projectSpace = view.getProjectSpace();
-				ProjectSpace projectSpaceVersion = ModelUtil
-						.clone(projectSpace);
-
-				if (i == 0) {
-					projectSpaceVersion.setBaseVersion(versionSpec);
-					projectSpace1 = projectSpaceVersion;
-					project1 = projectSpace1.getProject();
-				} else {
-					projectSpaceVersion.setBaseVersion(versionSpec);
-					projectSpace2 = projectSpaceVersion;
-					project2 = projectSpace2.getProject();
-				}
-			}
-			if (HistoryCompare.hasRegisteredExtensions()) {
-				// TODO replace null by the two EObjects representing the two
-				// Project Revisions
-				HistoryCompare.handleRegisteredExtensions(project1, project2);
-			}
-
-			// TreeNode element1 = (TreeNode) list.get(0);
-			// HistoryInfo historyInfo1 = (HistoryInfo) element1.getValue();
-			// final PrimaryVersionSpec versionSpec1 = ModelUtil
-			// .clone(historyInfo1.getPrimerySpec());
-			// final ProjectSpace projectSpace1 = view.getProjectSpace();
+			// for (int i = 0; i < list.size(); i++) {
+			// TreeNode element = (TreeNode) list.get(i);
+			// HistoryInfo historyInfo = (HistoryInfo) element.getValue();
+			// final PrimaryVersionSpec versionSpec = ModelUtil
+			// .clone(historyInfo.getPrimerySpec());
+			// final ProjectSpace projectSpace = view.getProjectSpace();
+			// ProjectSpace projectSpaceVersion = ModelUtil
+			// .clone(projectSpace);
 			//
-			// TreeNode element2 = (TreeNode) list.get(1);
-			// HistoryInfo historyInfo2 = (HistoryInfo) element2.getValue();
-			// final PrimaryVersionSpec versionSpec2 = ModelUtil
-			// .clone(historyInfo2.getPrimerySpec());
-			// final ProjectSpace projectSpace2 = view.getProjectSpace();
-			//
-			// ServerRequestCommandHandler handler = new
-			// ServerRequestCommandHandler() {
-			//
-			// @Override
-			// protected Object run() throws EmfStoreException {
-			// ProjectSpace pSpace1 = WorkspaceManager
-			// .getInstance()
-			// .getCurrentWorkspace()
-			// .getRevision(projectSpace1.getUsersession(),
-			// projectSpace1.getProjectInfo(),
-			// versionSpec1);
-			//
-			// ProjectSpace pSpace2 = WorkspaceManager
-			// .getInstance()
-			// .getCurrentWorkspace()
-			// .getRevision(projectSpace2.getUsersession(),
-			// projectSpace2.getProjectInfo(),
-			// versionSpec2);
+			// if (i == 0) {
+			// projectSpaceVersion.setBaseVersion(versionSpec);
+			// projectSpace1 = projectSpaceVersion;
+			// project1 = projectSpace1.getProject();
+			// } else {
+			// projectSpaceVersion.setBaseVersion(versionSpec);
+			// projectSpace2 = projectSpaceVersion;
+			// project2 = projectSpace2.getProject();
+			// }
+			// }
 			// if (HistoryCompare.hasRegisteredExtensions()) {
-			// HistoryCompare.handleRegisteredExtensions(
-			// pSpace1.getProject(), pSpace2.getProject());
+			// // TODO replace null by the two EObjects representing the two
+			// // Project Revisions
+			// HistoryCompare.handleRegisteredExtensions(project1, project2);
 			// }
-			// return null;
-			// }
-			//
-			// @Override
-			// public String getTaskTitle() {
-			// return "Resolving project versions...";
-			// }
-			// };
-			//
-			// try {
-			// handler.execute(new ExecutionEvent());
-			// } catch (ExecutionException e) {
-			// DialogHandler.showErrorDialog(e.getMessage());
-			// }
+
+			TreeNode element1 = (TreeNode) list.get(0);
+			HistoryInfo historyInfo1 = (HistoryInfo) element1.getValue();
+			final PrimaryVersionSpec versionSpec1 = ModelUtil
+					.clone(historyInfo1.getPrimerySpec());
+			final ProjectSpace projectSpace1 = view.getProjectSpace();
+
+			TreeNode element2 = (TreeNode) list.get(1);
+			HistoryInfo historyInfo2 = (HistoryInfo) element2.getValue();
+			final PrimaryVersionSpec versionSpec2 = ModelUtil
+					.clone(historyInfo2.getPrimerySpec());
+			final ProjectSpace projectSpace2 = view.getProjectSpace();
+
+			ServerRequestCommandHandler handler = new ServerRequestCommandHandler() {
+
+				@Override
+				protected Object run() throws EmfStoreException {
+					ProjectSpace pSpace1 = WorkspaceManager
+							.getInstance()
+							.getCurrentWorkspace()
+							.getRevision(projectSpace1.getUsersession(),
+									projectSpace1.getProjectInfo(),
+									versionSpec1);
+
+					ProjectSpace pSpace2 = WorkspaceManager
+							.getInstance()
+							.getCurrentWorkspace()
+							.getRevision(projectSpace2.getUsersession(),
+									projectSpace2.getProjectInfo(),
+									versionSpec2);
+					if (HistoryCompare.hasRegisteredExtensions()) {
+						HistoryCompare.handleRegisteredExtensions(
+								pSpace1.getProject(), pSpace2.getProject());
+					}
+					return null;
+				}
+
+				@Override
+				public String getTaskTitle() {
+					return "Resolving project versions...";
+				}
+			};
+
+			try {
+				handler.execute(new ExecutionEvent());
+			} catch (ExecutionException e) {
+				DialogHandler.showErrorDialog(e.getMessage());
+			}
 		}
 
 		return null;
