e716c1e0f025674d0b69eefe6011eb4d2e523857
taher.galal90 <taher.galal90@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-07-05 10:11:01 +0000

    add comments to methods

--- other/traceRecovery/org.unicase.traceRecovery/src/traceRecovery/ui/TreeItemHelper.java
+++ other/traceRecovery/org.unicase.traceRecovery/src/traceRecovery/ui/TreeItemHelper.java
@@ -10,7 +10,7 @@ import org.eclipse.swt.widgets.TreeItem;
 
 /**
  * @author taher
- *
+ *helps in setting the parent of tree items for teh directories tree.
  */
 public class TreeItemHelper {
 	TreeItem parent;
@@ -43,7 +43,13 @@ public class TreeItemHelper {
 	}
 
 	File dir;
-	
+	/**
+	 * constructor that will take the parent and the directory and link them together
+	 * @param parent
+	 * 			this is the parent of this directory so this is the directory 1 step up
+	 * @param dir
+	 * 			this is the current directory we are trying to add
+	 */
 	public TreeItemHelper(TreeItem parent, File dir){
 		this.parent = parent;
 		this.dir = dir;
