624a3e7afc589335869ccd36db78dc77e4407bb5
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-07-15 15:23:55 +0000

    Improved UMLModel name display.

--- other/Papyrus/org.unicase.papyrus.edit/src/org/unicase/papyrus/provider/UMLModelItemProvider.java
+++ other/Papyrus/org.unicase.papyrus.edit/src/org/unicase/papyrus/provider/UMLModelItemProvider.java
@@ -114,14 +114,12 @@ public class UMLModelItemProvider
 	 * This returns the label text for the adapted class.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
+	 * @generated NOT
 	 */
 	@Override
 	public String getText(Object object) {
 		String label = ((UMLModel)object).getName();
-		return label == null || label.length() == 0 ?
-			getString("_UI_UMLModel_type") :
-			getString("_UI_UMLModel_type") + " " + label;
+		return label;
 	}
 
 	/**
