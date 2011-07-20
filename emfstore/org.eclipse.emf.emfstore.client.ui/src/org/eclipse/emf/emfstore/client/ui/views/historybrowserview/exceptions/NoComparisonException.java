8de280e864ad0e1437d31b5e90cae80c02934a09
groeber@mytum.de <groeber@mytum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-07-20 10:03:50 +0000

    Initial import.

---
+++ emfstore/org.eclipse.emf.emfstore.client.ui/src/org/eclipse/emf/emfstore/client/ui/views/historybrowserview/exceptions/NoComparisonException.java
@@ -0,0 +1,13 @@
+package org.eclipse.emf.emfstore.client.ui.views.historybrowserview.exceptions;
+
+public class NoComparisonException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public NoComparisonException() {
+		super();
+	}
+
+	public NoComparisonException(String msg) {
+		super(msg);
+	}
+}
