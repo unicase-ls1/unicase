7cec7e027dab8fc54167e9babe4b0e11b33ef70a
taher.galal90 <taher.galal90@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-07-28 09:27:14 +0000

    removed unused imports and commented method

--- other/traceRecovery/org.unicase.traceRecovery/src/traceability/handler/Indexer.java
+++ other/traceRecovery/org.unicase.traceRecovery/src/traceability/handler/Indexer.java
@@ -5,63 +5,14 @@ package traceability.handler;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
-import org.unicase.metamodel.ModelElementId;
-import org.unicase.metamodel.Project;
-import org.unicase.model.Attachment;
-
 import org.unicase.model.UnicaseModelElement;
-import org.unicase.model.activity.ActivityObject;
-import org.unicase.model.bug.BugReport;
-import org.unicase.model.classes.Association;
-import org.unicase.model.classes.Attribute;
-import org.unicase.model.classes.Dependency;
-import org.unicase.model.classes.Literal;
-import org.unicase.model.classes.Method;
-import org.unicase.model.classes.MethodArgument;
-import org.unicase.model.classes.PackageElement;
-import org.unicase.model.component.Component;
-import org.unicase.model.component.ComponentService;
-import org.unicase.model.component.DeploymentNode;
-import org.unicase.model.document.Section;
-import org.unicase.model.meeting.Meeting;
-import org.unicase.model.meeting.MeetingSection;
-import org.unicase.model.organization.OrgUnit;
-import org.unicase.model.profile.Profile;
-import org.unicase.model.profile.Stereotype;
-import org.unicase.model.profile.StereotypeAttribute;
-import org.unicase.model.profile.StereotypeAttributeInstance;
-import org.unicase.model.profile.StereotypeInstance;
-import org.unicase.model.rationale.Assessment;
-import org.unicase.model.rationale.Comment;
-import org.unicase.model.rationale.Criterion;
-import org.unicase.model.rationale.Proposal;
-import org.unicase.model.rationale.Solution;
-import org.unicase.model.requirement.Actor;
-import org.unicase.model.requirement.ActorInstance;
-import org.unicase.model.requirement.FunctionalRequirement;
-import org.unicase.model.requirement.Scenario;
-import org.unicase.model.requirement.Step;
-import org.unicase.model.requirement.SystemFunction;
-import org.unicase.model.requirement.UseCase;
-import org.unicase.model.requirement.UserTask;
-import org.unicase.model.state.StateNode;
-import org.unicase.model.state.Transition;
-import org.unicase.model.task.Checkable;
-import org.unicase.model.task.WorkItem;
-import org.unicase.workspace.ProjectSpace;
-import org.unicase.workspace.Workspace;
-import org.unicase.workspace.WorkspaceManager;
 
 import traceRecovery.Directory;
-import traceRecovery.Query;
 import traceRecovery.TraceRecoveryFactory;
-import traceRecovery.ui.RunRecovery;
 import traceability.fortran.FortranCodeIndexer;
 import traceability.fortran.FortranSourceCodeAnalyzer;
 import traceability.java.JavaSourceCodeAnalyzer;
@@ -130,17 +81,17 @@ public class Indexer {
 		this.codeDir = codeDir;
 		this.indexDir = indexDir;
 
-		File index = new File(this.indexDir.getPath());
+//		File index = new File(this.indexDir.getPath());
 		File code = new File(this.codeDir.getPath());
 
 		File[] files = code.listFiles();
-		JavaSourceCodeIndexer javaIndexer = new JavaSourceCodeIndexer();
+//		JavaSourceCodeIndexer javaIndexer = new JavaSourceCodeIndexer();
 		FortranCodeIndexer fortranIndexer = new FortranCodeIndexer();
 
 		if (files == null && code != null) {
 			if (code.getName().endsWith(".java")
 					&& writer.getAnalyzer() instanceof JavaSourceCodeAnalyzer) {
-				javaIndexer.indexFileJava(writer, code);
+				JavaSourceCodeIndexer.indexFileJava(writer, code);
 			} else if ((code.getName().endsWith(".f")
 					|| code.getName().endsWith(".for")
 					|| code.getName().endsWith(".f90") || code.getName()
@@ -158,7 +109,7 @@ public class Indexer {
 				} else if (f.getName().endsWith(".java")
 						&& writer.getAnalyzer() instanceof JavaSourceCodeAnalyzer) {
 
-					javaIndexer.indexFileJava(writer, f);
+					JavaSourceCodeIndexer.indexFileJava(writer, f);
 				} else if ((f.getName().endsWith(".f")
 						|| f.getName().endsWith(".for")
 						|| f.getName().endsWith(".f90") || f.getName()
@@ -188,17 +139,17 @@ public class Indexer {
 		this.codeDir = codeDir;
 		this.indexDir = indexDir;
 
-		File index = new File(this.indexDir.getPath());
+//		File index = new File(this.indexDir.getPath());
 		File code = new File(this.codeDir.getPath());
 
 		File[] files = code.listFiles();
-		JavaSourceCodeIndexer javaIndexer = javIndexer;
+//		JavaSourceCodeIndexer javaIndexer = javIndexer;
 		FortranCodeIndexer fortranIndexer = fortIndexer;
 
 		if (files == null && code != null) {
 			if (code.getName().endsWith(".java")
 					&& writer.getAnalyzer() instanceof JavaSourceCodeAnalyzer) {
-				javaIndexer.indexFileJava(writer, code);
+				JavaSourceCodeIndexer.indexFileJava(writer, code);
 			} else if ((code.getName().endsWith(".f")
 					|| code.getName().endsWith(".for")
 					|| code.getName().endsWith(".f90") || code.getName()
@@ -216,7 +167,7 @@ public class Indexer {
 				} else if (f.getName().endsWith(".java")
 						&& writer.getAnalyzer() instanceof JavaSourceCodeAnalyzer) {
 
-					javaIndexer.indexFileJava(writer, f);
+					JavaSourceCodeIndexer.indexFileJava(writer, f);
 				} else if ((f.getName().endsWith(".f")
 						|| f.getName().endsWith(".for")
 						|| f.getName().endsWith(".f90") || f.getName()
@@ -274,123 +225,7 @@ public class Indexer {
 			
 			
 		}
-		// Field type = null;
-		// if (me instanceof OrgUnit) {
-		// type = new Field("type", "orgunit", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof BugReport) {
-		// type = new Field("type", "bugreport", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof WorkItem) {
-		// type = new Field("type", "workitem", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Checkable) {
-		// type = new Field("type", "checkable", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof PackageElement) {
-		// type = new Field("type", "packageelement", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Association) {
-		// type = new Field("type", "association", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Attribute) {
-		// type = new Field("type", "attribute", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Method) {
-		// type = new Field("type", "method", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof MethodArgument) {
-		// type = new Field("type", "methodargument", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Dependency) {
-		// type = new Field("type", "dependency", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Literal) {
-		// type = new Field("type", "literal", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof Section) {
-		// type = new Field("type", "section", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof FunctionalRequirement) {
-		// type = new Field("type", "functionalrequirement", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof UseCase) {
-		// type = new Field("type", "usecase", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof Scenario) {
-		// type = new Field("type", "scenario", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Actor) {
-		// type = new Field("type", "actor", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof ActorInstance) {
-		// type = new Field("type", "actorinstance", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Step) {
-		// type = new Field("type", "step", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof SystemFunction) {
-		// type = new Field("type", "systemfunction", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof UserTask) {
-		// type = new Field("type", "usertask", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Workspace) {
-		// type = new Field("type", "workspace", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Proposal) {
-		// type = new Field("type", "propsal", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof Solution) {
-		// type = new Field("type", "solution", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Criterion) {
-		// type = new Field("type", "criterion", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Assessment) {
-		// type = new Field("type", "assessment", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Comment) {
-		// type = new Field("type", "comment", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof Component) {
-		// type = new Field("type", "component", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof ComponentService) {
-		// type = new Field("type", "componentservice", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof DeploymentNode) {
-		// type = new Field("type", "deploymentnode", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Meeting) {
-		// type = new Field("type", "meeting", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof MeetingSection) {
-		// type = new Field("type", "meetingsection", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Transition) {
-		// type = new Field("type", "transiton", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof StateNode) {
-		// type = new Field("type", "statenode", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Attachment) {
-		// type = new Field("type", "attatchment", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof Profile) {
-		// type = new Field("type", "profile", Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof Stereotype) {
-		// type = new Field("type", "stereotype", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof StereotypeInstance) {
-		// type = new Field("type", "sterotypeinstance", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof StereotypeAttribute) {
-		// type = new Field("type", "stereotypeattribute", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof StereotypeAttributeInstance) {
-		// type = new Field("type", "stereotypeattirbuteinstance",
-		// Field.Store.YES, Field.Index.NO);
-		// } else if (me instanceof ActivityObject) {
-		// type = new Field("type", "activityobject", Field.Store.YES,
-		// Field.Index.NO);
-		// } else if (me instanceof org.unicase.model.activity.Transition) {
-		// type = new Field("type", "transition activity", Field.Store.YES,
-		// Field.Index.NO);
-		// }
-		// if(type != null){
-		// doc.add(type);
-		// }
+		
 		try {
 			this.writer.addDocument(doc);
 		} catch (IOException e) {
@@ -398,69 +233,5 @@ public class Indexer {
 		}
 	}
 
-	// private void indexFile(IndexWriter writer2, File f) {
-	// if (!f.exists() || !f.canRead()) {
-	// return;
-	// }
-	//
-	// }
-	//
-	// /**
-	// * gets the value of the writer of class.
-	// *
-	// * @return writer
-	// */
-	// public IndexWriter getWriter() {
-	// return writer;
-	// }
-	//
-	// /**
-	// * sets the value of the writer of this index.
-	// *
-	// * @param writer
-	// * the value of the writer used
-	// */
-	// public void setWriter(IndexWriter writer) {
-	// this.writer = writer;
-	// }
-	//
-	// /**
-	// * gets tha value of the code directory.
-	// *
-	// * @return codeDir
-	// */
-	// public Directory getCodeDir() {
-	// return codeDir;
-	// }
-	//
-	// /**
-	// * sets the value of the code directory.
-	// *
-	// * @param codeDir
-	// * takes the directory that the code index will be found in.
-	// */
-	// public void setCodeDir(Directory codeDir) {
-	// this.codeDir = codeDir;
-	// }
-	//
-	// /**
-	// * returns the value of the directory that the index file will be stored
-	// in.
-	// *
-	// * @return indexDir
-	// */
-	// public Directory getIndexDir() {
-	// return indexDir;
-	// }
-	//
-	// /**
-	// * set the value of the directory that the index file should be stored in.
-	// *
-	// * @param indexDir
-	// * the directory that i will need.
-	// */
-	// public void setIndexDir(Directory indexDir) {
-	// this.indexDir = indexDir;
-	// }
-	//
+
 }
