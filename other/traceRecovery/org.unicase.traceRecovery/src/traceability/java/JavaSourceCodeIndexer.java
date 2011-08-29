09e65a2938e59be4f3b54223bbfaa2b251153792
taher.galal90 <taher.galal90@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-08-29 13:13:00 +0000

    removed warnnings

--- other/traceRecovery/org.unicase.traceRecovery/src/traceability/java/JavaSourceCodeIndexer.java
+++ other/traceRecovery/org.unicase.traceRecovery/src/traceability/java/JavaSourceCodeIndexer.java
@@ -9,16 +9,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import javax.activation.FileDataSource;
-
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.index.IndexWriter;
 
 import traceRecovery.Directory;
 import traceRecovery.TraceRecoveryFactory;
-import traceability.fortran.FortranSourceCodeParser;
-import traceability.handler.Indexer;
+import traceRecovery.handler.Indexer;
 import traceability.java.JavaParser.JClass;
 import traceability.java.JavaParser.JMethod;
 
@@ -33,21 +30,21 @@ public class JavaSourceCodeIndexer extends Indexer{
 		
 	}
 
-	private static final String IMPLEMENTS = "implements";
-	private static final String IMPORT = "import";
+//	private static final String IMPLEMENTS = "implements";
+//	private static final String IMPORT = "import";
 	private static final String CLASS = "class";
 	private static final String METHOD = "method";
-	private static final String CODE = "code";
+//	private static final String CODE = "code";
 	private static final String COMMENT = "comment";
-	private static final String PARAMETER = "parameter";
-	private static final String EXTENDS = "extends";
+//	private static final String PARAMETER = "parameter";
+//	private static final String EXTENDS = "extends";
 	public static void main(String [] args) {
 
 		try {
 			File indexDir = new File(
 					"lucene-index");
-			File dataDir = new File(
-					"code");
+//			File dataDir = new File(
+//					"code");
 			Directory d = TraceRecoveryFactory.eINSTANCE.createDirectory();
 			d.setPath("code");
 			IndexWriter writer = new IndexWriter(indexDir,
@@ -101,7 +98,7 @@ public class JavaSourceCodeIndexer extends Indexer{
 		Document doc = new Document();
 		JavaParser parser = new JavaParser();
 		parser.setSource(f);
-		addImportDeclarations(doc, parser);
+//		addImportDeclarations(doc, parser);
 		addComments(doc, parser);
 		JClass cls = parser.getDeclaredClass();
 		addClass(doc, cls);
@@ -127,16 +124,16 @@ public class JavaSourceCodeIndexer extends Indexer{
 	 * @param parser
 	 * 			the java parser that will get me the imports of fthe class
 	 */
-	private static void addImportDeclarations(Document doc, JavaParser parser) {
-		ArrayList <String> imports = parser.getImportDeclarations();
-		if (imports == null)
-			return;
-		for (int i = 0; i < imports.size(); i++) {
-			String importName = imports.get(i);
-			Field impor = new Field(IMPORT, importName, Field.Store.YES, Field.Index.UN_TOKENIZED);
-			doc.add(impor);
-		}
-	}
+//	private static void addImportDeclarations(Document doc, JavaParser parser) {
+//		ArrayList <String> imports = parser.getImportDeclarations();
+//		if (imports == null)
+//			return;
+//		for (int i = 0; i < imports.size(); i++) {
+//			String importName = imports.get(i);
+//			Field impor = new Field(IMPORT, importName, Field.Store.YES, Field.Index.UN_TOKENIZED);
+//			doc.add(impor);
+//		}
+//	}
 
 	/**
 	 * add the comments to the document
@@ -146,11 +143,17 @@ public class JavaSourceCodeIndexer extends Indexer{
 	 * 			java parser to get the comments
 	 */
 	private static void addComments(Document doc, JavaParser parser) {
-		ArrayList comments = parser.getComments();
+		ArrayList<String> comments = parser.getComments();
+//		JavaSourceCodeAnalyzer analyzer = new JavaSourceCodeAnalyzer();
 		if (comments == null)
 			return;
 		for (int i = 0; i < comments.size(); i++) {
-			String docComment = (String) comments.get(i);
+//			StringReader reader = new StringReader(comments.get(i));
+//			TokenStream comm = analyzer.tokenStream(CLASS, reader);
+			
+			
+			
+			String docComment =  comments.get(i);
 			Field comment = new Field(COMMENT, docComment, Field.Store.YES, Field.Index.TOKENIZED);
 			doc.add(comment);
 		}
@@ -166,22 +169,22 @@ public class JavaSourceCodeIndexer extends Indexer{
 	private static void addClass(Document doc, JClass cls) {
 		Field cl = new Field(CLASS, cls.className, Field.Store.YES, Field.Index.TOKENIZED);
 		doc.add(cl);
-		String superCls = cls.superClass;
-		if (superCls != null){
-			Field supercl = new Field(EXTENDS, superCls, Field.Store.YES, Field.Index.TOKENIZED);
-			doc.add(supercl);
-		}
-		ArrayList interfaces = cls.interfaces;
-		for (int i = 0; i < interfaces.size(); i++) {
-			String interfaceName = (String) interfaces.get(i);
-			Field interfaceNam = new Field(IMPLEMENTS, interfaceName, Field.Store.YES, Field.Index.TOKENIZED);
-			doc.add(interfaceNam);
-		}
+//		String superCls = cls.superClass;
+//		if (superCls != null){
+//			Field supercl = new Field(EXTENDS, superCls, Field.Store.YES, Field.Index.TOKENIZED);
+//			doc.add(supercl);
+//		}
+//		ArrayList interfaces = cls.interfaces;
+//		for (int i = 0; i < interfaces.size(); i++) {
+//			String interfaceName = (String) interfaces.get(i);
+//			Field interfaceNam = new Field(IMPLEMENTS, interfaceName, Field.Store.YES, Field.Index.TOKENIZED);
+//			doc.add(interfaceNam);
+//		}
 
 		addMethods(cls, doc);
-		ArrayList innerCls = cls.innerClasses;
+		ArrayList<JClass> innerCls = cls.innerClasses;
 		for (int i = 0; i < innerCls.size(); i++) {
-			addClass(doc, (JClass) innerCls.get(i));
+			addClass(doc,  innerCls.get(i));
 		}
 
 	}
@@ -194,24 +197,24 @@ public class JavaSourceCodeIndexer extends Indexer{
 	 * 			the document to add to
 	 */
 	private static void addMethods(JClass cls, Document doc) {
-		ArrayList methods = cls.methodDeclarations;
+		ArrayList <JMethod> methods = cls.methodDeclarations;
 		for (int i = 0; i < methods.size(); i++) {
-			JMethod method = (JMethod) methods.get(i);
+			JMethod method =  methods.get(i);
 			Field meth = new Field(METHOD, method.methodName, Field.Store.YES, Field.Index.TOKENIZED);
 			doc.add(meth);
-			Field ret = new Field(METHOD, method.returnType, Field.Store.YES, Field.Index.TOKENIZED);
-			doc.add(ret);
-			ArrayList params = method.parameters;
-			for (int k = 0; k < params.size(); k++) {
-				String paramType = (String) params.get(k);
-				Field param = new Field(PARAMETER, paramType, Field.Store.YES, Field.Index.TOKENIZED);
-				doc.add(param);
-			}
-			String code = method.codeBlock;
-			if (code != null){	
-				Field cod = new Field(CODE, code, Field.Store.YES, Field.Index.TOKENIZED, Field.TermVector.WITH_POSITIONS);
-				doc.add(cod);
-			}
+//			Field ret = new Field(METHOD, method.returnType, Field.Store.YES, Field.Index.TOKENIZED);
+//			doc.add(ret);
+//			ArrayList params = method.parameters;
+//			for (int k = 0; k < params.size(); k++) {
+//				String paramType = (String) params.get(k);
+//				Field param = new Field(PARAMETER, paramType, Field.Store.YES, Field.Index.TOKENIZED);
+//				doc.add(param);
+//			}
+//			String code = method.codeBlock;
+//			if (code != null){	
+//				Field cod = new Field(CODE, code, Field.Store.YES, Field.Index.TOKENIZED, Field.TermVector.WITH_POSITIONS);
+//				doc.add(cod);
+//			}
 		}
 	}
 
