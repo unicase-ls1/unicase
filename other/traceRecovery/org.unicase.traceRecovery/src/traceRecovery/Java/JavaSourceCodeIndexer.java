0f1da65f164e225e39fd6aa6f2f0aa94474fc1f5
taher.galal90 <taher.galal90@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-08-31 22:07:38 +0000

    changes in indexing

--- other/traceRecovery/org.unicase.traceRecovery/src/traceRecovery/Java/JavaSourceCodeIndexer.java
+++ other/traceRecovery/org.unicase.traceRecovery/src/traceRecovery/Java/JavaSourceCodeIndexer.java
@@ -3,8 +3,6 @@
  */
 package traceRecovery.Java;
 
-
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,201 +18,201 @@ import traceRecovery.Java.JavaParser.JClass;
 import traceRecovery.Java.JavaParser.JMethod;
 
 /**
- * @author taher
- * will index the java code
+ * @author taher will index the java code
  */
-public class JavaSourceCodeIndexer extends Indexer{
+public class JavaSourceCodeIndexer extends Indexer {
 	public JavaSourceCodeIndexer() {
 		super();
-		
-		
+
 	}
 
-//	private static final String IMPLEMENTS = "implements";
-//	private static final String IMPORT = "import";
+	// private static final String IMPLEMENTS = "implements";
+	// private static final String IMPORT = "import";
 	private static final String CLASS = "class";
 	private static final String METHOD = "method";
-//	private static final String CODE = "code";
+	// private static final String CODE = "code";
 	private static final String COMMENT = "comment";
-//	private static final String PARAMETER = "parameter";
-//	private static final String EXTENDS = "extends";
-	public static void main(String [] args) {
 
-		try {
-			File indexDir = new File(
-					"lucene-index");
-//			File dataDir = new File(
-//					"code");
-			Directory d = TraceRecoveryFactory.eINSTANCE.createDirectory();
-			d.setPath("code");
-			IndexWriter writer = new IndexWriter(indexDir,
-					new JavaSourceCodeAnalyzer(), true);
-			indexDirectory(writer, d);
-//			indexDirectory(writer, dataDir);
-			int numFiles = writer.docCount();
-			writer.close();
-			System.out.println("Indexing "+ numFiles);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	// private static final String PARAMETER = "parameter";
+	// private static final String EXTENDS = "extends";
 
 	/**
 	 * this will index a certain directory
+	 * 
 	 * @param writer
-	 * 			the writer that will write to the index file
+	 *            the writer that will write to the index file
 	 * @param directory
-	 * 				the directory that will be indexed
+	 *            the directory that will be indexed
 	 * @throws IOException
 	 */
-	public static  void indexDirectory(IndexWriter writer, Directory directory)
+	public static void indexDirectory(IndexWriter writer, Directory directory)
 			throws IOException {
 		File dir = new File(directory.getPath());
 		File[] files = dir.listFiles();
 		for (int i = 0; i < files.length; i++) {
 			File f = files[i];
 			Directory d = TraceRecoveryFactory.eINSTANCE.createDirectory();
-				d.setPath(f.getAbsolutePath());
+			d.setPath(f.getAbsolutePath());
 			if (f.isDirectory())
 				indexDirectory(writer, d);
 			else if (f.getName().endsWith(".java"))
 				indexFileJava(writer, f);
-//			else if(f.getName().endsWith(".f") || f.getName().endsWith("for") || f.getName().endsWith("f90") || f.getName().endsWith("f95")){
-//				indexFileFortran(writer,f);
-//			}
+			// else if(f.getName().endsWith(".f") || f.getName().endsWith("for")
+			// || f.getName().endsWith("f90") || f.getName().endsWith("f95")){
+			// indexFileFortran(writer,f);
+			// }
 		}
 	}
 
 	/**
-	 * get the certain fileds of the file and index them 
+	 * get the certain fileds of the file and index them
+	 * 
 	 * @param writer
-	 * 			the writer that will write to the index file
+	 *            the writer that will write to the index file
 	 * @param f
-	 * 			the file that should be indexed and data retrieved from
+	 *            the file that should be indexed and data retrieved from
 	 */
-	public static  void indexFileJava(IndexWriter writer, File f) {
+	public static void indexFileJava(IndexWriter writer, File f) {
 		if (f.isHidden() || !f.exists() || !f.canRead())
 			return;
 		Document doc = new Document();
 		JavaParser parser = new JavaParser();
 		parser.setSource(f);
-//		addImportDeclarations(doc, parser);
-		addComments(doc, parser);
+		// addImportDeclarations(doc, parser);
+
 		JClass cls = parser.getDeclaredClass();
-		addClass(doc, cls);
-		
-		Field name = new Field("filename",f.getName(),Field.Store.YES,Field.Index.NO);
-		Field path = new Field("path",f.getAbsolutePath(),Field.Store.YES,Field.Index.NO);
-		doc.add(name);
-		doc.add(path);
-		
-		try {
-			writer.addDocument(doc);
-			writer.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (cls != null) {
+			addComments(doc, parser);
+			addClass(doc, cls);
+
+			Field name = new Field("filename", f.getName(), Field.Store.YES,
+					Field.Index.NO);
+			Field path = new Field("path", f.getAbsolutePath(),
+					Field.Store.YES, Field.Index.NO);
+			doc.add(name);
+			doc.add(path);
+
+			try {
+				writer.addDocument(doc);
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 
 	/**
 	 * add the imports to the document
+	 * 
 	 * @param doc
-	 * 			the document
+	 *            the document
 	 * @param parser
-	 * 			the java parser that will get me the imports of fthe class
+	 *            the java parser that will get me the imports of fthe class
 	 */
-//	private static void addImportDeclarations(Document doc, JavaParser parser) {
-//		ArrayList <String> imports = parser.getImportDeclarations();
-//		if (imports == null)
-//			return;
-//		for (int i = 0; i < imports.size(); i++) {
-//			String importName = imports.get(i);
-//			Field impor = new Field(IMPORT, importName, Field.Store.YES, Field.Index.UN_TOKENIZED);
-//			doc.add(impor);
-//		}
-//	}
+	// private static void addImportDeclarations(Document doc, JavaParser
+	// parser) {
+	// ArrayList <String> imports = parser.getImportDeclarations();
+	// if (imports == null)
+	// return;
+	// for (int i = 0; i < imports.size(); i++) {
+	// String importName = imports.get(i);
+	// Field impor = new Field(IMPORT, importName, Field.Store.YES,
+	// Field.Index.UN_TOKENIZED);
+	// doc.add(impor);
+	// }
+	// }
 
 	/**
 	 * add the comments to the document
+	 * 
 	 * @param doc
-	 * 			the document to add to
+	 *            the document to add to
 	 * @param parser
-	 * 			java parser to get the comments
+	 *            java parser to get the comments
 	 */
 	private static void addComments(Document doc, JavaParser parser) {
 		ArrayList<String> comments = parser.getComments();
-//		JavaSourceCodeAnalyzer analyzer = new JavaSourceCodeAnalyzer();
+		// JavaSourceCodeAnalyzer analyzer = new JavaSourceCodeAnalyzer();
 		if (comments == null)
 			return;
 		for (int i = 0; i < comments.size(); i++) {
-//			StringReader reader = new StringReader(comments.get(i));
-//			TokenStream comm = analyzer.tokenStream(CLASS, reader);
-			
-			
-			
-			String docComment =  comments.get(i);
-			Field comment = new Field(COMMENT, docComment, Field.Store.YES, Field.Index.TOKENIZED);
+			// StringReader reader = new StringReader(comments.get(i));
+			// TokenStream comm = analyzer.tokenStream(CLASS, reader);
+
+			String docComment = comments.get(i);
+			Field comment = new Field(COMMENT, docComment, Field.Store.YES,
+					Field.Index.TOKENIZED);
 			doc.add(comment);
 		}
 	}
 
 	/**
 	 * add the class to the document
+	 * 
 	 * @param doc
-	 * 			the document to add to
+	 *            the document to add to
 	 * @param cls
-	 * 			the class
+	 *            the class
 	 */
 	private static void addClass(Document doc, JClass cls) {
-		Field cl = new Field(CLASS, cls.className, Field.Store.YES, Field.Index.TOKENIZED);
-		doc.add(cl);
-//		String superCls = cls.superClass;
-//		if (superCls != null){
-//			Field supercl = new Field(EXTENDS, superCls, Field.Store.YES, Field.Index.TOKENIZED);
-//			doc.add(supercl);
-//		}
-//		ArrayList interfaces = cls.interfaces;
-//		for (int i = 0; i < interfaces.size(); i++) {
-//			String interfaceName = (String) interfaces.get(i);
-//			Field interfaceNam = new Field(IMPLEMENTS, interfaceName, Field.Store.YES, Field.Index.TOKENIZED);
-//			doc.add(interfaceNam);
-//		}
+		if (cls != null) {
+			Field cl = new Field(CLASS, cls.className, Field.Store.YES,
+					Field.Index.TOKENIZED);
+			doc.add(cl);
+			// String superCls = cls.superClass;
+			// if (superCls != null){
+			// Field supercl = new Field(EXTENDS, superCls, Field.Store.YES,
+			// Field.Index.TOKENIZED);
+			// doc.add(supercl);
+			// }
+			// ArrayList interfaces = cls.interfaces;
+			// for (int i = 0; i < interfaces.size(); i++) {
+			// String interfaceName = (String) interfaces.get(i);
+			// Field interfaceNam = new Field(IMPLEMENTS, interfaceName,
+			// Field.Store.YES, Field.Index.TOKENIZED);
+			// doc.add(interfaceNam);
+			// }
 
-		addMethods(cls, doc);
-		ArrayList<JClass> innerCls = cls.innerClasses;
-		for (int i = 0; i < innerCls.size(); i++) {
-			addClass(doc,  innerCls.get(i));
+			addMethods(cls, doc);
+			ArrayList<JClass> innerCls = cls.innerClasses;
+			for (int i = 0; i < innerCls.size(); i++) {
+				addClass(doc, innerCls.get(i));
+			}
 		}
-
 	}
 
 	/**
 	 * add the methods to the document
+	 * 
 	 * @param cls
-	 * 			the class 
+	 *            the class
 	 * @param doc
-	 * 			the document to add to
+	 *            the document to add to
 	 */
 	private static void addMethods(JClass cls, Document doc) {
-		ArrayList <JMethod> methods = cls.methodDeclarations;
+		ArrayList<JMethod> methods = cls.methodDeclarations;
 		for (int i = 0; i < methods.size(); i++) {
-			JMethod method =  methods.get(i);
-			Field meth = new Field(METHOD, method.methodName, Field.Store.YES, Field.Index.TOKENIZED);
+			JMethod method = methods.get(i);
+			Field meth = new Field(METHOD, method.methodName, Field.Store.YES,
+					Field.Index.TOKENIZED);
 			doc.add(meth);
-//			Field ret = new Field(METHOD, method.returnType, Field.Store.YES, Field.Index.TOKENIZED);
-//			doc.add(ret);
-//			ArrayList params = method.parameters;
-//			for (int k = 0; k < params.size(); k++) {
-//				String paramType = (String) params.get(k);
-//				Field param = new Field(PARAMETER, paramType, Field.Store.YES, Field.Index.TOKENIZED);
-//				doc.add(param);
-//			}
-//			String code = method.codeBlock;
-//			if (code != null){	
-//				Field cod = new Field(CODE, code, Field.Store.YES, Field.Index.TOKENIZED, Field.TermVector.WITH_POSITIONS);
-//				doc.add(cod);
-//			}
+			// Field ret = new Field(METHOD, method.returnType, Field.Store.YES,
+			// Field.Index.TOKENIZED);
+			// doc.add(ret);
+			// ArrayList params = method.parameters;
+			// for (int k = 0; k < params.size(); k++) {
+			// String paramType = (String) params.get(k);
+			// Field param = new Field(PARAMETER, paramType, Field.Store.YES,
+			// Field.Index.TOKENIZED);
+			// doc.add(param);
+			// }
+			// String code = method.codeBlock;
+			// if (code != null){
+			// Field cod = new Field(CODE, code, Field.Store.YES,
+			// Field.Index.TOKENIZED, Field.TermVector.WITH_POSITIONS);
+			// doc.add(cod);
+			// }
 		}
 	}
 
