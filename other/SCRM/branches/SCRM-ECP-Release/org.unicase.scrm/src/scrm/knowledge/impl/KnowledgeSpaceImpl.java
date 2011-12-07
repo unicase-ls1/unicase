89f0018a00f74943bb40eca81a761c118a14bb4c
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2011-12-07 14:12:14 +0000

    Latest adjustments for the new ECP release.

---
+++ other/SCRM/branches/SCRM-ECP-Release/org.unicase.scrm/src/scrm/knowledge/impl/KnowledgeSpaceImpl.java
@@ -0,0 +1,452 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package scrm.knowledge.impl;
+
+import java.util.Collection;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import scrm.SCRMDiagram;
+import scrm.SCRMModelElement;
+import scrm.ScrmPackage;
+import scrm.impl.SCRMModelElementImpl;
+
+import scrm.knowledge.KnowledgePackage;
+import scrm.knowledge.KnowledgeSpace;
+import scrm.knowledge.ScientificKnowledge;
+import scrm.lists.SCRMSpaceContainedModelElementsList;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Space</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link scrm.knowledge.impl.KnowledgeSpaceImpl#getRepresentingDiagram <em>Representing Diagram</em>}</li>
+ *   <li>{@link scrm.knowledge.impl.KnowledgeSpaceImpl#getContainingKnowledgeSpace <em>Containing Knowledge Space</em>}</li>
+ *   <li>{@link scrm.knowledge.impl.KnowledgeSpaceImpl#getContainedScientificKnowledge <em>Contained Scientific Knowledge</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class KnowledgeSpaceImpl extends SCRMModelElementImpl implements
+		KnowledgeSpace {
+	/**
+	 * The cached value of the '{@link #getRepresentingDiagram() <em>Representing Diagram</em>}' reference.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getRepresentingDiagram()
+	 * @generated
+	 * @ordered
+	 */
+	protected SCRMDiagram representingDiagram;
+
+	/**
+	 * The cached value of the '{@link #getContainedScientificKnowledge() <em>Contained Scientific Knowledge</em>}' containment reference list.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getContainedScientificKnowledge()
+	 * @generated NOT: custom type
+	 * @ordered
+	 */
+	protected SCRMSpaceContainedModelElementsList<SCRMModelElement> containedScientificKnowledge;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected KnowledgeSpaceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return KnowledgePackage.Literals.KNOWLEDGE_SPACE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public KnowledgeSpace getContainingKnowledgeSpace() {
+		if (eContainerFeatureID() != KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE)
+			return null;
+		return (KnowledgeSpace) eContainer();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public KnowledgeSpace basicGetContainingKnowledgeSpace() {
+		if (eContainerFeatureID() != KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE)
+			return null;
+		return (KnowledgeSpace) eInternalContainer();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetContainingKnowledgeSpace(
+			KnowledgeSpace newContainingKnowledgeSpace, NotificationChain msgs) {
+		msgs = eBasicSetContainer(
+				(InternalEObject) newContainingKnowledgeSpace,
+				KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE,
+				msgs);
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setContainingKnowledgeSpace(
+			KnowledgeSpace newContainingKnowledgeSpace) {
+		if (newContainingKnowledgeSpace != eInternalContainer()
+				|| (eContainerFeatureID() != KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE && newContainingKnowledgeSpace != null)) {
+			if (EcoreUtil.isAncestor(this, newContainingKnowledgeSpace))
+				throw new IllegalArgumentException(
+						"Recursive containment not allowed for " + toString());
+			NotificationChain msgs = null;
+			if (eInternalContainer() != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			if (newContainingKnowledgeSpace != null)
+				msgs = ((InternalEObject) newContainingKnowledgeSpace)
+						.eInverseAdd(
+								this,
+								KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE,
+								KnowledgeSpace.class, msgs);
+			msgs = basicSetContainingKnowledgeSpace(
+					newContainingKnowledgeSpace, msgs);
+			if (msgs != null)
+				msgs.dispatch();
+		} else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(
+					this,
+					Notification.SET,
+					KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE,
+					newContainingKnowledgeSpace, newContainingKnowledgeSpace));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public SCRMDiagram getRepresentingDiagram() {
+		if (representingDiagram != null && representingDiagram.eIsProxy()) {
+			InternalEObject oldRepresentingDiagram = (InternalEObject) representingDiagram;
+			representingDiagram = (SCRMDiagram) eResolveProxy(oldRepresentingDiagram);
+			if (representingDiagram != oldRepresentingDiagram) {
+				if (eNotificationRequired())
+					eNotify(new ENotificationImpl(
+							this,
+							Notification.RESOLVE,
+							KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM,
+							oldRepresentingDiagram, representingDiagram));
+			}
+		}
+		return representingDiagram;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public SCRMDiagram basicGetRepresentingDiagram() {
+		return representingDiagram;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public NotificationChain basicSetRepresentingDiagram(
+			SCRMDiagram newRepresentingDiagram, NotificationChain msgs) {
+		SCRMDiagram oldRepresentingDiagram = representingDiagram;
+		representingDiagram = newRepresentingDiagram;
+		if (eNotificationRequired()) {
+			ENotificationImpl notification = new ENotificationImpl(this,
+					Notification.SET,
+					KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM,
+					oldRepresentingDiagram, newRepresentingDiagram);
+			if (msgs == null)
+				msgs = notification;
+			else
+				msgs.add(notification);
+		}
+		return msgs;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT: update diagram in containment list
+	 */
+	public void setRepresentingDiagram(SCRMDiagram newRepresentingDiagram) {
+		if (newRepresentingDiagram != representingDiagram) {
+			getContainedScientificKnowledge()
+					.setDiagram(newRepresentingDiagram);
+			NotificationChain msgs = null;
+			if (representingDiagram != null)
+				msgs = ((InternalEObject) representingDiagram).eInverseRemove(
+						this, ScrmPackage.SCRM_DIAGRAM__REPRESENTED_SPACE,
+						SCRMDiagram.class, msgs);
+			if (newRepresentingDiagram != null)
+				msgs = ((InternalEObject) newRepresentingDiagram).eInverseAdd(
+						this, ScrmPackage.SCRM_DIAGRAM__REPRESENTED_SPACE,
+						SCRMDiagram.class, msgs);
+			msgs = basicSetRepresentingDiagram(newRepresentingDiagram, msgs);
+			if (msgs != null)
+				msgs.dispatch();
+		} else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET,
+					KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM,
+					newRepresentingDiagram, newRepresentingDiagram));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT: implemented abstract operation
+	 */
+	public EList<SCRMModelElement> getContainedModelElements() {
+		return getContainedScientificKnowledge();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated NOT: custom type
+	 */
+	public SCRMSpaceContainedModelElementsList<SCRMModelElement> getContainedScientificKnowledge() {
+		if (containedScientificKnowledge == null) {
+			containedScientificKnowledge = new SCRMSpaceContainedModelElementsList<SCRMModelElement>(
+					ScientificKnowledge.class,
+					this,
+					KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE,
+					KnowledgePackage.SCIENTIFIC_KNOWLEDGE__CONTAINING_KNOWLEDGE_SPACE);
+			containedScientificKnowledge.setDiagram(getRepresentingDiagram());
+		}
+		return containedScientificKnowledge;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public NotificationChain eInverseAdd(InternalEObject otherEnd,
+			int featureID, NotificationChain msgs) {
+		switch (featureID) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM:
+			if (representingDiagram != null)
+				msgs = ((InternalEObject) representingDiagram).eInverseRemove(
+						this, ScrmPackage.SCRM_DIAGRAM__REPRESENTED_SPACE,
+						SCRMDiagram.class, msgs);
+			return basicSetRepresentingDiagram((SCRMDiagram) otherEnd, msgs);
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			if (eInternalContainer() != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			return basicSetContainingKnowledgeSpace((KnowledgeSpace) otherEnd,
+					msgs);
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE:
+			return ((InternalEList<InternalEObject>) (InternalEList<?>) getContainedScientificKnowledge())
+					.basicAdd(otherEnd, msgs);
+		}
+		return super.eInverseAdd(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eInverseRemove(InternalEObject otherEnd,
+			int featureID, NotificationChain msgs) {
+		switch (featureID) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM:
+			return basicSetRepresentingDiagram(null, msgs);
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			return basicSetContainingKnowledgeSpace(null, msgs);
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE:
+			return ((InternalEList<?>) getContainedScientificKnowledge())
+					.basicRemove(otherEnd, msgs);
+		}
+		return super.eInverseRemove(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public NotificationChain eBasicRemoveFromContainerFeature(
+			NotificationChain msgs) {
+		switch (eContainerFeatureID()) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			return eInternalContainer()
+					.eInverseRemove(
+							this,
+							KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE,
+							KnowledgeSpace.class, msgs);
+		}
+		return super.eBasicRemoveFromContainerFeature(msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM:
+			if (resolve)
+				return getRepresentingDiagram();
+			return basicGetRepresentingDiagram();
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			if (resolve)
+				return getContainingKnowledgeSpace();
+			return basicGetContainingKnowledgeSpace();
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE:
+			return getContainedScientificKnowledge();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM:
+			setRepresentingDiagram((SCRMDiagram) newValue);
+			return;
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			setContainingKnowledgeSpace((KnowledgeSpace) newValue);
+			return;
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE:
+			getContainedScientificKnowledge().clear();
+			getContainedScientificKnowledge().addAll(
+					(Collection<? extends ScientificKnowledge>) newValue);
+			return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM:
+			setRepresentingDiagram((SCRMDiagram) null);
+			return;
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			setContainingKnowledgeSpace((KnowledgeSpace) null);
+			return;
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE:
+			getContainedScientificKnowledge().clear();
+			return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+		case KnowledgePackage.KNOWLEDGE_SPACE__REPRESENTING_DIAGRAM:
+			return representingDiagram != null;
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+			return basicGetContainingKnowledgeSpace() != null;
+		case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINED_SCIENTIFIC_KNOWLEDGE:
+			return containedScientificKnowledge != null
+					&& !containedScientificKnowledge.isEmpty();
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public int eBaseStructuralFeatureID(int derivedFeatureID, Class<?> baseClass) {
+		if (baseClass == ScientificKnowledge.class) {
+			switch (derivedFeatureID) {
+			case KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE:
+				return KnowledgePackage.SCIENTIFIC_KNOWLEDGE__CONTAINING_KNOWLEDGE_SPACE;
+			default:
+				return -1;
+			}
+		}
+		return super.eBaseStructuralFeatureID(derivedFeatureID, baseClass);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public int eDerivedStructuralFeatureID(int baseFeatureID, Class<?> baseClass) {
+		if (baseClass == ScientificKnowledge.class) {
+			switch (baseFeatureID) {
+			case KnowledgePackage.SCIENTIFIC_KNOWLEDGE__CONTAINING_KNOWLEDGE_SPACE:
+				return KnowledgePackage.KNOWLEDGE_SPACE__CONTAINING_KNOWLEDGE_SPACE;
+			default:
+				return -1;
+			}
+		}
+		return super.eDerivedStructuralFeatureID(baseFeatureID, baseClass);
+	}
+
+} //KnowledgeSpaceImpl
