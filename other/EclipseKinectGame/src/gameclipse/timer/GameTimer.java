2b6a0b4f8474f8cdd8a6ed02e4d296b2a61f9f66
neobsen@gmail.com <neobsen@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-01-18 22:03:08 +0000

    added timer + added timer label + added button to start/stop/restart + changed listener

---
+++ other/EclipseKinectGame/src/gameclipse/timer/GameTimer.java
@@ -0,0 +1,54 @@
+package gameclipse.timer;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+
+public class GameTimer {
+	
+	public static final String ID = "gameclipse.timer.GameTimer";
+	
+	private Runnable timer;
+	
+	private final int invocationTime=1000;
+	
+	private int startTime=0;
+	
+	private Display display = null;
+	
+	private Label label = null;
+	
+	public GameTimer(final Display display, final Label label) {
+		this.label = label;
+		this.display = display;
+		timer = new Runnable() {
+       		public void run() {
+       			if (label.isDisposed())
+       				return;
+       			label.setText("" + startTime);
+       			startTime++;
+       			display.timerExec(invocationTime, this);
+       		}
+		};
+	}
+	
+	public void startTimer() {
+		startTime=0;
+		display.timerExec(invocationTime, timer);	
+	}
+	
+	public void stopTimer() {
+		display.timerExec(-1,timer);
+	}
+	
+	public void reset() {
+		startTime=0;
+		label.setText("" + startTime);
+	}
+	
+	public int getTime() {
+		return startTime;
+	}
+	
+}
--- other/EclipseKinectGame/src/gameclipse/views/GameView.java
+++ other/EclipseKinectGame/src/gameclipse/views/GameView.java
@@ -5,31 +5,45 @@ import gameclipse.game.GameState;
 import gameclipse.game.IntroState;
 import gameclipse.game.SecondState;
 import gameclipse.game.ThirdState;
+import gameclipse.timer.GameTimer;
 
 import java.util.LinkedList;
 import java.util.List;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.ui.part.ViewPart;
 
-public class GameView extends ViewPart implements KeyListener {
+public class GameView extends ViewPart implements Listener {
 
 	public static final String ID = "gameclipse.views.GameView";
 
 	private List<GameState> gameStates = new LinkedList<GameState>();
 
 	private Label label = null;
+	
+	private Label label_time = null;
+	
+	private Button buttonStart = null;
 
 	private int index = 0;
+	
+	private boolean gameState = false;
 
 	private Composite thisComp = null;
 
 	private GameState currentState = null;
+	
+	private GameTimer gameTimer = null;
 
 	private void initStates() {
 		IntroState introState = new IntroState();
@@ -48,16 +62,78 @@ public class GameView extends ViewPart implements KeyListener {
 
 		// initialize the game states
 		initStates();
+		//Game not running
+		gameState = false;
 
+		//Define GridLayout
 		GridLayout layout = new GridLayout(2, false);
 		layout.marginWidth = 10;
+		layout.marginTop = 10;
+		layout.verticalSpacing = 20;
+		layout.horizontalSpacing = 10;
 		parent.setLayout(layout);
+		
+		//Add actions label
 		label = new Label(parent, SWT.NONE);
+		label.setFont(new Font(parent.getDisplay(),"Arial", 52, SWT.BOLD ));
+		GridData gridData = new GridData();
+		gridData.grabExcessHorizontalSpace = true;
+		gridData.horizontalAlignment = GridData.FILL;
+		label.setLayoutData(gridData);
+		
+		//Add start/stop/reset button
+		buttonStart = new Button(parent, SWT.None);
+		buttonStart.setText("Start Game!");
+		buttonStart.setFont(new Font(parent.getDisplay(), "Arial", 28, SWT.BOLD));
+		
+		//Add a label that shows the time
+		label_time = new Label(parent, SWT.None);
+		label_time.setFont(new Font(parent.getDisplay(), "Arial", 32, SWT.BOLD));
+		label_time.setLayoutData(gridData);
+		label_time.setText("0");
+		label_time.setAlignment(SWT.CENTER);
 
-		currentState = gameStates.get(index++);
+		//Initialize first gameState
+		currentState = gameStates.get(index);
 		currentState.paintScreen(label);
-
-		parent.addKeyListener(this);
+		
+		buttonStart.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				//Button has been clicked, what's the current action?
+				if(gameState) {
+					//Stop
+					gameState = false;
+					gameTimer.stopTimer();
+					buttonStart.setText("Reset Game");
+				} else if (!gameState) {
+					if (gameTimer.getTime() != 0) {
+						//Reset
+						gameTimer.reset();
+						index = 0;
+						currentState = gameStates.get(index);
+						currentState.paintScreen(label);
+						buttonStart.setText("Start Game");
+					} else {
+						//Start
+						buttonStart.setText("Stop Game");
+						gameState = true;
+						setFocus();
+						gameTimer.startTimer();
+					}
+				}
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				
+			}
+		});
+		
+		gameTimer = new GameTimer(thisComp.getDisplay(), label_time);
+		
+		parent.getDisplay().addFilter(SWT.KeyDown, this);
 		parent.setFocus();
 	}
 
@@ -65,26 +141,29 @@ public class GameView extends ViewPart implements KeyListener {
 		thisComp.setFocus();
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-
-	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {
+	public void handleEvent(Event e) {
 		char c = e.character;
 		switch (c) {
 		case 'a':
-			if (index == gameStates.size()) {
-				index = 0;
+			if(gameState) {
+				// perform action on the current state
+				currentState.performAction();
+	
+				// paint the next state (and update the current state)
+				currentState = gameStates.get(++index);
+				currentState.paintScreen(label);
+				
+				if (index == gameStates.size() - 1) {
+					index = 0;
+					gameState = false;
+					gameTimer.stopTimer();
+					buttonStart.setText("Reset Game");
+					currentState.paintScreen(label);
+					return;
+				}
 			}
-
-			// perform action on the current state
-			currentState.performAction();
-
-			// paint the next state (and update the current state)
-			currentState = gameStates.get(index++);
-			currentState.paintScreen(label);
 			break;
 		}
 	}
