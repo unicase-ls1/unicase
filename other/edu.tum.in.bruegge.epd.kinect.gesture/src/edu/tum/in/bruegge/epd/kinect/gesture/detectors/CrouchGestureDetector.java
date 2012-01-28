52df332c37f7bda9a156042983dc9777607d01c4
philip.achenbach@gmail.com <philip.achenbach@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-01-28 23:06:13 +0000

    First gesture implementations.

--- other/edu.tum.in.bruegge.epd.kinect.gesture/src/edu/tum/in/bruegge/epd/kinect/gesture/detectors/CrouchGestureDetector.java
+++ other/edu.tum.in.bruegge.epd.kinect.gesture/src/edu/tum/in/bruegge/epd/kinect/gesture/detectors/CrouchGestureDetector.java
@@ -1,18 +1,40 @@
 package edu.tum.in.bruegge.epd.kinect.gesture.detectors;
 
 import humanbodymodel.Human;
+import humandiagramgef.HumanBodyEnum;
 
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EAttribute;
 
 import edu.tum.in.bruegge.epd.kinect.gesture.Gesture;
+import edu.tum.in.bruegge.epd.kinect.gesture.impl.MovingAverageCalculator;
 
 public class CrouchGestureDetector extends Gesture {
-
+	
+	private static final int NUM_PERIODS = 15;
+	private static final float THRESHOLD = 0.4f;
+	
+	private MovingAverageCalculator yMovingAvgHead;
+	
+	public CrouchGestureDetector() {
+		this.yMovingAvgHead = new MovingAverageCalculator(NUM_PERIODS);
+	}
+	
 	@Override
 	public boolean isGestureDetected(Notification notification) {
-		String bodyPart=((Human)notification.getFeature()).getName();
-		if(bodyPart.equalsIgnoreCase("head")){
+		if (notification.getEventType() == Notification.SET && notification.wasSet()) {
+			EAttribute feature = (EAttribute) notification.getFeature();
+			Human humanBodyPart = (Human) notification.getNotifier();
 			
+			if ("y".equals(feature.getName()) && HumanBodyEnum.Head.name().equals(humanBodyPart.getName())) {
+				float sensorValue = notification.getNewFloatValue();
+				float avgHeadValue = this.yMovingAvgHead.calculateMovingAvg(sensorValue);
+				float delta = sensorValue - avgHeadValue;
+				
+				if (-delta > avgHeadValue * THRESHOLD) {
+					return true;
+				}
+			}
 		}
 		return false;
 	}
--- other/edu.tum.in.bruegge.epd.kinect.gesture/src/edu/tum/in/bruegge/epd/kinect/gesture/detectors/JumpGestureDetector.java
+++ other/edu.tum.in.bruegge.epd.kinect.gesture/src/edu/tum/in/bruegge/epd/kinect/gesture/detectors/JumpGestureDetector.java
@@ -1,14 +1,67 @@
 package edu.tum.in.bruegge.epd.kinect.gesture.detectors;
 
+import humanbodymodel.Human;
+import humandiagramgef.HumanBodyEnum;
+
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.EAttribute;
 
 import edu.tum.in.bruegge.epd.kinect.gesture.Gesture;
+import edu.tum.in.bruegge.epd.kinect.gesture.impl.MovingAverageCalculator;
 
 public class JumpGestureDetector extends Gesture {
 
+	private static final int NUM_PERIODS = 10;
+	private static final float THRESHOLD_HEAD = 0.1f;
+	private static final float THRESHOLD_FOOT = 0.1f;
+	
+	private MovingAverageCalculator yMovingAvgHead;
+	private MovingAverageCalculator yMovingAvgFoot;
+	
+	private boolean gestureHead = false;
+	private boolean gestureFootLeft = false;
+	private boolean gestureFootRight = false;
+	
+	float max = Float.MIN_VALUE;
+	float min = Float.MAX_VALUE;
+	
+	
+	public JumpGestureDetector() {
+		this.yMovingAvgHead = new MovingAverageCalculator(NUM_PERIODS);
+		this.yMovingAvgFoot = new MovingAverageCalculator(NUM_PERIODS * 2); // Two feet in one moving average
+	}
+	
 	@Override
 	public boolean isGestureDetected(Notification notification) {
-		// TODO Auto-generated method stub
+		if (notification.getEventType() == Notification.SET && notification.wasSet()) {
+			EAttribute feature = (EAttribute) notification.getFeature();
+			Human humanBodyPart = (Human) notification.getNotifier();
+			
+			if ("y".equals(feature.getName())) {
+				float sensorValue = notification.getNewFloatValue();
+				
+				if (HumanBodyEnum.Head.name().equals(humanBodyPart.getName())) {
+					float avgHeadValue = this.yMovingAvgHead.calculateMovingAvg(sensorValue);
+					float delta = sensorValue - avgHeadValue;
+					
+					gestureHead = (delta > avgHeadValue * THRESHOLD_HEAD);
+				} else if (HumanBodyEnum.Foot_Left.name().equals(humanBodyPart.getName())) {
+					float avgFootValue = this.yMovingAvgFoot.calculateMovingAvg(sensorValue);
+					float delta = sensorValue - avgFootValue;
+					
+					gestureFootLeft = (delta > -avgFootValue * THRESHOLD_FOOT);
+				} else if (HumanBodyEnum.Foot_Right.name().equals(humanBodyPart.getName())) {
+					float avgFootValue = this.yMovingAvgFoot.calculateMovingAvg(sensorValue);
+					float delta = sensorValue - avgFootValue;
+					
+					gestureFootRight = (delta > -avgFootValue * THRESHOLD_FOOT);
+				}
+				
+				if (gestureHead && gestureFootLeft && gestureFootRight) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 }
