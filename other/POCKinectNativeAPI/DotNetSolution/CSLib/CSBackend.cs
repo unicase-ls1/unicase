6bb03b269ae88c87d7f800fad66f390214fb2a99
deepaksrivastav@gmail.com <deepaksrivastav@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-02-05 15:39:45 +0000

    The Kinect Native API Proof of concept files. Includes .NET Solution and the Java project

---
+++ other/POCKinectNativeAPI/DotNetSolution/CSLib/CSBackend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class CSharp
+{
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    delegate void CFuncDelegate(IntPtr Obj, IntPtr Arg, [MarshalAs(UnmanagedType.LPStr)] string str);
+
+    public static void Function(IntPtr CFunc, IntPtr Obj, IntPtr Arg, String[] pUnmanagedStringArray)
+    {
+        CFuncDelegate func = (CFuncDelegate)Marshal.GetDelegateForFunctionPointer(CFunc, typeof(CFuncDelegate));
+
+        Console.WriteLine("[C#]Marshalling Arguments in C#");
+        for (int i = 0; i < pUnmanagedStringArray.Length; i++)
+        {
+            Console.WriteLine("[C#]" + pUnmanagedStringArray[i]);
+        }
+        string str = "Reply String from C#";
+        func.Invoke(Obj, Arg, str);
+    }
+}
