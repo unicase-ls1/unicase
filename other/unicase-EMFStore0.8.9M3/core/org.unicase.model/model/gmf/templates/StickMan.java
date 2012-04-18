230450ec4daf4a5d256091999bb265481548dd38
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-04-18 23:04:38 +0000

    Merged current trunk changes into branch.

--- other/unicase-EMFStore0.8.9M3/core/org.unicase.model/model/gmf/templates/StickMan.java
+++ other/unicase-EMFStore0.8.9M3/core/org.unicase.model/model/gmf/templates/StickMan.java
@@ -1,5 +1,8 @@
 /**
- * @generated
+ * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  */
 package org.eclipse.uml2.diagram.usecase.draw2d;
 
--- other/unicase-EMFStore0.8.9M3/core/org.unicase.ui.stem/src/org/unicase/ui/stem/views/sprintstatus/SprintStatusDropAdapter.java
+++ other/unicase-EMFStore0.8.9M3/core/org.unicase.ui.stem/src/org/unicase/ui/stem/views/sprintstatus/SprintStatusDropAdapter.java
@@ -1,9 +1,9 @@
 /**
- * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  */
-
 package org.unicase.ui.stem.views.sprintstatus;
 
 import java.util.List;
