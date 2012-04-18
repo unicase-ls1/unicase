258c3c8836d79ddedd7f688998e923eb5ca9faef
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-04-18 10:17:57 +0000

    Updated copyright

--- core/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/widgets/AbstractDashboardWidget.java
+++ core/org.unicase.ui.dashboard/src/org/unicase/ui/dashboard/view/widgets/AbstractDashboardWidget.java
@@ -1,7 +1,8 @@
 /**
- * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
  */
 package org.unicase.ui.dashboard.view.widgets;
 
