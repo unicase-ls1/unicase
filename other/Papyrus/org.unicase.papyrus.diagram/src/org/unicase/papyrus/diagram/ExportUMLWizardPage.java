b5abc208cd4f2b67039e9bb74a16820f01e7fa3c
Ioana.I.Negoita@gmail.com <Ioana.I.Negoita@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-04-30 09:33:39 +0000

    Wizard page for format selection from UML Diagram

--- other/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizardPage.java
+++ other/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizardPage.java
@@ -1,5 +1,6 @@
 package org.unicase.papyrus.diagram;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,15 +11,24 @@ import org.eclipse.emf.ecp.editor.MESuggestedSelectionDialog;
 import org.eclipse.emf.emfstore.common.model.Project;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.papyrus.umlutils.MessageUtil;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.swt.widgets.Label;
 
 public class ExportUMLWizardPage extends WizardPage {
 	
 	private final Project project;
+
 
 	protected ExportUMLWizardPage(Project project, String pageName) {
 		super(pageName);
@@ -44,7 +54,34 @@ public class ExportUMLWizardPage extends WizardPage {
 			return;
 		}
 		
+		// set title and description for the page
+
+		this.setTitle("Format selection");
+		this.setDescription("Please select the format you would like to export to.");
+		
+		// create the desired layout for this wizard page
+		GridLayout gl = new GridLayout();
+	
+		int ncol = 4;
+		gl.numColumns = ncol;
+		content.setLayout(gl);		
+		
+		// create the widgets and their grid data objects 
+		new Label (content, SWT.NONE).setText("Format:");						
+		final Combo formats = new Combo(content, SWT.BORDER | SWT.READ_ONLY);
+		
+		// set the formats available for exporting to
+		formats.setItems(new String[]{"ecore", "Java", "C++"});
+		formats.select(0);
+		  
+		GridData gd = new GridData();
+		gd.horizontalAlignment = GridData.BEGINNING;
+		gd.widthHint = 75;
+		formats.setLayoutData(gd);
+		
+		
 		setControl(content);
 	}
+
 
 }
