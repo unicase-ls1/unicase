6cdb53262559adfd0610ce1c249a24ebacce0f12
Ioana.I.Negoita@gmail.com <Ioana.I.Negoita@gmail.com@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-04-30 12:47:56 +0000

    Added the browse button to destination path

--- other/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizardPage.java
+++ other/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizardPage.java
@@ -1,9 +1,12 @@
 package org.unicase.papyrus.diagram;
 
 
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
@@ -11,15 +14,18 @@ import org.eclipse.emf.ecp.editor.MESuggestedSelectionDialog;
 import org.eclipse.emf.emfstore.common.model.Project;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.papyrus.umlutils.MessageUtil;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.uml2.uml.Package;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
@@ -28,7 +34,9 @@ import org.eclipse.swt.widgets.Label;
 public class ExportUMLWizardPage extends WizardPage {
 	
 	private final Project project;
-
+	private static String format;
+	private static String destDir;
+	private Text destinationDirField;
 
 	protected ExportUMLWizardPage(Project project, String pageName) {
 		super(pageName);
@@ -62,26 +70,85 @@ public class ExportUMLWizardPage extends WizardPage {
 		// create the desired layout for this wizard page
 		GridLayout gl = new GridLayout();
 	
-		int ncol = 4;
+		int ncol = 3;
 		gl.numColumns = ncol;
 		content.setLayout(gl);		
+		
+		// add destination path
+		new Label (content, SWT.NONE).setText("Destination folder: ");
+		final GridData gdPath = new GridData();
+		gdPath.horizontalAlignment = GridData.BEGINNING;
+		
+		destinationDirField = new Text(content, SWT.BORDER);
+		destinationDirField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		final Button destinationDirBtn = new Button(content, SWT.NONE);
+		destinationDirBtn.addSelectionListener(new SelectionAdapter(){
+			public void widgetSelected(SelectionEvent event){
+					browseForDestinationDir();
+				
+			}
+		});
+		destinationDirBtn.setText("Browse");
+		
 		
 		// create the widgets and their grid data objects 
 		new Label (content, SWT.NONE).setText("Format:");						
 		final Combo formats = new Combo(content, SWT.BORDER | SWT.READ_ONLY);
 		
+		
 		// set the formats available for exporting to
 		formats.setItems(new String[]{"ecore", "Java", "C++"});
 		formats.select(0);
-		  
+		format = formats.getItem(0);  
+		
 		GridData gd = new GridData();
 		gd.horizontalAlignment = GridData.BEGINNING;
 		gd.widthHint = 75;
 		formats.setLayoutData(gd);
 		
+		formats.addListener(SWT.Selection, new Listener(){
+
+			public void handleEvent(Event event) {
+				format = formats.getItem(formats.getSelectionIndex());
+				
+			}
+			
+		});
 		
 		setControl(content);
 	}
+	
+	public void browseForDestinationDir(){
+		
+		IPath path = browse(false);
+		if(path == null) {
+			return;
+		}
+		destDir = path.toString();
+		destinationDirField.setText(path.toString());
+	}
 
+	
 
+	public IPath browse(boolean mustExist) {
+				
+		DirectoryDialog dialog = new DirectoryDialog(getShell(), mustExist ? SWT.OPEN : SWT.SAVE);
+		
+		String result = dialog.open();
+		if( result == null ) {
+			return null;
+		}
+		
+		return new Path(result);
+	}
+	
+	public static String getFormat(){
+		return format;
+	}
+	
+	public static String getDestinationDir(){
+		return destDir;
+	}
+	
 }
