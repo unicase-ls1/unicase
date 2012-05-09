a4655f2962de6a64a2f4b816537de9ad7eebab08
asamk@gmx.de <asamk@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-05-09 11:24:41 +0000

    uml export wizard
    applying changes from Ioana and me that were first made in trunk/other/Papyrus

--- other/Papyruscodegeneration/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizard.java
+++ other/Papyruscodegeneration/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizard.java
@@ -1,24 +1,85 @@
 package org.unicase.papyrus.diagram;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.Monitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.emfstore.common.model.Project;
 import org.eclipse.jface.wizard.Wizard;
 
 public class ExportUMLWizard extends Wizard {
-	
+
 	private final Project project;
-	
+	private ExportUMLWizardPage filePage;
+
 	public ExportUMLWizard(Project project) {
 		super();
 		this.project = project;
 	}
-	
+
 	public void addPages() {
-		addPage(new ExportUMLWizardPage(project, "Export UML"));
+		filePage = new ExportUMLWizardPage(project, "Export UML");
+		addPage(filePage);
 	}
 
 	@Override
 	public boolean performFinish() {
+		if (filePage.getFormat().equals("ecore")) {
+			exportToEcore();
+		}
+
 		return true;
 	}
 
+	/**
+	 * Export the {@link org.eclipse.uml2.uml.Package uml Packages} selected in the WizardPage to the selected
+	 * destination folder
+	 */
+	private void exportToEcore() {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource modelResource = resourceSet
+			.createResource(URI.createURI(computeFileURI(filePage.getDestinationDir())));
+
+		List<org.eclipse.uml2.uml.Package> packages = filePage.getPackages();
+		PapyrusImporter importer = new PapyrusImporter(packages);
+		Monitor monitor = new BasicMonitor();
+		importer.doComputeEPackages(monitor);
+
+		modelResource.getContents().addAll(importer.getEPackages());
+
+		try {
+			modelResource.save(null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Check if the file "model.ecore" already exists in the export folder, if so increment a number at the end of the
+	 * file.
+	 * 
+	 * @param chosenPath the export folder
+	 * @return the complete export path with filename
+	 */
+	private String computeFileURI(String chosenPath) {
+		String path = chosenPath + "/model.ecore";
+
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+
+		int i = 1;
+		while (root.findMember(path) != null) {
+			i++;
+			path = chosenPath + "/model_" + i + ".ecore";
+		}
+
+		return path;
+	}
 }
