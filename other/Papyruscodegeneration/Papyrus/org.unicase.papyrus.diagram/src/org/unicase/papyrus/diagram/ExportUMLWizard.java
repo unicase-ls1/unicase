09bb2d77009cf3df7fd199ff195e5ee56e24a491
asamk@gmx.de <asamk@gmx.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-05-09 12:28:12 +0000

    generate ecore filename from package names
    -don't append a number to the filename if it already exists, but ask if it should be overwritten
    -don't close wizard if ecore is not selected

--- other/Papyruscodegeneration/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizard.java
+++ other/Papyruscodegeneration/Papyrus/org.unicase.papyrus.diagram/src/org/unicase/papyrus/diagram/ExportUMLWizard.java
@@ -12,6 +12,7 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.emfstore.common.model.Project;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.wizard.Wizard;
 
 public class ExportUMLWizard extends Wizard {
@@ -32,22 +33,34 @@ public class ExportUMLWizard extends Wizard {
 	@Override
 	public boolean performFinish() {
 		if (filePage.getFormat().equals("ecore")) {
-			exportToEcore();
+			return exportToEcore();
 		}
 
-		return true;
+		return false;
 	}
 
 	/**
 	 * Export the {@link org.eclipse.uml2.uml.Package uml Packages} selected in the WizardPage to the selected
 	 * destination folder
+	 * 
+	 * @return true, if the ecore was successfully exported
 	 */
-	private void exportToEcore() {
-		ResourceSet resourceSet = new ResourceSetImpl();
-		Resource modelResource = resourceSet
-			.createResource(URI.createURI(computeFileURI(filePage.getDestinationDir())));
-
+	private boolean exportToEcore() {
 		List<org.eclipse.uml2.uml.Package> packages = filePage.getPackages();
+
+		String path = computeFileURI(filePage.getDestinationDir(), packages);
+
+		// Check if resource already exists and ask if it should be overwritten
+		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+		if (root.findMember(path) != null
+			&& !MessageDialog.openQuestion(getShell(), "File already exists", "The file \"" + path
+				+ "\" already exists.\nOverwrite it?")) {
+			return false;
+		}
+
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource modelResource = resourceSet.createResource(URI.createURI(path));
+
 		PapyrusImporter importer = new PapyrusImporter(packages);
 		Monitor monitor = new BasicMonitor();
 		importer.doComputeEPackages(monitor);
@@ -56,29 +69,32 @@ public class ExportUMLWizard extends Wizard {
 
 		try {
 			modelResource.save(null);
+			return true;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+		return false;
 	}
 
 	/**
-	 * Check if the file "model.ecore" already exists in the export folder, if so increment a number at the end of the
-	 * file.
+	 * Create a fileName from the names of the given packages and append it to the chosenPath.
 	 * 
 	 * @param chosenPath the export folder
+	 * @param packages the packages that will be exported
 	 * @return the complete export path with filename
 	 */
-	private String computeFileURI(String chosenPath) {
-		String path = chosenPath + "/model.ecore";
-
-		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-
-		int i = 1;
-		while (root.findMember(path) != null) {
-			i++;
-			path = chosenPath + "/model_" + i + ".ecore";
+	private String computeFileURI(String chosenPath, List<org.eclipse.uml2.uml.Package> packages) {
+		// make a suitable fileName from the packageNames (usually only one package)
+		String fileName = "";
+		for (org.eclipse.uml2.uml.Package p : packages) {
+			fileName += p.getName() + "_";
 		}
+		if (fileName.length() > 0) {
+			fileName = fileName.substring(0, fileName.length() - 1);
+		}
+
+		String path = chosenPath + "/" + fileName + ".ecore";
 
 		return path;
 	}
