0dec235fc89e5b524b515262e3061caea6c2b5cc
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-06-26 10:05:44 +0000

    Branch to cope with the latest EMFStore/ECP release (0.9.1). A similar branch has been corrupted - this is the correct branch.

---
+++ org.unicase.ui.diagram.activityDiagram/src/org/unicase/ui/diagram/activityDiagram/navigator/ModelNavigatorSorter.java
@@ -0,0 +1,33 @@
+/**
+ * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ui.diagram.activityDiagram.navigator;
+
+import org.eclipse.jface.viewers.ViewerSorter;
+
+/**
+ * @generated
+ */
+public class ModelNavigatorSorter extends ViewerSorter {
+
+	/**
+	 * @generated
+	 */
+	private static final int GROUP_CATEGORY = 4003;
+
+	/**
+	 * @generated
+	 */
+	public int category(Object element) {
+		if (element instanceof org.unicase.ui.diagram.activityDiagram.navigator.ModelNavigatorItem) {
+			org.unicase.ui.diagram.activityDiagram.navigator.ModelNavigatorItem item = (org.unicase.ui.diagram.activityDiagram.navigator.ModelNavigatorItem) element;
+			return org.unicase.ui.diagram.activityDiagram.part.ModelVisualIDRegistry
+					.getVisualID(item.getView());
+		}
+		return GROUP_CATEGORY;
+	}
+
+}
