66ce54fd37aa4317491d25001b60f94479948723
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-06-26 10:07:25 +0000

    Branch to cope with the latest EMFStore/ECP release (0.9.1). A similar branch has been corrupted - this is the correct branch.

---
+++ org.unicase.ui.tableview/src/org/unicase/ui/tableview/labelproviders/StatusLabelProvider.java
@@ -0,0 +1,112 @@
+/**
+ * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ui.tableview.labelproviders;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.swt.graphics.Image;
+import org.unicase.model.UnicaseModelElement;
+import org.unicase.model.task.WorkItem;
+import org.unicase.model.task.util.CircularDependencyException;
+import org.unicase.model.task.util.MEState;
+import org.unicase.ui.tableview.Activator;
+
+/**
+ * This label provider shows the status of a modelelement represented as an image.
+ * 
+ * @author helming
+ */
+public class StatusLabelProvider extends ColumnLabelProvider implements IColorProvider {
+
+	private Map<String, Image> images;
+	private static final String OPEN = "open";
+	private static final String CLOSED = "closed";
+	private static final String BLOCKED = "blocked";
+	private static final String OPEN_RESOLVED = "open_resolved";
+
+	/**
+	 * Constructor.
+	 */
+	public StatusLabelProvider() {
+		images = new HashMap<String, Image>();
+
+		ImageDescriptor imageDescriptor = Activator.getImageDescriptor("icons/open.png");
+		images.put(OPEN, imageDescriptor.createImage());
+
+		imageDescriptor = Activator.getImageDescriptor("icons/closed.gif");
+		images.put(CLOSED, imageDescriptor.createImage());
+
+		imageDescriptor = Activator.getImageDescriptor("icons/blocked.gif");
+		images.put(BLOCKED, imageDescriptor.createImage());
+
+		imageDescriptor = Activator.getImageDescriptor("icons/open_resolved.png");
+		images.put(OPEN_RESOLVED, imageDescriptor.createImage());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.jface.viewers.BaseLabelProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+
+		images.get(OPEN).dispose();
+		images.get(CLOSED).dispose();
+		images.get(BLOCKED).dispose();
+		images.get(OPEN_RESOLVED).dispose();
+
+		images.clear();
+
+		super.dispose();
+	}
+
+	/**
+	 * . {@inheritDoc}
+	 */
+	@Override
+	public Image getImage(Object element) {
+		if (element instanceof UnicaseModelElement) {
+			UnicaseModelElement me = (UnicaseModelElement) element;
+			String status = MEState.CLOSED;
+			try {
+				status = me.getMEState().getStatus();
+			} catch (CircularDependencyException e) {
+				// JH Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			if (status.equals(MEState.OPEN)) {
+				if (me instanceof WorkItem && ((WorkItem) me).isResolved()) {
+					return images.get(OPEN_RESOLVED);
+				}
+				return images.get(OPEN);
+
+			} else if (status.equals(MEState.CLOSED)) {
+				return images.get(CLOSED);
+
+			} else if (status.equals(MEState.BLOCKED)) {
+				return images.get(BLOCKED);
+			}
+
+		}
+		return null;
+	}
+
+	/**
+	 * . {@inheritDoc}
+	 */
+	@Override
+	public String getText(Object element) {
+		return "";
+	}
+
+}
---
+++ org.unicase.ui.tableview/src/org/unicase/ui/tableview/viewer/METableViewerContentProvider.java
@@ -0,0 +1,91 @@
+/**
+ * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * All rights reserved. This program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.ui.tableview.viewer;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.emfstore.common.model.Project;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.unicase.model.UnicaseModelElement;
+
+/**
+ * This is the content provider for TaskView. Taskview shows checkables only.
+ * 
+ * @author zardosht
+ */
+public class METableViewerContentProvider implements IStructuredContentProvider {
+
+	private org.eclipse.emf.emfstore.common.model.Project project;
+	private EClass meType;
+	private Collection<? extends UnicaseModelElement> directInput;
+
+	/**
+	 * {@inheritDoc} Returns checables only.
+	 * 
+	 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+	 */
+	public Object[] getElements(Object inputElement) {
+		if (meType == null || project == null) {
+			if (directInput != null) {
+				return directInput.toArray();
+			}
+			return new Object[0];
+		}
+
+		List<? extends UnicaseModelElement> content = project.getAllModelElementsbyClass(meType,
+			new BasicEList<UnicaseModelElement>());
+		return content.toArray();
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	public void dispose() {
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object,
+	 *      java.lang.Object)
+	 */
+	@SuppressWarnings("unchecked")
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		if (newInput != oldInput) {
+			if (newInput instanceof org.eclipse.emf.emfstore.common.model.Project) {
+				this.project = (Project) newInput;
+				directInput = null;
+			} else if (newInput instanceof Collection) {
+				this.directInput = (Collection<? extends UnicaseModelElement>) newInput;
+				project = null;
+				// meType = null;
+			} else if (newInput == null) {
+				this.project = null;
+				directInput = Collections.emptyList();
+			}
+		}
+
+	}
+
+	/**
+	 * This content provider shows all MEs of this type in project.
+	 * 
+	 * @param contentType model element type
+	 */
+	public void setMEType(EClass contentType) {
+		this.meType = contentType;
+	}
+
+}
