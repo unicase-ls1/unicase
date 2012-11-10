605ef91a2981608d59c2131849f1c4341e7ceb5c
yasmine8726 <yasmine8726@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-11-10 16:43:36 +0000

    integrate code of SCRM and unicase

---
+++ other/SCRM/branches/unicase_scrm/org.unicase.diagram/src/org/unicase/model/diagram/impl/MERelativeBendpointsImpl.java
@@ -0,0 +1,44 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ * 
+ */
+package org.unicase.model.diagram.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.gmf.runtime.notation.impl.RelativeBendpointsImpl;
+
+import org.unicase.model.diagram.DiagramPackage;
+import org.unicase.model.diagram.MERelativeBendpoints;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>ME Relative Bendpoints</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class MERelativeBendpointsImpl extends RelativeBendpointsImpl implements
+		MERelativeBendpoints {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected MERelativeBendpointsImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return DiagramPackage.Literals.ME_RELATIVE_BENDPOINTS;
+	}
+
+} //MERelativeBendpointsImpl
