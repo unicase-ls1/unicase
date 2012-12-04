0205104ecff125c16c05e33671cf9a51188dcd41
yasmine8726 <yasmine8726@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2012-12-04 22:09:55 +0000

    scrm extension for IW

---
+++ other/SCRM/branches/IW_SCRM/org.unicase.model/src/org/unicase/model/rationale/validation/DiscussionShouldBeIssueConstraint.java
@@ -0,0 +1,59 @@
+/**
+ * <copyright> Copyright (c) 2008-2009 Jonas Helming, Maximilian Koegel. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
+ */
+package org.unicase.model.rationale.validation;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.validation.AbstractModelConstraint;
+import org.eclipse.emf.validation.EMFEventType;
+import org.eclipse.emf.validation.IValidationContext;
+import org.unicase.metamodel.util.ModelUtil;
+import org.unicase.model.UnicaseModelElement;
+import org.unicase.model.rationale.Comment;
+import org.unicase.model.rationale.Issue;
+import org.unicase.model.util.ValidationConstraintHelper;
+
+/**
+ * Checks whether a activity is set for this ActionItem.
+ * 
+ * @author wesendonk
+ * @author naughton
+ */
+public class DiscussionShouldBeIssueConstraint extends AbstractModelConstraint {
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.emf.validation.AbstractModelConstraint#validate(org.eclipse.emf.validation.IValidationContext)
+	 */
+	@Override
+	public IStatus validate(IValidationContext context) {
+		EObject eObject = context.getTarget();
+		EMFEventType eventType = context.getEventType();
+
+		if (eventType != EMFEventType.NULL || eObject instanceof Comment || eObject instanceof Issue) {
+			return context.createSuccessStatus();
+		}
+
+		int count = 0;
+		for (EObject element : ModelUtil.getAllContainedModelElements(eObject, false)) {
+			if (element instanceof Comment) {
+				count += ModelUtil.getAllContainedModelElements(element, false).size();
+				count++;
+			}
+		}
+		if (count > 6) {
+			EStructuralFeature errorFeature = ValidationConstraintHelper.getErrorFeatureForModelElement(
+				(UnicaseModelElement) eObject, "comments");
+			context.addResult(errorFeature);
+			return context.createFailureStatus(new Object[] { eObject.eClass().getName() + ": '"
+				+ eObject.eClass().getName() + "'" });
+		}
+		return context.createSuccessStatus();
+
+	}
+}
