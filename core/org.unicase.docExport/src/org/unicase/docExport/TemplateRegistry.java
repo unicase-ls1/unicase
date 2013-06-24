f6932386b317d3ada11dbb910d9d7281b6601bf8
harutuni@in.tum.de <harutuni@in.tum.de@aacd7a68-ac13-11de-892d-7b2b76e7707e>
2013-06-24 17:47:39 +0000

    Fixed DocExport-bug and updated emfstore client version.

--- core/org.unicase.docExport/src/org/unicase/docExport/TemplateRegistry.java
+++ core/org.unicase.docExport/src/org/unicase/docExport/TemplateRegistry.java
@@ -40,20 +40,24 @@ import org.unicase.docExport.exportModel.Template;
 import org.unicase.docExport.exportModel.builders.DefaultDocumentTemplateFactory;
 
 /**
- * This utility class handles the persistent templates. The default templates which are always loaded on startup are
- * stored in the folder "defaultTemplates". The files must be from the same as as the template export function returns.
+ * This utility class handles the persistent templates. The default templates
+ * which are always loaded on startup are stored in the folder
+ * "defaultTemplates". The files must be from the same as as the template export
+ * function returns.
  * 
  * @author Sebastian Hoecht
  */
 public final class TemplateRegistry {
 
 	/**
-	 * The name of the temporary file which is used to unzip the default templates.
+	 * The name of the temporary file which is used to unzip the default
+	 * templates.
 	 */
 	private static final String DEFAULT_TEMPLATE_TMP = "defaultTemplate.tmp";
 
 	/**
-	 * The folder in the plug-in where all default templates are stored which are loaded before each export request.
+	 * The folder in the plug-in where all default templates are stored which
+	 * are loaded before each export request.
 	 */
 	private static final String DEFAULT_TEMPLATES_FOLDER = "defaultTemplates";
 
@@ -63,45 +67,53 @@ public final class TemplateRegistry {
 	public static final String DOCEXPORT_IMAGES = "docExportImages";
 
 	/**
-	 * The resource set for the EMF XML file. This is a singleton since it should only be loaded once.
+	 * The resource set for the EMF XML file. This is a singleton since it
+	 * should only be loaded once.
 	 */
 	private static ResourceSet resourceSet;
 
 	/**
-	 * If there is no template, a new default template will be created automatically, and this name will be used.
+	 * If there is no template, a new default template will be created
+	 * automatically, and this name will be used.
 	 */
 	private static final String DEFAULT_TEMPLATE_NAME = "default";
 
 	/**
 	 * The Unicase folder, where the projects and templates are stored.
 	 */
-	private static final String UNICASE_FOLDER = Configuration.getWorkspaceDirectory();
+	private static final String UNICASE_FOLDER = Configuration
+			.getWorkspaceDirectory();
 
 	/**
-	 * The folder within the Unicase folder, where the templates and template images are stored.
+	 * The folder within the Unicase folder, where the templates and template
+	 * images are stored.
 	 */
 	private static final String DOCUMENT_EXPORT_FOLDER = "docExport";
 
 	/**
-	 * The filename of the EMF XML file, containing a set of templates. The version number of the template file name
-	 * must be incremented, if there are major EMF model changes causing incompatibility for older templates.
+	 * The filename of the EMF XML file, containing a set of templates. The
+	 * version number of the template file name must be incremented, if there
+	 * are major EMF model changes causing incompatibility for older templates.
 	 */
 	private static final String TEMPLATES_FILE_NAME = "templates7";
 
 	/**
 	 * The folder where all templates should be saved.
 	 */
-	public static final String TEMPLATE_FOLDER = UNICASE_FOLDER + DOCUMENT_EXPORT_FOLDER + File.separatorChar;
+	public static final String TEMPLATE_FOLDER = UNICASE_FOLDER
+			+ DOCUMENT_EXPORT_FOLDER + File.separatorChar;
 
 	/**
 	 * The folder where all logo images of the templates should be saved.
 	 */
-	public static final String TEMPLATE_IMAGE_FOLDER = TEMPLATE_FOLDER + "images" + File.separatorChar;
+	public static final String TEMPLATE_IMAGE_FOLDER = TEMPLATE_FOLDER
+			+ "images" + File.separatorChar;
 
 	private static final int ME_COUNT_DEFAULT = 0;
 
 	/**
-	 * This is a help variable which counts the number of ModelElements rendered. This is ONLY for benchmark purposes.
+	 * This is a help variable which counts the number of ModelElements
+	 * rendered. This is ONLY for benchmark purposes.
 	 */
 	private static int meCount = ME_COUNT_DEFAULT;
 
@@ -112,59 +124,77 @@ public final class TemplateRegistry {
 	};
 
 	/**
-	 * Loads all default templates from the default template folders. This is a not so easy job, because the templates
-	 * are stored in single zip files containing an EMF XML file with a template and a logo image. These files need to
-	 * be unzipped and saved in the local unicase folder.
+	 * Loads all default templates from the default template folders. This is a
+	 * not so easy job, because the templates are stored in single zip files
+	 * containing an EMF XML file with a template and a logo image. These files
+	 * need to be unzipped and saved in the local unicase folder.
 	 * 
-	 * @throws DefaultTemplateLoadException the default template couln't be loaded for any reason.
+	 * @throws DefaultTemplateLoadException
+	 *             the default template couln't be loaded for any reason.
 	 */
-	public static void loadDefaultTemplatesFromZipFile() throws DefaultTemplateLoadException {
+	public static void loadDefaultTemplatesFromZipFile()
+			throws DefaultTemplateLoadException {
 
 		try {
-			URL templateFolderUrl = FileLocator.find(Activator.getDefault().getBundle(), new Path(
-				DEFAULT_TEMPLATES_FOLDER + File.separatorChar), Collections.EMPTY_MAP);
+			URL templateFolderUrl = FileLocator.find(Activator.getDefault()
+					.getBundle(), new Path(DEFAULT_TEMPLATES_FOLDER
+					+ File.separatorChar), Collections.EMPTY_MAP);
 
 			// loading the template in the developer version is easy
 			if (isDeveloperVersion()) {
-				File templateFolder = new File(FileLocator.resolve(templateFolderUrl).getPath());
+				File templateFolder = new File(FileLocator.resolve(
+						templateFolderUrl).getPath());
 				File[] files = templateFolder.listFiles();
 				for (int i = 0; i < files.length; i++) {
 					Path path = new Path(files[i].getAbsolutePath());
 					if (path.getFileExtension().equals("zip")) {
-						ImportTemplate.importTemplate(files[i].getAbsolutePath(), true);
+						ImportTemplate.importTemplate(
+								files[i].getAbsolutePath(), true);
 					}
 				}
 			} else {
-				// loading the templates in the deployed version is very complicated, because you have to
-				// list all files within the templates folder, which can't be done with File.list()
+				// loading the templates in the deployed version is very
+				// complicated, because you have to
+				// list all files within the templates folder, which can't be
+				// done with File.list()
 				// because This function doesn't work for jar files
-				URL pluginJarFile = FileLocator.find(Activator.getDefault().getBundle(), new Path(""),
-					Collections.EMPTY_MAP);
+				URL pluginJarFile = FileLocator.find(Activator.getDefault()
+						.getBundle(), new Path(""), Collections.EMPTY_MAP);
 
-				String jarFilePath = FileLocator.resolve(pluginJarFile).getPath();
+				String jarFilePath = FileLocator.resolve(pluginJarFile)
+						.getPath();
 
-				// I couldn't find a function, which generates the correct path string.
+				// I couldn't find a function, which generates the correct path
+				// string.
 				// so i take this one and manipulate it.
 				// cut the last file separator and the "!"
-				jarFilePath = jarFilePath.substring(0, jarFilePath.length() - 1);
+				jarFilePath = jarFilePath
+						.substring(0, jarFilePath.length() - 2);
 				jarFilePath = jarFilePath.replace("file:", "");
 
 				JarFile jarFile = new JarFile(jarFilePath);
 				Enumeration<JarEntry> jarEnum = jarFile.entries();
 
-				// walk through all entries in the jar file of the docExport bundle
+				// walk through all entries in the jar file of the docExport
+				// bundle
 				while (jarEnum.hasMoreElements()) {
 					JarEntry jarEntry = jarEnum.nextElement();
-					// filter the entries to the entries starting with the default templates folder
+					// filter the entries to the entries starting with the
+					// default templates folder
 					if (jarEntry.getName().startsWith(DEFAULT_TEMPLATES_FOLDER)) {
-						Path path = new Path(jarFilePath + File.separatorChar + jarEntry.getName());
-						// make sure, that you don't try to import with a wrong file.
-						if (path.getFileExtension() != null && path.getFileExtension().equals("zip")) {
+						Path path = new Path(jarFilePath + File.separatorChar
+								+ jarEntry.getName());
+						// make sure, that you don't try to import with a wrong
+						// file.
+						if (path.getFileExtension() != null
+								&& path.getFileExtension().equals("zip")) {
 							jarFile.getInputStream(jarEntry);
 							InputStream fis = jarFile.getInputStream(jarEntry);
-							FileOutputStream fos = new FileOutputStream(TEMPLATE_FOLDER + DEFAULT_TEMPLATE_TMP);
+							FileOutputStream fos = new FileOutputStream(
+									TEMPLATE_FOLDER + DEFAULT_TEMPLATE_TMP);
 							readZipFile(fis, fos);
-							ImportTemplate.importTemplate(TEMPLATE_FOLDER + DEFAULT_TEMPLATE_TMP, true);
+							ImportTemplate.importTemplate(TEMPLATE_FOLDER
+									+ DEFAULT_TEMPLATE_TMP, true);
 						}
 					}
 				}
@@ -180,18 +210,23 @@ public final class TemplateRegistry {
 
 	private static boolean isDeveloperVersion() {
 		Bundle docExportBundle = Platform.getBundle("org.unicase.docExport");
-		String version = (String) docExportBundle.getHeaders().get(org.osgi.framework.Constants.BUNDLE_VERSION);
+		String version = docExportBundle.getHeaders().get(
+				org.osgi.framework.Constants.BUNDLE_VERSION);
 		return version.endsWith("qualifier");
 	}
 
 	/**
-	 * Reads a zip file and copies the data to a non zip file allowing direct file access.
+	 * Reads a zip file and copies the data to a non zip file allowing direct
+	 * file access.
 	 * 
-	 * @param fis the zip file which is read.
-	 * @param fos the template file which is written.
+	 * @param fis
+	 *            the zip file which is read.
+	 * @param fos
+	 *            the template file which is written.
 	 * @throws DefaultTemplateLoadException
 	 */
-	private static void readZipFile(InputStream fis, FileOutputStream fos) throws DefaultTemplateLoadException {
+	private static void readZipFile(InputStream fis, FileOutputStream fos)
+			throws DefaultTemplateLoadException {
 		try {
 			byte[] buf = new byte[1024];
 			int i = 0;
@@ -217,13 +252,17 @@ public final class TemplateRegistry {
 	}
 
 	/**
-	 * Loads a Template from the templates file. If the Template could not be found, null is returned.
+	 * Loads a Template from the templates file. If the Template could not be
+	 * found, null is returned.
 	 * 
-	 * @param templateName the name of the template which shall be loaded
+	 * @param templateName
+	 *            the name of the template which shall be loaded
 	 * @return the Template which shall be loaded
-	 * @throws TemplatesFileNotFoundException The Templates file could not be found
+	 * @throws TemplatesFileNotFoundException
+	 *             The Templates file could not be found
 	 */
-	public static Template getTemplate(String templateName) throws TemplatesFileNotFoundException {
+	public static Template getTemplate(String templateName)
+			throws TemplatesFileNotFoundException {
 
 		Resource resource;
 		try {
@@ -247,14 +286,17 @@ public final class TemplateRegistry {
 	}
 
 	/**
-	 * Saves a template to the disc in the template folder of this plug-in. The filename will be template.getName(). +
-	 * ".template".
+	 * Saves a template to the disc in the template folder of this plug-in. The
+	 * filename will be template.getName(). + ".template".
 	 * 
-	 * @param template the template which shall be saved
-	 * @throws TemplatesFileNotFoundException The templates file could not be found, and therefore the Template could
-	 *             not be saved.
+	 * @param template
+	 *            the template which shall be saved
+	 * @throws TemplatesFileNotFoundException
+	 *             The templates file could not be found, and therefore the
+	 *             Template could not be saved.
 	 */
-	public static void saveTemplate(Template template) throws TemplatesFileNotFoundException {
+	public static void saveTemplate(Template template)
+			throws TemplatesFileNotFoundException {
 		Resource resource;
 		resource = getTemplatesResource();
 
@@ -288,13 +330,18 @@ public final class TemplateRegistry {
 	}
 
 	/**
-	 * Returns a list of all available templates. Additionally, all default templates are loaded.
+	 * Returns a list of all available templates. Additionally, all default
+	 * templates are loaded.
 	 * 
-	 * @return an ArrayList of all templates which a stored in the template folder
-	 * @throws TemplatesFileNotFoundException The Template resource could not be loaded, and therefore, there is no
-	 *             template. This is a fatal Exception disabling all export functions.
+	 * @return an ArrayList of all templates which a stored in the template
+	 *         folder
+	 * @throws TemplatesFileNotFoundException
+	 *             The Template resource could not be loaded, and therefore,
+	 *             there is no template. This is a fatal Exception disabling all
+	 *             export functions.
 	 */
-	public static ArrayList<Template> getAllTemplates() throws TemplatesFileNotFoundException {
+	public static ArrayList<Template> getAllTemplates()
+			throws TemplatesFileNotFoundException {
 
 		ArrayList<Template> templates = new ArrayList<Template>();
 
@@ -306,10 +353,13 @@ public final class TemplateRegistry {
 			try {
 				loadDefaultTemplatesFromZipFile();
 			} catch (DefaultTemplateLoadException e) {
-				WorkspaceUtil.log("An Error occured while loading the default templates", e, IStatus.WARNING);
+				WorkspaceUtil.log(
+						"An Error occured while loading the default templates",
+						e, IStatus.WARNING);
 			}
 		} catch (IOException e) {
-			WorkspaceUtil.log("Fetching all templates failed", new TemplatesFileNotFoundException(e), IStatus.WARNING);
+			WorkspaceUtil.log("Fetching all templates failed",
+					new TemplatesFileNotFoundException(e), IStatus.WARNING);
 
 			Template template = createNewDefaultDocumentTemplate();
 			saveTemplate(template);
@@ -328,7 +378,8 @@ public final class TemplateRegistry {
 	}
 
 	/**
-	 * @param meCount the meCount to set
+	 * @param meCount
+	 *            the meCount to set
 	 */
 	public static void setMeCount(int meCount) {
 		TemplateRegistry.meCount = meCount;
@@ -348,10 +399,12 @@ public final class TemplateRegistry {
 
 	/**
 	 * @return the static resource containing all templates
-	 * @throws TemplatesFileNotFoundException There was no templates file, and a new one has been created, but it
-	 *             couldn't be saved
+	 * @throws TemplatesFileNotFoundException
+	 *             There was no templates file, and a new one has been created,
+	 *             but it couldn't be saved
 	 */
-	public static Resource getTemplatesResource() throws TemplatesFileNotFoundException {
+	public static Resource getTemplatesResource()
+			throws TemplatesFileNotFoundException {
 		if (resourceSet == null) {
 			resourceSet = new ResourceSetImpl();
 		}
@@ -364,8 +417,9 @@ public final class TemplateRegistry {
 				resource.save(null);
 				return resource;
 			} catch (IOException e) {
-				WorkspaceUtil.log("The templates file could not be saved after creating a new resource",
-					new Exception(), IStatus.WARNING);
+				WorkspaceUtil
+						.log("The templates file could not be saved after creating a new resource",
+								new Exception(), IStatus.WARNING);
 				throw new TemplatesFileNotFoundException(e);
 			}
 		} else {
@@ -376,10 +430,13 @@ public final class TemplateRegistry {
 	/**
 	 * Persistently deletes a template.
 	 * 
-	 * @param template the template which shall be deleted.
-	 * @throws TemplatesFileNotFoundException the template could not be deleted
+	 * @param template
+	 *            the template which shall be deleted.
+	 * @throws TemplatesFileNotFoundException
+	 *             the template could not be deleted
 	 */
-	public static void deleteTemplate(Template template) throws TemplatesFileNotFoundException {
+	public static void deleteTemplate(Template template)
+			throws TemplatesFileNotFoundException {
 		Resource resource;
 		resource = getTemplatesResource();
 
