a24d177e32ca8488d2c8c511a08455f5c48cb44a
Nitesh Narayan <narayan@in.tum.de>
2015-10-09 10:18:14 +0200

    updating copyright headers

--- org.unicase.ui.unicasecommon/src/org/unicase/ui/unicasecommon/UnicaseClientVersionProvider.java
+++ org.unicase.ui.unicasecommon/src/org/unicase/ui/unicasecommon/UnicaseClientVersionProvider.java
@@ -1,5 +1,5 @@
 /**
- * <copyright> Copyright (c) 2009-2012 Chair of Applied Software Engineering, Technische Universität München (TUM).
+ * <copyright> Copyright (c) 2015-2016 Chair of Applied Software Engineering, Technische Universität München (TUM).
  * All rights reserved. This program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html </copyright>
